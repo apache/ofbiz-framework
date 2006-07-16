420a90904982678c3732ebf8fe6174a3f2866c01
Jacopo Cappellato <jacopoc@apache.org>
2006-07-16 08:10:15 +0000

    Conversion from MIT to ASL2.0 for the files of the accounting component. Many thanks to Scott Gray for the patch (Apache OFBIZ-87).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@422403 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/invoice/InvoiceWorker.java
+++ applications/accounting/src/org/ofbiz/accounting/invoice/InvoiceWorker.java
@@ -1,7 +1,5 @@
 /*
- * $Id: InvoiceWorker.java 7412 2006-04-25 14:09:08Z jacopo $
- *
- * Copyright 2003-2006 The Apache Software Foundation
+ * Copyright 2001-2006 The Apache Software Foundation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
