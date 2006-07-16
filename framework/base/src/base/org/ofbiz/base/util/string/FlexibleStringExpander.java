12aad676b69f0187294ba449482bd21bf2ba6fb7
Jacopo Cappellato <jacopoc@apache.org>
2006-07-16 09:30:14 +0000

    Conversion from MIT to ASL2.0 for the files of the base component. Many thanks to Scott Gray for the patch (Apache OFBIZ-87).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@422417 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/base/org/ofbiz/base/util/string/FlexibleStringExpander.java
+++ framework/base/src/base/org/ofbiz/base/util/string/FlexibleStringExpander.java
@@ -1,6 +1,4 @@
 /*
- * $Id: FlexibleStringExpander.java 7406 2006-04-25 11:07:19Z jonesde $
- *
  * Copyright 2001-2006 The Apache Software Foundation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
