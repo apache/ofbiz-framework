01a44d4551740ba5dfe2805d187e80305013069b
Jacopo Cappellato <jacopoc@apache.org>
2006-07-20 10:24:29 +0000

    License change from MIT to ASL2.0 for the files of the shark component. Thanks to Scott Gray for the patch (Apache OFBIZ-87).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@423868 13f79535-47bb-0310-9956-ffa450edef68

--- framework/shark/src/org/ofbiz/shark/expression/ResourceIteratorCondExprBldr.java
+++ framework/shark/src/org/ofbiz/shark/expression/ResourceIteratorCondExprBldr.java
@@ -1,7 +1,5 @@
 /*
- * $Id: ResourceIteratorCondExprBldr.java 7426 2006-04-26 23:35:58Z jonesde $
- *
- * Copyright 2004-2006 The Apache Software Foundation
+ * Copyright 2001-2006 The Apache Software Foundation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
