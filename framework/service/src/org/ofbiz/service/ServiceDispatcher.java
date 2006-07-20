9cd3112064c2a30b3a6a8821842234ecfcc0bd13
Jacopo Cappellato <jacopoc@apache.org>
2006-07-20 10:19:51 +0000

    License change from MIT to ASL2.0 for the files of the service component. Thanks to Scott Gray for the patch (Apache OFBIZ-87).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@423866 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/ServiceDispatcher.java
+++ framework/service/src/org/ofbiz/service/ServiceDispatcher.java
@@ -1,6 +1,4 @@
 /*
- * $Id: ServiceDispatcher.java 7606 2006-05-15 21:00:25Z jaz $
- *
  * Copyright 2001-2006 The Apache Software Foundation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
