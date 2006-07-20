de1b33bba99cefe826b3c5ae2fa93d247d874b69
Jacopo Cappellato <jacopoc@apache.org>
2006-07-20 10:19:51 +0000

    License change from MIT to ASL2.0 for the files of the service component. Thanks to Scott Gray for the patch (Apache OFBIZ-87).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@423866 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/test/ServiceEngineTests.java
+++ framework/service/src/org/ofbiz/service/test/ServiceEngineTests.java
@@ -1,7 +1,5 @@
 /*
- * $Id: ServiceEngineTests.java 7551 2006-05-10 00:08:10Z jonesde $
- *
- * Copyright 2004-2006 The Apache Software Foundation
+ * Copyright 2001-2006 The Apache Software Foundation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
