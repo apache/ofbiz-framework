299c9d65aa817dc90a01b965373b8e16195a717f
Jacopo Cappellato <jacopoc@apache.org>
2006-07-21 07:02:43 +0000

    Added ASL2.0 to start scripts.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@424208 13f79535-47bb-0310-9956-ffa450edef68

--- ant.bat
+++ ant.bat
@@ -1,3 +1,20 @@
+ECHO OFF
+REM #####################################################################
+REM # Copyright 2001-2006 The Apache Software Foundation
+REM #
+REM # Licensed under the Apache License, Version 2.0 (the "License"); you may not
+REM # use this file except in compliance with the License. You may obtain a copy of
+REM # the License at
+REM #
+REM # http://www.apache.org/licenses/LICENSE-2.0
+REM #
+REM # Unless required by applicable law or agreed to in writing, software
+REM # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+REM # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+REM # License for the specific language governing permissions and limitations
+REM # under the License.
+REM #####################################################################
+ECHO ON
 
 "%JAVA_HOME%\bin\java" -jar framework/base/lib/ant-launcher.jar %1 %2 %3 %4 %5 %6
 
