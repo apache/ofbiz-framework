4089eef5809c79ccd5aab07138499a831585adb8
Hans Bakker <hansbak@apache.org>
2006-10-02 07:39:12 +0000

    add a service to get one generic item using the performFind service
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@451904 13f79535-47bb-0310-9956-ffa450edef68

--- framework/common/src/org/ofbiz/common/FindServices.java
+++ framework/common/src/org/ofbiz/common/FindServices.java
@@ -623,4 +623,32 @@ public class FindServices {
         }
         return normalizedFields;
     }
+    /**
+     * Returns the first generic item of the service 'performFind'
+     * Same parameters as performFind service but returns a single GenericValue
+     * 
+     * @param dctx
+     * @param context
+     * @return
+     */
+    public static Map performFindItem(DispatchContext dctx, Map context) {
+        Map result = org.ofbiz.common.FindServices.performFind(dctx,context);
+        
+        List list = null;
+        GenericValue item= null;
+        try{
+            EntityListIterator it = (EntityListIterator) result.get("listIt");
+            list = it.getPartialList(1, 1); // list starts at '1'
+            if (list != null && list.size() > 0 ) {
+                item = (GenericValue) list.get(0);
+            }
+            it.close();
+        } catch (Exception e) {
+            Debug.logInfo("Problem getting list Item" + e,module);
+        }
+        
+        result.put("item",item);
+        result.remove("listIt");
+        return result;
+    }
 }
