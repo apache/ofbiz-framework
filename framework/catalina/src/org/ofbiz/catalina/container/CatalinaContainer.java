1e2805f408c49b77ad21322106c06414e4d3bd84
Jacopo Cappellato <jacopoc@apache.org>
2006-10-08 15:31:07 +0000

    Removed author information from all the files of the "catalina" component: keeping this information in source files is discouraged by the ASF (clearer information about the contributors of a file can be derived from the commit logs).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@454160 13f79535-47bb-0310-9956-ffa450edef68

--- framework/catalina/src/org/ofbiz/catalina/container/CatalinaContainer.java
+++ framework/catalina/src/org/ofbiz/catalina/container/CatalinaContainer.java
@@ -119,9 +119,6 @@ import org.xml.sax.SAXException;
 /**
  * CatalinaContainer -  Tomcat 5
  *
- * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
- * @version    $Rev$
- * @since      3.1
  */
 public class CatalinaContainer implements Container {
 
