080680236a1e4eed3f6db98b6b2e5332398fd7f9
Jacques Le Roux <jleroux@apache.org>
2006-10-24 10:58:36 +0000

    A patch from Adam Heath "Upgrade bsf to something a bit more modern" (https://issues.apache.org/jira/browse/OFBIZ-401)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@467304 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Enumeration;
 
-import com.ibm.bsf.BSFManager;
+import org.apache.bsf.BSFManager;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilHttp;
