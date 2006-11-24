cbbf0c16c9c2f900be4c03561b8f0de17ad7758a
Johannes Cornelius Bakker <hansbak@apache.org>
2006-11-24 06:42:24 +0000

    system now accepts incoming email messages with multilevel multiparts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@478781 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/src/org/ofbiz/content/email/EmailWorker.java
+++ applications/content/src/org/ofbiz/content/email/EmailWorker.java
@@ -30,6 +30,7 @@ import org.ofbiz.entity.GenericValue;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ServiceUtil;
 import org.ofbiz.entity.util.ByteWrapper;
 
 public class EmailWorker {
@@ -43,9 +44,8 @@ public class EmailWorker {
     	return fieldValue;
     }
     
-    public static int addAttachmentsToCommEvent(MimeMessage message, String communicationEventId, int bodyContentIndex, LocalDispatcher dispatcher, GenericValue userLogin) 
+    public static int addAttachmentsToCommEvent(MimeMessage message, String communicationEventId, LocalDispatcher dispatcher, GenericValue userLogin) 
     	throws MessagingException, IOException, GenericServiceException {
-    	int attachmentCount =0;
     	Map commEventMap = new HashMap();
     	commEventMap.put("communicationEventId", communicationEventId);
     	commEventMap.put("contentTypeId", "DOCUMENT");
@@ -55,55 +55,76 @@ public class EmailWorker {
 		if (subject != null && subject.length() > 80) { 
 			subject = subject.substring(0,80); // make sure not too big for database field. (20 characters for filename)
 		}
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    	
-		Multipart multipart = (Multipart)message.getContent();
-		int multipartCount = multipart.getCount();
-		for (int i=0; i < multipartCount; i++) {
-			Part part = multipart.getBodyPart(i);
-			String thisContentTypeRaw = part.getContentType();
-            int idx2 = thisContentTypeRaw.indexOf(";");
-            if (idx2 == -1) idx2 = thisContentTypeRaw.length();
-            String thisContentType = thisContentTypeRaw.substring(0, idx2);
-			String disposition = part.getDisposition();
-			
-			// The first test should not pass, because if it exists, it should be the bodyContentIndex part
-			if (((disposition == null) && (i == 0) && (i != bodyContentIndex)  && thisContentType.startsWith("text")) 
-               || ((disposition != null)
-					 && (disposition.equals(Part.ATTACHMENT) || disposition.equals(Part.INLINE))
-				     && (i != bodyContentIndex)) )
-		    {
-				String attFileName = part.getFileName();
-				if (attFileName != null && attFileName.length() > 17) {
-					attFileName = attFileName.substring(0,17);
-				}
-				commEventMap.put("contentName", subject + "-" + i + " " + attFileName);
-        		commEventMap.put("drMimeTypeId", thisContentType);
-        		if (thisContentType.startsWith("text")) {
-        			String content = (String)part.getContent();
-        			commEventMap.put("drDataResourceTypeId", "ELECTRONIC_TEXT");
-        			commEventMap.put("textData", content);
-        		} else {
-    				
-        			InputStream is = part.getInputStream();
-        			int c;
-        			while ((c = is.read()) > -1) {
-        				baos.write(c);
-        			}
-        			
-    				ByteWrapper imageData = new ByteWrapper(baos.toByteArray());
-    				int len = imageData.getLength();
-    				if (Debug.infoOn()) Debug.logInfo("imageData length: " + len, module);
-                    commEventMap.put("drDataResourceName", part.getFileName());
-    				commEventMap.put("imageData", imageData);
-    				commEventMap.put("drDataResourceTypeId", "IMAGE_OBJECT");
-            		commEventMap.put("_imageData_contentType", thisContentType);
-        		}
-        		dispatcher.runSync("createCommContentDataResource", commEventMap);
-    			attachmentCount++;
-		    }
-		}
+		currentIndex = "";
+		attachmentCount = 0;
+		return addMultipartAttachementToComm((Multipart)message.getContent(), commEventMap, subject, dispatcher, userLogin);
 
+    }
+    private static String currentIndex = "";
+    private static int attachmentCount = 0;
+    private static int addMultipartAttachementToComm(Multipart multipart, Map commEventMap, String subject, LocalDispatcher dispatcher, GenericValue userLogin)
+    throws MessagingException, IOException, GenericServiceException {
+    	try {
+    		int multipartCount = multipart.getCount();
+    		for (int i=0; i < multipartCount; i++) {
+    			Part part = multipart.getBodyPart(i);
+    			String thisContentTypeRaw = part.getContentType();
+    			int idx2 = thisContentTypeRaw.indexOf(";");
+    			if (idx2 == -1) idx2 = thisContentTypeRaw.length();
+    			String thisContentType = thisContentTypeRaw.substring(0, idx2);
+    			String disposition = part.getDisposition();
+    			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+    			if (thisContentType.startsWith("multipart") || thisContentType.startsWith("Multipart")) {
+    				currentIndex = currentIndex.concat("." + i);
+    				return	addMultipartAttachementToComm((Multipart) part.getContent(), commEventMap, subject, dispatcher, userLogin);
+    			}
+    			
+    			if(currentIndex.concat("." + i).equals(EmailServices.contentIndex)) continue;
+
+    			// The first test should not pass, because if it exists, it should be the bodyContentIndex part
+    			if (((disposition == null) && (i == 0) && thisContentType.startsWith("text")) 
+    					|| ((disposition != null)
+    							&& (disposition.equals(Part.ATTACHMENT) || disposition.equals(Part.INLINE))
+    							) )
+    			{
+    				String attFileName = part.getFileName();
+    				if (attFileName != null && attFileName.length() > 17) {
+    					attFileName = attFileName.substring(0,17);
+    				}
+    				commEventMap.put("contentName", subject + "-" + attachmentCount + " " + attFileName);
+    				commEventMap.put("drMimeTypeId", thisContentType);
+    				if (thisContentType.startsWith("text")) {
+    					String content = (String)part.getContent();
+    					commEventMap.put("drDataResourceTypeId", "ELECTRONIC_TEXT");
+    					commEventMap.put("textData", content);
+    				} else {
+
+    					InputStream is = part.getInputStream();
+    					int c;
+    					while ((c = is.read()) > -1) {
+    						baos.write(c);
+    					}
+
+    					ByteWrapper imageData = new ByteWrapper(baos.toByteArray());
+    					int len = imageData.getLength();
+    					if (Debug.infoOn()) Debug.logInfo("imageData length: " + len, module);
+    					commEventMap.put("drDataResourceName", part.getFileName());
+    					commEventMap.put("imageData", imageData);
+    					commEventMap.put("drDataResourceTypeId", "IMAGE_OBJECT");
+    					commEventMap.put("_imageData_contentType", thisContentType);
+    				}
+    				dispatcher.runSync("createCommContentDataResource", commEventMap);
+    				attachmentCount++;
+    			}
+    		}
+    	} catch (MessagingException e) {
+    		Debug.logError(e, module);
+    	} catch (IOException e) {
+    		Debug.logError(e, module);
+    	} catch (GenericServiceException e) {
+    		Debug.logError(e, module);
+    	}
     	return attachmentCount;
     }
 }
