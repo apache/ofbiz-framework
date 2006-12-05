53c1eff60c051f86f0970e514b4eb17e8b8f3431
Jacopo Cappellato <jacopoc@apache.org>
2006-12-05 22:00:29 +0000

    First commit of the "hhfacility" component a special purpose application for warehouse operations done using hand held devices. The application is still in a very initial stage, a lot of cleanups still need to be done, but it is a nice start. It has been contributed by Daniel Goodwin and Ray Barlow; see OFBIZ-226 for details. Daniel, Ray many thanks to both of you.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ofbiz/trunk@482801 13f79535-47bb-0310-9956-ffa450edef68

---
+++ specialpurpose/hhfacility/src/org/ofbiz/hhfacility/FacilitySession.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2001-2006 The Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ofbiz.hhfacility;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.base.util.GeneralRuntimeException;
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.webapp.control.RequestHandler;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.ServiceUtil;
+
+public class FacilitySession {
+
+    public static final String module = FacilitySession.class.getName();
+
+    public static final String findProductsById(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        String idValueStr = request.getParameter("idValue");
+        String facilityIdStr = request.getParameter("facilityId");
+        LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+
+        if (UtilValidate.isEmpty(idValueStr)) {
+            return "success";
+        }
+
+        Map productsMap = null;
+        try {
+            productsMap = dispatcher.runSync("findProductsById", UtilMisc.toMap("idValue", idValueStr, "facilityId", facilityIdStr));
+        } catch (GenericServiceException e) {
+            Debug.logError(e, "Problem in findProductsById", module);
+            return "error";
+        }
+
+        if (ServiceUtil.isError(productsMap)) {
+            return "error";
+        }
+
+        List productList = (List)productsMap.get("productList");
+        if (productList != null && productList.size() == 1) {
+            // Found only one product so go get it and redirect to the edit page
+            ServletContext ctx = (ServletContext) request.getAttribute("servletContext");
+            RequestHandler rh = (RequestHandler) ctx.getAttribute("_REQUEST_HANDLER_");
+            GenericValue product = EntityUtil.getFirst(productList);
+            String requestName = "/productstocktake?facilityId=" + facilityIdStr + "&productId=" + product.getString("productId");;
+            String target = rh.makeLink(request, response, requestName, false, false, false);
+            try {
+                response.sendRedirect(target);
+                return "none";
+            } catch (IOException e) {
+                Debug.logError(e, "Could not send redirect to: " + target, module);
+            }
+        }
+        request.setAttribute("productList", productList);
+        return "success";
+    }
+}
