aca6719ef8c131e4cb810b1c3015ecf83dbf94d5
Andrew Zeneski <jaz@apache.org>
2007-01-04 23:46:49 +0000

    added missing file from previous commit
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@492816 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/service/src/org/ofbiz/service/ModelServiceIface.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2001-2007 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ofbiz.service;
+
+/**
+ * ModelServiceIface
+ */
+public class ModelServiceIface {
+
+    protected String service;
+    protected boolean optional;
+
+    public ModelServiceIface(String service, boolean optional) {
+        this.service = service;
+        this.optional = optional;
+    }
+
+    public String getService() {
+        return this.service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public boolean isOptional() {
+        return this.optional;
+    }
+
+    public void isOptional(boolean optional) {
+        this.optional = optional;
+    }
+}
