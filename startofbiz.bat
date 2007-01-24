dad67935f7c3b73ec62b6955e8bfb6d973876fd7
Jacques Le Roux <jleroux@apache.org>
2007-01-24 17:15:54 +0000

    A (large) patch from Scott Gray  "Update ASL header" (https://issues.apache.org/jira/browse/OFBIZ-637). I put also some svn:ignore for shark build and lib diretories and assetmaint/build
    I did some by hand lately, too much to remember. They were files with old header but without copyright. A 1st attempt I suppose, will have to check this after this commit.
    
    I add some problems to commit this patch in one piece because it's so huge (specially with Eclipse I had to turn to Tortoise) !
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@499486 13f79535-47bb-0310-9956-ffa450edef68

--- startofbiz.bat
+++ startofbiz.bat
@@ -1,18 +1,21 @@
 ECHO OFF
 REM #####################################################################
-REM # Copyright 2001-2006 The Apache Software Foundation
-REM #
-REM # Licensed under the Apache License, Version 2.0 (the "License"); you may not
-REM # use this file except in compliance with the License. You may obtain a copy of
-REM # the License at
-REM #
-REM # http://www.apache.org/licenses/LICENSE-2.0
-REM #
-REM # Unless required by applicable law or agreed to in writing, software
-REM # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-REM # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-REM # License for the specific language governing permissions and limitations
-REM # under the License.
+REM Licensed to the Apache Software Foundation (ASF) under one
+REM or more contributor license agreements.  See the NOTICE file
+REM distributed with this work for additional information
+REM regarding copyright ownership.  The ASF licenses this file
+REM to you under the Apache License, Version 2.0 (the
+REM "License"); you may not use this file except in compliance
+REM with the License.  You may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing,
+REM software distributed under the License is distributed on an
+REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM KIND, either express or implied.  See the License for the
+REM specific language governing permissions and limitations
+REM under the License.
 REM #####################################################################
 ECHO ON
 
@@ -20,4 +23,3 @@ ECHO ON
 
 REM This one is for more of a debugging mode
 REM "%JAVA_HOME%\bin\java" -Xms256M -Xmx512M -Duser.language=en -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > framework\logs\console.log
-
