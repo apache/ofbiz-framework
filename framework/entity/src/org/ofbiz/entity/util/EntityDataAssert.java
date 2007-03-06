bf807aa5a077bc87fa3faea4556dc1a59134d786
David E. Jones <jonesde@apache.org>
2007-03-06 22:57:38 +0000

    Applied patch from Joe Eckard which implements the entity-xml-assert type of test and adds a small example test file for it that just validates against seed data; small change to reorganize, but mostly as-is
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@515340 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/util/EntityDataAssert.java
+++ framework/entity/src/org/ofbiz/entity/util/EntityDataAssert.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -35,7 +35,6 @@ import org.xml.sax.SAXException;
 
 /**
  * Some utility routines for loading seed data.
- *
  */
 public class EntityDataAssert {
 
@@ -43,7 +42,7 @@ public class EntityDataAssert {
 
     public static int assertData(URL dataUrl, GenericDelegator delegator, List errorMessages) throws GenericEntityException, SAXException, ParserConfigurationException, IOException {
         int rowsChecked = 0;
-        
+
         if (dataUrl == null) {
             String errMsg = "Cannot assert/check data, dataUrl was null";
             errorMessages.add(errMsg);
@@ -51,18 +50,18 @@ public class EntityDataAssert {
             return 0;
         }
 
-        Debug.logVerbose("[install.loadData] Loading XML Resource: \"" + dataUrl.toExternalForm() + "\"", module);
+        Debug.logVerbose("Loading XML Resource: " + dataUrl.toExternalForm(), module);
 
         try {
             List checkValueList = delegator.readXmlDocument(dataUrl);
             Iterator checkValueIter = checkValueList.iterator();
             while (checkValueIter.hasNext()) {
                 GenericValue checkValue = (GenericValue) checkValueIter.next();
-                
+
                 // to check get the PK, find by that, compare all fields
                 GenericPK checkPK = checkValue.getPrimaryKey();
                 GenericValue currentValue = delegator.findByPrimaryKey(checkPK);
-                
+
                 ModelEntity modelEntity = currentValue.getModelEntity();
                 List nonpkFieldNameList = modelEntity.getNoPkFieldNames();
                 Iterator nonpkFieldNameIter = nonpkFieldNameList.iterator();
@@ -70,29 +69,18 @@ public class EntityDataAssert {
                     String nonpkFieldName = (String) nonpkFieldNameIter.next();
                     Object checkField = checkValue.get(nonpkFieldName);
                     Object currentField = currentValue.get(nonpkFieldName);
-                    
-                    boolean matches = false;
-                    if (checkField == null) {
-                        if (currentField == null) {
-                            matches = true;
-                        }
-                    } else {
-                        if (checkField.equals(currentField)) {
-                            matches = true;
-                        }
-                    }
-                    
-                    if (!matches) {
+
+                    if (checkField != null && !checkField.equals(currentField)) {
                         StringBuffer matchError = new StringBuffer();
-                        matchError.append("Field [" + modelEntity.getEntityName() + "." + nonpkFieldName + "] did not match; file value [" + checkField  + "], db value [" + currentField + "] pk [" + checkPK + "]");
+                        matchError.append("Field [" + modelEntity.getEntityName() + "." + nonpkFieldName + "] did not match; file value [" + checkField + "], db value [" + currentField + "] pk [" + checkPK + "]");
                         errorMessages.add(matchError.toString());
                     }
                 }
-                
+
                 rowsChecked++;
             }
         } catch (GenericEntityException e) {
-            String xmlError = "Error checking/asserting XML Resource \"" + dataUrl.toExternalForm() + "\"; Error was: " + e.getMessage();
+            String xmlError = "Error checking/asserting XML Resource: " + dataUrl.toExternalForm() + "; Error was: " + e.getMessage();
             Debug.logError(e, xmlError, module);
             // instead of adding this as a message, throw the real exception; then caller has more control
             //errorMessages.add(xmlError);
