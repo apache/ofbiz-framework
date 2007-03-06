26cb26f00188a426f761bf8a89bd71ab720c7f95
David E. Jones <jonesde@apache.org>
2007-03-06 18:10:41 +0000

    Added stub and xsd file extension for entity-xml-assert test case
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@515235 13f79535-47bb-0310-9956-ffa450edef68

--- framework/testtools/src/org/ofbiz/testtools/ModelTestSuite.java
+++ framework/testtools/src/org/ofbiz/testtools/ModelTestSuite.java
@@ -92,6 +92,8 @@ public class ModelTestSuite {
                 }
             } else if ("service-test".equals(nodeName)) {
                 this.testList.add(new ServiceTest(caseName, this, childElement));
+            } else if ("entity-xml-assert".equals(nodeName)) {
+                this.testList.add(new EntityXmlAssertTest(caseName, this, childElement));
             } else if ("jython-test".equals(nodeName)) {
                 this.testList.add(new JythonTest(caseName, this, childElement));
             }
