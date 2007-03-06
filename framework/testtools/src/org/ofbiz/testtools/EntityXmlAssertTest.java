feb63ed2c1414f560a4422d03161338220ad1b6b
David E. Jones <jonesde@apache.org>
2007-03-06 22:57:38 +0000

    Applied patch from Joe Eckard which implements the entity-xml-assert type of test and adds a small example test file for it that just validates against seed data; small change to reorganize, but mostly as-is
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@515340 13f79535-47bb-0310-9956-ffa450edef68

--- framework/testtools/src/org/ofbiz/testtools/EntityXmlAssertTest.java
+++ framework/testtools/src/org/ofbiz/testtools/EntityXmlAssertTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,14 +19,26 @@
 package org.ofbiz.testtools;
 
 import junit.framework.TestResult;
+import junit.framework.AssertionFailedError;
 
 import org.w3c.dom.Element;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.util.EntityDataAssert;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.location.FlexibleLocation;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.net.URL;
 
 public class EntityXmlAssertTest extends TestCaseBase {
 
     public static final String module = ServiceTest.class.getName();
-    
+
     protected String entityXmlUrlString;
+    protected int testCaseCount;
 
     /**
      * @param modelTestSuite
@@ -34,15 +46,43 @@ public class EntityXmlAssertTest extends TestCaseBase {
     public EntityXmlAssertTest(String caseName, ModelTestSuite modelTestSuite, Element mainElement) {
         super(caseName, modelTestSuite);
         this.entityXmlUrlString = mainElement.getAttribute("entity-xml-url");
+        this.testCaseCount = 0;
+        try {
+            URL entityXmlURL = FlexibleLocation.resolveLocation(entityXmlUrlString);
+            List checkValueList = modelTestSuite.getDelegator().readXmlDocument(entityXmlURL);
+            this.testCaseCount = checkValueList.size();
+        } catch (Exception e) {
+            Debug.logError(e, "Error getting test case count", module);
+        }
     }
 
     public int countTestCases() {
-        return 1;
+        return this.testCaseCount;
     }
 
     public void run(TestResult result) {
-        // TODO Auto-generated method stub
-        
-        // NOTE: use EntityDataAssert.java
+
+        result.startTest(this);
+
+        try {
+
+            URL entityXmlURL = FlexibleLocation.resolveLocation(entityXmlUrlString);
+            GenericDelegator delegator = modelTestSuite.getDelegator();
+            List errorMessages = new ArrayList();
+
+            EntityDataAssert.assertData(entityXmlURL, delegator, errorMessages);
+
+            if (UtilValidate.isNotEmpty(errorMessages)) {
+                for (Iterator failureIterator = errorMessages.iterator(); failureIterator.hasNext();) {
+                    String failureMessage = (String) failureIterator.next();
+                    result.addFailure(this, new AssertionFailedError(failureMessage));
+                }
+            }
+
+        } catch (Exception e) {
+            result.addError(this, e);
+        }
+
+        result.endTest(this);
     }
 }
