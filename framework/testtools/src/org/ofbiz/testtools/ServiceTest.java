8dd413a946c59e1e794ceb23bc21ac79a2921daf
David E. Jones <jonesde@apache.org>
2007-03-08 01:21:49 +0000

    Appled patches from Joe Eckard for the ServiceTest implementation, and example to use it; Jira #OFBIZ-791; I made a small change to the patch to show the key and value for the error message Map
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@515876 13f79535-47bb-0310-9956-ffa450edef68

--- framework/testtools/src/org/ofbiz/testtools/ServiceTest.java
+++ framework/testtools/src/org/ofbiz/testtools/ServiceTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,13 +19,24 @@
 package org.ofbiz.testtools;
 
 import junit.framework.TestResult;
+import junit.framework.AssertionFailedError;
 
 import org.w3c.dom.Element;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.service.ModelService;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+
+import java.util.Map;
+import java.util.List;
+import java.util.Iterator;
 
 public class ServiceTest extends TestCaseBase {
 
     public static final String module = ServiceTest.class.getName();
-    
+
     protected String serviceName;
 
     /**
@@ -41,7 +52,44 @@ public class ServiceTest extends TestCaseBase {
     }
 
     public void run(TestResult result) {
-        // TODO Auto-generated method stub
-        
+
+        result.startTest(this);
+
+        LocalDispatcher dispatcher = modelTestSuite.getDispatcher();
+
+        try {
+
+            Map serviceResult = dispatcher.runSync(serviceName, UtilMisc.toMap("testCase", this));
+
+            // do something with the errorMessage
+            String errorMessage = (String) serviceResult.get(ModelService.ERROR_MESSAGE);
+            if (UtilValidate.isNotEmpty(errorMessage)) {
+                result.addFailure(this, new AssertionFailedError(errorMessage));
+            }
+
+            // do something with the errorMessageList
+            List errorMessageList = (List) serviceResult.get(ModelService.ERROR_MESSAGE_LIST);
+            if (UtilValidate.isNotEmpty(errorMessageList)) {
+                Iterator i = errorMessageList.iterator();
+                while (i.hasNext()) {
+                    result.addFailure(this, new AssertionFailedError((String) i.next()));
+                }
+            }
+
+            // do something with the errorMessageMap
+            Map errorMessageMap = (Map) serviceResult.get(ModelService.ERROR_MESSAGE_MAP);
+            if (!UtilValidate.isEmpty(errorMessageMap)) {
+                Iterator i = errorMessageMap.entrySet().iterator();
+                while (i.hasNext()) {
+                    Map.Entry entry = (Map.Entry) i.next();
+                    result.addFailure(this, new AssertionFailedError(entry.getKey() + ": " + entry.getValue()));
+                }
+            }
+
+        } catch (GenericServiceException e) {
+            result.addError(this, e);
+        }
+
+        result.endTest(this);
     }
 }
