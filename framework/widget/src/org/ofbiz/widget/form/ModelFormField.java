10f9b9d82d27f30ce278ee2470ba486bc7efc3c6
David E. Jones <jonesde@apache.org>
2007-05-20 06:37:22 +0000

    Small improvements to include alt-target, actions and row-actions for extended forms; also change to not try to get from a GenericEntity if it isn't a field of that entity, avoids lots of warning messages
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@539831 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/form/ModelFormField.java
+++ framework/widget/src/org/ofbiz/widget/form/ModelFormField.java
@@ -43,6 +43,7 @@ import org.ofbiz.base.util.collections.FlexibleMapAccessor;
 import org.ofbiz.base.util.collections.MapStack;
 import org.ofbiz.base.util.string.FlexibleStringExpander;
 import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntity;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.condition.EntityCondition;
@@ -637,7 +638,14 @@ public class ModelFormField {
             Object retVal = null;
             if (this.entryAcsr != null && !this.entryAcsr.isEmpty()) {
                 //Debug.logInfo("Getting entry, using entryAcsr for field " + this.getName() + " of form " + this.modelForm.getName(), module);
-                retVal = this.entryAcsr.get(dataMap);
+                if (dataMap instanceof GenericEntity) {
+                	GenericEntity genEnt = (GenericEntity) dataMap;
+                	if (genEnt.getModelEntity().isField(this.entryAcsr.getOriginalName())) {
+                		genEnt.get(this.entryAcsr.getOriginalName());
+                	}
+                } else {
+                	retVal = this.entryAcsr.get(dataMap);
+                }
             } else {
                 //Debug.logInfo("Getting entry, no entryAcsr so using field name " + this.name + " for field " + this.getName() + " of form " + this.modelForm.getName(), module);
                 // if no entry name was specified, use the field's name
