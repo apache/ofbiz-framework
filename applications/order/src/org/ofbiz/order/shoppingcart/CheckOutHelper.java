590e3daf86114c7100a63f35c5e27b03ce47f6ba
Jacopo Cappellato <jacopoc@apache.org>
2007-05-24 14:45:01 +0000

    Misc code cleanups, no functional changes.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@541311 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/src/org/ofbiz/order/shoppingcart/CheckOutHelper.java
+++ applications/order/src/org/ofbiz/order/shoppingcart/CheckOutHelper.java
@@ -342,7 +342,7 @@ public class CheckOutHelper {
               result = ServiceUtil.returnSuccess();
           }
           return result;
-      }
+    }
 
 
     public Map setCheckOutOptions(String shippingMethod, String shippingContactMechId, Map selectedPaymentMethods,
@@ -841,8 +841,6 @@ public class CheckOutHelper {
             RETRY_ON_ERROR = "Y";
         }
 
-        // Check the payment preferences; if we have ANY w/ status PAYMENT_NOT_AUTH invoke payment service.
-        boolean requireAuth = false;
         List allPaymentPreferences = null;
         try {
             allPaymentPreferences = delegator.findByAnd("OrderPaymentPreference", UtilMisc.toMap("orderId", orderId));
@@ -905,12 +903,9 @@ public class CheckOutHelper {
         Map paymentFields = UtilMisc.toMap("statusId", "PAYMENT_NOT_AUTH");
         List onlinePaymentPrefs = EntityUtil.filterByAnd(allPaymentPreferences, paymentFields);
 
-        if (onlinePaymentPrefs != null && onlinePaymentPrefs.size() > 0) {
-            requireAuth = true;
-        }
-
+        // Check the payment preferences; if we have ANY w/ status PAYMENT_NOT_AUTH invoke payment service.
         // Invoke payment processing.
-        if (requireAuth) {
+        if (onlinePaymentPrefs != null && onlinePaymentPrefs.size() > 0) {
             boolean autoApproveOrder = UtilValidate.isEmpty(productStore.get("autoApproveOrder")) || "Y".equalsIgnoreCase(productStore.getString("autoApproveOrder"));
             if (orderTotal == 0 && autoApproveOrder) {
                 // if there is nothing to authorize; don't bother
@@ -1363,7 +1358,7 @@ public class CheckOutHelper {
         return result;
     }
 
-    public double availableAccountBalance(String billingAccountId) {
+    public static double availableAccountBalance(String billingAccountId, LocalDispatcher dispatcher) {
         if (billingAccountId == null) return 0.0;
         try {
             Map res = dispatcher.runSync("calcBillingAccountBalance", UtilMisc.toMap("billingAccountId", billingAccountId));
@@ -1375,6 +1370,10 @@ public class CheckOutHelper {
             Debug.logError(e, module);
         }
         return 0.0;
+    }
+
+    public double availableAccountBalance(String billingAccountId) {
+        return availableAccountBalance(billingAccountId, dispatcher);
     }
 
     public Map makeBillingAccountMap(List paymentPrefs) {
