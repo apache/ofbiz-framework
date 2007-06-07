9c916e816e24668e9abbf60d49c52629298e6cb6
Jacopo Cappellato <jacopoc@apache.org>
2007-06-07 15:58:26 +0000

    Implemented first simple version of the Screen Widget Renderer for Fo (Formatting Objects): OFBIZ-809 It is now possible to decorate the screen definition with container and label elements. For a poc see the agreement item product report.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@545225 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/widget/src/org/ofbiz/widget/fo/FoScreenRenderer.java
@@ -0,0 +1,150 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.widget.fo;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.UtilFormatOut;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.webapp.control.RequestHandler;
+import org.ofbiz.webapp.taglib.ContentUrlTag;
+import org.ofbiz.widget.WidgetContentWorker;
+import org.ofbiz.widget.screen.ModelScreenWidget;
+import org.ofbiz.widget.screen.ScreenStringRenderer;
+import org.ofbiz.service.LocalDispatcher;
+import javolution.util.FastMap;
+
+/**
+ * Widget Library - HTML Form Renderer implementation
+ */
+public class FoScreenRenderer implements ScreenStringRenderer {
+
+    public static final String module = FoScreenRenderer.class.getName();
+
+    public FoScreenRenderer() {}
+
+    // This is a util method to get the style from a property file
+    public static String getFoStyle(String styleName) {
+        String value = UtilProperties.getPropertyValue("fo-styles.properties", styleName);
+        if (value.equals(styleName)) {
+            return "";
+        }
+        return value;
+    }
+
+    public void renderSectionBegin(Writer writer, Map context, ModelScreenWidget.Section section) throws IOException {
+        // do nothing, this is just a place holder container for HTML
+    }
+    public void renderSectionEnd(Writer writer, Map context, ModelScreenWidget.Section section) throws IOException {
+        // do nothing, this is just a place holder container for HTML
+    }
+
+    public void renderContainerBegin(Writer writer, Map context, ModelScreenWidget.Container container) throws IOException {
+        writer.write("<fo:block");
+
+        String style = container.getStyle(context);
+        if (UtilValidate.isNotEmpty(style)) {
+            writer.append(" ");
+            writer.append(FoScreenRenderer.getFoStyle(style));
+        }
+        writer.write(">");
+        appendWhitespace(writer);
+    }
+    public void renderContainerEnd(Writer writer, Map context, ModelScreenWidget.Container container) throws IOException {
+        writer.write("</fo:block>");
+        appendWhitespace(writer);
+    }
+
+    public void renderLabel(Writer writer, Map context, ModelScreenWidget.Label label) throws IOException {
+        String labelText = label.getText(context);
+        if (UtilValidate.isEmpty(labelText)) {
+            // nothing to render
+            return;
+        }
+        // open tag
+        String style = label.getStyle(context);
+        if (UtilValidate.isNotEmpty(style)) {
+            writer.write("<fo:inline ");
+            writer.append(FoScreenRenderer.getFoStyle(style));
+            writer.write(">");
+            // the text
+            writer.write(labelText);
+            // close tag
+            writer.write("</fo:inline>");
+        } else {
+            writer.write(labelText);
+        }
+        appendWhitespace(writer);
+    }
+
+    public void renderLink(Writer writer, Map context, ModelScreenWidget.Link link) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderImage(Writer writer, Map context, ModelScreenWidget.Image image) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderContentBegin(Writer writer, Map context, ModelScreenWidget.Content content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderContentBody(Writer writer, Map context, ModelScreenWidget.Content content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderContentEnd(Writer writer, Map context, ModelScreenWidget.Content content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderContentFrame(Writer writer, Map context, ModelScreenWidget.Content content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderSubContentBegin(Writer writer, Map context, ModelScreenWidget.SubContent content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderSubContentBody(Writer writer, Map context, ModelScreenWidget.SubContent content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void renderSubContentEnd(Writer writer, Map context, ModelScreenWidget.SubContent content) throws IOException {
+        // TODO: not implemented
+    }
+
+    public void appendWhitespace(Writer writer) throws IOException {
+        // appending line ends for now, but this could be replaced with a simple space or something
+        writer.write("\r\n");
+    }
+}
