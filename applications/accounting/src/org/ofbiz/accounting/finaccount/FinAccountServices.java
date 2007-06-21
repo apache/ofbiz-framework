293c90dd8ec551267c337cafffa10947d5ad1873
Andrew Zeneski <jaz@apache.org>
2007-06-21 20:05:11 +0000

    added comments to financial account transactions
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@549593 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/finaccount/FinAccountServices.java
+++ applications/accounting/src/org/ofbiz/accounting/finaccount/FinAccountServices.java
@@ -145,8 +145,10 @@ public class FinAccountServices {
 
             // create the credit transaction
             Map creditTransResult = dispatcher.runSync("createFinAccountTrans",
-                    UtilMisc.toMap("finAccountTransTypeId", "ADJUSTMENT", "finAccountId", creditAccount.getString("finAccountId"),
-                            "partyId", partyId, "amount", context.get("amount"), "userLogin", userLogin));
+                    UtilMisc.toMap("finAccountTransTypeId", "ADJUSTMENT", "finAccountId",
+                            creditAccount.getString("finAccountId"), "partyId", partyId, "amount",
+                            context.get("amount"), "comment", context.get("comment"), "userLogin", userLogin));
+
             if (ServiceUtil.isError(creditTransResult) || ServiceUtil.isFailure(creditTransResult)) {
                 return creditTransResult;
             }
