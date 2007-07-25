e6396bcd6f965fb125226a9e13c26feb6e8dac1f
Jacopo Cappellato <jacopoc@apache.org>
2007-07-25 10:04:28 +0000

    This is the first version of the screen output cache mechanism. It is still not ready for production because it needs much more tests, but I'm already committing it mainly for three reasons: 1) all the changes are well isolated 2) unless you explicitly set use-cache to true in a screen definition, everything will work as usual 3) I'd love to get some feedback from the community
    To test it: just set the use-cache attribute to true in a screen definition (<screen name='someName' use-cache='true'>), then you'll find a new entry in the WebTools-->cache  screen.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@559384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/widget/src/org/ofbiz/widget/cache/AbstractCache.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.widget.cache;
+
+import org.ofbiz.base.util.cache.UtilCache;
+
+public abstract class AbstractCache {
+
+    protected String id;
+
+    protected AbstractCache(String id) {
+        this.id = id;
+    }
+
+    public void remove(String widgetName) {
+        UtilCache.clearCache(getCacheName(widgetName));
+    }
+
+    public void clear() {
+        UtilCache.clearCachesThatStartWith(getCacheNamePrefix());
+    }
+
+    public String getCacheNamePrefix() {
+        return "widgetcache." + id + ".";
+    }
+
+    public String getCacheName(String widgetName) {
+        return getCacheNamePrefix() + widgetName;
+    }
+
+    protected UtilCache getCache(String widgetName) {
+        synchronized (UtilCache.utilCacheTable) {
+            return (UtilCache) UtilCache.utilCacheTable.get(getCacheName(widgetName));
+        }
+    }
+
+    protected UtilCache getOrCreateCache(String widgetName) {
+        synchronized (UtilCache.utilCacheTable) {
+            String name = getCacheName(widgetName);
+            UtilCache cache = (UtilCache) UtilCache.utilCacheTable.get(name);
+            if (cache == null) {
+                cache = new UtilCache(name, 0, 0, true);
+                cache.setPropertiesParams(new String[] {name});
+            }
+            return cache;
+        }
+    }
+}
---
+++ framework/widget/src/org/ofbiz/widget/cache/WidgetContextCacheKey.java
@@ -0,0 +1,195 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.widget.cache;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javolution.util.FastMap;
+import javolution.util.FastSet;
+
+public class WidgetContextCacheKey {
+
+    public static final String module = WidgetContextCacheKey.class.getName();
+    
+    private static Set fieldNamesToSkip;
+    
+    static {
+        fieldNamesToSkip = FastSet.newInstance();
+        fieldNamesToSkip.add("globalContext");
+        fieldNamesToSkip.add("delegator");
+        fieldNamesToSkip.add("dispatcher");
+        fieldNamesToSkip.add("security");
+        fieldNamesToSkip.add("webSiteId");
+        fieldNamesToSkip.add("userLogin");
+        fieldNamesToSkip.add("screens");
+        fieldNamesToSkip.add("nullField");
+        fieldNamesToSkip.add("availableLocales");
+        fieldNamesToSkip.add("availableTimeZones");
+        fieldNamesToSkip.add("autoUserLogin");
+        fieldNamesToSkip.add("person");
+        fieldNamesToSkip.add("partyGroup");
+        fieldNamesToSkip.add("timeZone");
+        fieldNamesToSkip.add("sessionAttributes");
+        fieldNamesToSkip.add("requestAttributes");
+        fieldNamesToSkip.add("JspTaglibs");
+        fieldNamesToSkip.add("requestParameters");
+        fieldNamesToSkip.add("page");
+        fieldNamesToSkip.add("controlPath");
+        fieldNamesToSkip.add("contextRoot");
+        fieldNamesToSkip.add("serverRoot");
+        fieldNamesToSkip.add("checkLoginUrl");
+        fieldNamesToSkip.add("externalLoginKey");
+        fieldNamesToSkip.add("externalKeyParam");
+        fieldNamesToSkip.add("nowTimestamp");
+        fieldNamesToSkip.add("session");
+        fieldNamesToSkip.add("request");
+        fieldNamesToSkip.add("response");
+        fieldNamesToSkip.add("application");
+        fieldNamesToSkip.add("formStringRenderer");
+        fieldNamesToSkip.add("null");
+        fieldNamesToSkip.add("sections");
+        fieldNamesToSkip.add("uiLabelMap");
+        // remove
+        fieldNamesToSkip.add("layoutSettings");
+        fieldNamesToSkip.add("activeApp");
+        fieldNamesToSkip.add("appheaderTemplate");
+        fieldNamesToSkip.add("servletContext");
+        // parameters
+        fieldNamesToSkip.add("visit");
+        fieldNamesToSkip.add("visitor");
+    }
+            
+    protected Map context;
+
+    public WidgetContextCacheKey(Map context) {
+        this.context = FastMap.newInstance();
+        this.context.putAll(context);
+    }
+
+    public int hashCode() {
+        return 0;
+    }
+    public boolean equals(Object obj) {
+        WidgetContextCacheKey key = null;
+        if (obj instanceof WidgetContextCacheKey) {
+            key = (WidgetContextCacheKey)obj;
+        }
+        if (key == null || key.context == null) {
+            return this.context == null;
+        }
+        if (this.context == null) {
+            return false;
+        }
+
+        Set unifiedContext = FastSet.newInstance();
+        unifiedContext.addAll(this.context.keySet());
+        unifiedContext.addAll(key.context.keySet());
+        Iterator fieldNameIt = unifiedContext.iterator();
+        while (fieldNameIt.hasNext()) {
+            String fieldName = (String)fieldNameIt.next();
+            if (fieldNamesToSkip.contains(fieldName)) {
+                continue;
+            }
+            Object field1 = this.context.get(fieldName);
+            Object field2 = key.context.get(fieldName);
+            if (field1 == null && field2 == null) {
+                continue;
+            }
+            if ((field1 == null || field2 == null) && field1 != field2) {
+                Debug.logWarning("Screen Key doesn't match for :" + fieldName + "; value1 = " + field1 + "; value2 = " + field2, module);
+                return false;
+            }
+            if ("parameters".equals(fieldName)) {
+                if (!parametersAreEqual((Map)field1, (Map)field2)) {
+                    return false;
+                }
+                continue;
+            }
+            if (!field1.equals(field2)) {
+                Debug.logWarning("Screen Key doesn't match for :" + fieldName + "; value1 = " + field1 + "; value2 = " + field2, module);
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public String toString() {
+        Map printableMap = FastMap.newInstance();
+        Iterator fieldNameIt = this.context.keySet().iterator();
+        while (fieldNameIt.hasNext()) {
+            String fieldName = (String)fieldNameIt.next();
+            if (!fieldNamesToSkip.contains(fieldName) && !"parameters".equals(fieldName)) {
+                printableMap.put(fieldName, this.context.get(fieldName));
+            }
+        }
+        return printMap(printableMap) + "\n" + printMap((Map)this.context.get("parameters"));
+    }
+
+    public static String printMap(Map map) {
+        Map printableMap = FastMap.newInstance();
+        Iterator fieldNameIt = map.keySet().iterator();
+        while (fieldNameIt.hasNext()) {
+            String fieldName = (String)fieldNameIt.next();
+            if (!fieldNamesToSkip.contains(fieldName) &&
+                    !fieldName.startsWith("javax.servlet") &&
+                    !fieldName.startsWith("org.apache") &&
+                    !fieldName.startsWith("_CLIENT_")) {
+                printableMap.put(fieldName, map.get(fieldName));
+            }
+        }
+        return UtilMisc.printMap(printableMap);
+    }
+
+    public static boolean parametersAreEqual(Map map1, Map map2) {
+        Set unifiedContext = FastSet.newInstance();
+        unifiedContext.addAll(map1.keySet());
+        unifiedContext.addAll(map2.keySet());
+        Iterator fieldNameIt = unifiedContext.iterator();
+        while (fieldNameIt.hasNext()) {
+            String fieldName = (String)fieldNameIt.next();
+            if (fieldNamesToSkip.contains(fieldName)) {
+                continue;
+            }
+            if (fieldName.startsWith("javax.servlet") ||
+                    fieldName.startsWith("org.apache") ||
+                    fieldName.startsWith("_CLIENT_")) {
+                continue;
+            }
+            Object field1 = map1.get(fieldName);
+            Object field2 = map2.get(fieldName);
+            if (field1 == null && field2 == null) {
+                continue;
+            }
+            if ((field1 == null || field2 == null) && field1 != field2) {
+                Debug.logWarning("Screen Key doesn't match for :" + fieldName + "; value1 = " + field1 + "; value2 = " + field2, module);
+                return false;
+            }
+            if (!field1.equals(field2)) {
+                Debug.logWarning("Screen Key doesn't match for :" + fieldName + "; value1 = " + field1 + "; value2 = " + field2, module);
+                return false;
+            }
+        }
+        return true;
+    }
+}
