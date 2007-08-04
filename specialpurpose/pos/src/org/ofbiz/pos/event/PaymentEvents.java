2fe09134771a063a7ba506e9a822a0e2cd3ac9ee
Jacques Le Roux <jleroux@apache.org>
2007-08-04 11:15:14 +0000

    A patch from Bilgin Ibryam "POS order status error" (https://issues.apache.org/jira/browse/OFBIZ-1176)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@562693 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/pos/src/org/ofbiz/pos/event/PaymentEvents.java
+++ specialpurpose/pos/src/org/ofbiz/pos/event/PaymentEvents.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -43,7 +43,7 @@ public class PaymentEvents {
             Debug.log("Processing [Cash] Amount : " + amount, module);
 
             // add the payment
-            trans.addPayment("CASH", amount);
+            trans.addPayment("CASH", amount, "N/A", null);
         } catch (GeneralException e) {
             // errors handled
         }
@@ -184,7 +184,7 @@ public class PaymentEvents {
                         break;
                 }
             }
-        }        
+        }
     }
 
     private static void processNoPayment(PosScreen pos, String paymentMethodTypeId) {
