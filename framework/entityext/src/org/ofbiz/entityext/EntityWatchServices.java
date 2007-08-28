39fefa21df2cc2f117ab4d07baf2dc5589026f21
David E. Jones <jonesde@apache.org>
2007-08-28 00:47:58 +0000

    Added set element for Entity ECA rules, and a simple service to be run as an EECA for watching entire entities or single fields on entities
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@570282 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/entityext/src/org/ofbiz/entityext/EntityWatchServices.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.entityext;
+
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.security.Security;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.jdbc.DatabaseUtil;
+import org.ofbiz.entity.model.ModelEntity;
+import org.ofbiz.entity.model.ModelField;
+import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilURL;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+
+import java.util.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+import java.net.URISyntaxException;
+
+public class EntityWatchServices {
+
+    public static final String module = EntityWatchServices.class.getName();
+
+    /**
+     * This service is meant to be called through an Entity ECA (EECA) to watch an entity
+     * 
+     * @param dctx
+     * @param context
+     * @return
+     */
+    public static Map watchEntity(DispatchContext dctx, Map context) {
+        GenericValue newValue = (GenericValue) context.get("newValue");
+        String fieldName = (String) context.get("fieldName");
+        
+        if (newValue == null) {
+            return ServiceUtil.returnSuccess();
+        }
+        
+        GenericValue currentValue = null;
+        try {
+            currentValue = dctx.getDelegator().findByPrimaryKey(newValue.getPrimaryKey());
+        } catch (GenericEntityException e) {
+            String errMsg = "Error finding currentValue for primary key [" + newValue.getPrimaryKey() + "]: " + e.toString();
+            Debug.logError(e, errMsg, module);
+        }
+        
+        if (currentValue != null) {
+            if (UtilValidate.isNotEmpty(fieldName)) {
+                // just watch the field
+                Object currentFieldValue = currentValue.get(fieldName);
+                Object newFieldValue = newValue.get(fieldName);
+                boolean changed = false;
+                if (currentFieldValue != null) {
+                    if (!currentFieldValue.equals(newFieldValue)) {
+                        changed = true;
+                    }
+                } else {
+                    if (newFieldValue != null) {
+                        changed = true;
+                    }
+                }
+                
+                if (changed) {
+                    String errMsg = "Watching entity [" + currentValue.getEntityName() + "] field [" + fieldName + "] value changed from [" + currentFieldValue + "] to [" + newFieldValue + "]";
+                    Debug.log(new Exception(errMsg), errMsg, module);
+                }
+            } else {
+                // watch the whole entity
+                if (!currentValue.equals(newValue)) {
+                    String errMsg = "Watching entity [" + currentValue.getEntityName() + "] whole value changed from [" + currentValue + "] to [" + newValue + "]";
+                    Debug.log(new Exception(errMsg), errMsg, module);
+                }
+            }
+        }
+        
+        return ServiceUtil.returnSuccess();
+    }
+}
