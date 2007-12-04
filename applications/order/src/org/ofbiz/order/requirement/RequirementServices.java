5a0336c3a8f0b8c901a2f476373bf011a4bce3e6
Si Chen <sichen@apache.org>
2007-12-04 19:03:31 +0000

    Refactor ATP requirements based on minimum stock so that the service can be run on order status change rather than on inventory reservation.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@601039 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/src/org/ofbiz/order/requirement/RequirementServices.java
+++ applications/order/src/org/ofbiz/order/requirement/RequirementServices.java
@@ -232,5 +232,93 @@ public class RequirementServices {
         }
         return ServiceUtil.returnSuccess();
     }
+
+    // note that this service is designed to work only when a sales order status changes from CREATED -> APPROVED because HOLD -> APPROVED is too complex
+    public static Map createATPRequirementsForOrder(DispatchContext ctx, Map context) {
+        GenericDelegator delegator = ctx.getDelegator();
+        LocalDispatcher dispatcher = ctx.getDispatcher();
+        GenericValue userLogin = (GenericValue) context.get("userLogin");
+
+        /*
+         * The strategy in this service is to begin making requirements when the product falls below the
+         * ProductFacility.minimumStock.  Because the minimumStock is an upper bound, the quantity to be required
+         * is either that required to bring the ATP back up to the minimumStock level or the amount ordered,
+         * whichever is less.
+         *
+         * If there is a way to support reorderQuantity without losing the order item -> requirement association data,
+         * then this service should be updated.
+         *
+         * The result is that this service generates many small requirements when stock levels are low for a product,
+         * which is perfectly fine since the system is capable of creating POs in bulk from aggregate requirements.
+         * The only concern would be a UI to manage numerous requirements with ease, preferrably by aggregating
+         * on productId.
+         */
+        String orderId = (String) context.get("orderId");
+        try {
+            GenericValue order = delegator.findByPrimaryKey("OrderHeader", UtilMisc.toMap("orderId", orderId));
+            GenericValue productStore = order.getRelatedOneCache("ProductStore");
+            String facilityId = productStore.getString("inventoryFacilityId");
+            List orderItems = order.getRelated("OrderItem");
+            for (Iterator iter = orderItems.iterator(); iter.hasNext(); ) {
+                GenericValue item = (GenericValue) iter.next();
+                GenericValue product = item.getRelatedOne("Product");
+                if (product == null) continue;
+                if (! "PRODRQM_ATP".equals(product.get("requirementMethodEnumId"))) continue;
+
+                Double quantity = item.getDouble("quantity");
+                Double cancelQuantity = item.getDouble("cancelQuantity");
+                double ordered = quantity.doubleValue() - (cancelQuantity == null ? 0.0 : cancelQuantity.doubleValue());
+                if (ordered <= 0.0) continue;
+
+                // get the minimum stock for this facility (don't do anything if not configured)
+                GenericValue productFacility = delegator.findByPrimaryKey("ProductFacility", UtilMisc.toMap("facilityId", facilityId, "productId", product.get("productId")));
+                if (productFacility == null || productFacility.get("minimumStock") == null) continue;
+                double minimumStock = productFacility.getDouble("minimumStock").doubleValue();
+
+                // get the facility ATP for product, which should be updated for this item's reservation
+                Map results = dispatcher.runSync("getInventoryAvailableByFacility", UtilMisc.toMap("userLogin", userLogin, "productId", product.get("productId"), "facilityId", facilityId));
+                if (ServiceUtil.isError(results)) return results;
+                double atp = ((Double) results.get("availableToPromiseTotal")).doubleValue(); // safe since this is a required OUT param
+
+                // the minimum stock is an upper bound, therefore we either require up to the minimum stock or the input required quantity, whichever is less
+                double shortfall = minimumStock - atp;
+                double required = Math.min(ordered, shortfall);
+                if (required <= 0.0) continue;
+
+                // count all current requirements for this product
+                double requirementQty = 0.0;
+                List conditions = UtilMisc.toList(
+                        new EntityExpr("facilityId", EntityOperator.EQUALS, facilityId),
+                        new EntityExpr("productId", EntityOperator.EQUALS, product.get("productId")),
+                        new EntityExpr("requirementTypeId", EntityOperator.EQUALS, "PRODUCT_REQUIREMENT"),
+                        new EntityExpr("statusId", EntityOperator.NOT_EQUAL, "REQ_ORDERED"),
+                        new EntityExpr("statusId", EntityOperator.NOT_EQUAL, "REQ_REJECTED")
+                );
+                List requirements = delegator.findByAnd("Requirement", conditions);
+                for (Iterator riter = requirements.iterator(); riter.hasNext(); ) {
+                    GenericValue requirement = (GenericValue) riter.next();
+                    requirementQty += (requirement.get("quantity") == null ? 0.0 : requirement.getDouble("quantity").doubleValue());
+                }
+
+                // if we the existing requirements are not enough, then create a new requirement for the difference
+                required -= requirementQty;
+                if (required <= 0.0) continue;
+
+                Map input = UtilMisc.toMap("userLogin", userLogin, "facilityId", facilityId, "productId", product.get("productId"), "quantity", new Double(required), "requirementTypeId", "PRODUCT_REQUIREMENT");
+                results = dispatcher.runSync("createRequirement", input);
+                if (ServiceUtil.isError(results)) return results;
+                String requirementId = (String) results.get("requirementId");
+
+                input = UtilMisc.toMap("userLogin", userLogin, "orderId", order.get("orderId"), "orderItemSeqId", item.get("orderItemSeqId"), "requirementId", requirementId, "quantity", new Double(required));
+                results = dispatcher.runSync("createOrderRequirementCommitment", input);
+                if (ServiceUtil.isError(results)) return results;
+            }
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+        } catch (GenericServiceException e) {
+            Debug.logError(e, module);
+        }
+        return ServiceUtil.returnSuccess();
+    }
 }
 
