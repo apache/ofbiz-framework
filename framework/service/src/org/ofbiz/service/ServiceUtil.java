a0ffc4e42b1630888c2c297998ef4dd326be1f2e
Andrew Zeneski <jaz@apache.org>
2007-12-18 17:12:58 +0000

    added new service to reset scheduled jobs
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@605258 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/ServiceUtil.java
+++ framework/service/src/org/ofbiz/service/ServiceUtil.java
@@ -604,4 +604,45 @@ public class ServiceUtil {
         }
         return UtilGenerics.checkMap(UtilMisc.toMap(args));
     }
+
+    public static Map resetJob(DispatchContext dctx, Map context) {
+        GenericDelegator delegator = dctx.getDelegator();
+        Security security = dctx.getSecurity();
+        GenericValue userLogin = (GenericValue) context.get("userLogin");
+        Locale locale = getLocale(context);
+
+        if (!security.hasPermission("SERVICE_INVOKE_ANY", userLogin)) {
+            String errMsg = UtilProperties.getMessage(ServiceUtil.resource, "serviceUtil.no_permission_to_run", locale) + ".";
+            return ServiceUtil.returnError(errMsg);
+        }
+
+        String jobId = (String) context.get("jobId");
+        Map fields = UtilMisc.toMap("jobId", jobId);
+        GenericValue job;
+        try {
+            job = delegator.findByPrimaryKey("JobSandbox", fields);
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+            return ServiceUtil.returnError(e.getMessage());
+        }
+
+        // update the job
+        if (job != null) {
+            job.set("statusId", "SERVICE_PENDING");
+            job.set("startDateTime", null);
+            job.set("finishDateTime", null);
+            job.set("cancelDateTime", null);
+            job.set("runByInstanceId", null);
+
+            // save the job
+            try {
+                job.store();
+            } catch (GenericEntityException e) {
+                Debug.logError(e, module);
+                return ServiceUtil.returnError(e.getMessage());
+            }
+        }
+
+        return ServiceUtil.returnSuccess();
+    }
 }
