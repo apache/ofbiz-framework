apache/ofbiz-framework

commit 62fc857ef8cf71ee7e2cb1b54002ab6d27bc3c63

Disabled 'Tell-A-Friend' and 'Customer Reviews' links from Order manager. Small cleanups.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@611802 13f79535-47bb-0310-9956-ffa450edef68

applications/order/webapp/ordermgr/entry/checkoutoptions.ftl
@@ -276,7 +276,7 @@ function submitForm(form, mode, value) {
                       </#list>
                       </b>
                       </div>
-                      <div class="tabletext">${uiLabelMap.OrderUpdateEmailAddress} <a href="<@ofbizUrl>viewprofile?DONE_PAGE=quickcheckout</@ofbizUrl>" class="buttontext">${uiLabelMap.PartyProfile}</a>.</div>
+                      <div class="tabletext">${uiLabelMap.OrderUpdateEmailAddress} <a href="${customerDetailLink}${shoppingCart.getPartyId()}" target="partymgr" class="buttontext">${uiLabelMap.PartyProfile}</a>.</div>
                       <br/>
                       <div class="tabletext">${uiLabelMap.OrderCommaSeperatedEmailAddresses}:</div>
                       <input type="text" class="inputBox" size="30" name="order_additional_emails" value="${shoppingCart.getOrderAdditionalEmails()?if_exists}"/>

applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
@@ -1652,7 +1652,7 @@ public class PaymentGatewayServices {
         if (authResult != null) {
             processAuthResult(dctx, result, userLogin, paymentPreference);
             resultPassed = authResult.booleanValue();
-            authServiceType = ("PAYMENT_NOT_AUTH".equals(initialStatus)) ? AUTH_SERVICE_TYPE : REAUTH_SERVICE_TYPE;;
+            authServiceType = ("PAYMENT_NOT_AUTH".equals(initialStatus)) ? AUTH_SERVICE_TYPE : REAUTH_SERVICE_TYPE;
         }
         if (captureResult != null) {
             processCaptureResult(dctx, result, userLogin, paymentPreference, authServiceType);

applications/content/src/org/ofbiz/content/content/ContentWorker.java
@@ -1603,7 +1603,7 @@ public class ContentWorker implements org.ofbiz.widget.ContentWorkerInterface {
             String key = (String)it.next();
             if ("request response session".indexOf(key) < 0) {
                 Object obj = map.get(key);
-                s += spc + key + sep;;
+                s += spc + key + sep;
                 if (obj instanceof GenericValue) {
                     GenericValue gv = (GenericValue)obj;
                     GenericPK pk = gv.getPrimaryKey();

applications/ecommerce/widget/CatalogScreens.xml
@@ -230,7 +230,7 @@ under the License.
                 <set field="leftbarScreenName" value="leftbar"/>
                 <set field="rightbarScreenName" value="rightbar"/>
                 <set field="MainColumnStyle" value="center"/>
-
+                <set field="isEcommerceScreen" value="Y"/>
                 <set field="titleProperty" value="PageTitleProductPage"/>
                 <set field="configproductdetailScreen" value="component://ecommerce/widget/CatalogScreens.xml#configproductdetail"/>
                 <script location="component://order/webapp/ordermgr/WEB-INF/actions/entry/catalog/product.bsh"/>

applications/order/webapp/ordermgr/entry/catalog/configproductdetail.ftl
@@ -240,12 +240,14 @@ ${virtualJavaScript?if_exists}
         <div class="tabletext"><b>${uiLabelMap.ProductUsuallyShipsIn} <font color='red'>${daysToShip}</font> ${uiLabelMap.CommonDays}!<b></div>
       </#if>
 
-      <#-- tell a friend -->
-      <div class="tabletext">&nbsp;</div>
-      <div class="tabletext">
-        <a href="javascript:popUpSmall('<@ofbizUrl>tellafriend?productId=${product.productId}</@ofbizUrl>','tellafriend');" class="buttontext">${uiLabelMap.CommonTellAFriend}</a>
-      </div>
-
+      <#-- show tell a friend details only in ecommerce application -->
+      <#if (isEcommerceScreen?exists && isEcommerceScreen?default("N") == "Y")>
+        <div class="tabletext">&nbsp;</div>
+        <div class="tabletext">
+          <a href="javascript:popUpSmall('<@ofbizUrl>tellafriend?productId=${product.productId}</@ofbizUrl>','tellafriend');" class="buttontext">${uiLabelMap.CommonTellAFriend}</a>
+        </div>
+      </#if>
+     
       <#if disFeatureList?exists && 0 < disFeatureList.size()>
         <p>&nbsp;</p>
         <#list disFeatureList as currentFeature>
@@ -532,6 +534,7 @@ ${virtualJavaScript?if_exists}
   <tr><td colspan="2"><hr class='sepbar'></td></tr>
 
   <#-- Product Reviews -->
+  <#if (isEcommerceScreen?exists && isEcommerceScreen?default("N") == "Y")>
   <tr>
     <td colspan="2">
       <div class="tableheadtext">${uiLabelMap.EcommerceCustomerReviews}:</div>
@@ -587,11 +590,12 @@ ${virtualJavaScript?if_exists}
     </tr>
     <tr>
       <td colspan="2">
-        <a href="<@ofbizUrl>reviewProduct?category_id=${categoryId?if_exists}&product_id=${product.productId}</@ofbizUrl>" class="buttontext">${uiLabelMap.ProductBeTheFirstToReviewThisProduct}!</a>
+        <a href="<@ofbizUrl>reviewProduct?category_id=${categoryId?if_exists}&product_id=${product.productId}</@ofbizUrl>" class="buttontext">${uiLabelMap.ProductBeTheFirstToReviewThisProduct}</a>
       </td>
     </tr>
   </#if>
 </table>
+</#if>
 
 <#-- Upgrades/Up-Sell/Cross-Sell -->
   <#macro associated assocProducts beforeName showName afterName formNamePrefix targetRequestName>

applications/order/webapp/ordermgr/entry/catalog/productdetail.ftl
@@ -372,12 +372,14 @@ ${virtualJavaScript?if_exists}
         <div class="tabletext"><b>${uiLabelMap.ProductUsuallyShipsIn} <font color="red">${daysToShip}</font> ${uiLabelMap.CommonDays}!<b></div>
       </#if>
 
-      <#-- tell a friend -->
-      <div class="tabletext">&nbsp;</div>
-      <div class="tabletext">
-        <a href="javascript:popUpSmall('<@ofbizUrl>tellafriend?productId=${product.productId}</@ofbizUrl>','tellafriend');" class="buttontext">${uiLabelMap.CommonTellAFriend}</a>
-      </div>
-      <br/>
+      <#-- show tell a friend details only in ecommerce application -->
+      <#if (isEcommerceScreen?exists && isEcommerceScreen?default("N") == "Y")>
+        <div class="tabletext">&nbsp;</div>
+        <div class="tabletext">
+          <a href="javascript:popUpSmall('<@ofbizUrl>tellafriend?productId=${product.productId}</@ofbizUrl>','tellafriend');" class="buttontext">${uiLabelMap.CommonTellAFriend}</a>
+        </div>
+        <br/>
+      </#if>
 
       <#if disFeatureList?exists && 0 < disFeatureList.size()>
       <p>&nbsp;</p>
@@ -559,7 +561,8 @@ ${virtualJavaScript?if_exists}
   <#-- Any attributes/etc may go here -->
 
   <#-- Product Reviews -->
-  <div id="reviews">
+  <#if (isEcommerceScreen?exists && isEcommerceScreen?default("N") == "Y")>  
+    <div id="reviews">
       <div class="tableheadtext">${uiLabelMap.EcommerceCustomerReviews}:</div>
       <#if averageRating?exists && (averageRating?double > 0) && numRatings?exists && (numRatings?double > 1)>
           <div class="tabletext">${uiLabelMap.EcommerceAverageRating}: ${averageRating} <#if numRatings?exists>(${uiLabelMap.CommonFrom} ${numRatings} ${uiLabelMap.EcommerceRatings})</#if></div>
@@ -600,11 +603,11 @@ ${virtualJavaScript?if_exists}
       <#else>
         <div class="tabletext">${uiLabelMap.ProductProductNotReviewedYet}.</div>
         <div>
-            <a href="<@ofbizUrl>reviewProduct?category_id=${categoryId?if_exists}&product_id=${product.productId}</@ofbizUrl>" class="linktext">${uiLabelMap.ProductBeTheFirstToReviewThisProduct}!</a>
+            <a href="<@ofbizUrl>reviewProduct?category_id=${categoryId?if_exists}&product_id=${product.productId}</@ofbizUrl>" class="linktext">${uiLabelMap.ProductBeTheFirstToReviewThisProduct}</a>
         </div>
       </#if>
-  </div>
-
+    </div>
+  </#if>
 <#-- Upgrades/Up-Sell/Cross-Sell -->
   <#macro associated assocProducts beforeName showName afterName formNamePrefix targetRequestName>
   <#assign targetRequest = "product">

applications/product/src/org/ofbiz/product/store/ProductStoreWorker.java
@@ -690,7 +690,7 @@ public class ProductStoreWorker {
             return isInventoryAvailable;
 
         } else {
-            GenericValue product = productConfig.getProduct();;
+            GenericValue product = productConfig.getProduct();
             List productFacilities = null;
 
             try {

commit 0c11142fd3a7d189828185ccca01f5d16e11c0e6

Fix a bug introduced by me in r611802.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@612589 13f79535-47bb-0310-9956-ffa450edef68

applications/order/webapp/ordermgr/entry/checkoutoptions.ftl
@@ -276,7 +276,8 @@ function submitForm(form, mode, value) {
                       </#list>
                       </b>
                       </div>
-                      <div class="tabletext">${uiLabelMap.OrderUpdateEmailAddress} <a href="<@ofbizUrl>viewprofile?DONE_PAGE=quickcheckout</@ofbizUrl>" class="buttontext">${uiLabelMap.PartyProfile}</a>.</div>
+                      <div class="tabletext">${uiLabelMap.OrderUpdateEmailAddress} <a href="<#if customerDetailLink?exists>${customerDetailLink}${shoppingCart.getPartyId()}" target="partymgr"
+                        <#else><@ofbizUrl>viewprofile?DONE_PAGE=quickcheckout</@ofbizUrl>"</#if> class="buttontext">${uiLabelMap.PartyProfile}</a>.</div>
                       <br/>
                       <div class="tabletext">${uiLabelMap.OrderCommaSeperatedEmailAddresses}:</div>
                       <input type="text" class="inputBox" size="30" name="order_additional_emails" value="${shoppingCart.getOrderAdditionalEmails()?if_exists}"/>