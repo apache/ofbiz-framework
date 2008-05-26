d110abcd29bb36caa903a1d5fcbcca878d93bf06
David E. Jones <jonesde@apache.org>
2008-05-26 15:44:25 +0000

    A few cleanups, taking advantage of dot syntax, done while testing
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@660218 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/webapp/catalog/WEB-INF/actions/product/EditProductFeatures.groovy
+++ applications/product/webapp/catalog/WEB-INF/actions/product/EditProductFeatures.groovy
@@ -19,31 +19,19 @@
 
 import org.ofbiz.entity.*;
 
-
 productFeatureAndAppls = delegator.findByAnd('ProductFeatureAndAppl',
         ['productId' : productId],
         ['sequenceNum', 'productFeatureApplTypeId', 'productFeatureTypeId', 'description']);
-if (productFeatureAndAppls != null) {
-    context.put('productFeatureAndAppls', productFeatureAndAppls);
-}
+context.productFeatureAndAppls = productFeatureAndAppls;
 
 productFeatureCategories = delegator.findAll('ProductFeatureCategory', ['description']);
-if (productFeatureCategories != null) {
-    context.put('productFeatureCategories', productFeatureCategories);
-}
+context.productFeatureCategories = productFeatureCategories;
 
 productFeatureApplTypes = delegator.findAll('ProductFeatureApplType', ['description']);
-if (productFeatureApplTypes != null) {
-    context.put('productFeatureApplTypes', productFeatureApplTypes);
-}
+context.productFeatureApplTypes = productFeatureApplTypes;
 
 productFeatureGroups = delegator.findAll('ProductFeatureGroup', ['description']);
-if (productFeatureGroups != null) {
-    context.put('productFeatureGroups', productFeatureGroups);
-}
+context.productFeatureGroups = productFeatureGroups;
 
 productFeatureTypes = delegator.findAll('ProductFeatureType', ['description']);
-if (productFeatureTypes != null) {
-    context.put('productFeatureTypes', productFeatureTypes);
-}
-
+context.productFeatureTypes = productFeatureTypes;
