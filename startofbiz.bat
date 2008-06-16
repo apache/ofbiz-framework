27ca68788eb045c07dedf45a7c540b0b95234cdf
Jacques Le Roux <jleroux@apache.org>
2008-06-16 20:54:28 +0000

    Small update
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@668301 13f79535-47bb-0310-9956-ffa450edef68

--- startofbiz.bat
+++ startofbiz.bat
@@ -22,4 +22,4 @@ ECHO ON
 "%JAVA_HOME%\bin\java" -Xms256M -Xmx512M -jar ofbiz.jar > runtime\logs\console.log
 
 REM This one is for more of a debugging mode
-REM "%JAVA_HOME%\bin\java" -Xms256M -Xmx512M -Duser.language=en -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > runtime\logs\console.log
+REM "%JAVA_HOME%\bin\java" -Xms256M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > runtime\logs\console.log
