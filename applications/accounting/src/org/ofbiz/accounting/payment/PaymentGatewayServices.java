700247b5e48a1773823968e599840ad3f6fb1ed2
Jacques Le Roux <jleroux@apache.org>
2008-06-27 07:22:35 +0000

    A patch from Chris Lombardi "POS: CVV2 code is not always deleted from the DB" (https://issues.apache.org/jira/browse/OFBIZ-1716) - OFBIZ-1716
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@672130 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
+++ applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
@@ -1770,16 +1770,16 @@ public class PaymentGatewayServices {
             // set the status of the OrderPaymentPreference
             if (context != null && authResult.booleanValue()) {
                 orderPaymentPreference.set("statusId", "PAYMENT_AUTHORIZED");
-                orderPaymentPreference.set("securityCode", null);
-                orderPaymentPreference.set("track2", null);                
             } else if (context != null && !authResult.booleanValue()) {
                 orderPaymentPreference.set("statusId", "PAYMENT_DECLINED");
             } else {
                 orderPaymentPreference.set("statusId", "PAYMENT_ERROR");
-                orderPaymentPreference.set("securityCode", null);
-                orderPaymentPreference.set("track2", null);                
             }
             
+            // remove sensitive credit card data regardless of outcome
+            orderPaymentPreference.set("securityCode", null);
+            orderPaymentPreference.set("track2", null);    
+                
             boolean needsNsfRetry = needsNsfRetry(orderPaymentPreference, context, delegator);
             if (needsNsfRetry) {
                 orderPaymentPreference.set("needsNsfRetry", "Y");
