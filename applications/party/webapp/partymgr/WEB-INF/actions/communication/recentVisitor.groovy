8cfb6c738fe71cd35df94a3a38bbc4d71ed185a6
Johannes Cornelius Bakker <hansbak@apache.org>
2008-06-28 09:14:01 +0000

    misplaced file in previous commit
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@672482 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/party/webapp/partymgr/WEB-INF/actions/communication/recentVisitor.groovy
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.ofbiz.entity.*
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.entity.util.*
+import org.ofbiz.base.util.*;
+
+lastDate = UtilDateTime.addDaysToTimestamp(UtilDateTime.nowTimestamp(), new Integer("-21")); // should be there the last 3 weeks.
+searchCondition = EntityCondition.makeCondition("fromDate", EntityOperator.GREATER_THAN, lastDate);
+options = new EntityFindOptions(false, 0, 0,, true);
+context.recentParties = delegator.findList("PartyNameVisitView", searchCondition, (Set)["partyId", "firstName", "middleName", "lastName", "groupName"], null, options, true);
