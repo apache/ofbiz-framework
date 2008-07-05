f38c498fb5a95d5ec90e619485c7c71443dc6c61
Jacques Le Roux <jleroux@apache.org>
2008-07-05 19:08:35 +0000

    More concise
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@674227 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
+++ applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
@@ -996,8 +996,7 @@ public class PaymentGatewayServices {
 
         if (testOrderId == null || !allSameOrder) {
             Debug.logWarning("Attempt to settle Invoice #" + invoiceId + " which contained none/multiple orders", module);
-            Map result = ServiceUtil.returnFailure("Attempt to settle Invoice #" + invoiceId + " which contained none/multiple orders");
-            return result;
+            return ServiceUtil.returnFailure("Attempt to settle Invoice #" + invoiceId + " which contained none/multiple orders");
         }
 
         // get the invoice amount (amount to bill)
