77d555cb858e23cb32fa11c77f74d20d082c22bc
Scott Gray <lektran@apache.org>
2008-07-14 09:11:46 +0000

    A few more bsh -> groovy conversions (and they're tested I promise!)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@676519 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/order/webapp/ordermgr/WEB-INF/actions/order/CompanyHeader.groovy
@@ -0,0 +1,193 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+ // this script is used to get the company's logo header information for orders, invoices, and returns.  It can either take order, invoice, returnHeader from
+ // parameters or use orderId, invoiceId, or returnId to look them up.  
+ // if none of these parameters are available then fromPartyId is used or "Company" as fallback
+
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.util.*;
+import org.ofbiz.party.contact.*;
+import org.ofbiz.order.order.OrderReadHelper;
+import java.sql.Timestamp;
+
+orderHeader = parameters.orderHeader;
+orderId = parameters.orderId;
+invoice = parameters.invoice;
+invoiceId = parameters.invoiceId;
+shipmentId = parameters.shipmentId;
+returnHeader = parameters.returnHeader;
+returnId = parameters.returnId;
+quote = null;
+quoteId = parameters.quoteId;
+fromPartyId = parameters.fromPartyId;
+
+if (!orderHeader && orderId) {
+    orderHeader = delegator.findByPrimaryKey("OrderHeader", [orderId : orderId]);
+} else if (shipmentId) {
+    shipment = delegator.findByPrimaryKey("Shipment", [shipmentId : shipmentId]);
+    orderHeader = shipment.getRelatedOne("PrimaryOrderHeader");
+}
+
+if (!invoice && invoiceId)	{
+    invoice = delegator.findByPrimaryKey("Invoice", [invoiceId : invoiceId]);
+}
+
+if (!returnHeader && returnId) {
+    returnHeader = delegator.findByPrimaryKey("ReturnHeader", [returnId : returnId]);
+}
+
+if (quoteId) {
+    quote = delegator.findByPrimaryKey("Quote", [quoteId : quoteId]);
+}
+
+// defaults:
+logoImageUrl = null; // the default value, "/images/ofbiz_powered.gif", is set in the screen decorators
+partyId = null;
+
+// get the logo partyId from order or invoice - note that it is better to do comparisons this way in case the there are null values
+if (orderHeader) {
+    orh = new OrderReadHelper(orderHeader);
+    // for sales order, the logo party is the "BILL_FROM_VENDOR" of the order.  If that's not available, we'll use the OrderHeader's ProductStore's payToPartyId
+    if ("SALES_ORDER".equals(orderHeader.orderTypeId)) {
+        if (orh.getBillToParty()) {
+            partyId = orh.getBillFromParty().partyId;
+        } else {
+            productStore = orderHeader.getRelatedOne("ProductStore");
+            if (orderHeader.orderTypeId.equals("SALES_ORDER") && productStore?.payToPartyId) {
+                partyId = productStore.payToPartyId;
+            }
+        }
+    // purchase orders - use the BILL_TO_CUSTOMER of the order
+    } else if ("PURCHASE_ORDER".equals(orderHeader.orderTypeId)) {
+        partyId = orh.getBillToParty().partyId;
+        billToCustomer = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderRole", [roleTypeId : "BILL_TO_CUSTOMER"]));
+        if (billToCustomer) {
+            partyId = billToCustomer.partyId;
+        }
+    }
+} else if (invoice) {
+    if ("SALES_INVOICE".equals(invoice.invoiceTypeId) && invoice.partyIdFrom) {
+        partyId = invoice.partyIdFrom;
+    }
+    if ("PURCHASE_INVOICE".equals(invoice.invoiceTypeId) || "CUST_RTN_INVOICE".equals(invoice.invoiceTypeId) && invoice.partyId) {
+        partyId = invoice.partyId;
+    }
+} else if (returnHeader) {
+    if ("CUSTOMER_RETURN".equals(returnHeader.returnHeaderTypeId) && returnHeader.toPartyId) {
+        partyId = returnHeader.toPartyId;
+    }
+} else if (quote) {
+    productStore = quote.getRelatedOne("ProductStore");
+    if (productStore?.payToPartyId) {
+        partyId = productStore.payToPartyId;
+    }
+}
+
+// if partyId wasn't found use fromPartyId-parameter
+if (!partyId) {
+    if (fromPartyId) {
+        partyId = fromPartyId;
+    } else {
+        partyId = "Company";
+    }
+}
+
+// the logo
+partyGroup = delegator.findByPrimaryKey("PartyGroup", [partyId : partyId]);
+if (partyGroup?.logoImageUrl) {
+    logoImageUrl = partyGroup.logoImageUrl;
+}
+context.logoImageUrl = logoImageUrl;
+
+// the company name
+companyName = "not found";
+if (partyGroup?.groupName) {
+    companyName = partyGroup.groupName;
+}
+context.companyName = companyName;
+
+// the address
+addresses = delegator.findByAnd("PartyContactMechPurpose", [partyId : partyId, contactMechPurposeTypeId : "GENERAL_LOCATION"]);
+selAddresses = EntityUtil.filterByDate(addresses, nowTimestamp, "fromDate", "thruDate", true);        
+address = null;
+if (selAddresses) {
+    address = delegator.findByPrimaryKey("PostalAddress", [contactMechId : selAddresses[0].contactMechId]);
+}
+if (address)	{
+   // get the country name and state/province abbreviation
+   country = address.getRelatedOneCache("CountryGeo");
+   if (country) {
+      context.countryName = country.geoName;
+   }
+   stateProvince = address.getRelatedOneCache("StateProvinceGeo");
+   if (stateProvince) {
+       context.stateProvinceAbbr = stateProvince.abbreviation;
+   }
+}
+context.postalAddress = address;
+
+//telephone
+phones = delegator.findByAnd("PartyContactMechPurpose", [partyId : partyId, contactMechPurposeTypeId : "PRIMARY_PHONE"]);
+selPhones = EntityUtil.filterByDate(phones, nowTimestamp, "fromDate", "thruDate", true);
+if (selPhones) {
+    context.phone = delegator.findByPrimaryKey("TelecomNumber", [contactMechId : selPhones[0].contactMechId]);
+}
+
+//Email
+emails = delegator.findByAnd("PartyContactMechPurpose", [partyId : partyId, contactMechPurposeTypeId : "PRIMARY_EMAIL"]);
+selEmails = EntityUtil.filterByDate(emails, nowTimestamp, "fromDate", "thruDate", true);
+if (selEmails) {
+    context.email = delegator.findByPrimaryKey("ContactMech", [contactMechId : selEmails[0].contactMechId]);
+} else {    //get email address from party contact mech
+    contacts = delegator.findByAnd("PartyContactMech", [partyId : partyId]);
+    selContacts = EntityUtil.filterByDate(contacts, nowTimestamp, "fromDate", "thruDate", true);
+    if (selContacts) {
+        i = selContacts.iterator();
+        while (i.hasNext())	{
+            email = i.next().getRelatedOne("ContactMech");
+            if ("ELECTRONIC_ADDRESS".equals(email.contactMechTypeId))	{
+                context.email = email;
+                break;
+            }
+        }
+    }
+}
+
+// website
+contacts = delegator.findByAnd("PartyContactMech", [partyId : partyId]);
+selContacts = EntityUtil.filterByDate(contacts, nowTimestamp, "fromDate", "thruDate", true);        
+if (selContacts) {
+    Iterator i = selContacts.iterator();
+    while (i.hasNext())	{
+        website = i.next().getRelatedOne("ContactMech");
+        if ("WEB_ADDRESS".equals(website.contactMechTypeId)) {
+            context.website = website;
+            break;
+        }
+    }
+}
+
+//Bank account
+paymentMethods = delegator.findByAnd("PaymentMethod", [partyId : partyId, paymentMethodTypeId : "EFT_ACCOUNT"]);
+selPayments = EntityUtil.filterByDate(paymentMethods, nowTimestamp, "fromDate", "thruDate", true);        
+if (selPayments) {
+    context.eftAccount = delegator.findByPrimaryKey("EftAccount", [paymentMethodId : selPayments[0].paymentMethodId]);
+}
---
+++ applications/order/webapp/ordermgr/WEB-INF/actions/order/OrderStats.groovy
@@ -0,0 +1,242 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.*;
+import java.sql.Timestamp;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.entity.util.*;
+import org.ofbiz.base.util.*;
+
+double calcItemTotal(List headers) {
+    double total = 0.00;
+    headers.each { header ->
+        total += header.grandTotal ?: 0.00;
+    }
+    return total;
+}
+
+double calcItemCount(List items) {
+    double count = 0.00;
+    items.each { item ->
+        count += item.quantity ?: 0.00;
+    }
+    return count;
+}
+
+cal = Calendar.getInstance();
+cal.set(Calendar.AM_PM, Calendar.AM);
+cal.set(Calendar.HOUR, 0);
+cal.set(Calendar.MINUTE, 0);
+cal.set(Calendar.SECOND, 0);    
+cal.set(Calendar.MILLISECOND, 0);   
+dayBegin = new Timestamp(cal.getTime().getTime());            
+   
+cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+weekBegin = new Timestamp(cal.getTime().getTime());       
+ 
+cal.set(Calendar.DAY_OF_MONTH, 1);
+monthBegin = new Timestamp(cal.getTime().getTime());      
+  
+cal.set(Calendar.MONTH, 0);
+yearBegin = new Timestamp(cal.getTime().getTime());       
+  
+// order status report
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("orderItemSeqId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("orderPaymentPreferenceId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("statusDatetime", EntityOperator.GREATER_THAN_EQUAL_TO, dayBegin)],
+                    EntityOperator.AND);
+dayList = delegator.findList("OrderStatus", ecl, null, null, null, false);
+context.dayOrder = EntityUtil.filterByAnd(dayList, [statusId : "ORDER_ORDERED"]);
+context.dayApprove = EntityUtil.filterByAnd(dayList, [statusId : "ORDER_APPROVED"]);
+context.dayComplete = EntityUtil.filterByAnd(dayList, [statusId : "ORDER_COMPLETED"]);
+context.dayCancelled = EntityUtil.filterByAnd(dayList, [statusId : "ORDER_CANCELLED"]);
+context.dayRejected = EntityUtil.filterByAnd(dayList, [statusId : "ORDER_REJECTED"]);
+  
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("orderItemSeqId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("orderPaymentPreferenceId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("statusDatetime", EntityOperator.GREATER_THAN_EQUAL_TO, weekBegin)],
+                    EntityOperator.AND);
+weekList = delegator.findList("OrderStatus", ecl, null, null, null, false);
+context.weekOrder = EntityUtil.filterByAnd(weekList, [statusId : "ORDER_ORDERED"]);
+context.weekApprove = EntityUtil.filterByAnd(weekList, [statusId: "ORDER_APPROVED"]);
+context.weekComplete = EntityUtil.filterByAnd(weekList, [statusId : "ORDER_COMPLETED"]);
+context.weekCancelled = EntityUtil.filterByAnd(weekList, [statusId : "ORDER_CANCELLED"]);
+context.weekRejected = EntityUtil.filterByAnd(weekList, [statusId : "ORDER_REJECTED"]);
+  
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("orderItemSeqId", EntityOperator.EQUALS, null),
+                        EntityCondition.makeCondition("orderPaymentPreferenceId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("statusDatetime", EntityOperator.GREATER_THAN_EQUAL_TO, monthBegin)],
+                    EntityOperator.AND);
+monthList = delegator.findList("OrderStatus", ecl, null, null, null, false);
+context.monthOrder = EntityUtil.filterByAnd(monthList, [statusId : "ORDER_ORDERED"]);
+context.monthApprove = EntityUtil.filterByAnd(monthList, [statusId : "ORDER_APPROVED"]);
+context.monthComplete = EntityUtil.filterByAnd(monthList, [statusId : "ORDER_COMPLETED"]);
+context.monthCancelled = EntityUtil.filterByAnd(monthList, [statusId : "ORDER_CANCELLED"]);
+context.monthRejected = EntityUtil.filterByAnd(monthList, [statusId : "ORDER_REJECTED"]);
+  
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("orderItemSeqId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("orderPaymentPreferenceId", EntityOperator.EQUALS, null), 
+                        EntityCondition.makeCondition("statusDatetime", EntityOperator.GREATER_THAN_EQUAL_TO, yearBegin)],
+                    EntityOperator.AND);
+yearList = delegator.findList("OrderStatus", ecl, null, null, null, false);
+context.yearOrder = EntityUtil.filterByAnd(yearList, [statusId : "ORDER_ORDERED"]);
+context.yearApprove = EntityUtil.filterByAnd(yearList, [statusId : "ORDER_APPROVED"]);
+context.yearComplete = EntityUtil.filterByAnd(yearList, [statusId : "ORDER_COMPLETED"]);
+context.yearCancelled = EntityUtil.filterByAnd(yearList, [statusId : "ORDER_CANCELLED"]);
+context.yearRejected = EntityUtil.filterByAnd(yearList, [statusId : "ORDER_REJECTED"]);
+  
+// order totals and item counts
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_REJECTED"), 
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, dayBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+dayItems = delegator.findList("OrderHeaderAndItems", ecl, null, null, null, false);
+dayItemsPending = EntityUtil.filterByAnd(dayItems, [itemStatusId : "ITEM_ORDERED"]);
+
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_REJECTED"), 
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, dayBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+dayHeaders = delegator.findList("OrderHeader", ecl, null, null, null, false);
+dayHeadersPending = EntityUtil.filterByAnd(dayHeaders, [statusId : "ORDER_ORDERED"]);
+                                                 
+dayItemTotal = calcItemTotal(dayHeaders);
+dayItemCount = calcItemCount(dayItems);
+dayItemTotalPending = calcItemTotal(dayHeadersPending);
+dayItemCountPending = calcItemCount(dayItemsPending);
+dayItemTotalPaid = dayItemTotal - dayItemTotalPending;
+dayItemCountPaid = dayItemCount - dayItemCountPending;
+context.dayItemTotal = dayItemTotal;
+context.dayItemCount = dayItemCount;
+context.dayItemTotalPending = dayItemTotalPending;
+context.dayItemCountPending = dayItemCountPending;
+context.dayItemTotalPaid = dayItemTotalPaid;
+context.dayItemCountPaid = dayItemCountPaid;
+
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_REJECTED"), 
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, weekBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+weekItems = delegator.findList("OrderHeaderAndItems", ecl, null, null, null, false);
+weekItemsPending = EntityUtil.filterByAnd(weekItems, [itemStatusId : "ITEM_ORDERED"]);
+
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_REJECTED"), 
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, weekBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+weekHeaders = delegator.findList("OrderHeader", ecl, null, null, null, false);
+weekHeadersPending = EntityUtil.filterByAnd(weekHeaders, [statusId : "ORDER_ORDERED"]);
+         
+weekItemTotal = calcItemTotal(weekHeaders);
+weekItemCount = calcItemCount(weekItems);
+weekItemTotalPending = calcItemTotal(weekHeadersPending);
+weekItemCountPending = calcItemCount(weekItemsPending);  
+weekItemTotalPaid = weekItemTotal - weekItemTotalPending;
+weekItemCountPaid = weekItemCount - weekItemCountPending;
+context.weekItemTotal = weekItemTotal;
+context.weekItemCount = weekItemCount;
+context.weekItemTotalPending = weekItemTotalPending;
+context.weekItemCountPending = weekItemCountPending;
+context.weekItemTotalPaid = weekItemTotalPaid;
+context.weekItemCountPaid = weekItemCountPaid;
+
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_REJECTED"), 
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, monthBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+monthItems = delegator.findList("OrderHeaderAndItems", ecl, null, null, null, false);
+monthItemsPending = EntityUtil.filterByAnd(monthItems, [itemStatusId : "ITEM_ORDERED"]);
+
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_REJECTED"), 
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, monthBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+monthHeaders = delegator.findList("OrderHeader", ecl, null, null, null, false);
+monthHeadersPending = EntityUtil.filterByAnd(monthHeaders, [statusId : "ORDER_ORDERED"]);
+
+monthItemTotal = calcItemTotal(monthHeaders);
+monthItemCount = calcItemCount(monthItems);
+monthItemTotalPending = calcItemTotal(monthHeadersPending);
+monthItemCountPending = calcItemCount(monthItemsPending);    
+monthItemTotalPaid = monthItemTotal - monthItemTotalPending;
+monthItemCountPaid = monthItemCount - monthItemCountPending;
+context.monthItemTotal = monthItemTotal;
+context.monthItemCount = monthItemCount;
+context.monthItemTotalPending = monthItemTotalPending;
+context.monthItemCountPending = monthItemCountPending;
+context.monthItemTotalPaid = monthItemTotalPaid;
+context.monthItemCountPaid = monthItemCountPaid;
+
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_REJECTED"), 
+                        EntityCondition.makeCondition("itemStatusId", EntityOperator.NOT_EQUAL, "ITEM_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, yearBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+yearItems = delegator.findList("OrderHeaderAndItems", ecl, null, null, null, false);
+yearItemsPending = EntityUtil.filterByAnd(yearItems, [itemStatusId : "ITEM_ORDERED"]);
+   
+ecl = EntityCondition.makeCondition([
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_REJECTED"), 
+                        EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "ORDER_CANCELLED"), 
+                        EntityCondition.makeCondition("orderDate", EntityOperator.GREATER_THAN_EQUAL_TO, yearBegin), 
+                        EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER")],
+                    EntityOperator.AND);
+yearHeaders = delegator.findList("OrderHeader", ecl, null, null, null, false);
+yearHeadersPending = EntityUtil.filterByAnd(yearHeaders, [statusId : "ORDER_ORDERED"]);
+
+yearItemTotal = calcItemTotal(yearHeaders);
+yearItemCount = calcItemCount(yearItems);
+yearItemTotalPending = calcItemTotal(yearHeadersPending);
+yearItemCountPending = calcItemCount(yearItemsPending);  
+yearItemTotalPaid = yearItemTotal - yearItemTotalPending;
+yearItemCountPaid = yearItemCount - yearItemCountPending;
+context.yearItemTotal = yearItemTotal;
+context.yearItemCount = yearItemCount;
+context.yearItemTotalPending = yearItemTotalPending;
+context.yearItemCountPending = yearItemCountPending;
+context.yearItemTotalPaid = yearItemTotalPaid;
+context.yearItemCountPaid = yearItemCountPaid;
+    
+// order state report
+waitingPayment = delegator.findByAnd("OrderHeader", [statusId : "ORDER_ORDERED", orderTypeId : "SALES_ORDER"]);
+context.waitingPayment = waitingPayment.size();
+
+waitingApproval = delegator.findByAnd("OrderHeader", [statusId : "ORDER_PROCESSING", orderTypeId : "SALES_ORDER"]);
+context.waitingApproval = waitingApproval.size();
+
+waitingComplete = delegator.findByAnd("OrderHeader", [statusId : "ORDER_APPROVED", orderTypeId : "SALES_ORDER"]);
+context.waitingComplete = waitingComplete.size();
