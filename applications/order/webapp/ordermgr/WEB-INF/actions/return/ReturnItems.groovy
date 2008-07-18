9998800521e49e8ffc3fe8888497b44e59da27d4
Scott Gray <lektran@apache.org>
2008-07-18 22:31:54 +0000

    A few more bsh -> groovy conversions
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@678066 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/order/webapp/ordermgr/WEB-INF/actions/return/ReturnItems.groovy
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.util.EntityFindOptions;
+import org.ofbiz.order.order.OrderReadHelper;
+import java.math.BigDecimal;
+
+returnId = parameters.returnId;
+if (!returnId) return;
+context.returnId = returnId;
+
+orderId = parameters.orderId;
+context.orderId = orderId;
+
+returnHeader = delegator.findByPrimaryKey("ReturnHeader", [returnId : returnId]);
+context.returnHeader = returnHeader;
+
+returnItems = delegator.findByAnd("ReturnItem", [returnId : returnId]);
+context.returnItems = returnItems;
+
+// these are just the adjustments not associated directly with a return item--the rest are gotten with a .getRelated on the returnItems in the .FTL
+returnAdjustments = delegator.findByAnd("ReturnAdjustment", [returnId : returnId, returnItemSeqId : "_NA_"], ["returnItemSeqId", "returnAdjustmentTypeId"]);
+context.returnAdjustments = returnAdjustments;
+
+returnTypes = delegator.findList("ReturnType", null, null, ["sequenceId"], null, false);
+context.returnTypes = returnTypes;
+
+itemStatus = delegator.findByAnd("StatusItem", [statusTypeId : "INV_SERIALIZED_STTS"], ["statusId", "description"]);
+context.itemStatus = itemStatus;
+
+returnReasons = delegator.findList("ReturnReason", null, null, ["sequenceId"], null, false);
+context.returnReasons = returnReasons;
+
+itemStts = delegator.findByAnd("StatusItem", [statusTypeId : "INV_SERIALIZED_STTS"], ["sequenceId"]);
+context.itemStts = itemStts;
+
+returnItemTypeMap = delegator.findByAnd("ReturnItemTypeMap", [returnHeaderTypeId : "CUSTOMER_RETURN"]);
+typeMap = [:];
+returnItemTypeMap.each { value ->
+    typeMap[value.returnItemMapKey] = value.returnItemTypeId;
+}
+context.returnItemTypeMap = typeMap;
+
+if (orderId) {
+    order = delegator.findByPrimaryKey("OrderHeader", [orderId : orderId]);
+    returnRes = dispatcher.runSync("getReturnableItems", [orderId : orderId]);
+    context.returnableItems = returnRes.returnableItems;
+
+    orh = new OrderReadHelper(order);
+    context.orh = orh;
+    context.orderHeaderAdjustments = orh.getAvailableOrderHeaderAdjustments();
+
+    // get the order shipping amount
+    shipRes = dispatcher.runSync("getOrderShippingAmount", [orderId : orderId]);
+    shippingAmount = shipRes.shippingAmount;
+    context.shippingAmount = shippingAmount;
+}
+
+partyOrders = delegator.findByAnd("OrderHeaderAndRoles", [roleTypeId : "PLACING_CUSTOMER", partyId : returnHeader.fromPartyId], ["orderId"]);
+context.partyOrders = partyOrders;
+
+// get the list of return shipments associated to the return
+findOptions = new EntityFindOptions();
+findOptions.setDistinct(true);
+returnShipmentIds = delegator.findList("ReturnItemShipment", EntityCondition.makeCondition("returnId", returnId), ["shipmentId"] as Set, null, findOptions, true);
+context.returnShipmentIds = returnShipmentIds;
---
+++ applications/order/webapp/ordermgr/WEB-INF/actions/task/OrderTaskList.groovy
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.util.*;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.base.util.*;
+import org.ofbiz.order.task.*;
+
+context.userLogin = userLogin;
+
+// create the sort order
+sort = parameters.sort;
+sortOrder = ["currentStatusId", "-priority", "orderDate"];
+if (sort) {
+    if (sort.equals("name")) {
+        sortOrder.add(0, "firstName");
+        sortOrder.add(0, "lastName");
+    } else if (sort.equals("grandTotal")) {
+        sortOrder.add(0, "-grandTotal");
+    } else {
+        sortOrder.add(0, sort);
+    }
+}
+
+partyBase = [EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, "CAL_ACCEPTED"), EntityCondition.makeCondition("wepaPartyId", EntityOperator.EQUALS, userLogin.partyId)];
+partyRole = [EntityCondition.makeCondition("orderRoleTypeId", EntityOperator.EQUALS, "PLACING_CUSTOMER"), EntityCondition.makeCondition("orderRoleTypeId", EntityOperator.EQUALS, "SUPPLIER_AGENT")];
+partyExpr = [EntityCondition.makeCondition(partyBase, EntityOperator.AND), EntityCondition.makeCondition(partyRole, EntityOperator.OR)];
+partyCond = EntityCondition.makeCondition(partyExpr, EntityOperator.AND);
+partyTasks = delegator.findList("OrderTaskList", partyCond, null, sortOrder, null, false);
+
+if (partyTasks) partyTasks = EntityUtil.filterByDate(partyTasks);
+context.partyTasks = partyTasks;
+
+// Build a map of orderId and currency
+orderCurrencyMap = [:];
+partyTasks.each { ptItem ->
+    orderHeader = delegator.findByPrimaryKey("OrderHeader", [orderId : ptItem.orderId]);
+    orderCurrencyMap[ptItem.orderId] = orderHeader.currencyUom;
+}
+
+// get this user's roles
+partyRoles = delegator.findByAnd("PartyRole", [partyId = userLogin.partyId]);  
+
+// build the role list
+pRolesList = [];
+partyRoles.each { partyRole ->
+    if (!partyRole.roleTypeId.equals("_NA_"))
+        pRolesList.add(EntityCondition.makeCondition("roleTypeId", EntityOperator.EQUALS, partyRole.roleTypeId));
+}
+
+custList = [EntityCondition.makeCondition("orderRoleTypeId", EntityOperator.EQUALS, "PLACING_CUSTOMER"), EntityCondition.makeCondition("orderRoleTypeId", EntityOperator.EQUALS, "SUPPLIER_AGENT")];
+baseList = [EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "CAL_CANCELLED"), EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "CAL_COMPLETED"), EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "CAL_DELEGATED")];
+expressions = [];
+expressions.add(EntityCondition.makeCondition(custList, EntityOperator.OR));
+if (pRolesList) expressions.add(EntityCondition.makeCondition(pRolesList, EntityOperator.OR));
+expressions.add(EntityCondition.makeCondition(baseList, EntityOperator.AND));
+conditions = EntityCondition.makeCondition(expressions, EntityOperator.AND);
+    
+// invoke the query
+roleTasks = delegator.findList("OrderTaskList", conditions, null, sortOrder, null, false);    
+roleTasks = EntityUtil.filterByAnd(roleTasks, baseList);
+roleTasks = EntityUtil.filterByDate(roleTasks);  
+context.roleTasks = roleTasks;
+
+// Add to the map of orderId and currency
+roleTasks.each { rtItem ->
+    orderHeader = delegator.findByPrimaryKey("OrderHeader", [orderId : rtItem.orderId]);
+    orderCurrencyMap[rtItem.orderId] = orderHeader.currencyUom;
+}
+context.orderCurrencyMap = orderCurrencyMap;
+
+context.now = nowTimestamp;
+
+// purchase order schedule
+poList = delegator.findByAnd("OrderHeaderAndRoles", [partyId : userLogin.partyId, orderTypeId : "PURCHASE_ORDER"]);
+poIter = poList.iterator();
+listedPoIds = new HashSet();
+while (poIter.hasNext()) {
+    poGv = poIter.next();
+    poOrderId = poGv.orderId;
+    if (listedPoIds.contains(poOrderId)) {
+    	poIter.remove();
+    } else {
+        listedPoIds.add(poOrderId);
+    }
+}
+context.poList = poList;
+
