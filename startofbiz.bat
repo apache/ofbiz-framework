66169692f3d4000b7bc0221e6da920f659eff40b
Jacques Le Roux <jleroux@apache.org>
2008-08-09 13:33:55 +0000

    Make startofbiz.bat looks more like startofbiz.sh Mostly interesting for RMI settings
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@684263 13f79535-47bb-0310-9956-ffa450edef68

--- startofbiz.bat
+++ startofbiz.bat
@@ -1,25 +1,55 @@
-ECHO OFF
-REM #####################################################################
-REM Licensed to the Apache Software Foundation (ASF) under one
-REM or more contributor license agreements.  See the NOTICE file
-REM distributed with this work for additional information
-REM regarding copyright ownership.  The ASF licenses this file
-REM to you under the Apache License, Version 2.0 (the
-REM "License"); you may not use this file except in compliance
-REM with the License.  You may obtain a copy of the License at
+Echo off
+rem #####################################################################
+rem Licensed to the Apache Software Foundation (ASF) under one
+rem or more contributor license agreements.  See the NOTICE file
+rem distributed with this work for additional information
+rem regarding copyright ownership.  The ASF licenses this file
+rem to you under the Apache License, Version 2.0 (the
+rem "License"); you may not use this file except in compliance
+rem with the License.  You may obtain a copy of the License at
 REM
-REM http://www.apache.org/licenses/LICENSE-2.0
+rem http://www.apache.org/licenses/LICENSE-2.0
 REM
-REM Unless required by applicable law or agreed to in writing,
-REM software distributed under the License is distributed on an
-REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-REM KIND, either express or implied.  See the License for the
-REM specific language governing permissions and limitations
-REM under the License.
-REM #####################################################################
+rem Unless required by applicable law or agreed to in writing,
+rem software distributed under the License is distributed on an
+rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+rem KIND, either express or implied.  See the License for the
+rem specific language governing permissions and limitations
+rem under the License.
+rem #####################################################################
 ECHO ON
 
+rem ### Console log file
+rem set OFBIZ_LOG=runtime\logs\console.log
+
+rem ### Delete the last log
+rem del %OFBIZ_LOG%
+
+rem ###VM args block ####################################################
+rem set MEMIF=-Xms128M -Xmx512M
+rem # RMI settings
+rem set DEBUG=-Dsun.rmi.server.exceptionTrace=true
+rem # Automatic IP address for Windows
+rem ipconfig | find "IP." | find /v "::" | find /v "0.0.0.0" > tmp.tmp
+rem FOR /F "tokens=2* delims=:" %%A IN (tmp.tmp)  DO FOR %%B IN (%%A) DO SET IPADDR=%%B
+rem del tmp.tmp
+rem set RMIIF=-Djava.rmi.server.hostname=%IPADDR%
+rem # Not needed anymore, for history
+rem set MISC=-Duser.language=en 
+rem set VMARGS=%MEMIF% %MISC% %DEBUG% %RMIIF%
+rem ####################################################################
+
+rem ### Worldpay Config
+rem set VMARGS=-Xbootclasspath/p:applications\accounting\lib\cryptix.jar %VMARGS%
+
+
+rem ### Different ways of launching OFBiz ##############################
+rem ### start ofbiz with previous set VMARGS
+rem "%JAVA_HOME%\bin\java" %VMARGS% -jar ofbiz.jar > %OFBIZ_LOG%
+
+rem ### This one is for more of a debugging mode
+rem "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > runtime\logs\console.log
+
+rem ### Simple easy to read line
 "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -jar ofbiz.jar > runtime\logs\console.log
 
-REM This one is for more of a debugging mode
-REM "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > runtime\logs\console.log
