96ce248a19244164d693e15142d81be20c25f78f
Jacques Le Roux <jleroux@apache.org>
2008-08-09 11:27:07 +0000

    Commented out 	MISC="-Duser.language=en" as it's anymore needed
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@684228 13f79535-47bb-0310-9956-ffa450edef68

--- startofbiz.sh
+++ startofbiz.sh
@@ -35,7 +35,7 @@ ADMIN="-Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY"
 #IPADDR=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 #RMIIF="-Djava.rmi.server.hostname=$IPADDR"
 MEMIF="-Xms128M -Xmx512M"
-MISC="-Duser.language=en"
+#MISC="-Duser.language=en"
 VMARGS="$MEMIF $MISC $DEBUG $RMIIF $ADMIN"
 
 # Worldpay Config
