5cec2bc92e6c52914d77f188c200a4e35d056062
Johannes Cornelius Bakker <hansbak@apache.org>
2008-11-01 17:39:55 +0000

    hide several buttons and do not show closed projects on lists
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@709732 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/projectmgr/webapp/projectmgr/WEB-INF/actions/ListCurrentProjects.groovy
+++ specialpurpose/projectmgr/webapp/projectmgr/WEB-INF/actions/ListCurrentProjects.groovy
@@ -27,7 +27,7 @@ projects = [];
 allProjects.each { project ->
 	result = dispatcher.runSync("getProject", ["userLogin" : parameters.userLogin, "projectId" : project.workEffortId]);
 	if (result.projectInfo) {
-		if (!result.projectInfo.currentStatusId.equals("PTS_COMPLETED") && !result.projectInfo.currentStatusId.equals("PTS_CANCELLED")) {
+		if (!result.projectInfo.currentStatusId.equals("PTS_COMPLETED") && !result.projectInfo.currentStatusId.equals("PTS_CANCELLED") && !result.projectInfo.currentStatusId.equals("PRJ_CLOSED")) {
 			resultAssign = delegator.findByAnd("WorkEffortPartyAssignment", ["partyId" : parameters.userLogin.partyId, "workEffortId" : project.workEffortId])
 			if (security.hasEntityPermission("PROJECTMGR", "_ADMIN", session) 
 					|| ((security.hasEntityPermission("PROJECTMGR", "_ROLE_ADMIN", session) || security.hasEntityPermission("PROJECTMGR", "_ROLE_VIEW", session)) && resultAssign)) {
