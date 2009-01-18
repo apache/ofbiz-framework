0500d6baef5aeef1d82d2794cb62ad736fa844f1
Jacques Le Roux <jleroux@apache.org>
2009-01-18 09:09:32 +0000

    A 1st commit for "Add geolocation to data model" (https://issues.apache.org/jira/browse/OFBIZ-1923) OFBIZ-1923
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@735404 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/party/webapp/partymgr/WEB-INF/actions/party/GeoLocation.groovy
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.ofbiz.party.party.PartyWorker;
+
+partyId = parameters.partyId ?: parameters.party_id;
+userLoginId = parameters.userlogin_id ?: parameters.userLoginId;
+
+if (!partyId && userLoginId) {
+    thisUserLogin = delegator.findByPrimaryKey("UserLogin", [userLoginId : userLoginId]);
+    if (thisUserLogin) {
+        partyId = thisUserLogin.partyId;
+    }
+}
+context.partyId = partyId; 
+
+latestGeoPoint = PartyWorker.findPartyLatestGeoPoint(partyId, delegator);
+context.latestGeoPoint = latestGeoPoint;
+
+if (latestGeoPoint.uomId) {
+    elevationUom = delegator.findOne("Uom", [uomId : latestGeoPoint.uomId], false);
+    context.elevationUomAbbr = elevationUom.abbreviation;
+}
+ 
