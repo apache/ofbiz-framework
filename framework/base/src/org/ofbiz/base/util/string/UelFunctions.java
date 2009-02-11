86b75fa34001f10333e4cc18d06bc1a1ec1a18c5
Adrian Crum <adrianc@apache.org>
2009-02-11 18:22:19 +0000

    Oops, I meant to clean up this code before committing.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@743440 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/string/UelFunctions.java
+++ framework/base/src/org/ofbiz/base/util/string/UelFunctions.java
@@ -378,12 +378,11 @@ public class UelFunctions {
         boolean result = false;
         try {
             URL url = FlexibleLocation.resolveLocation(str);
-            if (url == null) {
-                return false;
+            if (url != null) {
+                InputStream is = url.openStream();
+                result = true;
+                is.close();
             }
-            InputStream is = url.openStream();
-            result = true;
-            is.close();
         } catch (Exception e) {}
         return result;
     }
