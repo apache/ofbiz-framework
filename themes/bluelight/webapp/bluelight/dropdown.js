fdd75e0aa8bc8f3875cf09208e8e890f751f5b7b
David E. Jones <jonesde@apache.org>
2009-02-20 06:57:44 +0000

    Applied patch from Ryan Foster to fix issues with bluelight theme, especially in IE; part of Jira #OFBIZ-2197
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@746171 13f79535-47bb-0310-9956-ffa450edef68

---
+++ themes/bluelight/webapp/bluelight/dropdown.js
@@ -0,0 +1,23 @@
+var DropDownMenu = Class.create();
+DropDownMenu.prototype = {
+ initialize: function(menuElement) {
+	menuElement.childElements().each(function(node){
+		// if there is a submenu
+		var submenu = $A(node.getElementsByTagName("ul")).first();
+		if(submenu != null){
+			// make sub-menu invisible
+			Element.extend(submenu).setStyle({display: 'none'});
+			// toggle the visibility of the submenu
+			node.onmouseover = node.onmouseout = function(){
+				Element.toggle(submenu);
+			}
+		}
+	});
+}
+};
+
+Event.observe(window, "load", function(){
+	var mainmenu = new DropDownMenu($('main-navigation'));
+    var appmenu = new DropDownMenu($('app-navigation'));
+});
+
