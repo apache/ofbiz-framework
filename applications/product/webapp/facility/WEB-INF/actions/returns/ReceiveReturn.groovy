c91097da5e0f643f301a07c1c400784cc15d09cf
Jacques Le Roux <jleroux@apache.org>
2009-02-28 16:09:28 +0000

    Fix "Error when receiving return from customer" https://issues.apache.org/jira/browse/OFBIZ-2207 OFBIZ-2207
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@748873 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/webapp/facility/WEB-INF/actions/returns/ReceiveReturn.groovy
+++ applications/product/webapp/facility/WEB-INF/actions/returns/ReceiveReturn.groovy
@@ -20,6 +20,7 @@
 import org.ofbiz.entity.*;
 import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.entity.util.*;
+import org.ofbiz.base.util.*;
 
 facilityId = request.getParameter("facilityId"); 
 returnId = request.getParameter("returnId");
