5a3432956dc555e767bd58dc56be8b2aaa9aecfb
Adam Heath <doogie@apache.org>
2009-03-13 01:12:11 +0000

    New webslinger component.  This is being added to framework, based on the desire to make use of it in a support role.
    It is *not* enabled yet, not part of the build, nor used at runtime
    by the server.  That will come shortly.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@753081 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/webslinger/src/org/ofbiz/webslinger/EntityTransactionUtil.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.webslinger;
+
+import javax.transaction.Transaction;
+
+import org.ofbiz.entity.transaction.TransactionUtil;
+import java.util.concurrent.Callable;
+
+public class EntityTransactionUtil {
+    public static <V> V doNewTransaction(String ifErrorMessage, Callable<V> callable) throws Throwable {
+        Transaction tx = TransactionUtil.suspend();
+        try {
+            return doTransaction(ifErrorMessage, callable);
+        } finally {
+            TransactionUtil.resume(tx);
+        }
+    }
+
+    public static <V> V doTransaction(String ifErrorMessage, Callable<V> callable) throws Throwable {
+        boolean tx = TransactionUtil.begin();
+        Throwable transactionAbortCause = null;
+        try {
+            try {
+                return callable.call();
+            } catch (Throwable t) {
+                while (t.getCause() != null) {
+                    t = t.getCause();
+                }
+                throw t;
+            }
+        } catch (Error e) {
+            transactionAbortCause = e;
+            throw e;
+        } catch (RuntimeException e) {
+            transactionAbortCause = e;
+            throw e;
+        } catch (Throwable t) {
+            transactionAbortCause = t;
+            throw t;
+        } finally {
+            if (transactionAbortCause == null) {
+                TransactionUtil.commit(tx);
+            } else {
+                transactionAbortCause.printStackTrace();
+                TransactionUtil.rollback(tx, ifErrorMessage, transactionAbortCause);
+            }
+        }
+    }
+}
---
+++ framework/webslinger/src/org/ofbiz/webslinger/WebslingerServerEngine.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.webslinger;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Map;
+
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ModelService;
+import org.ofbiz.service.ServiceDispatcher;
+import org.ofbiz.service.engine.GenericAsyncEngine;
+
+import org.webslinger.WebslingerServletContext;
+
+public class WebslingerServerEngine extends GenericAsyncEngine {
+    public WebslingerServerEngine(ServiceDispatcher dispatcher) {
+        super(dispatcher);
+    }
+
+    public void runSyncIgnore(String localName, ModelService modelService, Map<String, Object> context) throws GenericServiceException {
+        runSync(localName, modelService, context);
+    }
+
+    public Map<String, Object> runSync(String localName, ModelService modelService, Map<String, Object> context) throws GenericServiceException {
+        GenericDelegator delegator = dispatcher.getDelegator();
+        try {
+            GenericValue found = EntityUtil.getFirst(delegator.findByAndCache("WebslingerLayout", UtilMisc.toMap("webslingerServerId", modelService.location)));
+            if (found == null) throw new GenericServiceException("Couldn't find server mapping for(" + modelService.location + ")");
+            return (Map<String, Object>) WebslingerServletContext.invokeInVM(found.getString("hostName"), 8080, modelService.invoke, context);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (GenericServiceException e) {
+            throw e;
+        } catch (Exception e) {
+            throw (GenericServiceException) new GenericServiceException(e.getMessage()).initCause(e);
+        }
+    }
+}
