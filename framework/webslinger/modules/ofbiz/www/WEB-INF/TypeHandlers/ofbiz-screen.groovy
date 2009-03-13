bacca49e49bfa6d7bd25e6f58b30851f638898ec
Adam Heath <doogie@apache.org>
2009-03-13 05:41:16 +0000

    Major new feature, showing advanced webslinger integration.  Files with an '.ofbiz-screen' extension can now contain a screen definition snippet.  This is a one-way integration; the screen widget can't call back into webslinger controlled space(yet).
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@753127 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/webslinger/modules/ofbiz/www/WEB-INF/TypeHandlers/ofbiz-screen.groovy
@@ -0,0 +1,41 @@
+import org.ofbiz.base.util.StringUtil
+import org.ofbiz.base.util.UtilXml
+import org.ofbiz.base.util.collections.MapStack
+import org.ofbiz.base.util.template.FreeMarkerWorker
+import org.ofbiz.widget.screen.ScreenFactory
+import org.ofbiz.widget.screen.ScreenRenderer
+import org.ofbiz.widget.html.HtmlFormRenderer
+import org.ofbiz.widget.html.HtmlScreenRenderer
+
+import org.webslinger.commons.vfs.VFSUtil
+import org.webslinger.container.FileInfo
+
+System.err.println("webslinger.command=[$webslinger.command]")
+switch (webslinger.command) {
+    case 'init':
+        return new HtmlScreenRenderer()
+    case 'get-creator':
+        return [
+            createValue: { fi, name ->
+                def file = fi.file
+                def singleScreenText = VFSUtil.getString(file)
+                def fullDocumentText = '<screens xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://ofbiz.apache.org/dtds/widget-screen.xsd">' + singleScreenText + '</screens>'
+                def doc = UtilXml.readXmlDocument(fullDocumentText)
+                def screens = ScreenFactory.readScreenDocument(doc, file.toString())
+                if (screens.size() != 1) throw new IllegalArgumentException('wrong size')
+                return screens.values().iterator().next()
+            }
+        ] as FileInfo.Creator
+}
+def target = webslinger.payload
+def modelScreen = target.pathContext.info.getCachedItem(null)
+def screenRenderer = webslinger.initObject
+//response.contentType = 'text/html'
+def targetContext = MapStack.create(target.context)
+def screens = new ScreenRenderer(response.writer, targetContext, screenRenderer);
+screens.populateContextForRequest(request, response, webslinger.webslingerServletContext)
+FreeMarkerWorker.getSiteParameters(request, targetContext)
+targetContext.formStringRenderer = new HtmlFormRenderer(request, response)
+targetContext.simpleEncoder = StringUtil.htmlEncoder
+modelScreen.renderScreenString(response.writer, targetContext, screenRenderer)
+return null
