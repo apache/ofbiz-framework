b844d6a60e2790cadd7a14b6065df5d02c942ad9
Adam Heath <doogie@apache.org>
2009-03-13 01:12:11 +0000

    New webslinger component.  This is being added to framework, based on the desire to make use of it in a support role.
    It is *not* enabled yet, not part of the build, nor used at runtime
    by the server.  That will come shortly.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@753081 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/webslinger/src/org/ofbiz/webslinger/WebslingerBSFServiceEngine.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.webslinger;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.bsf.BSFException;
+
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ModelService;
+import org.ofbiz.service.ServiceDispatcher;
+import org.ofbiz.service.engine.GenericAsyncEngine;
+
+public class WebslingerBSFServiceEngine extends GenericAsyncEngine {
+    private static final String[] paramNames;
+    private static final Class<?>[] paramTypes;
+    static {
+        paramNames = new String[] { "dispatcher", "context" };
+        paramTypes = new Class<?>[] { DispatchContext.class, Map.class };
+    }
+
+    public WebslingerBSFServiceEngine(ServiceDispatcher dispatcher) {
+        super(dispatcher);
+    }
+
+    public void runSyncIgnore(String localName, ModelService modelService, Map<String, Object> context) throws GenericServiceException {
+        runSync(localName, modelService, context);
+    }
+
+    public Map<String, Object> runSync(String localName, ModelService modelService, Map<String, Object> context) throws GenericServiceException {
+        Object[] params = new Object[] { dispatcher.getLocalContext(localName), context };
+        try {
+            return (Map) WebslingerContainer.runEvent(modelService.engineName, modelService.location, paramNames, paramTypes, params);
+        } catch (IOException e) {
+            throw (GenericServiceException) new GenericServiceException(e.getMessage()).initCause(e);
+        } catch (BSFException e) {
+            throw (GenericServiceException) new GenericServiceException(e.getMessage()).initCause(e);
+        }
+    }
+}
---
+++ framework/webslinger/src/org/ofbiz/webslinger/WebslingerContextMapper.java
@@ -0,0 +1,206 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.webslinger;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.management.JMException;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.NameScope;
+
+import org.ofbiz.base.util.UtilFormatOut;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericEntity;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.transaction.TransactionFactory;
+import org.ofbiz.entity.cache.Cache;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.security.SecurityFactory;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.WebAppDispatcher;
+
+import org.webslinger.AbstractMappingWebslingerServletContextFactory;
+import org.webslinger.WebslingerServletContext;
+import org.webslinger.container.WebslingerContainer;
+import org.webslinger.lang.ObjectUtil;
+import org.webslinger.servlet.WebslingerServlet;
+import org.webslinger.util.TTLObject;
+import org.webslinger.collections.ArrayUtil;
+import org.webslinger.collections.CollectionUtil;
+
+public class WebslingerContextMapper extends AbstractMappingWebslingerServletContextFactory {
+    protected ServletContext servletContext;
+    protected GenericDelegator delegator;
+    protected final ArrayList<URL> globalReaderURLs = new ArrayList<URL>();
+
+    public void init(ServletConfig config) throws ServletException, IOException {
+        System.err.println(org.webslinger.commons.vfs.flat.FlatFileProvider.class);
+        servletContext = config.getServletContext();
+        String delegatorName = servletContext.getInitParameter("entityDelegatorName");
+        delegator = GenericDelegator.getGenericDelegator(delegatorName);
+        String readerFiles = servletContext.getInitParameter("serviceReaderUrls");
+        if (readerFiles != null) {
+            for (String reader: CollectionUtil.split(readerFiles, ";")) {
+                URL url =  config.getServletContext().getResource(reader);
+                if (url != null) globalReaderURLs.add(url);
+            }
+        }
+        super.init(config, UtilProperties.getPropertyValue("webslinger.properties", "moduleBase"));
+    }
+
+    protected Layout[] getStartLayouts() throws Exception {
+        ArrayList<Layout> layouts = new ArrayList<Layout>();
+        try {
+            for (GenericValue value: delegator.findByAnd("WebslingerServer", UtilMisc.toMap("loadAtStart", "Y"))) {
+                layouts.add(new OfbizLayout(value));
+            }
+        } catch (GenericEntityException e) {
+        }
+        return layouts.toArray(new Layout[layouts.size()]);
+    }
+
+    public void initializeRequest(WebslingerServletContext context, HttpServletRequest request) {
+        request.setAttribute("servletContext", context);
+        Object delegator = context.getAttribute("delegator");
+        Object dispatcher = context.getAttribute("dispatcher");
+        Object security = context.getAttribute("security");
+        request.setAttribute("delegator", delegator);
+        request.setAttribute("dispatcher", dispatcher);
+        request.setAttribute("security", security);
+        // FIXME!!! These next two are a hack until proper fake/wrapped session support is done in webslinger
+        servletContext.setAttribute("delegator", delegator);
+        servletContext.setAttribute("dispatcher", dispatcher);
+        servletContext.setAttribute("security", security);
+    }
+
+    protected void initializeContext(WebslingerServletContext context, Layout layout) throws Exception {
+        OfbizLayout ofbizLayout = (OfbizLayout) layout;
+        GenericDelegator delegator = GenericDelegator.getGenericDelegator(ofbizLayout.delegatorName);
+        context.setAttribute("delegator", delegator);
+        context.setAttribute("dispatcher", createLocalDispatcher(context, layout.getTarget(), delegator));
+        context.setAttribute("security", SecurityFactory.getInstance(delegator));
+    }
+
+    protected LocalDispatcher createLocalDispatcher(WebslingerServletContext context, String name, GenericDelegator delegator) throws IOException {
+        ArrayList<URL> readerURLs = new ArrayList<URL>(globalReaderURLs);
+        String readerFiles = context.getInitParameter("serviceReaderUrls");
+        if (readerFiles != null) {
+            for (String reader: CollectionUtil.split(readerFiles, ";")) {
+                URL url =  context.getResource(reader);
+                if (url != null) readerURLs.add(url);
+            }
+        }
+        System.err.println(readerURLs);
+        return new WebAppDispatcher(name, delegator, readerURLs);
+    }
+
+    protected Set<String> getSuffixes() throws Exception {
+        Cache cache = delegator.getCache();
+        Set<String> suffixes;
+        synchronized (WebslingerContextMapper.class) {
+            suffixes = (Set<String>) cache.get("WebslingerHostSuffix", null, "WebslingerContextMapper.Suffixes");
+            if (suffixes == null) {
+                suffixes = new HashSet<String>();
+                for (GenericValue value: delegator.findList("WebslingerHostSuffix", null, null, null, null, false)) {
+                    suffixes.add(value.getString("hostSuffix"));
+                }
+                cache.put("WebslingerHostSuffix", null, "WebslingerContextMapper.Suffixes", suffixes);
+            }
+        }
+        return suffixes;
+    }
+
+    protected Layout lookupLayout(String hostName, String contextPath) throws Exception {
+        GenericValue layout = EntityUtil.getOnly(delegator.findByAndCache("WebslingerLayout", UtilMisc.toMap("hostName", hostName, "contextPath", contextPath)));
+        if (layout == null) return null;
+        return new OfbizLayout(layout);
+    }
+
+    protected class OfbizLayout implements Layout {
+        private final String id;
+        private final String target;
+        private final String[] bases;
+        private final int hashCode;
+        protected final String delegatorName;
+        protected final String dispatcherName;
+
+        protected OfbizLayout(GenericValue server) throws GenericEntityException {
+            id = server.getString("webslingerServerId");
+            target = server.getString("target");
+            List<GenericValue> baseValues = server.getRelatedCache("WebslingerServerBase", UtilMisc.toList("seqNum"));
+            bases = new String[baseValues.size()];
+            for (int i = 0; i < bases.length; i++) {
+                GenericValue baseValue = baseValues.get(i);
+                bases[i] = baseValue.getString("baseName");
+            }
+            delegatorName = server.getString("delegatorName");
+            dispatcherName = server.getString("dispatcherName");
+            hashCode = target.hashCode() ^ ObjectUtil.hashCodeHelper(delegatorName) ^ ArrayUtil.hashCodeHelper(bases);
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTarget() {
+            return target;
+        }
+
+        public String[] getBases() {
+            return bases;
+        }
+
+        public int hashCode() {
+            return hashCode;
+        }
+
+        public boolean equals(Object o) {
+            if (!(o instanceof OfbizLayout)) return false;
+            OfbizLayout other = (OfbizLayout) o;
+            if (!target.equals(other.target)) return false;
+            if (!ObjectUtil.equalsHelper(delegatorName, other.delegatorName)) return false;
+            return ArrayUtil.equalsHelper(bases, other.bases);
+        }
+    }
+}
