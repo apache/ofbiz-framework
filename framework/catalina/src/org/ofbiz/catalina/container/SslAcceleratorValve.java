03b05398b97cf6437aefb6b1407afa2aab154efe
Adam Heath <doogie@apache.org>
2009-03-21 23:22:57 +0000

    Fix purely empty whitespace lines, and remove trailing whitespace.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@757067 13f79535-47bb-0310-9956-ffa450edef68

--- framework/catalina/src/org/ofbiz/catalina/container/SslAcceleratorValve.java
+++ framework/catalina/src/org/ofbiz/catalina/container/SslAcceleratorValve.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -29,13 +29,13 @@ import org.apache.catalina.valves.ValveBase;
 /**
  * To use add (or uncomment) the following line to the Tomcat/Catalina configuarion (ie in ofbiz-containers.xml under the <property name="default-server" value="engine"> element)
  *    <property name="ssl-accelerator-port" value="8443"/>
- *    
+ *
  * Once that is done just setup a connector just like the example http-connector and have it listen on the port you set in the ssl-accelerator-port value.
  */
 public class SslAcceleratorValve extends ValveBase {
-        
+ 
     protected Integer sslAcceleratorPort = null;
-    
+ 
     public void setSslAcceleratorPort(Integer sslAcceleratorPort) {
         this.sslAcceleratorPort = sslAcceleratorPort;
     }
@@ -45,7 +45,7 @@ public class SslAcceleratorValve extends ValveBase {
     }
 
     public @Override void invoke(Request req, Response resp) throws IOException, ServletException {
-        if (sslAcceleratorPort != null && req.getLocalPort() == sslAcceleratorPort.intValue()) { 
+        if (sslAcceleratorPort != null && req.getLocalPort() == sslAcceleratorPort.intValue()) {
             req.setSecure(true);
         }
 
