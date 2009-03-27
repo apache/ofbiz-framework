8093dce9c4451c2295550b1dd12ddf4df705e31e
Adam Heath <doogie@apache.org>
2009-03-27 17:02:56 +0000

    Fix lines that *only* have whitespace.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@759267 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/webpos/src/org/ofbiz/webpos/session/WebPosSession.java
+++ specialpurpose/webpos/src/org/ofbiz/webpos/session/WebPosSession.java
@@ -39,9 +39,9 @@ import org.ofbiz.webapp.control.LoginWorker;
 import org.ofbiz.webpos.transaction.WebPosTransaction;
 
 public class WebPosSession {
- 
+
     public static final String module = WebPosSession.class.getName();
- 
+
     private String id = null;
     private Map<String, Object> attributes = FastMap.newInstance();
     private GenericValue userLogin = null;
@@ -55,7 +55,7 @@ public class WebPosSession {
     private Boolean mgrLoggedIn = null;
     private WebPosTransaction webPosTransaction = null;
     private ShoppingCart cart = null;
- 
+
     public WebPosSession(String id, Map<String, Object> attributes, GenericValue userLogin, Locale locale, String productStoreId, String facilityId, String currencyUomId, GenericDelegator delegator, LocalDispatcher dispatcher, ShoppingCart cart) {
         this.id = id;
         this.attributes = attributes;
@@ -64,7 +64,7 @@ public class WebPosSession {
         this.productStoreId = productStoreId;
         this.facilityId = facilityId;
         this.currencyUomId = currencyUomId;
- 
+
         if (UtilValidate.isNotEmpty(delegator))
         {
             this.delegator = delegator;
@@ -73,28 +73,28 @@ public class WebPosSession {
             this.delegator = this.getDelegator();
             this.delegatorName = delegator.getDelegatorName();
         }
- 
+
         this.dispatcher = dispatcher;
         this.cart = cart;
         Debug.logInfo("Created WebPosSession [" + id + "]", module);
     }
- 
+
     public GenericValue getUserLogin() {
         return this.userLogin;
     }
- 
+
     public void setAttribute(String name, Object value) {
         this.attributes.put(name, value);
     }
- 
+
     public Object getAttribute(String name) {
         return this.attributes.get(name);
     }
- 
+
     public String getId() {
         return this.id;
     }
- 
+
     public String getUserLoginId() {
         if (UtilValidate.isEmpty(getUserLogin())) {
             return null;
@@ -102,7 +102,7 @@ public class WebPosSession {
             return this.getUserLogin().getString("userLoginId");
         }
     }
- 
+
     public String getUserPartyId() {
         if (UtilValidate.isEmpty(getUserLogin())) {
             return null;
@@ -110,69 +110,69 @@ public class WebPosSession {
             return this.getUserLogin().getString("partyId");
         }
     }
- 
+
     public Locale getLocale() {
         return (Locale) this.locale;
     }
- 
+
     public void setLocale(Locale locale) {
         this.locale = locale;
     }
- 
+
     public String getProductStoreId() {
         return this.productStoreId;
     }
- 
+
     public void setProductStoreId(String productStoreId) {
         this.productStoreId = productStoreId;
     }
- 
+
     public String getFacilityId() {
         return this.facilityId;
     }
- 
+
     public void setFacilityId(String facilityId) {
         this.facilityId = facilityId;
     }
- 
+
     public String getCurrencyUomId() {
         return this.currencyUomId;
     }
- 
+
     public void setCurrencyUomId(String currencyUomId) {
         this.currencyUomId = currencyUomId;
     }
- 
+
     public GenericDelegator getDelegator() {
         if (UtilValidate.isEmpty(delegator)) {
             delegator = GenericDelegator.getGenericDelegator(delegatorName);
         }
         return delegator;
     }
- 
+
     public LocalDispatcher getDispatcher() {
         return dispatcher;
     }
- 
+
     public ShoppingCart getCart() {
         return this.cart;
     }
- 
+
     public void logout() {
         if (UtilValidate.isNotEmpty(webPosTransaction)) {
             webPosTransaction.closeTx();
             webPosTransaction = null;
         }
- 
+
         if (UtilValidate.isNotEmpty(getUserLogin())) {
             LoginWorker.setLoggedOut(this.getUserLogin().getString("userLoginId"), this.getDelegator());
         }
     }
- 
+
     public void login(String username, String password, LocalDispatcher dispatcher) throws UserLoginFailure {
         this.checkLogin(username, password, dispatcher);
     }
- 
+
     public GenericValue checkLogin(String username, String password, LocalDispatcher dispatcher) throws UserLoginFailure {
         // check the required parameters and objects
         if (UtilValidate.isEmpty(dispatcher)) {
@@ -184,7 +184,7 @@ public class WebPosSession {
         if (UtilValidate.isEmpty(password)) {
             throw new UserLoginFailure(UtilProperties.getMessage("PartyUiLabels", "PartyPasswordMissing", getLocale()));
         }
- 
+
         // call the login service
         Map<String, Object> result = null;
         try {
@@ -195,7 +195,7 @@ public class WebPosSession {
         } catch (Throwable t) {
             Debug.logError(t, "Throwable caught!", module);
         }
- 
+
         // check for errors
         if (ServiceUtil.isError(result)) {
             throw new UserLoginFailure(ServiceUtil.getErrorMessage(result));
@@ -207,7 +207,7 @@ public class WebPosSession {
             return ul;
         }
     }
- 
+
     public boolean hasRole(GenericValue userLogin, String roleTypeId) {
         if (UtilValidate.isEmpty(userLogin) || UtilValidate.isEmpty(roleTypeId)) {
             return false;
@@ -220,45 +220,45 @@ public class WebPosSession {
             Debug.logError(e, module);
             return false;
         }
- 
+
         if (UtilValidate.isEmpty(partyRole)) {
             return false;
         }
- 
+
         return true;
     }
- 
+
     public boolean isManagerLoggedIn() {
         if (UtilValidate.isEmpty(mgrLoggedIn)) {
             mgrLoggedIn = new Boolean(hasRole(getUserLogin(), "MANAGER"));
         }
         return mgrLoggedIn.booleanValue();
     }
- 
+
     public WebPosTransaction getCurrentTransaction() {
         if (UtilValidate.isEmpty(webPosTransaction)) {
             webPosTransaction = new WebPosTransaction(this);
         }
         return webPosTransaction;
     }
- 
+
     public void setCurrentTransaction(WebPosTransaction webPosTransaction) {
         this.webPosTransaction = webPosTransaction;
     }
- 
+
     public class UserLoginFailure extends GeneralException {
         public UserLoginFailure() {
             super();
         }
- 
+
         public UserLoginFailure(String str) {
             super(str);
         }
- 
+
         public UserLoginFailure(String str, Throwable nested) {
             super(str, nested);
         }
- 
+
         public UserLoginFailure(Throwable nested) {
             super(nested);
         }
