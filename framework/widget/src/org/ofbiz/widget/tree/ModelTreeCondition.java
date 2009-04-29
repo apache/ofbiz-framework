4027891ed9c67f9619309fef962c94dc38578b37
Andrew Zeneski <jaz@apache.org>
2009-04-29 21:27:59 +0000

    integration with new Authz API - JIRA OFBIZ-2381
    NOTE: when using the action="" the old API is always used; when no action is specified the new API is used
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@769943 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/tree/ModelTreeCondition.java
+++ framework/widget/src/org/ofbiz/widget/tree/ModelTreeCondition.java
@@ -42,6 +42,7 @@ import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entityext.permission.EntityPermissionChecker;
 import org.ofbiz.minilang.operation.BaseCompare;
 import org.ofbiz.security.Security;
+import org.ofbiz.security.authz.Authorization;
 import org.w3c.dom.Element;
 
 /**
@@ -209,15 +210,17 @@ public class ModelTreeCondition {
                 String permission = permissionExdr.expandString(context);
                 String action = actionExdr.expandString(context);
 
+                Authorization authz = (Authorization) context.get("authorization");
                 Security security = (Security) context.get("security");
                 if (action != null && action.length() > 0) {
+                    //Debug.logWarning("Deprecated method hasEntityPermission() was called; the action field should no longer be used", module);
                     // run hasEntityPermission
                     if (security.hasEntityPermission(permission, action, userLogin)) {
                         return true;
                     }
                 } else {
                     // run hasPermission
-                    if (security.hasPermission(permission, userLogin)) {
+                    if (authz.hasPermission(userLogin.getString("userLoginId"), permission, context, true)) {
                         return true;
                     }
                 }
