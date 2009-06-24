317b35f01fb8cbee16e61fe3f9914a1685c0798e
Adrian Crum <adrianc@apache.org>
2009-06-24 05:58:56 +0000

    A rudimentary WebDAV servlet. This is in preparation for the forthcoming iCalendar improvements. It is only a beginning - it needs to be built out.
    I put this in the framework because it is reusable and extensible. For example, a WebDAV interface to the Content component could be useful.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@787915 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/webapp/src/org/ofbiz/webapp/webdav/PropFindHelper.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.webapp.webdav;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import javolution.util.FastList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/** PROPFIND HTTP method helper class. This class provides helper methods for
+ * working with WebDAV PROPFIND requests and responses.*/
+public class PropFindHelper extends ResponseHelper {
+
+    protected final Document requestDocument;
+    
+    public PropFindHelper(Document requestDocument) throws IOException, SAXException, ParserConfigurationException {
+        this.requestDocument = requestDocument;
+    }
+    
+    public Element createPropElement(List<Element> propList) {
+        Element element = this.responseDocument.createElementNS(DAV_NAMESPACE_URI, "D:prop");
+        if (propList != null && propList.size() > 0) {
+            for (Element propElement : propList) {
+                element.appendChild(propElement);
+            }
+        }
+        return element;
+    }
+
+    public Element createPropStatElement(Element prop, String stat) {
+        Element element = this.responseDocument.createElementNS(DAV_NAMESPACE_URI, "D:propstat");
+        element.appendChild(prop);
+        element.appendChild(createStatusElement(stat));
+        return element;
+    }
+
+    public List<Element> getFindPropsList(String nameSpaceUri) {
+        List<Element> result = FastList.newInstance();
+        NodeList nodeList = this.requestDocument.getElementsByTagNameNS(nameSpaceUri == null ? "*" : nameSpaceUri, "prop");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i).getFirstChild();
+            while (node != null) {
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    result.add((Element) node);
+                }
+                node = node.getNextSibling();
+            }
+        }
+        return result;
+    }
+
+    public boolean isAllProp() {
+        NodeList nodeList = this.requestDocument.getElementsByTagNameNS(DAV_NAMESPACE_URI, "allprop");
+        return nodeList.getLength() > 0;
+    }
+
+    public boolean isPropName() {
+        NodeList nodeList = this.requestDocument.getElementsByTagNameNS(DAV_NAMESPACE_URI, "propname");
+        return nodeList.getLength() > 0;
+    }
+
+}
---
+++ framework/webapp/src/org/ofbiz/webapp/webdav/WebDavServlet.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.webapp.webdav;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.security.Security;
+import org.ofbiz.security.SecurityFactory;
+import org.ofbiz.security.authz.Authorization;
+import org.ofbiz.security.authz.AuthorizationFactory;
+import org.ofbiz.service.GenericDispatcher;
+import org.ofbiz.service.LocalDispatcher;
+
+/** Implements a WebDAV servlet. The servlet simply forwards WebDAV requests
+ * to a <code>RequestHandlerFactory</code> instance, whose class is specified
+ * in the web application's web.xml file:<p><code>
+ * &lt;context-param&gt;<br>
+ * &nbsp;&nbsp;&lt;param-name&gt;requestHandlerFactoryClass&lt;/param-name&gt;<br>
+ * &nbsp;&nbsp;&lt;param-value&gt;com.mydomain.MyWebDAVFactory&lt;/param-value&gt;<br>
+ * &lt;/context-param&gt;</code></p>
+ */
+@SuppressWarnings("serial")
+public class WebDavServlet extends GenericServlet {
+
+    public static final String module = WebDavServlet.class.getName();
+
+    protected Authorization authz = null;
+    protected GenericDelegator delegator = null;
+    protected LocalDispatcher dispatcher = null;
+    protected RequestHandlerFactory handlerFactory = null;
+    protected Security security = null;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+        try {
+            super.init(config);
+            ServletContext context = this.getServletContext();
+            String delegatorName = context.getInitParameter("entityDelegatorName");
+            this.delegator = GenericDelegator.getGenericDelegator(delegatorName);
+            String dispatcherName = context.getInitParameter("localDispatcherName");
+            this.dispatcher = GenericDispatcher.getLocalDispatcher(dispatcherName, this.delegator);
+            this.security = SecurityFactory.getInstance(this.delegator);
+            this.authz = AuthorizationFactory.getInstance(this.delegator);
+            String factoryClassName = context.getInitParameter("requestHandlerFactoryClass");
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            this.handlerFactory = (RequestHandlerFactory) loader.loadClass(factoryClassName).newInstance();
+        } catch (Exception e) {
+            Debug.logError(e, "Error while initializing WebDAV servlet: ", module);
+            throw new ServletException(e);
+        }
+        if (Debug.verboseOn()) {
+            StringBuilder buff = new StringBuilder("WebDAV servlet initialized: delegator = ");
+            buff.append(this.delegator.getDelegatorName());
+            buff.append(", dispatcher = ");
+            buff.append(this.dispatcher.getName());
+            buff.append(", security = ");
+            buff.append(this.security.getClass().getName());
+            buff.append(", authz = ");
+            buff.append(this.authz.getClass().getName());
+            buff.append(", handler factory = ");
+            buff.append(this.handlerFactory.getClass().getName());
+            Debug.logVerbose(buff.toString(), module);
+        }
+    }
+
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        request.setAttribute("delegator", this.delegator);
+        request.setAttribute("dispatcher", this.dispatcher);
+        request.setAttribute("security", this.security);
+        request.setAttribute("authz", this.authz);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        RequestHandler handler = this.handlerFactory.getHandler(httpRequest.getMethod());
+        try {
+            handler.handleRequest(httpRequest, (HttpServletResponse) response, this.getServletContext());
+        } catch (IOException e) {
+            throw e;
+        } catch (ServletException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ServletException(e);
+        } finally {
+            HttpSession session = httpRequest.getSession();
+            session.invalidate();
+        }
+    }
+
+}
