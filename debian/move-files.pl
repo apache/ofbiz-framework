d1983d8dea95f00fb6fd839dff096d1bc195d456
Adam Heath <doogie@apache.org>
2009-07-01 15:43:22 +0000

    Ignore git files when processing.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@790223 13f79535-47bb-0310-9956-ffa450edef68

--- debian/move-files.pl
+++ debian/move-files.pl
@@ -90,7 +90,7 @@ sub copylink($$$) {
 }
 system('rm', '-rf', 'debian/ofbiz', 'debian/ofbiz-specialpurpose');
 
-open(FIND, '-|', qw(find -printf %P\0)) || die("Couldn't run find");
+open(FIND, '-|', qw(find -not -path */.git/* -printf %P\0)) || die("Couldn't run find");
 $/ = "\0";
 while (<FIND>) {
 	chomp;
