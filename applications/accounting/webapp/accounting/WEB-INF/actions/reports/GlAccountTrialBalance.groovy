4d82d727dc07eee18512581896ce4b6255ebc2ea
Anil Patel <apatel@apache.org>
2009-07-08 11:50:42 +0000

    New report "GL Account Trial Balance"
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@792105 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/reports/GlAccountTrialBalance.groovy
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.base.util.UtilDateTime;
+import org.ofbiz.accounting.util.UtilAccounting;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+if (organizationPartyId) {
+    context.currentOrganization = delegator.findOne("PartyNameView", [partyId : organizationPartyId], false);
+    if (parameters.glAccountId) {
+        glAccount = delegator.findOne("GlAccount", [glAccountId : parameters.glAccountId], false);
+        isDebitAccount = UtilAccounting.isDebitAccount(glAccount);
+        context.isDebitAccount = isDebitAccount;
+        context.glAccount = glAccount;
+    }
+
+    currentTimePeriod = null;
+    BigDecimal balanceOfTheAcctgForYear = BigDecimal.ZERO;
+
+    if (parameters.timePeriod) {
+        currentTimePeriod = delegator.findOne("CustomTimePeriod", [customTimePeriodId : parameters.timePeriod], false);
+        previousTimePeriod = dispatcher.runSync("getPreviousTimePeriod", 
+                [customTimePeriodId : parameters.timePeriod, userLogin : userLogin]);
+
+        if (previousTimePeriod) {
+            glAccountHistory = delegator.findOne("GlAccountHistory", 
+                    [customTimePeriodId : previousTimePeriod.previousTimePeriod.customTimePeriodId, glAccountId : parameters.glAccountId, organizationPartyId : organizationPartyId], false);
+            if (glAccountHistory) {
+                context.openingBalance = glAccountHistory.endingBalance;
+                balanceOfTheAcctgForYear = glAccountHistory.endingBalance;
+            }
+        }
+    }
+
+    if (currentTimePeriod) {
+        context.currentTimePeriod = currentTimePeriod;
+        customTimePeriodStartDate = UtilDateTime.getMonthStart(UtilDateTime.toTimestamp(currentTimePeriod.fromDate), timeZone, locale);
+        customTimePeriodEndDate = UtilDateTime.getMonthEnd(UtilDateTime.toTimestamp(currentTimePeriod.fromDate), timeZone, locale);
+
+        Calendar calendarTimePeriodStartDate = UtilDateTime.toCalendar(customTimePeriodStartDate);
+        glAcctgTrialBalanceList = [];
+        BigDecimal totalOfYearToDateDebit = BigDecimal.ZERO;
+        BigDecimal totalOfYearToDateCredit = BigDecimal.ZERO;
+        isPosted = parameters.isPosted;
+
+        while (customTimePeriodEndDate <= currentTimePeriod.thruDate) {
+
+            if ("ALL".equals(isPosted)) {
+                isPosted = "";
+            }
+            acctgTransEntriesAndTransTotal = dispatcher.runSync("getAcctgTransEntriesAndTransTotal", 
+                    [customTimePeriodStartDate : customTimePeriodStartDate, customTimePeriodEndDate : customTimePeriodEndDate, organizationPartyId : organizationPartyId, glAccountId : parameters.glAccountId, isPosted : isPosted, userLogin : userLogin]);
+            totalOfYearToDateDebit = totalOfYearToDateDebit + acctgTransEntriesAndTransTotal.debitTotal;
+            acctgTransEntriesAndTransTotal.totalOfYearToDateDebit = totalOfYearToDateDebit;
+            totalOfYearToDateCredit = totalOfYearToDateCredit + acctgTransEntriesAndTransTotal.creditTotal;
+            acctgTransEntriesAndTransTotal.totalOfYearToDateCredit = totalOfYearToDateCredit;
+
+            if (isDebitAccount) {
+                balanceOfTheAcctgForYear = balanceOfTheAcctgForYear + acctgTransEntriesAndTransTotal.debitCreditDifference;
+                acctgTransEntriesAndTransTotal.balanceOfTheAcctgForYear = balanceOfTheAcctgForYear;
+            } else {
+                balanceOfTheAcctgForYear = balanceOfTheAcctgForYear + acctgTransEntriesAndTransTotal.debitCreditDifference;
+                acctgTransEntriesAndTransTotal.balanceOfTheAcctgForYear = balanceOfTheAcctgForYear;
+            }
+
+            glAcctgTrialBalanceList.add(acctgTransEntriesAndTransTotal);
+
+            calendarTimePeriodStartDate.roll(Calendar.MONTH, 1);
+            Timestamp retStampStartDate = new Timestamp(calendarTimePeriodStartDate.getTimeInMillis());
+            retStampStartDate.setNanos(0);
+            customTimePeriodStartDate = retStampStartDate;
+            customTimePeriodEndDate = UtilDateTime.getMonthEnd(UtilDateTime.toTimestamp(retStampStartDate), timeZone, locale);
+        }
+        context.glAcctgTrialBalanceList = glAcctgTrialBalanceList;
+    }
+}
