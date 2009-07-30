6c9ebad97f3e3da6b642c1f91368cdb0621f8a7c
Anil Patel <apatel@apache.org>
2009-07-30 08:38:10 +0000

    No functional changes. Formating code.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@799202 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/webapp/ar/WEB-INF/actions/BatchPayments.groovy
+++ applications/accounting/webapp/ar/WEB-INF/actions/BatchPayments.groovy
@@ -22,52 +22,52 @@ import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.entity.condition.EntityOperator;
 if ("Y".equals(parameters.noConditionFind)) {
-List paymentCond = [];
-if (paymentMethodTypeId) {
-    paymentCond.add(EntityCondition.makeCondition("paymentMethodTypeId", EntityOperator.EQUALS, paymentMethodTypeId));
-}
-if (fromDate) {
-    paymentCond.add(EntityCondition.makeCondition("effectiveDate", EntityOperator.GREATER_THAN_EQUAL_TO, fromDate));
-}
-if (thruDate) {
-    paymentCond.add(EntityCondition.makeCondition("effectiveDate", EntityOperator.LESS_THAN_EQUAL_TO, thruDate));
-}
-if (partyIdFrom) {
-    paymentCond.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.EQUALS, partyIdFrom));
-}
-if (organizationPartyId) {
-    paymentCond.add(EntityCondition.makeCondition("partyIdTo", EntityOperator.EQUALS, organizationPartyId));
-}
-if (finAccountId) {
-    finAccountTransList = delegator.findList("FinAccountTrans", EntityCondition.makeCondition([finAccountId : finAccountId]), null, null, null, false);
-    if (finAccountTransList) {
-        finAccountTransIds = EntityUtil.getFieldListFromEntityList(finAccountTransList, "finAccountTransId", true);
-        paymentCond.add(EntityCondition.makeCondition("finAccountTransId", EntityOperator.IN, finAccountTransIds));
+    List paymentCond = [];
+    if (paymentMethodTypeId) {
+        paymentCond.add(EntityCondition.makeCondition("paymentMethodTypeId", EntityOperator.EQUALS, paymentMethodTypeId));
     }
-} else {
-    paymentCond.add(EntityCondition.makeCondition("finAccountTransId", EntityOperator.NOT_EQUAL, null));
-}
-payments = delegator.findList("Payment", EntityCondition.makeCondition(paymentCond, EntityOperator.AND), null, null, null, false);
-paymentListWithCreditCard = [];
-paymentListWithoutCreditCard = [];
-if (payments) {
-    payments.each { payment ->
-        paymentGroupMembers = EntityUtil.filterByDate(delegator.findList("PaymentGroupMember", EntityCondition.makeCondition([paymentId : payment.paymentId]), null, null, null, false));
-        if (!paymentGroupMembers) {
-            if (cardType && payment.paymentMethodId) {
-                creditCard = delegator.findOne("CreditCard", [paymentMethodId : payment.paymentMethodId], false);
-                if (creditCard.cardType == cardType) {
-                    paymentListWithCreditCard.add(payment);
+    if (fromDate) {
+        paymentCond.add(EntityCondition.makeCondition("effectiveDate", EntityOperator.GREATER_THAN_EQUAL_TO, fromDate));
+    }
+    if (thruDate) {
+        paymentCond.add(EntityCondition.makeCondition("effectiveDate", EntityOperator.LESS_THAN_EQUAL_TO, thruDate));
+    }
+    if (partyIdFrom) {
+        paymentCond.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.EQUALS, partyIdFrom));
+    }
+    if (organizationPartyId) {
+        paymentCond.add(EntityCondition.makeCondition("partyIdTo", EntityOperator.EQUALS, organizationPartyId));
+    }
+    if (finAccountId) {
+        finAccountTransList = delegator.findList("FinAccountTrans", EntityCondition.makeCondition([finAccountId : finAccountId]), null, null, null, false);
+        if (finAccountTransList) {
+            finAccountTransIds = EntityUtil.getFieldListFromEntityList(finAccountTransList, "finAccountTransId", true);
+            paymentCond.add(EntityCondition.makeCondition("finAccountTransId", EntityOperator.IN, finAccountTransIds));
+        }
+    } else {
+        paymentCond.add(EntityCondition.makeCondition("finAccountTransId", EntityOperator.NOT_EQUAL, null));
+    }
+    payments = delegator.findList("Payment", EntityCondition.makeCondition(paymentCond, EntityOperator.AND), null, null, null, false);
+    paymentListWithCreditCard = [];
+    paymentListWithoutCreditCard = [];
+    if (payments) {
+        payments.each { payment ->
+            paymentGroupMembers = EntityUtil.filterByDate(delegator.findList("PaymentGroupMember", EntityCondition.makeCondition([paymentId : payment.paymentId]), null, null, null, false));
+            if (!paymentGroupMembers) {
+                if (cardType && payment.paymentMethodId) {
+                    creditCard = delegator.findOne("CreditCard", [paymentMethodId : payment.paymentMethodId], false);
+                    if (creditCard.cardType == cardType) {
+                        paymentListWithCreditCard.add(payment);
+                    }
+                } else if (UtilValidate.isEmpty(cardType)) {
+                    paymentListWithoutCreditCard.add(payment);
                 }
-            } else if (UtilValidate.isEmpty(cardType)) {
-                paymentListWithoutCreditCard.add(payment);
             }
         }
+        if (paymentListWithCreditCard) {
+            context.paymentList = paymentListWithCreditCard;
+        } else {
+            context.paymentList = paymentListWithoutCreditCard;
+        }
     }
-    if (paymentListWithCreditCard) {
-        context.paymentList = paymentListWithCreditCard;
-    } else {
-        context.paymentList = paymentListWithoutCreditCard;
-    }
-}
 }
