4851c2da00de617b886213357088e5e0ecc44f0b
Scott Gray <lektran@apache.org>
2009-08-03 04:57:57 +0000

    Some fixes to the usps priority mail international rate estimate service First pass implementation of a usps priority mail international shipping label service
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@800198 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/product/src/org/ofbiz/shipment/shipment/ShipmentWorker.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.shipment.shipment;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javolution.util.FastMap;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilHttp;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.product.product.ProductWorker;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.ModelService;
+
+/**
+ * ShipmentWorker - Worker methods for Shipment and related entities
+ */
+public class ShipmentWorker {
+
+    public static final String module = ShipmentWorker.class.getName();
+
+    /*
+     * Returns the value of a given ShipmentPackageContent record.  Calculated by working out the total value (from the OrderItems) of all ItemIssuances
+     * for the ShipmentItem then dividing that by the total quantity issued for the same to get an average item value then multiplying that by the package 
+     * content quantity.
+     * Note: No rounding of the calculation is performed so you will need to round it to the accuracy that you require 
+     */
+    public static BigDecimal getShipmentPackageContentValue(GenericValue shipmentPackageContent) {
+        BigDecimal quantity = shipmentPackageContent.getBigDecimal("quantity");
+
+        BigDecimal value = new BigDecimal("0");
+
+        // lookup the issuance to find the order
+        List<GenericValue> issuances = null;
+        try {
+            GenericValue shipmentItem = shipmentPackageContent.getRelatedOne("ShipmentItem");
+            issuances = shipmentItem.getRelated("ItemIssuance");
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+        }
+
+        BigDecimal totalIssued = BigDecimal.ZERO;
+        BigDecimal totalValue = BigDecimal.ZERO;
+        if (UtilValidate.isNotEmpty(issuances)) {
+            for (GenericValue issuance : issuances) {
+                // we only need one
+                BigDecimal issuanceQuantity = issuance.getBigDecimal("quantity");
+                BigDecimal issuanceCancelQuantity = issuance.getBigDecimal("cancelQuantity");
+                if (issuanceCancelQuantity != null) {
+                    issuanceQuantity = issuanceQuantity.subtract(issuanceCancelQuantity);
+                }
+                // get the order item
+                GenericValue orderItem = null;
+                try {
+                    orderItem = issuance.getRelatedOne("OrderItem");
+                } catch (GenericEntityException e) {
+                    Debug.logError(e, module);
+                }
+
+                if (orderItem != null) {
+                    // get the value per unit - (base price * amount)
+                    BigDecimal selectedAmount = orderItem.getBigDecimal("selectedAmount");
+                    if (selectedAmount == null || selectedAmount.compareTo(BigDecimal.ZERO) <= 0) {
+                        selectedAmount = BigDecimal.ONE;
+                    }
+
+                    BigDecimal unitPrice = orderItem.getBigDecimal("unitPrice");
+                    BigDecimal itemValue = unitPrice.multiply(selectedAmount);
+
+                    // total value for package (per unit * quantity)
+                    totalIssued = totalIssued.add(issuanceQuantity);
+                    totalValue = totalValue.add(itemValue.multiply(issuanceQuantity));
+                }
+            }
+        }
+        // take the average value of the issuances and multiply it by the shipment package content quantity
+        value = totalValue.divide(totalIssued, 10, BigDecimal.ROUND_HALF_EVEN).multiply(quantity);
+        return value;
+    }
+}
+
