963c118e8d90734e58deccdb489c58e2f9da13bb
Ashish Vijaywargiya <ashish@apache.org>
2009-08-17 06:19:34 +0000

    Applied patch from jira issue OFBIZ-2835 (Under Accounting - Report tab create a new screen for showing comparative income statements.) Following points are covered in this patch:
    1) Added New tab under Accounting -> Organization Gl Setting (Company) -> Reports with name Comparative Income Statment .
    2) Used groovy for preparing list of compared income/expenses statements.
    3) Added list form to show compared Income/expenses statements. 
    
    Thanks Awdesh and Sumit for the contribution.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@804869 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/reports/ComparativeIncomeStatement.groovy
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.math.BigDecimal; 
+import java.sql.Timestamp;
+import org.ofbiz.entity.util.EntityUtil;
+
+if (period1FromDate && period1ThruDate && organizationPartyId && period2FromDate && period2ThruDate) {
+    onlyIncludePeriodTypeIdList = [];
+    onlyIncludePeriodTypeIdList.add("FISCAL_YEAR");
+    glAccountIncomeList = [];
+    glAccountExpenseList = [];
+    periodExpenses = [];
+    periodIncomes = [];
+    glAccountTotalsMapForPeriod1 = getGlAccountTotals(onlyIncludePeriodTypeIdList, period1FromDate, period1ThruDate, organizationPartyId, parameters.glFiscalTypeId);
+    period1Expenses = [];
+    period1Incomes = [];
+    if (glAccountTotalsMapForPeriod1) {
+        period1Expenses = glAccountTotalsMapForPeriod1.expenses;
+        period1Incomes = glAccountTotalsMapForPeriod1.income;
+        if (period1Incomes) 
+            periodIncomes.addAll(period1Incomes);
+        if (period1Expenses) 
+            periodExpenses.addAll(period1Expenses);
+    }
+    glAccountTotalsMapForPeriod2 = getGlAccountTotals(onlyIncludePeriodTypeIdList ,period2FromDate ,period2ThruDate ,organizationPartyId , parameters.glFiscalTypeId);
+    period2Expenses = [];
+    period2Incomes = [];
+    if (glAccountTotalsMapForPeriod2) {
+        period2Expenses = glAccountTotalsMapForPeriod2.expenses;
+        period2Incomes = glAccountTotalsMapForPeriod2.income;
+        period2Expenses.each { period2Expense ->
+            if (!((periodExpenses.glAccountId).contains(period2Expense.glAccountId)))
+                periodExpenses.add(period2Expense);
+        }
+        period2Incomes.each { period2Income ->
+            if (!((periodIncomes.glAccountId).contains(period2Income.glAccountId)))
+                periodIncomes.add(period2Income);
+        }
+    }
+    periodExpenses.each { periodExpense ->
+        period1TotalAmount = BigDecimal.ZERO;
+        period2TotalAmount = BigDecimal.ZERO; 
+        if ((period1Expenses.glAccountId).contains(periodExpense.glAccountId)) {
+            period1Expenses.each { period1Expense ->
+                if(periodExpense.glAccountId.equals(period1Expense.glAccountId))
+                    period1TotalAmount = period1Expense.totalAmount; 
+            }
+        }
+        if ((period2Expenses.glAccountId).contains(periodExpense.glAccountId)) {
+            period2Expenses.each { period2Expense ->
+                if(periodExpense.glAccountId.equals(period2Expense.glAccountId))
+                    period2TotalAmount = period2Expense.totalAmount; 
+            }
+        }
+        glAccountExpenseList.add([glAccountId : periodExpense.glAccountId , period1TotalAmount : period1TotalAmount , period2TotalAmount : period2TotalAmount]);
+        context.glAccountExpenseList = glAccountExpenseList;
+    }
+    periodIncomes.each { periodIncome ->
+        period1TotalAmount = BigDecimal.ZERO;
+        period2TotalAmount = BigDecimal.ZERO;
+        if ((period1Incomes.glAccountId).contains(periodIncome.glAccountId)) {
+            period1Incomes.each { period1Income ->
+                if(periodIncome.glAccountId.equals(period1Income.glAccountId))
+                    period1TotalAmount = period1Income.totalAmount; 
+            }
+        }
+        if ((period2Incomes.glAccountId).contains(periodIncome.glAccountId)) {
+            period2Incomes.each { period2Income ->
+                if(periodIncome.glAccountId.equals(period2Income.glAccountId))
+                    period2TotalAmount = period2Income.totalAmount; 
+            }
+        }
+        glAccountIncomeList.add([glAccountId : periodIncome.glAccountId , period1TotalAmount : period1TotalAmount , period2TotalAmount : period2TotalAmount]);
+        context.glAccountIncomeList = glAccountIncomeList;
+    }
+}
+
+private Map getGlAccountTotals(List onlyIncludePeriodTypeIdList, Timestamp fromDate, Timestamp thruDate, String organizationPartyId, String glFiscalTypeId) {
+    customTimePeriodResult = dispatcher.runSync("findCustomTimePeriods", [findDate : thruDate, organizationPartyId : organizationPartyId, onlyIncludePeriodTypeIdList : onlyIncludePeriodTypeIdList, userLogin : userLogin]);
+    if (customTimePeriodResult) {
+        customTimePeriod = EntityUtil.getFirst(customTimePeriodResult.customTimePeriodList);
+        if (customTimePeriod) {
+            customTimePeriodFromDate = new Timestamp((customTimePeriod.fromDate).getTime());
+            customTimePeriodThruDate = new Timestamp((customTimePeriod.thruDate).getTime());
+            if (customTimePeriodFromDate.compareTo(fromDate) > 0) 
+                fromDate =  customTimePeriodFromDate;
+            if (customTimePeriodThruDate.compareTo(thruDate) < 0) 
+                thruDate =  customTimePeriodThruDate;
+            context.financialYearFromDate = customTimePeriodFromDate;
+        }
+        prepareIncomeStatement = dispatcher.runSync("prepareIncomeStatement",
+                [fromDate : fromDate, thruDate : thruDate, organizationPartyId : organizationPartyId, glFiscalTypeId : glFiscalTypeId, userLogin : userLogin]);
+        return glAccountTotalsMap = prepareIncomeStatement.glAccountTotalsMap;
+    }
+}
