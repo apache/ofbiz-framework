3429865136bad749e4e9257a13a85f6886fbff04
Jacopo Cappellato <jacopoc@apache.org>
2009-08-26 16:16:23 +0000

    Created method that "creates a child element with the given namespace supportive name and appends it to the element child node list". Patch from Ashish Vijaywargiya
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@808093 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/UtilXml.java
+++ framework/base/src/org/ofbiz/base/util/UtilXml.java
@@ -586,6 +586,14 @@ public class UtilXml {
         newElement.appendChild(document.createTextNode(childElementValue));
         return newElement;
     }
+
+    /** Creates a child element with the given namespace supportive name and appends it to the element child node list. */    
+    public static Element addChildElementNSElement(Element element, String childElementName,
+            Document document, String nameSpaceUrl) {
+        Element newElement = document.createElementNS(nameSpaceUrl, childElementName);
+        element.appendChild(newElement);         
+        return element;
+    }
     
     /** Creates a child element with the given namespace supportive name and appends it to the element child node list.
      *  Also creates a Text node with the given value and appends it to the new elements child node list.
