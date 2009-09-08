b9e1b86fd025080e89edb0784cc18077540a8208
Ashish Vijaywargiya <ashish@apache.org>
2009-09-08 10:28:55 +0000

    Moving a method that prepares EbayConfigurations to Helper class - These configuration settings can be used by few methods / services present inside ImportOrdersFromEbay.java. Will reduce the duplicate code.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@812437 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/ebay/src/org/ofbiz/ebay/ProductsExportToEbay.java
+++ specialpurpose/ebay/src/org/ofbiz/ebay/ProductsExportToEbay.java
@@ -66,7 +66,7 @@ public class ProductsExportToEbay {
         GenericDelegator delegator = dctx.getDelegator();
         Map result = null;
         try {
-            Map<String, Object> eBayConfigResult = buildEbayConfig(context, delegator);
+            Map<String, Object> eBayConfigResult = EbayHelper.buildEbayConfig(context, delegator);
             StringBuffer dataItemsXml = new StringBuffer();
             Map resultMap = buildDataItemsXml(dctx, context, dataItemsXml, eBayConfigResult.get("token").toString());
             if (!ServiceUtil.isFailure(resultMap)) {
@@ -473,7 +473,7 @@ public class ProductsExportToEbay {
         Map result = null;
 
         try {
-            Map<String, Object> eBayConfigResult = buildEbayConfig(context, delegator);
+            Map<String, Object> eBayConfigResult = EbayHelper.buildEbayConfig(context, delegator);
             String categoryParent = "";
             String levelLimit = "";
             if (categoryCode != null) {
@@ -553,38 +553,6 @@ public class ProductsExportToEbay {
         return results;
     }
     
-    private static Map<String, Object> buildEbayConfig(Map<String, Object> context, GenericDelegator delegator) {
-        Map<String, Object> buildEbayConfigContext = FastMap.newInstance();
-        String productStoreId = (String) context.get("productStoreId");
-        if (UtilValidate.isNotEmpty(productStoreId)) {
-            GenericValue eBayConfig = null;
-            try {
-                eBayConfig = delegator.findOne("EbayConfig", false, UtilMisc.toMap("productStoreId", productStoreId));
-            } catch (GenericEntityException e) {
-                Debug.logError("Unable to find value for EbayConfig", module);
-                e.printStackTrace();
-            }
-            if (UtilValidate.isNotEmpty(eBayConfig)) {
-                buildEbayConfigContext.put("devID", eBayConfig.getString("devId"));
-                buildEbayConfigContext.put("appID", eBayConfig.getString("appId"));
-                buildEbayConfigContext.put("certID", eBayConfig.getString("certId"));
-                buildEbayConfigContext.put("token", eBayConfig.getString("token"));
-                buildEbayConfigContext.put("compatibilityLevel", eBayConfig.getString("compatibilityLevel"));
-                buildEbayConfigContext.put("siteID", eBayConfig.getString("siteId"));
-                buildEbayConfigContext.put("xmlGatewayUri", eBayConfig.getString("xmlGatewayUri"));
-            }
-        } else {
-            buildEbayConfigContext.put("devID", UtilProperties.getPropertyValue(configFileName, "eBayExport.devID"));
-            buildEbayConfigContext.put("appID", UtilProperties.getPropertyValue(configFileName, "eBayExport.appID"));
-            buildEbayConfigContext.put("certID", UtilProperties.getPropertyValue(configFileName, "eBayExport.certID"));
-            buildEbayConfigContext.put("token", UtilProperties.getPropertyValue(configFileName, "eBayExport.token"));
-            buildEbayConfigContext.put("compatibilityLevel", UtilProperties.getPropertyValue(configFileName, "eBayExport.compatibilityLevel"));
-            buildEbayConfigContext.put("siteID", UtilProperties.getPropertyValue(configFileName, "eBayExport.siteID"));
-            buildEbayConfigContext.put("xmlGatewayUri", UtilProperties.getPropertyValue(configFileName, "eBayExport.xmlGatewayUri"));
-        }    
-        return buildEbayConfigContext;
-    }    
-
     private static Map exportToEbayResponse(String msg) {
         Map result = FastMap.newInstance();
         try {
