d34dbebce886086b272ee7f08c0dee23d830ee0b
Hans Bakker <hansbak@apache.org>
2009-09-16 07:48:51 +0000

    all upload content screens (hopefully we did not forget any) can now also select an existing contentId which can be selected from a navigation tree. Added also the possibility when a file is uploaded to put it at a particular leave in the tree. Programmed by my employee Berm
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@815651 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/content/webapp/content/WEB-INF/actions/content/GetContentLookupList.groovy
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+ import org.ofbiz.entity.condition.*;
+ import org.ofbiz.entity.util.*;
+ import org.ofbiz.entity.*;
+ import org.ofbiz.base.util.*;
+ import javolution.util.FastList;
+ import javolution.util.FastSet;
+ import javolution.util.FastMap;
+ import org.ofbiz.entity.transaction.TransactionUtil;
+ import org.ofbiz.entity.util.EntityListIterator;
+ import org.ofbiz.entity.GenericEntity;
+ import org.ofbiz.entity.model.ModelField;
+ import org.ofbiz.base.util.UtilValidate;
+ import org.ofbiz.entity.model.ModelEntity;
+ import org.ofbiz.entity.model.ModelReader;
+
+try {
+    viewIndex = Integer.valueOf((String)parameters.get("VIEW_INDEX")).intValue();
+} catch (NumberFormatException nfe) {
+    	viewIndex = 0;
+}
+
+context.viewIndexFirst = 0;
+context.viewIndex = viewIndex;
+context.viewIndexPrevious = viewIndex-1;
+context.viewIndexNext = viewIndex+1;
+String curFindString="";
+
+ModelReader reader = delegator.getModelReader();
+ModelEntity modelEntity = reader.getModelEntity("ContentAssocViewTo");
+GenericEntity findByEntity = delegator.makeValue("ContentAssocViewTo");
+List errMsgList = FastList.newInstance();
+for (int fnum = 0; fnum <  modelEntity.getFieldsSize(); fnum++) {
+    ModelField field = modelEntity.getField(fnum);
+    String fval = parameters.get(field.getName());
+    if (fval != null) {
+        if (fval.length() > 0) {
+            curFindString = curFindString + "&" + field.getName() + "=" + fval;
+            try {
+                findByEntity.setString(field.getName(), fval);
+            } catch (NumberFormatException nfe) {
+                Debug.logError(nfe, "Caught an exception : " + nfe.toString(), "GetContentLookupList.groovy");
+                errMsgList.add("Entered value is non-numeric for numeric field: " + field.getName());
+            }
+        }
+    }
+}
+if (errMsgList) {
+    request.setAttribute("_ERROR_MESSAGE_LIST_", errMsgList);
+}
+
+curFindString = UtilFormatOut.encodeQuery(curFindString);
+context.curFindString = curFindString;
+try {
+	viewSize = Integer.valueOf((String)parameters.get("VIEW_SIZE")).intValue();
+} catch (NumberFormatException nfe) {
+	
+}
+
+context.viewSize = viewSize;
+
+int lowIndex = viewIndex*viewSize+1;
+int highIndex = (viewIndex+1)*viewSize;
+
+context.lowIndex = lowIndex;
+int arraySize = 0;
+List resultPartialList = null;
+	conditions = [EntityCondition.makeCondition("contentIdStart", EntityOperator.EQUALS,(String)parameters.get("contentId"))];
+
+if ((highIndex - lowIndex + 1) > 0) {
+	// get the results as an entity list iterator
+	boolean beganTransaction = false;
+	if(resultPartialList==null){
+    try {
+    	beganTransaction = TransactionUtil.begin();
+    	allConditions = EntityCondition.makeCondition( conditions, EntityOperator.AND );
+    	fieldsToSelect = FastSet.newInstance();
+    	//fieldsToSelect=["contentId", "contentName", "mimeTypeId"] as Set;
+    	findOptions = new EntityFindOptions(true, EntityFindOptions.TYPE_SCROLL_INSENSITIVE, EntityFindOptions.CONCUR_READ_ONLY, true);
+    	EntityListIterator listIt=null;
+    	listIt = delegator.find("ContentAssocViewTo", allConditions, null, null, ["contentId ASC"], findOptions);
+    	resultPartialList = listIt.getPartialList(lowIndex, highIndex - lowIndex + 1);
+    	
+    	arraySize = listIt.getResultsSizeAfterPartialList();
+    	if (arraySize < highIndex) {
+    		highIndex = arraySize;
+    	}
+    	listIt.close();
+	} catch (GenericEntityException e) {
+		Debug.logError(e, "Failure in operation, rolling back transaction", "GetContentLookupList.groovy");
+		try {
+			// only rollback the transaction if we started one...
+			TransactionUtil.rollback(beganTransaction, "Error looking up entity values in WebTools Entity Data Maintenance", e);
+		} catch (GenericEntityException e2) {
+			Debug.logError(e2, "Could not rollback transaction: " + e2.toString(), "GetContentLookupList.groovy");
+		}
+		// after rolling back, rethrow the exception
+		throw e;
+	} finally {
+		// only commit the transaction if we started one... this will throw an exception if it fails
+		TransactionUtil.commit(beganTransaction);
+	}
+	}
+}
+context.highIndex = highIndex;
+context.arraySize = arraySize;
+context.resultPartialList = resultPartialList;
+
+viewIndexLast = (int) (arraySize/viewSize);
+context.viewIndexLast = viewIndexLast;
+contentAssoc = FastList.newInstance();
+context.contentAssoc=resultPartialList;
---
+++ applications/content/webapp/content/lookup/ContentTreeLookupList.ftl
@@ -0,0 +1,101 @@
+<#--
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+  -->
+<div class="screenlet">
+<table cellspacing="0" width="100%">
+	<tr colspan="3">
+  	<td align="left">
+                    <#if (viewIndex > 0)>
+                    	<#assign url='/views/'+tabButtonItem+'?'+curFindString+'&amp;VIEW_SIZE='+viewSize+'&amp;VIEW_INDEX='+viewIndexFirst>
+                        <a href="javascript:nextPrevDocumentList('${url}');" class="nav-next">${uiLabelMap.CommonFirst}</a>|
+                      	<#assign url='/views/'+tabButtonItem+'?'+curFindString+'&amp;VIEW_SIZE='+viewSize+'&amp;VIEW_INDEX='+viewIndexPrevious>
+						<a href="javascript:nextPrevDocumentList('${url}');" class="nav-previous">${uiLabelMap.CommonPrevious}</a>|
+                    </#if>
+                    <#if (arraySize > 0)>
+						${lowIndex} - ${highIndex} ${uiLabelMap.CommonOf} ${arraySize}
+                    </#if>
+                    <#if (arraySize > highIndex)>
+                    	<#assign url='/views/'+tabButtonItem+'?'+curFindString+'&amp;VIEW_SIZE='+viewSize+'&amp;VIEW_INDEX='+viewIndexNext>|
+                    	<a href="javascript:nextPrevDocumentList('${url}');" class="nav-next">${uiLabelMap.CommonNext}</a>
+                        <#assign url='/views/'+tabButtonItem+'?'+curFindString+'&amp;VIEW_SIZE='+viewSize+'&amp;VIEW_INDEX='+viewIndexLast>|
+                        <a href="javascript:nextPrevDocumentList('${url}');" class="nav-next">${uiLabelMap.CommonLast}</a>
+                    </#if>
+        </td>
+        <td></td>
+        <td></td>
+  </tr>
+   <#if (arraySize > 0)>
+   		<tr><td colspan="3"><hr/></td></tr>
+   </#if>
+</table>
+<table class="basic-table hover-bar" cellspacing="0">
+<#if tabButtonItem=="ListContentTree">
+<#--Form ListContentTree-->
+  <tr class="header-row">
+    <td>${uiLabelMap.FormFieldTitle_contentId}</td>
+    <td>${uiLabelMap.FormFieldTitle_coContentName}</td>
+    <td>${uiLabelMap.FormFieldTitle_mimeTypeId}</td>
+  </tr>
+<#elseif tabButtonItem=="ListDocument">
+<#--Form ListDocument-->
+ <tr class="header-row">
+    <td>${uiLabelMap.FormFieldTitle_contentId}</td>
+    <td>${uiLabelMap.FormFieldTitle_contentTypeId}</td>
+    <td>${uiLabelMap.FormFieldTitle_mimeTypeId}</td>
+    <td>${uiLabelMap.FormFieldTitle_contentStatusId}</td>
+    <td>${uiLabelMap.FormFieldTitle_caCratedDate}</td>
+    <td>${uiLabelMap.CommonDelete}</td>
+  </tr>
+</#if>
+<#if contentAssoc?has_content>  
+   	<#assign alt_row = false/>
+   	<#assign listcount=0>
+  	<#list contentAssoc as contentData>
+  	<#if tabButtonItem=="ListContentTree">
+		<#--Form ListContentTree-->
+  		<tr <#if alt_row> class="alternate-row"</#if>> 
+  			<td><a class="plain" href="javascript:set_value('${contentData.contentId?if_exists}')">${contentData.contentId?if_exists}</a></td>
+  			<td>${contentData.contentName?if_exists}</td>
+  			<td>${contentData.mimeTypeId?if_exists}</td>
+  		</tr>
+  	<#elseif tabButtonItem=="ListDocument">
+  		<#--Form ListDocument-->
+  		<tr <#if alt_row> class="alternate-row"</#if>>
+  			<td><a class="plain" href="/content/control/editContent?contentId=${contentData.contentId?if_exists}">${contentData.contentId?if_exists}</a></td>
+  			<td>${contentData.contentTypeId?if_exists}</td>
+  			<td>${contentData.mimeTypeId?if_exists}</td>
+  			<td>${contentData.statusId?if_exists}</td>
+  			<#if contentData.caFromDate?has_content>
+             <#assign caFromDate = Static["org.ofbiz.base.util.UtilDateTime"].toDateString(contentData.caFromDate, "dd/MM/yyyy")/>
+            </#if> 
+  			<td>${caFromDate?if_exists}</td>
+  			<td><a href="javascript:document.listDocumentForm_${listcount}.submit()" >${uiLabelMap.CommonDelete}</a></td>
+  		</tr>
+  		<form action="<@ofbizUrl>removeDocumentFromTree</@ofbizUrl>" name="listDocumentForm_${listcount}" method="post">
+        	<input type="hidden" name="contentId" value="${contentData.contentIdStart?if_exists}"/>
+        	<input type="hidden" name="contentIdTo" value="${contentData.contentId?if_exists}"/>
+        	<input type="hidden" name="contentAssocTypeId" value="${contentData.caContentAssocTypeId?if_exists}"/>
+        	<input type="hidden" name="fromDate" value="${contentData.fromDate?if_exists}"/>
+  		</form>
+ 	</#if>
+ 		<#assign alt_row = !alt_row/>
+ 		<#assign listcount=listcount+1>
+  	</#list>
+</#if>
+</table>
+</div>
