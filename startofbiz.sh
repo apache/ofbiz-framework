01dc5a038a3ba037c35f09c7e76b5f29a051effb
Scott Gray <lektran@apache.org>
2009-09-24 00:05:05 +0000

    Added the maxPermSize jvm arg the various startup methods since OFBiz now seems to require more than the default 64m.  Also removed startofbizNoLog.bat since it seems somewhat superfluous, instead made startofbiz.bat log to the console by default and added a comment about how to change it if desired.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@818321 13f79535-47bb-0310-9956-ffa450edef68

--- startofbiz.sh
+++ startofbiz.sh
@@ -35,7 +35,7 @@ ADMIN="-Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY"
 #automatic IP address for linux
 #IPADDR=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 #RMIIF="-Djava.rmi.server.hostname=$IPADDR"
-MEMIF="-Xms128M -Xmx512M"
+MEMIF="-Xms128M -Xmx512M -XX:MaxPermSize=128m"
 #MISC="-Duser.language=en"
 VMARGS="$MEMIF $MISC $DEBUG $RMIIF $ADMIN"
 
