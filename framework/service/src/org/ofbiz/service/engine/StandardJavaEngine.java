52a03030bb1efa854e5974ddec6cdadd4f0a4de0
Jacques Le Roux <jleroux@apache.org>
2009-09-27 10:35:21 +0000

    A new feature from Shi Yusen (https://issues.apache.org/jira/browse/OFBIZ-2948) - OFBIZ-2948 This change will make it possible to invoke both static method and instance method. See http://java.sun.com/j2se/1.5.0/docs/api/java/lang/reflect/Method.html#invoke(java.lang.Object,%20java.lang.Object...) Thanks to Scott for optimisation advice
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@819281 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/engine/StandardJavaEngine.java
+++ framework/service/src/org/ofbiz/service/engine/StandardJavaEngine.java
@@ -20,6 +20,7 @@ package org.ofbiz.service.engine;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Map;
 
 import org.ofbiz.service.DispatchContext;
@@ -95,7 +96,11 @@ public final class StandardJavaEngine extends GenericAsyncEngine {
         try {
             Class<?> c = cl.loadClass(this.getLocation(modelService));
             Method m = c.getMethod(modelService.invoke, DispatchContext.class, Map.class);
-            result = m.invoke(null, dctx, context);
+            if (Modifier.isStatic(m.getModifiers())) {
+                result = m.invoke(null, dctx, context);
+            } else {
+                result = m.invoke(c.newInstance(), dctx, context);
+            }            
         } catch (ClassNotFoundException cnfe) {
             throw new GenericServiceException("Cannot find service [" + modelService.name + "] location class", cnfe);
         } catch (NoSuchMethodException nsme) {
