bdb62ab9efe343ab24295ac850833aa27cfaf220
Jacques Le Roux <jleroux@apache.org>
2009-10-13 21:37:49 +0000

    In this case I don't need to persist the property. This changes just do that : set temporary the property in memory, and reset it back to its original value
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@824937 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/UtilProperties.java
+++ framework/base/src/org/ofbiz/base/util/UtilProperties.java
@@ -325,82 +325,97 @@ public class UtilProperties implements java.io.Serializable {
     }
 
     /** Sets the specified value of the specified property name to the specified resource/properties file
-    * @param resource The name of the resource - must be a file
-    * @param name The name of the property in the properties file
-    * @param value The value of the property in the properties file */
-    public static void setPropertyValue(String resource, String name, String value) {
-        if (resource == null || resource.length() <= 0) return;
-        if (name == null || name.length() <= 0) return;
+     * @param resource The name of the resource - must be a file
+     * @param name The name of the property in the properties file
+     * @param value The value of the property in the properties file */
+     public static void setPropertyValue(String resource, String name, String value) {
+         if (resource == null || resource.length() <= 0) return;
+         if (name == null || name.length() <= 0) return;
 
-        Properties properties = getProperties(resource);
-        if (properties == null) {
-            return;
-        }
+         Properties properties = getProperties(resource);
+         if (properties == null) {
+             return;
+         }
 
-        try {
-            properties.setProperty(name, value);
-            FileOutputStream propFile = new FileOutputStream(resource);
-            if ("XuiLabels".equals(name)) {
-                properties.store(propFile,
-                    "##############################################################################\n"
-                    +"# Licensed to the Apache Software Foundation (ASF) under one                   \n"
-                    +"# or more contributor license agreements.  See the NOTICE file                 \n"
-                    +"# distributed with this work for additional information                        \n"
-                    +"# regarding copyright ownership.  The ASF licenses this file                   \n"
-                    +"# to you under the Apache License, Version 2.0 (the                            \n"
-                    +"# \"License\"); you may not use this file except in compliance                 \n"
-                    +"# with the License.  You may obtain a copy of the License at                   \n"
-                    +"#                                                                              \n"
-                    +"# http://www.apache.org/licenses/LICENSE-2.0                                   \n"
-                    +"#                                                                              \n"
-                    +"# Unless required by applicable law or agreed to in writing,                   \n"
-                    +"# software distributed under the License is distributed on an                  \n"
-                    +"# \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY                     \n"
-                    +"# KIND, either express or implied.  See the License for the                    \n"
-                    +"# specific language governing permissions and limitations                      \n"
-                    +"# under the License.                                                           \n"
-                    +"###############################################################################\n"
-                    +"#                                                                              \n"
-                    +"# Dynamically modified by OFBiz Framework (org.ofbiz.base.util : UtilProperties.setPropertyValue)\n"
-                    +"#                                                                              \n"
-                    +"# By default the screen is 1024x768 wide. If you want to use another screen size,\n"
-                    +"# you must create a new directory under specialpurpose/pos/screens, like the 800x600.\n"
-                    +"# You must also set the 3 related parameters (StartClass, ClientWidth, ClientHeight) accordingly.\n"
-                    +"#");
-            } else {
-                properties.store(propFile,
-                    "##############################################################################\n"
-                    +"# Licensed to the Apache Software Foundation (ASF) under one                   \n"
-                    +"# or more contributor license agreements.  See the NOTICE file                 \n"
-                    +"# distributed with this work for additional information                        \n"
-                    +"# regarding copyright ownership.  The ASF licenses this file                   \n"
-                    +"# to you under the Apache License, Version 2.0 (the                            \n"
-                    +"# \"License\"); you may not use this file except in compliance                 \n"
-                    +"# with the License.  You may obtain a copy of the License at                   \n"
-                    +"#                                                                              \n"
-                    +"# http://www.apache.org/licenses/LICENSE-2.0                                   \n"
-                    +"#                                                                              \n"
-                    +"# Unless required by applicable law or agreed to in writing,                   \n"
-                    +"# software distributed under the License is distributed on an                  \n"
-                    +"# \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY                     \n"
-                    +"# KIND, either express or implied.  See the License for the                    \n"
-                    +"# specific language governing permissions and limitations                      \n"
-                    +"# under the License.                                                           \n"
-                    +"###############################################################################\n"
-                    +"#                                                                              \n"
-                    +"# Dynamically modified by OFBiz Framework (org.ofbiz.base.util : UtilProperties.setPropertyValue)\n"
-                    +"# The comments have been removed, you may still find them on the OFBiz repository... \n"
-                    +"#");
-                
-            }
+         try {
+             properties.setProperty(name, value);
+             FileOutputStream propFile = new FileOutputStream(resource);
+             if ("XuiLabels".equals(name)) {
+                 properties.store(propFile,
+                     "##############################################################################\n"
+                     +"# Licensed to the Apache Software Foundation (ASF) under one                   \n"
+                     +"# or more contributor license agreements.  See the NOTICE file                 \n"
+                     +"# distributed with this work for additional information                        \n"
+                     +"# regarding copyright ownership.  The ASF licenses this file                   \n"
+                     +"# to you under the Apache License, Version 2.0 (the                            \n"
+                     +"# \"License\"); you may not use this file except in compliance                 \n"
+                     +"# with the License.  You may obtain a copy of the License at                   \n"
+                     +"#                                                                              \n"
+                     +"# http://www.apache.org/licenses/LICENSE-2.0                                   \n"
+                     +"#                                                                              \n"
+                     +"# Unless required by applicable law or agreed to in writing,                   \n"
+                     +"# software distributed under the License is distributed on an                  \n"
+                     +"# \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY                     \n"
+                     +"# KIND, either express or implied.  See the License for the                    \n"
+                     +"# specific language governing permissions and limitations                      \n"
+                     +"# under the License.                                                           \n"
+                     +"###############################################################################\n"
+                     +"#                                                                              \n"
+                     +"# Dynamically modified by OFBiz Framework (org.ofbiz.base.util : UtilProperties.setPropertyValue)\n"
+                     +"#                                                                              \n"
+                     +"# By default the screen is 1024x768 wide. If you want to use another screen size,\n"
+                     +"# you must create a new directory under specialpurpose/pos/screens, like the 800x600.\n"
+                     +"# You must also set the 3 related parameters (StartClass, ClientWidth, ClientHeight) accordingly.\n"
+                     +"#");
+             } else {
+                 properties.store(propFile,
+                     "##############################################################################\n"
+                     +"# Licensed to the Apache Software Foundation (ASF) under one                   \n"
+                     +"# or more contributor license agreements.  See the NOTICE file                 \n"
+                     +"# distributed with this work for additional information                        \n"
+                     +"# regarding copyright ownership.  The ASF licenses this file                   \n"
+                     +"# to you under the Apache License, Version 2.0 (the                            \n"
+                     +"# \"License\"); you may not use this file except in compliance                 \n"
+                     +"# with the License.  You may obtain a copy of the License at                   \n"
+                     +"#                                                                              \n"
+                     +"# http://www.apache.org/licenses/LICENSE-2.0                                   \n"
+                     +"#                                                                              \n"
+                     +"# Unless required by applicable law or agreed to in writing,                   \n"
+                     +"# software distributed under the License is distributed on an                  \n"
+                     +"# \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY                     \n"
+                     +"# KIND, either express or implied.  See the License for the                    \n"
+                     +"# specific language governing permissions and limitations                      \n"
+                     +"# under the License.                                                           \n"
+                     +"###############################################################################\n"
+                     +"#                                                                              \n"
+                     +"# Dynamically modified by OFBiz Framework (org.ofbiz.base.util : UtilProperties.setPropertyValue)\n"
+                     +"# The comments have been removed, you may still find them on the OFBiz repository... \n"
+                     +"#");
+                 
+             }
 
-            propFile.close();
-        } catch (FileNotFoundException e) {
-            Debug.log(e, "Unable to located the resource file.", module);
-        } catch (IOException e) {
-            Debug.logError(e, module);
-        }
-    }
+             propFile.close();
+         } catch (FileNotFoundException e) {
+             Debug.log(e, "Unable to located the resource file.", module);
+         } catch (IOException e) {
+             Debug.logError(e, module);
+         }
+     }
+
+     /** Sets the specified value of the specified property name to the specified resource/properties in memory, does not persist it 
+      * @param resource The name of the resource
+      * @param name The name of the property in the resource
+      * @param value The value of the property to set in memory */
+      public static void setPropertyValueInMemory(String resource, String name, String value) {
+          if (resource == null || resource.length() <= 0) return;
+          if (name == null || name.length() <= 0) return;
+
+          Properties properties = getProperties(resource);
+          if (properties == null) {
+              return;
+          }
+          properties.setProperty(name, value);
+      }
 
     // ========= Locale & Resource Based Methods ==========
 
