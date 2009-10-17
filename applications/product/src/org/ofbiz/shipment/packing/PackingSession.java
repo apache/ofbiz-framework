3e5071b2726fb29c83ca17a87968d8d14bae6563
Scott Gray <lektran@apache.org>
2009-10-17 09:38:02 +0000

    Prevent an NPE when a primaryOrderId hasn't been supplied, also set partyIdFrom using the facility.ownerPartyId in this situation. (This bug was exposed using the unit tests)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@826206 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/src/org/ofbiz/shipment/packing/PackingSession.java
+++ applications/product/src/org/ofbiz/shipment/packing/PackingSession.java
@@ -725,25 +725,33 @@ public class PackingSession implements java.io.Serializable {
         if (UtilValidate.isNotEmpty(orderRoleShipTo)) {
             newShipment.put("partyIdTo", orderRoleShipTo.getString("partyId"));
         }
-        String partyIdFrom = null; 
-        GenericValue orderItemShipGroup = EntityUtil.getFirst(delegator.findByAnd("OrderItemShipGroup", UtilMisc.toMap("orderId", primaryOrderId, "shipGroupSeqId", primaryShipGrp)));
-        if (UtilValidate.isNotEmpty(orderItemShipGroup.getString("vendorPartyId"))) {
-            partyIdFrom = orderItemShipGroup.getString("vendorPartyId");
-        } else if (UtilValidate.isNotEmpty(orderItemShipGroup.getString("facilityId"))) {
-            GenericValue facility = delegator.findOne("Facility", UtilMisc.toMap("facilityId", orderItemShipGroup.getString("facilityId")), false);
+        String partyIdFrom = null;
+        if (primaryOrderId != null) {
+            GenericValue orderItemShipGroup = EntityUtil.getFirst(delegator.findByAnd("OrderItemShipGroup", UtilMisc.toMap("orderId", primaryOrderId, "shipGroupSeqId", primaryShipGrp)));
+            if (UtilValidate.isNotEmpty(orderItemShipGroup.getString("vendorPartyId"))) {
+                partyIdFrom = orderItemShipGroup.getString("vendorPartyId");
+            } else if (UtilValidate.isNotEmpty(orderItemShipGroup.getString("facilityId"))) {
+                GenericValue facility = delegator.findOne("Facility", UtilMisc.toMap("facilityId", orderItemShipGroup.getString("facilityId")), false);
+                if (UtilValidate.isNotEmpty(facility.getString("ownerPartyId"))) {
+                    partyIdFrom = facility.getString("ownerPartyId");
+                }
+            }
+            if (UtilValidate.isEmpty(partyIdFrom)) {
+                GenericValue orderRoleShipFrom = EntityUtil.getFirst(delegator.findByAnd("OrderRole", UtilMisc.toMap("orderId", primaryOrderId, "roleTypeId", "SHIP_FROM_VENDOR")));
+                if (UtilValidate.isNotEmpty(orderRoleShipFrom)) {
+                    partyIdFrom = orderRoleShipFrom.getString("partyId");
+                } else {
+                    orderRoleShipFrom = EntityUtil.getFirst(delegator.findByAnd("OrderRole", UtilMisc.toMap("orderId", primaryOrderId, "roleTypeId", "BILL_FROM_VENDOR")));
+                    partyIdFrom = orderRoleShipFrom.getString("partyId");
+                }
+            }
+        } else if (this.facilityId != null) {
+            GenericValue facility = delegator.findOne("Facility", UtilMisc.toMap("facilityId", this.facilityId), false);
             if (UtilValidate.isNotEmpty(facility.getString("ownerPartyId"))) {
                 partyIdFrom = facility.getString("ownerPartyId");
             }
         }
-        if (UtilValidate.isEmpty(partyIdFrom)) {
-            GenericValue orderRoleShipFrom = EntityUtil.getFirst(delegator.findByAnd("OrderRole", UtilMisc.toMap("orderId", primaryOrderId, "roleTypeId", "SHIP_FROM_VENDOR")));
-            if (UtilValidate.isNotEmpty(orderRoleShipFrom)) {
-                partyIdFrom = orderRoleShipFrom.getString("partyId");
-            } else {
-                orderRoleShipFrom = EntityUtil.getFirst(delegator.findByAnd("OrderRole", UtilMisc.toMap("orderId", primaryOrderId, "roleTypeId", "BILL_FROM_VENDOR")));
-                partyIdFrom = orderRoleShipFrom.getString("partyId");
-            }
-        }
+
         newShipment.put("partyIdFrom", partyIdFrom);
         Debug.log("Creating new shipment with context: " + newShipment, module);
         Map<String, Object> newShipResp = this.getDispatcher().runSync("createShipment", newShipment);
