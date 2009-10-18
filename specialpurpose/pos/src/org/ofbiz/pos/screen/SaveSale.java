23b7bcecf4986d4790f8edddd9c1733d813c7f2b
Jacques Le Roux <jleroux@apache.org>
2009-10-18 20:32:05 +0000

    Following conventions on static variable names
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@826517 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/pos/src/org/ofbiz/pos/screen/SaveSale.java
+++ specialpurpose/pos/src/org/ofbiz/pos/screen/SaveSale.java
@@ -53,7 +53,7 @@ public class SaveSale extends XPage {
     //protected XButton m_saveAndPrint = null; //FIXME : button does not exist yet
     protected static PosTransaction m_trans = null;
     public static SimpleDateFormat sdf = new SimpleDateFormat(UtilProperties.getMessage(PosTransaction.resource,"PosDateTimeFormat",Locale.getDefault()));
-    private static boolean showKeyboardInSaveSale = UtilProperties.propertyValueEqualsIgnoreCase("parameters", "ShowKeyboardInSaveSale", "Y");
+    private static boolean SHOW_KEYBOARD_IN_SAVE_SALE = UtilProperties.propertyValueEqualsIgnoreCase("parameters", "SHOW_KEYBOARD_IN_SAVE_SALE", "Y");
 
     //TODO : make getter and setter for members (ie m_*) if needed (extern calls).  For that in Eclipse use Source/Generate Getters and setters
 
@@ -128,7 +128,7 @@ public class SaveSale extends XPage {
     }
 
     public synchronized void editSaleName() {
-        if (wasMouseClicked() && showKeyboardInSaveSale) {
+        if (wasMouseClicked() && SHOW_KEYBOARD_IN_SAVE_SALE) {
             try {
                 Keyboard keyboard = new Keyboard(m_pos);
                 keyboard.setText(m_saleName.getText());
