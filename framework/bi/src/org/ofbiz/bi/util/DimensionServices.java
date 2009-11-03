9c0b9e62e07c1cf8bbac386edbb24aab3c7a93fd
Jacques Le Roux <jleroux@apache.org>
2009-11-03 08:05:40 +0000

    A patch from Bob Morley "Resolve java warnings exposed in Eclipse : framework - bi" (https://issues.apache.org/jira/browse/OFBIZ-3103) - OFBIZ-3103
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@832331 13f79535-47bb-0310-9956-ffa450edef68

--- framework/bi/src/org/ofbiz/bi/util/DimensionServices.java
+++ framework/bi/src/org/ofbiz/bi/util/DimensionServices.java
@@ -18,34 +18,19 @@ under the License.
 */
 package org.ofbiz.bi.util;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import com.ibm.icu.util.Calendar;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
-
-import javolution.util.FastList;
 import javolution.util.FastMap;
 
-import org.ofbiz.base.util.Debug;
-import org.ofbiz.base.util.GeneralException;
-import org.ofbiz.base.util.UtilDateTime;
 import org.ofbiz.base.util.UtilGenerics;
 import org.ofbiz.base.util.UtilMisc;
-import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.Delegator;
-import org.ofbiz.entity.util.EntityUtil;
-
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.service.DispatchContext;
-import org.ofbiz.service.GenericServiceException;
-import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ServiceUtil;
 
 public class DimensionServices {
@@ -55,8 +40,6 @@ public class DimensionServices {
     public static Map<String, Object> getDimensionIdFromNaturalKey(DispatchContext ctx, Map<String, ? extends Object> context) {
         Map<String, Object> resultMap = ServiceUtil.returnSuccess();
         Delegator delegator = ctx.getDelegator();
-        LocalDispatcher dispatcher = ctx.getDispatcher();
-
         String dimensionEntityName = (String) context.get("dimensionEntityName");
         Map<String, ? extends Object> naturalKeyFields = UtilGenerics.cast(context.get("naturalKeyFields"));
         GenericValue lastDimensionValue = null;
@@ -74,8 +57,6 @@ public class DimensionServices {
 
     public static Map<String, Object> storeGenericDimension(DispatchContext ctx, Map<String, ? extends Object> context) {
         Delegator delegator = ctx.getDelegator();
-        LocalDispatcher dispatcher = ctx.getDispatcher();
-
         GenericValue dimensionValue = (GenericValue) context.get("dimensionValue");
         List<String> naturalKeyFields = UtilGenerics.checkList(context.get("naturalKeyFields"), String.class);
         String updateMode = (String) context.get("updateMode");
