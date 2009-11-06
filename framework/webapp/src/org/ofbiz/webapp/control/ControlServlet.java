60eb225add0aef5fd955dab79a83cce45363f094
Johannes Cornelius Bakker <hansbak@apache.org>
2009-11-06 03:55:56 +0000

    1. fix framework for using jsp tag in freemarker. 2. change order building birt component to after common component in framework/build.xml file 3. specific birt container before beanshell container in framework/base/config/ofbiz-containers.xml file 4. specific loading birt component after bi component in framework/component-load.xml 5. list all birt's jar files that have Eclipse Public License - v 1.0 in LICENSE file 6. change example report's path in mail form's tooltip. 7. delete four duplicated jar files.   - framework/birt/lib/axis.jar   - framework/birt/lib/commons-discovery-0.2.jar   - framework/birt/lib/axis-ant.jar   - framework/birt/lib/commons-cli-1.0.jar
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@833294 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
@@ -48,6 +48,9 @@ import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.webapp.stats.ServerHitBin;
 import org.ofbiz.webapp.stats.VisitHandler;
 
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.ext.servlet.ServletContextHashModel;
+
 /**
  * ControlServlet.java - Master servlet for the web application.
  */
@@ -191,6 +194,9 @@ public class ControlServlet extends HttpServlet {
         request.setAttribute("security", security);
 
         request.setAttribute("_REQUEST_HANDLER_", requestHandler);
+        
+        ServletContextHashModel ftlServletContext = new ServletContextHashModel(this, BeansWrapper.getDefaultInstance());
+        request.setAttribute("ftlServletContext", ftlServletContext);
 
         // setup some things that should always be there
         UtilHttp.setInitialRequestInfo(request);
