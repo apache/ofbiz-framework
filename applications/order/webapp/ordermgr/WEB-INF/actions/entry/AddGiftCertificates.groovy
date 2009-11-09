0664a962446d4c7f2f9413b1e0a2a927bc6ff24f
Ashish Vijaywargiya <ashish@apache.org>
2009-11-09 12:53:03 +0000

    Applied improved patch from jira issue OFBIZ-3165 - Add additional screen for gift card certificate buying functionality by CSR in Order manager application in add item page. Following points are covered in this patch:
    1) Removed hard-coded product ids for Gift certificates products from GiftCertificates.ftl and added new category type called "GIFT_CARD_CATEGORY" and related demo data, also added groovy script for fetching logic to address this issue.
    
    2) Also handled hard-coded surveyId in AddGiftCertificates.groovy
    
    3) Added user friendly messages if required data is not available .
    
    4) Added missing license header in GiftCertificates.ftl
    
    Thanks Divesh for the contribution.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@834042 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/order/webapp/ordermgr/WEB-INF/actions/entry/AddGiftCertificates.groovy
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import javolution.util.FastList;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.product.store.ProductStoreWorker;
+
+cart = session.getAttribute("shoppingCart");
+productStoreId = ProductStoreWorker.getProductStoreId(request);
+if (productStoreId == null) {
+    productStoreId = cart.getProductStoreId();
+}
+
+// Get Gift cards availbale in data
+
+giftCardCategories = delegator.findList("ProductCategory", EntityCondition.makeCondition("productCategoryTypeId", EntityOperator.EQUALS, "GIFT_CARD_CATEGORY"), null, null, null, false);
+giftCardProductList = FastList.newInstance();
+if (UtilValidate.isNotEmpty(giftCardCategories)) {
+    giftCardCategories.each { giftCardCategory -> 
+        giftCardCategoryMembers = delegator.findList("ProductCategoryMember", EntityCondition.makeCondition("productCategoryId", EntityOperator.EQUALS, giftCardCategory.productCategoryId), null, null, null, false);
+        if (UtilValidate.isNotEmpty(giftCardCategoryMembers)) {
+            giftCardCategoryMembers.each { giftCardCategoryMember -> 
+                giftCardProducts = delegator.findList("ProductAndPriceView", EntityCondition.makeCondition("productId", EntityOperator.EQUALS, giftCardCategoryMember.productId), null, null, null, false);
+                if (UtilValidate.isNotEmpty(giftCardProducts)) {
+                    giftCardProducts.each { giftCardProduct ->
+                        giftCardProductList.add(giftCardProduct);
+                    }
+                }
+            }
+        }
+    }
+}
+context.giftCardProductList = giftCardProductList;
+
+// Get Survey Id for Gift Certificates
+
+ProductStoreFinActSetting = delegator.findOne("ProductStoreFinActSetting", [productStoreId : productStoreId, finAccountTypeId : "GIFTCERT_ACCOUNT"], false);
+context.surveyId = ProductStoreFinActSetting.purchaseSurveyId;
+
