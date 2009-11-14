a5c93b079a0615ee89baf72656d9dbeab11ba78d
Scott Gray <lektran@apache.org>
2009-11-14 22:41:55 +0000

    Find Shipment search wasn't performing no condition finds
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@836279 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/webapp/facility/WEB-INF/actions/shipment/FindShipment.groovy
+++ applications/product/webapp/facility/WEB-INF/actions/shipment/FindShipment.groovy
@@ -90,13 +90,14 @@ if (maxDate && maxDate.length() > 8) {
     paramListBuffer.append(maxDate);
     findShipmentExprs.add(EntityCondition.makeCondition("estimatedShipDate", EntityOperator.LESS_THAN_EQUAL_TO, ObjectType.simpleTypeConvert(maxDate, "Timestamp", null, null)));
 }
-
 if ("Y".equals(lookupFlag)) {
     context.paramList = paramListBuffer.toString();
 
-    if (findShipmentExprs.size() > 0) {
         findOpts = new EntityFindOptions(true, EntityFindOptions.TYPE_SCROLL_INSENSITIVE, EntityFindOptions.CONCUR_READ_ONLY, true);
-        mainCond = EntityCondition.makeCondition(findShipmentExprs, EntityOperator.AND);
+        mainCond = null;
+        if (findShipmentExprs.size() > 0) {
+            mainCond = EntityCondition.makeCondition(findShipmentExprs, EntityOperator.AND);
+        }
         orderBy = ['-estimatedShipDate'];
 
         beganTransaction = false;
@@ -104,8 +105,8 @@ if ("Y".equals(lookupFlag)) {
             beganTransaction = TransactionUtil.begin();
 
             // get the indexes for the partial list
-            lowIndex = (((viewIndex - 1) * viewSize) + 1);
-            highIndex = viewIndex * viewSize;
+            lowIndex = viewIndex * viewSize + 1;
+            highIndex = (viewIndex + 1) * viewSize;
             findOpts.setMaxRows(highIndex);
             // using list iterator
             orli = delegator.find("Shipment", mainCond, null, null, orderBy, findOpts);
@@ -139,12 +140,6 @@ if ("Y".equals(lookupFlag)) {
             // only commit the transaction if we started one... this will throw an exception if it fails
             TransactionUtil.commit(beganTransaction);
         }
-    } else {
-        shipmentList = [] as ArrayList;
-        shipmentListSize = 0;
-        highIndex = 0;
-        lowIndex = 0;
-    }
 
     context.shipmentList = shipmentList;
     context.listSize = shipmentListSize;
