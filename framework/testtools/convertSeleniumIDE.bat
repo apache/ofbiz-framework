811b6ef17824946bc4e1d6b58aca434edd06c182
Jacques Le Roux <jleroux@apache.org>
2009-12-02 18:14:39 +0000

    A enhanced patch from Erwan de FERRIERES "Bug when using convert selenium html tests to xml" (https://issues.apache.org/jira/browse/OFBIZ-3282) - OFBIZ-3282 This line was causing the error : <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> What Erwan done :     * corrected the imports in the script     * used sed to remove this line, it is now creating a temp file without the line which is given to the converter.
    I have personally modified the convertSeleniumIDE.bat file but did not test (nor Linux version, I suppose it's ok). I guess this will mostly used on Linux Server and should work anyway.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@886233 13f79535-47bb-0310-9956-ffa450edef68

--- framework/testtools/convertSeleniumIDE.bat
+++ framework/testtools/convertSeleniumIDE.bat
@@ -1,7 +1,8 @@
 set OFBIZ_HOME=../..
 set CP=./build/lib/ofbiz-testtools.jar
-set CP=%CP%;./lib/httpclient-4.0-beta1.jar
 set CP=%CP%;./lib/selenium-java-client-driver.jar
+set CP=%CP%;%OFBIZ_HOME%/framework/base/build/lib/ofbiz-base.jar
+set CP=%CP%;%OFBIZ_HOME%/framework/base/lib/httpclient-4.0.jar
 set CP=%CP%;%OFBIZ_HOME%/framework/base/lib/jdom-1.1.jar
 set CP=%CP%;%OFBIZ_HOME%/framework/base/lib/scripting/jython-nooro.jar
 set CP=%CP%;%OFBIZ_HOME%/framework/base/lib/junit.jar
@@ -13,4 +14,10 @@ rem echo %CP%
 rem For Example:
 rem convertSeleniumIDE.bat <recorded_script> <converted_script>
 
-"%JAVA_HOME%/bin/java.exe" -cp %CP% org.ofbiz.testtools.seleniumxml.SeleniumIDEConverter %1 %2
+rem following commands needs MkTemp for Windows (http://gnuwin32.sourceforge.net/packages/mktemp.htm)
+rem and Sed for Windows (http://gnuwin32.sourceforge.net/packages/sed.htm) to work
+tempFile=`mktemp tempFileXXX.html` 
+sed '2d' %1 > $tempFile
+rem BEware the following command is provided for convenience and remains untestet
+"%JAVA_HOME%/bin/java.exe" -cp %CP% org.ofbiz.testtools.seleniumxml.SeleniumIDEConverter "%tempFile%" %2
+del "%tempFile%"
