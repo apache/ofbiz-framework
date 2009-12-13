004c3b9e81aadbb5456a4006c82b77d4c2971f66
Jacques Le Roux <jleroux@apache.org>
2009-12-13 11:41:26 +0000

    Fix possible NPEs
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@890050 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/pos/src/org/ofbiz/pos/PosTransaction.java
+++ specialpurpose/pos/src/org/ofbiz/pos/PosTransaction.java
@@ -433,7 +433,7 @@ public class PosTransaction implements Serializable {
             Delegator delegator = cart.getDelegator();
             GenericValue product = null;
             product = delegator.findByPrimaryKeyCache("Product", UtilMisc.toMap("productId", productId));
-            if ("AGGREGATED".equals(product.getString("productTypeId"))) {
+            if (UtilValidate.isNotEmpty(product) && "AGGREGATED".equals(product.getString("productTypeId"))) {
                 return true;
             }
         } catch (GenericEntityException e) {
@@ -492,7 +492,7 @@ public class PosTransaction implements Serializable {
             GenericValue product = null;
             ProductConfigWrapper pcw = null;
             product = delegator.findByPrimaryKeyCache("Product", UtilMisc.toMap("productId", productId));
-            if ("AGGREGATED".equals(product.getString("productTypeId"))) {
+            if (UtilValidate.isNotEmpty(product) && "AGGREGATED".equals(product.getString("productTypeId"))) {
                 // if it's an aggregated item, load the configwrapper and set to defaults
                 pcw = new ProductConfigWrapper(delegator, session.getDispatcher(), productId, null, null, null, null, null, null);
                 pcw.setDefaultConfig();
