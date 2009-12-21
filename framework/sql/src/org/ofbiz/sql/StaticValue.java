56e997fdabe572a18bf92f4f2cb25e770c8c876a
Adam Heath <doogie@apache.org>
2009-12-21 06:03:33 +0000

    StaticValues are constant.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@892703 13f79535-47bb-0310-9956-ffa450edef68

--- framework/sql/src/org/ofbiz/sql/StaticValue.java
+++ framework/sql/src/org/ofbiz/sql/StaticValue.java
@@ -18,6 +18,6 @@
  */
 package org.ofbiz.sql;
 
-public abstract class StaticValue extends Value {
+public abstract class StaticValue extends ConstantValue {
     public abstract String getDefaultName();
 }
