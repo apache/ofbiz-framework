28105e7c3dbf5032076ffb2837cd7dab052afd0f
Adam Heath <doogie@apache.org>
2009-12-21 06:05:21 +0000

    Bunch of sql stmts and other commands that provide for more coverage. Still not actually testing the output, other than printing.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@892706 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/sql/src/org/ofbiz/sql/test/GoodParseAll.sql
@@ -0,0 +1,49 @@
+SELECT
+	a.*,
+	b.* EXCLUDE (partyId, partyId),
+	c.* EXCLUDE (partyId),
+	d.roleTypeId,
+	SUM(a.partyId) AS fooBar,
+	FOO(a.partyId, 1) AS baz
+FROM
+	Party a LEFT JOIN Person b USING partyId
+	LEFT JOIN PartyGroup c ON b.partyId = c.partyId
+	JOIN PartyRole d ON c.partyId = d.partyId AND c.partyId = d.partyId
+RELATION TYPE one TITLE MainA Person USING partyId
+RELATION TITLE MainB Person USING partyId
+RELATION TYPE one Person USING partyId
+RELATION PartyGroup USING partyId
+WHERE
+	a.partyTypeId = 'PERSON'
+	AND
+	b.lastName LIKE ?lastName
+	AND
+	b.birthDate BETWEEN '1974-12-01' AND '1974-12-31'
+	OR
+	(
+		b.partyId IN ('1', '2', '3', '4')
+		AND
+		b.gender = 'M'
+	)
+	
+HAVING
+	b.firstName LIKE '%foo%'
+ORDER BY
+	LOWER(lastName), firstName, birthDate DESC
+OFFSET 5
+LIMIT 10
+;
+
+INSERT INTO Party (partyId, partyTypeId, statusId) VALUES
+	('a', 'PERSON', 'PARTY_DISABLED'),
+	(5, 'PARTY_GROUP', ?name);
+INSERT INTO Person (partyId, firstName) SELECT partyId, (partyId || '-auto') AS firstName FROM Party WHERE partyId IN ('a', 'b');
+UPDATE Person SET (lastName) = (('auto-' || partyId)) WHERE partyId IN ('a', 'b');
+UPDATE Person SET (lastName, height, width) = (('auto-' || partyId), 5, 7) WHERE partyId IN ('a', 'b');
+UPDATE Person SET lastName = ('auto-' || partyId), height = 6, width = 5, nickname = 'a' WHERE partyId IN ('a', 'b');
+DELETE FROM Person WHERE partyId IN ('a', 'b');
+DELETE FROM Party WHERE partyId IN ('a', 'b');
+CREATE VIEW viewOne AS SELECT a.* FROM Party a;
+/*
+UPDATE Person SET firstName = partyId || '-auto' WHERE partyId IN ('a', 'b');
+*/
