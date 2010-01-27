e08fe380d1b6e1e796171e9ac50bcd85f4b8b6d2
David E. Jones <jonesde@apache.org>
2010-01-27 08:48:55 +0000

    Change based on recent dev list discussion so that break-on-error acts more like an ignore error type of thing; when combined with require-new-transaction=true this will effectively result in no issues from the called service bubbling up to the calling service; with this change manually clearing out the error message and response code is no longer necessary
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@903567 13f79535-47bb-0310-9956-ffa450edef68

--- framework/minilang/src/org/ofbiz/minilang/method/callops/CallService.java
+++ framework/minilang/src/org/ofbiz/minilang/method/callops/CallService.java
@@ -247,16 +247,20 @@ public class CallService extends MethodOperation {
                 result = methodContext.getDispatcher().runSync(serviceName, inMap, timeout, requireNewTransaction);
             }
         } catch (GenericServiceException e) {
-            Debug.logError(e, module);
             String errMsg = "ERROR: Could not complete the " + simpleMethod.getShortDescription() + " process [problem invoking the [" + serviceName + "] service with the map named [" + inMapAcsr + "] containing [" + inMap + "]: " + e.getMessage() + "]";
-            if (methodContext.getMethodType() == MethodContext.EVENT) {
-                methodContext.putEnv(simpleMethod.getEventErrorMessageName(), errMsg);
-                methodContext.putEnv(simpleMethod.getEventResponseCodeName(), errorCode);
-            } else if (methodContext.getMethodType() == MethodContext.SERVICE) {
-                methodContext.putEnv(simpleMethod.getServiceErrorMessageName(), errMsg);
-                methodContext.putEnv(simpleMethod.getServiceResponseMessageName(), errorCode);
+            Debug.logError(e, errMsg, module);
+            if (breakOnError) {
+                if (methodContext.getMethodType() == MethodContext.EVENT) {
+                    methodContext.putEnv(simpleMethod.getEventErrorMessageName(), errMsg);
+                    methodContext.putEnv(simpleMethod.getEventResponseCodeName(), errorCode);
+                } else if (methodContext.getMethodType() == MethodContext.SERVICE) {
+                    methodContext.putEnv(simpleMethod.getServiceErrorMessageName(), errMsg);
+                    methodContext.putEnv(simpleMethod.getServiceResponseMessageName(), errorCode);
+                }
+                return false;
+            } else {
+                return true;
             }
-            return false;
         }
 
         if (resultsToMap.size() > 0) {
@@ -318,7 +322,7 @@ public class CallService extends MethodOperation {
         String messageSuffixStr = messageSuffix.getMessage(methodContext.getLoader(), methodContext);
 
         String errorMessage = ServiceUtil.makeErrorMessage(result, messagePrefixStr, messageSuffixStr, errorPrefixStr, errorSuffixStr);
-        if (UtilValidate.isNotEmpty(errorMessage)) {
+        if (UtilValidate.isNotEmpty(errorMessage) && breakOnError) {
             errorMessage += UtilProperties.getMessage(resource, "simpleMethod.error_show_service_name", UtilMisc.toMap("serviceName", serviceName, "methodName", simpleMethod.getMethodName()), locale);
             if (methodContext.getMethodType() == MethodContext.EVENT) {
                 methodContext.putEnv(simpleMethod.getEventErrorMessageName(), errorMessage);
@@ -350,15 +354,25 @@ public class CallService extends MethodOperation {
 
         // handle the result
         String responseCode = result.containsKey(ModelService.RESPONSE_MESSAGE) ? (String) result.get(ModelService.RESPONSE_MESSAGE) : successCode;
-        if (methodContext.getMethodType() == MethodContext.EVENT) {
-            methodContext.putEnv(simpleMethod.getEventResponseCodeName(), responseCode);
-        } else if (methodContext.getMethodType() == MethodContext.SERVICE) {
-            methodContext.putEnv(simpleMethod.getServiceResponseMessageName(), responseCode);
-        }
 
-        if (errorCode.equals(responseCode) && breakOnError) {
-            return false;
+        if (errorCode.equals(responseCode)) {
+            if (breakOnError) {
+                if (methodContext.getMethodType() == MethodContext.EVENT) {
+                    methodContext.putEnv(simpleMethod.getEventResponseCodeName(), responseCode);
+                } else if (methodContext.getMethodType() == MethodContext.SERVICE) {
+                    methodContext.putEnv(simpleMethod.getServiceResponseMessageName(), responseCode);
+                }
+                return false;
+            } else {
+                // avoid responseCode here since we are ignoring the error
+                return true;
+            }
         } else {
+            if (methodContext.getMethodType() == MethodContext.EVENT) {
+                methodContext.putEnv(simpleMethod.getEventResponseCodeName(), responseCode);
+            } else if (methodContext.getMethodType() == MethodContext.SERVICE) {
+                methodContext.putEnv(simpleMethod.getServiceResponseMessageName(), responseCode);
+            }
             return true;
         }
     }
