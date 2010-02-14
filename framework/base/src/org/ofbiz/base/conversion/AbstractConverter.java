62861a8ed5ca4fc5387bdca61d81e18c35b26519
Adam Heath <doogie@apache.org>
2010-02-14 22:39:03 +0000

    AbstractConverter now implement ConverterLoader, and thee register call was moved to loadConverters().
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@910105 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/conversion/AbstractConverter.java
+++ framework/base/src/org/ofbiz/base/conversion/AbstractConverter.java
@@ -24,13 +24,16 @@ import org.ofbiz.base.util.ObjectType;
  * and it implements the <code>canConvert</code>, <code>getSourceClass</code>,
  * and <code>getTargetClass</code> methods.
  */
-public abstract class AbstractConverter<S, T> implements Converter<S, T> {
+public abstract class AbstractConverter<S, T> implements Converter<S, T>, ConverterLoader {
     private final Class<? super S> sourceClass;
     private final Class<? super T> targetClass;
 
     protected AbstractConverter(Class<? super S> sourceClass, Class<? super T> targetClass) {
         this.sourceClass = sourceClass;
         this.targetClass = targetClass;
+    }
+
+    public void loadConverters() {
         Converters.registerConverter(this);
     }
 
