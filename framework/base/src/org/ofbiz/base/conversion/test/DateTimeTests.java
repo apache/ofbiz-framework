679342925fecc4dd7de3f402d768bb9ab0b1bb85
Adam Heath <doogie@apache.org>
2010-02-15 22:25:33 +0000

    Add more DateTime conversion tests.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@910357 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/conversion/test/DateTimeTests.java
+++ framework/base/src/org/ofbiz/base/conversion/test/DateTimeTests.java
@@ -41,11 +41,18 @@ public class DateTimeTests extends GenericTestCaseBase {
         assertConversion("DateToLong", new DateTimeConverters.DateToLong(), utilDate, currentTime);
         assertConversion("DateToSqlDate", new DateTimeConverters.DateToSqlDate(), utilDate, new java.sql.Date(currentTime));
         assertConversion("DateToString", new DateTimeConverters.DateToString(), utilDate, utilDate.toString());
+        //assertConversion("StringToDate", new DateTimeConverters.StringToDate(), utilDate.toString(), utilDate);
         assertConversion("DateToTimestamp", new DateTimeConverters.DateToTimestamp(), utilDate, new java.sql.Timestamp(currentTime));
         // Source class = java.sql.Date
         java.sql.Date sqlDate = new java.sql.Date(currentTime);
         assertConversion("SqlDateToDate", new DateTimeConverters.SqlDateToDate(), sqlDate, utilDate);
         assertConversion("SqlDateToString", new DateTimeConverters.SqlDateToString(), sqlDate, sqlDate.toString());
-        assertConversion("SqlDateToTimestamp", new DateTimeConverters.SqlDateToTimestamp(), sqlDate, new java.sql.Timestamp(currentTime));
+        //assertConversion("StringToSqlDate", new DateTimeConverters.StringToSqlDate(), sqlDate.toString(), sqlDate);
+        java.sql.Timestamp timestamp = new java.sql.Timestamp(currentTime);
+        assertConversion("SqlDateToTimestamp", new DateTimeConverters.SqlDateToTimestamp(), sqlDate, timestamp);
+        assertConversion("StringToTimestamp", new DateTimeConverters.StringToTimestamp(), timestamp.toString(), timestamp);
+        assertConversion("TimestampToDate", new DateTimeConverters.TimestampToDate(), timestamp, utilDate);
+        assertConversion("TimestampToSqlDate", new DateTimeConverters.TimestampToSqlDate(), timestamp, sqlDate);
+        //assertConversion("TimestampToString", new DateTimeConverters.TimestampToString(), timestamp, timestamp.toString());
     }
 }
