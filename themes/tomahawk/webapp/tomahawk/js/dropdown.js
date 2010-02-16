1608935bd274e2f6a6194c51e6561e9ad9e65097
Adam Heath <doogie@apache.org>
2010-02-16 19:08:04 +0000

    New theme, tomahawk, created by Bruno Busco and Erik Schuessler of Brainfood.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@910652 13f79535-47bb-0310-9956-ffa450edef68

---
+++ themes/tomahawk/webapp/tomahawk/js/dropdown.js
@@ -0,0 +1,49 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+var DropDownMenu = Class.create();
+DropDownMenu.prototype = {
+ initialize: function(menuElement) {
+    var menuTitle = $A(menuElement.getElementsByTagName("h2")).first();
+    menuElement.childElements().each(function(node){
+        // if there is a submenu
+        var submenu = $A(node.getElementsByTagName("ul")).first();
+        if(submenu != null){
+            // make sub-menu invisible
+            Element.extend(submenu).setStyle({display: 'none'});
+            // toggle the visibility of the submenu
+            if (menuTitle != null) {
+                menuTitle.onmouseover = menuTitle.onmouseout = function(){Element.toggle(submenu);};
+                menuTitle = null;
+            }
+            node.onmouseover = node.onmouseout = function(){Element.toggle(submenu);};
+        }
+    });
+  }
+};
+
+Event.observe(window, "load", function(){
+
+});
+
+document.observe('dom:loaded', function(){
+    var mainmenu = new DropDownMenu($('main-navigation'));
+    var appmenu = new DropDownMenu($('app-navigation'));
+});
+
