5411c45919c65928ecc89f2c780d9bc48e655622
Adam Heath <doogie@apache.org>
2010-02-21 01:27:32 +0000

    Move FlexibleStringExpander tests to a separate file.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@912272 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/test/BaseUnitTests.java
+++ framework/base/src/org/ofbiz/base/test/BaseUnitTests.java
@@ -112,37 +112,6 @@ public class BaseUnitTests extends TestCase {
         }
     }
 
-    public void testFlexibleStringExpander() {
-        FlexibleStringExpander fse = FlexibleStringExpander.getInstance(null);
-        assertTrue("null FlexibleStringExpander", fse.isEmpty());
-        String compare = "Hello World!";
-        fse = FlexibleStringExpander.getInstance(compare);
-        assertEquals("null context", compare, fse.expandString(null));
-        Map<String, Object> testMap = new HashMap<String, Object>();
-        testMap.put("var", "World");
-        fse = FlexibleStringExpander.getInstance("Hello ${var}!");
-        assertTrue("simple replacement", compare.equals(fse.expandString(testMap)));
-        testMap.put("nested", "Hello ${var}");
-        fse = FlexibleStringExpander.getInstance("${nested}!");
-        assertTrue("hidden (runtime) nested replacement", compare.equals(fse.expandString(testMap)));
-        fse = FlexibleStringExpander.getInstance("${'Hello ${var}'}!");
-        assertTrue("visible nested replacement", compare.equals(fse.expandString(testMap)));
-        fse = FlexibleStringExpander.getInstance("${bsh:return \"Hello \" + var + \"!\";}");
-        assertTrue("bsh: script", compare.equals(fse.expandString(testMap)));
-        fse = FlexibleStringExpander.getInstance("${groovy:return \"Hello \" + var + \"!\";}");
-        assertTrue("groovy: script", compare.equals(fse.expandString(testMap)));
-        testMap.put("testMap", testMap);
-        fse = FlexibleStringExpander.getInstance("Hello ${testMap.var}!");
-        assertTrue("UEL integration: Map", compare.equals(fse.expandString(testMap)));
-        List<String> testList = new ArrayList<String>();
-        testList.add("World");
-        testMap.put("testList", testList);
-        fse = FlexibleStringExpander.getInstance("Hello ${testList[0]}!");
-        assertTrue("UEL integration: List", compare.equals(fse.expandString(testMap)));
-        fse = FlexibleStringExpander.getInstance("This is an \\${escaped} expression");
-        assertTrue("Escaped expression", "This is an ${escaped} expression".equals(fse.expandString(testMap)));
-    }
-
     // These tests rely upon FlexibleStringExpander, so they
     // should follow the FlexibleStringExpander tests.
     public void testFlexibleMapAccessor() {
