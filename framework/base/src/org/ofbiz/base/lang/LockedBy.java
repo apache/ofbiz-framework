1efa599df597548cfa2a08f0790369191775d190
Adam Heath <doogie@apache.org>
2010-04-04 18:52:19 +0000

    Annotation used to document how some other variable or method is protected under some other locking mechanism.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@930732 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/base/src/org/ofbiz/base/lang/LockedBy.java
@@ -0,0 +1,20 @@
+package org.ofbiz.base.lang;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/** This annotation is meanted to be used to describe how some variable
+ * or method is protected by some other locking mechanism.  It's value
+ * is free-form, and can contain anything.  It will generally contain
+ * the name of an instance variable, including <code>this</code>.
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+public @interface LockedBy {
+    String value();
+}
