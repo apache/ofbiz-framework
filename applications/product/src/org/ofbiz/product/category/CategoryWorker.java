f2fe9ac1e4d2ee84d710e96ae719b13f018c63d7
Jacques Le Roux <jleroux@apache.org>
2010-04-14 10:06:00 +0000

    Fix a bug reported by Ravindra Mandre on user ML. After some time spent on this I don't clearly see why David's changes are not working. I guess only the second would be needed but as they are the same I preferred to revert both
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@933902 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/src/org/ofbiz/product/category/CategoryWorker.java
+++ applications/product/src/org/ofbiz/product/category/CategoryWorker.java
@@ -269,7 +269,12 @@ public class CategoryWorker {
                 // if cur category is in crumb, remove everything after it and return
                 int cindex = trail.lastIndexOf(currentCategoryId);
 
-                trail = trail.subList(0, cindex);
+                if (cindex < (trail.size() - 1)) {
+                    for (int i = trail.size() - 1; i > cindex; i--) {
+                        String deadCat = trail.remove(i);
+                        //if (Debug.infoOn()) Debug.logInfo("[CategoryWorker.setTrail] Removed after current category index: " + i + " catname: " + deadCat, module);
+                    }
+                }
                 return trail;
             } else {
                 // current category is not in the list, and no previous category was specified, go back to the beginning
@@ -293,7 +298,12 @@ public class CategoryWorker {
         } else {
             // remove all categories after the previous category, preparing for adding the current category
             int index = trail.indexOf(previousCategoryId);
-            trail = trail.subList(0, index);
+            if (index < (trail.size() - 1)) {
+                for (int i = trail.size() - 1; i > index; i--) {
+                    String deadCat = trail.remove(i);
+                    //if (Debug.infoOn()) Debug.logInfo("[CategoryWorker.setTrail] Removed after current category index: " + i + " catname: " + deadCat, module);
+                }
+            }
         }
 
         // add the current category to the end of the list
