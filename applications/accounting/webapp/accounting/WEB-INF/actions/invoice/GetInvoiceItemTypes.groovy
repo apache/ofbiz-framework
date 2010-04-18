911a066540af82c1f636b9e33dc66422ef0cdee4
Scott Gray <lektran@apache.org>
2010-04-18 20:20:54 +0000

    Added missing import.  Fixes bug report by Blas Rodriguez Somoza in OFBIZ-3712.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@935403 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/webapp/accounting/WEB-INF/actions/invoice/GetInvoiceItemTypes.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/invoice/GetInvoiceItemTypes.groovy
@@ -20,6 +20,7 @@
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityConditionBuilder;
 import org.ofbiz.entity.condition.EntityConditionList;
 import org.ofbiz.entity.condition.EntityExpr;
 import org.ofbiz.entity.condition.EntityOperator;
