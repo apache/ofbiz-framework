1e9824ab0cfe6c10cb53cd7a62828e4f6e48c734
Jacques Le Roux <jleroux@apache.org>
2010-05-31 10:33:52 +0000

    * Remove useless white spaces in macro (make harder handling links in javascript) * Implement target-parameter in case of layered lookup (was missing)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@949710 13f79535-47bb-0310-9956-ffa450edef68

--- framework/images/webapp/images/fieldlookup.js
+++ framework/images/webapp/images/fieldlookup.js
@@ -51,7 +51,7 @@ function call_fieldlookupLayer(target, viewName, lookupWidth, lookupHeight, look
         return lookup_error("Lookup can't be created, one of these variables is missing: target=" + target + " viewName=" + viewName);
     }
 
-    var fieldLookupPopup = new FieldLookupPopup(target, viewName, lookupWidth, lookupHeight, lookupPosition, fadeBackground);
+    var fieldLookupPopup = new FieldLookupPopup(target, viewName, lookupWidth, lookupHeight, lookupPosition, fadeBackground, arguments);
     fieldLookupPopup.showLookup();
     this.target = target;
 }
@@ -60,8 +60,8 @@ function call_fieldlookupLayer3(target, target2, viewName, lookupWidth, lookupHe
     if (isEmpty(target) || isEmpty(target2) || isEmpty(viewName)) {
         return lookup_error("Lookup can't be created, one of these variables is missing: target=" + target + " target2=" + target2 + " viewName=" + viewName);
     }
-        
-    var fieldLookupPopup = new FieldLookupPopup(target, viewName, lookupWidth, lookupHeight, lookupPosition, fadeBackground);
+
+    var fieldLookupPopup = new FieldLookupPopup(target, viewName, lookupWidth, lookupHeight, lookupPosition, fadeBackground, arguments);
     fieldLookupPopup.showLookup();
     this.target = target;
     this.target2 = target2;
@@ -202,7 +202,21 @@ var GLOBAL_LOOKUP_REF = new FieldLookupCounter;
 * position - normal (under the target field), center (layer is centered), etc. (see widget-form.xsd), default: top-left 
 */
 var FieldLookupPopup = Class.create({
-    initialize: function (target, viewName, lookupWidth, lookupHeight, position, fadeBackground) {
+    initialize: function (target, viewName, lookupWidth, lookupHeight, position, fadeBackground, args) {
+        if (args != null) {
+            var argString = "";
+            if (args.length > 7) {
+                for (var i = 7; i < args.length; i++) {
+                    if ((viewName.indexOf("?") == -1) && (i - 6) == 1) {
+                        sep = "?";
+                    } else {
+                        sep = "&";
+                    }
+                    argString += sep + "parm" + (i - 6) + "=" + args[i];
+                }
+            viewName += argString;
+            }
+        }
 
         //fade the background if the flag is set
         if (fadeBackground != "false") {
@@ -253,6 +267,7 @@ var FieldLookupPopup = Class.create({
 
     createElement: function () {
         var parent = this.parentTarget;
+        
         var that = this;
         
         //set global reference
@@ -330,7 +345,6 @@ var FieldLookupPopup = Class.create({
         var lookupCont = new Element('DIV', {
             id: "fieldLookupContent"
         });
-
         new Ajax.Request(this.viewName, {
             parameters: { presentation : "layer" },
             onSuccess: function (transport) {
@@ -583,6 +597,7 @@ function modifySubmitButton (lookupDiv) {
         
         //find the lookup form
         var forms = lookupDiv.getElementsByTagName('form');
+        
         var lookupForm = null;
         for (var i = 0; i < forms.length; i++) {
             if (! isEmpty(forms[i].getAttribute('id'))) {
@@ -593,12 +608,14 @@ function modifySubmitButton (lookupDiv) {
         if (lookupForm == null) {
             return;
         }
-        
+
         //set new form name and id
         oldFormName = lookupForm.getAttribute('name');
         lookupForm.setAttribute('name', 'form_' + GLOBAL_LOOKUP_REF.getReference(ACTIVATED_LOOKUP).globalRef);
         lookupForm.setAttribute('id', 'form_' + GLOBAL_LOOKUP_REF.getReference(ACTIVATED_LOOKUP).globalRef);
         lookupForm = $('form_' + GLOBAL_LOOKUP_REF.getReference(ACTIVATED_LOOKUP).globalRef);
+        
+        
         
         //set new links for lookups
         var newLookups = $A(lookupDiv.getElementsByClassName('field-lookup'));
@@ -620,7 +637,7 @@ function modifySubmitButton (lookupDiv) {
         //disable the form action
         var formAction = lookupForm.getAttribute('action');
         lookupForm.setAttribute('action', '');
-
+        
         //modify the form submit button
         for (var i = 0; i < lookupForm.length; i++) {
             var ele = lookupForm.elements[i];
@@ -744,7 +761,7 @@ function lookupAjaxRequest(request) {
     lookupContent = (GLOBAL_LOOKUP_REF.getReference(ACTIVATED_LOOKUP).contentRef);
     
     // get request arguments
-    var arg = request.substring(request.indexOf('?')+1,(request.length));
+    var arg = request.substring(request.indexOf('?')+1,(request.length));    
 
     new Ajax.Request(request, {
         method: 'post',
