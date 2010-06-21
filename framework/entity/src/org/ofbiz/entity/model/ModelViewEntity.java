df20c1d8803da1540f695d3a4ec094abb96846ac
Erwan de Ferrieres <erwan@apache.org>
2010-06-21 13:08:30 +0000

    Adding null condition when using entity-condition in views
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@956565 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/model/ModelViewEntity.java
+++ framework/entity/src/org/ofbiz/entity/model/ModelViewEntity.java
@@ -1300,6 +1300,8 @@ public class ModelViewEntity extends ModelEntity {
                             EntityOperator.OR,
                             EntityCondition.makeCondition(lhs, EntityOperator.EQUALS, null));
                 }
+            } else if ( value == null && (operator.equals(EntityOperator.EQUALS) || operator.equals(EntityOperator.NOT_EQUAL))) {
+                return EntityCondition.makeCondition(lhs, UtilGenerics.<EntityComparisonOperator<?,?>>cast(operator), null);
             } else {
                 if (ignoreCase) {
                     // use the stuff to upper case both sides
