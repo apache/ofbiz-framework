ad81f1d1f143dba8494ff60bdd5b7aaca4e1cb44
Scott Gray <lektran@apache.org>
2010-07-31 23:47:55 +0000

    Null check was incorrectly being used to determine if the directory existed
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@981104 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/component/ComponentConfig.java
+++ framework/base/src/org/ofbiz/base/component/ComponentConfig.java
@@ -350,7 +350,7 @@ public class ComponentConfig {
         this.rootLocation = rootLocation.replace('\\', '/');
 
         File rootLocationDir = new File(rootLocation);
-        if (rootLocationDir == null) {
+        if (!rootLocationDir.exists()) {
             throw new ComponentException("The given component root location is does not exist: " + rootLocation);
         }
         if (!rootLocationDir.isDirectory()) {
