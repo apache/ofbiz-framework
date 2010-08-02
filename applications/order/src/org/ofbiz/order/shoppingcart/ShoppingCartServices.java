be8ece3717ef441144017c052228daa499701233
Adam Heath <doogie@apache.org>
2010-08-02 22:33:00 +0000

    When loading an order into a cart, handle per-item tax adjustments correctly, by storing them in CartShipItemInfo, instead of the ShoppingCartItem.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@981699 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/src/org/ofbiz/order/shoppingcart/ShoppingCartServices.java
+++ applications/order/src/org/ofbiz/order/shoppingcart/ShoppingCartServices.java
@@ -539,6 +539,9 @@ public class ShoppingCartServices {
                 List<GenericValue> itemAdjustments = orh.getOrderItemAdjustments(item);
                 if (itemAdjustments != null) {
                     for(GenericValue itemAdjustment : itemAdjustments) {
+                        if ("SALES_TAX".equals(itemAdjustment.get("orderAdjustmentTypeId"))) {
+                            continue;
+                        }
                         cartItem.addAdjustment(itemAdjustment);
                     }
                 }
@@ -549,6 +552,7 @@ public class ShoppingCartServices {
                 int itemIndex = 0;
                 for (GenericValue item : orderItems) {
 
+                    List<GenericValue> orderItemAdjustments = orh.getOrderItemAdjustments(item);
                     // set the item's ship group info
                     List<GenericValue> shipGroupAssocs = orh.getOrderItemShipGroupAssocs(item);
                     for (int g = 0; g < shipGroupAssocs.size(); g++) {
@@ -567,6 +571,16 @@ public class ShoppingCartServices {
                         }
 
                         cart.setItemShipGroupQty(itemIndex, shipGroupQty, cartShipGroupIndex);
+
+                        List<GenericValue> shipGroupItemAdjustments = EntityUtil.filterByAnd(orderItemAdjustments, UtilMisc.toMap("shipGroupSeqId", cartShipGroupIndexStr));
+                        ShoppingCart.CartShipInfo csi = cart.getShipInfo(cartShipGroupIndex);
+                        List itemTaxAdj = csi.getShipItemInfo(cart.findCartItem(itemIndex)).itemTaxAdj;
+                        for(GenericValue shipGroupItemAdjustment : shipGroupItemAdjustments) {
+                            if ("SALES_TAX".equals(shipGroupItemAdjustment.get("orderAdjustmentTypeId"))) {
+                                itemTaxAdj.add(shipGroupItemAdjustment);
+                                continue;
+                            }
+                        }
                     }
                     itemIndex ++;
                 }
