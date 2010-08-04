fde2b450023a2da008168d7e2137f91f36c6b618
Jacopo Cappellato <jacopoc@apache.org>
2010-08-04 14:53:54 +0000

    Improved error handling: the java event was hiding the error returned by the service.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@982273 13f79535-47bb-0310-9956-ffa450edef68

--- applications/manufacturing/src/org/ofbiz/manufacturing/jobshopmgt/ProductionRunEvents.java
+++ applications/manufacturing/src/org/ofbiz/manufacturing/jobshopmgt/ProductionRunEvents.java
@@ -37,6 +37,7 @@ import org.ofbiz.entity.GenericPK;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.ServiceUtil;
 
 public class ProductionRunEvents {
 
@@ -88,6 +89,10 @@ public class ProductionRunEvents {
             inputMap.put("lotId", parameters.get("lotId"));
             inputMap.put("userLogin", userLogin);
             Map result = dispatcher.runSync("productionRunDeclareAndProduce", inputMap);
+            if (ServiceUtil.isError(result)) {
+                request.setAttribute("_ERROR_MESSAGE_", ServiceUtil.getErrorMessage(result));
+                return "error";
+            }
         } catch (GenericServiceException e) {
             String errMsg = "Error issuing materials: " + e.toString();
             Debug.logError(e, errMsg, module);
