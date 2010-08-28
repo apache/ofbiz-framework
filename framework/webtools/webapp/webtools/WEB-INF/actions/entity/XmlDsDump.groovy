35ddfcfd606e58a89ce1627421db54bd2a4e4ea6
Scott Gray <lektran@apache.org>
2010-08-28 11:21:33 +0000

    Minor groovy tidy up
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@990343 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webtools/webapp/webtools/WEB-INF/actions/entity/XmlDsDump.groovy
+++ framework/webtools/webapp/webtools/WEB-INF/actions/entity/XmlDsDump.groovy
@@ -212,8 +212,7 @@ if (tobrowser) {
                 }
 
                 curNumberWritten = 0;
-                valuesIter = values.iterator();
-                while ((value = valuesIter.next()) != null) {
+                while (value = values.next()) {
                     value.writeXmlText(writer, "");
                     numberWritten++;
                     curNumberWritten++;
@@ -269,8 +268,7 @@ if (tobrowser) {
                     isFirst = true;
                     writer = null;
                     fileSplitNumber = 1;
-                    valuesIter = values.iterator();
-                    while ((value = valuesIter.next()) != null) {
+                    while (value = values.next()) {
                         //Don't bother writing the file if there's nothing
                         //to put into it
                         if (isFirst) {
