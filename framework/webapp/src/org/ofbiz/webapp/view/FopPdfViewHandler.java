2a0225bcbce3143a87b2f2ac3ef2b72338d25295
Scott Gray <lektran@apache.org>
2010-08-28 09:47:15 +0000

    Set the response content type to what was provided in the view-map definition instead of hard-coding application/pdf, allows for any type of document supported by fop to be rendered by the view handler.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@990335 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/view/FopPdfViewHandler.java
+++ framework/webapp/src/org/ofbiz/webapp/view/FopPdfViewHandler.java
@@ -66,7 +66,7 @@ public class FopPdfViewHandler extends JPublishViewHandler {
         }
 
         // set the content type and length
-        response.setContentType("application/pdf");
+        response.setContentType(contentType);
         response.setContentLength(out.size());
 
         // write to the browser
