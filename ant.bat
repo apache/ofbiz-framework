ea8732dc35bc5c80d4be0e8e8efc93e3c21e44a9
Adam Heath <doogie@apache.org>
2010-09-15 17:57:01 +0000

    Add framework/base/lib/ant to ant's library path.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@997424 13f79535-47bb-0310-9956-ffa450edef68

--- ant.bat
+++ ant.bat
@@ -19,5 +19,5 @@ REM # under the License.
 REM #####################################################################
 ECHO ON
 
-"%JAVA_HOME%\bin\java" -jar framework/base/lib/ant-launcher-1.7.1.jar %1 %2 %3 %4 %5 %6
+"%JAVA_HOME%\bin\java" -jar framework/base/lib/ant-launcher-1.7.1.jar -lib framework/base/lib/ant %1 %2 %3 %4 %5 %6
 
