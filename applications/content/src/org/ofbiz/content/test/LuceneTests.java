7699fa9a32857dc2ab476d6639a56592bbcff553
Erwan de Ferrieres <erwan@apache.org>
2010-12-19 15:57:13 +0000

    Adding test and test data for lucene search
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1050883 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/content/src/org/ofbiz/content/test/LuceneTests.java
@@ -0,0 +1,88 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+package org.ofbiz.content.test;
+
+import javolution.util.FastMap;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.content.search.SearchWorker;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.service.testtools.OFBizTestCase;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public class LuceneTests extends OFBizTestCase {
+
+    protected GenericValue userLogin = null;
+
+    public LuceneTests(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        userLogin = delegator.findByPrimaryKey("UserLogin", UtilMisc.toMap("userLoginId", "system"));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        //destruction index
+    }
+
+    public void testCreateIndex() throws Exception {
+        Map ctx = FastMap.newInstance();
+        ctx.put("contentId", "WebStoreCONTENT");
+        ctx.put("userLogin", userLogin);
+        Map resp = dispatcher.runSync("indexTree", ctx);
+
+        assertEquals(7, resp.get("goodIndexCount"));
+
+        List<String> badIndexList = (List<String>) resp.get("badIndexList");
+        assertEquals(8, badIndexList.size());
+    }
+
+    public void testSearchTermHand() throws Exception {
+        BooleanQuery combQuery = new BooleanQuery();
+        String queryLine = "hand";
+        IndexReader reader = IndexReader.open(FSDirectory.open(new File(SearchWorker.getIndexPath(null))), true);
+
+        Searcher searcher = new IndexSearcher(reader);
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+
+        QueryParser parser = new QueryParser(Version.LUCENE_30, "content", analyzer);
+        Query query = parser.parse(queryLine);
+        combQuery.add(query, BooleanClause.Occur.MUST);
+
+        TopScoreDocCollector collector = TopScoreDocCollector.create(10, false);
+        searcher.search(combQuery, collector);
+
+        assertEquals("Only 1 result expected from the testdata", 1, collector.getTotalHits());
+    }
+}
