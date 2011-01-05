429825a4406cc758d11b4c94a843d212c1d1865b
Jacopo Cappellato <jacopoc@apache.org>
2011-01-05 11:05:13 +0000

    hot-deploy component are now always loaded in alphanumeric order; before this change they were loaded randomly (in a platform dependedent way).
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1055411 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/container/ComponentContainer.java
+++ framework/base/src/org/ofbiz/base/container/ComponentContainer.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 
 import org.ofbiz.base.component.AlreadyLoadedException;
@@ -195,7 +196,9 @@ public class ComponentContainer implements Container {
                     Debug.logError(e, "Unable to load components from URL: " + configUrl.toExternalForm(), module);
                 }
             } else {
-                for (String sub: parentPath.list()) {
+                String[] fileNames = parentPath.list();
+                Arrays.sort(fileNames);
+                for (String sub: fileNames) {
                     try {
                         File componentPath = FileUtil.getFile(parentPath.getCanonicalPath() + "/" + sub);
                         if (componentPath.isDirectory() && !sub.equals("CVS") && !sub.equals(".svn")) {
