8470aeee36788655bc976f492783c27287dbfcad
Jacques Le Roux <jleroux@apache.org>
2011-01-19 10:28:01 +0000

    A slightly modified patch from Sascha "In Content Manager's CMS sub-module CMSContentFind page doesn't show the content list." (https://issues.apache.org/jira/browse/OFBIZ-2746) - OFBIZ-2746
    Sascha: There are some more issues in the groovy file
    In entity field [ContentAssoc.fromDate] set the value passed in [java.lang.String] is not compatible with the Java type of the field [java.sql.Timestamp]
    I'm not quite sure why this is going wrong. Maybe we have to explicitly create an Timestamp here.
    DataResourceWorker.getDataResourceTextCache(dataResource, "text/html", Locale.getDefault(), templateRoot, delegator);
    should be replaced with
    DataResourceWorker.getDataResourceText(dataResource, "text/html", Locale.getDefault(), templateRoot, delegator, true);
    
    JLR: this patch allows now to create a content from CMSContentFind but I'm not quire sure all is resolved. I jave also set the fromDate field in the calling screen as Timestamp type, not sure it's needed but it can't hurt. Nevertheless I think there are still some issues. But at least now it does not break
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1060744 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/webapp/content/WEB-INF/actions/cms/CmsEditAddPrep.groovy
+++ applications/content/webapp/content/WEB-INF/actions/cms/CmsEditAddPrep.groovy
@@ -22,6 +22,7 @@ import org.ofbiz.minilang.SimpleMapProcessor
 import org.ofbiz.content.ContentManagementWorker
 import org.ofbiz.content.content.ContentWorker
 import org.ofbiz.content.data.DataResourceWorker
+import org.ofbiz.webapp.ftl.FreeMarkerViewHandler
 
 userLogin = session.getAttribute("userLogin");
 contentAssocDataResourceViewFrom = delegator.makeValue("ContentAssocDataResourceViewFrom");
@@ -71,8 +72,8 @@ if (dataResourceId) {
     SimpleMapProcessor.runSimpleMapProcessor("component://content/script/org/ofbiz/content/ContentManagementMapProcessors.xml", "dataResourceOut", dataResource, contentAssocDataResourceViewFrom, new ArrayList(), Locale.getDefault());
     templateRoot = [:];
     FreeMarkerViewHandler.prepOfbizRoot(templateRoot, request, response);
-    txt = DataResourceWorker.getDataResourceTextCache(dataResource, "text/html", Locale.getDefault(), templateRoot, delegator);
-    
+    txt = DataResourceWorker.getDataResourceText(dataResource, "text/html", Locale.getDefault(), templateRoot, delegator, true);
+
     if (txt) {
         textData = UtilFormatOut.encodeXmlValue(txt);
     }
