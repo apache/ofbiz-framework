f729c27c2d93fab315ac6b88a6363babf3114fc2
Marco Risaliti <mrisaliti@apache.org>
2011-02-28 19:53:15 +0000

    Remove an unused import in GenericDelegator (OFBIZ-4102)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1075507 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/GenericDelegator.java
+++ framework/entity/src/org/ofbiz/entity/GenericDelegator.java
@@ -27,7 +27,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
