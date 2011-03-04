d112488267b4de577778c85c797fa1e17ea31851
Marco Risaliti <mrisaliti@apache.org>
2011-03-04 23:11:42 +0000

    Remove some warning in GenericMapSet (OFBIZ-4102)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1078203 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/collections/GenericMapSet.java
+++ framework/base/src/org/ofbiz/base/util/collections/GenericMapSet.java
@@ -28,8 +28,8 @@ public abstract class GenericMapSet<K, V, M extends Map<K, V>, I> extends Generi
 
     @Override
     public final boolean equals(Object o) {
-        if (!(o instanceof Set)) return false;
-        Set other = (Set) o;
+        if (!(o instanceof Set<?>)) return false;
+        Set<?> other = (Set<?>) o;
         if (source.size() != other.size()) return false;
         for (I item: this) {
             if (!other.contains(item)) return false;
