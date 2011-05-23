d0f86ff34c698970f9873cad0556818b4727c9b1
Marco Risaliti <mrisaliti@apache.org>
2011-05-23 21:01:53 +0000

    Removed some javadoc warning (wrong param/return) into webtools component no functional changes
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1126748 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webtools/src/org/ofbiz/webtools/WebToolsServices.java
+++ framework/webtools/src/org/ofbiz/webtools/WebToolsServices.java
@@ -872,9 +872,9 @@ public class WebToolsServices {
 
     /** Performs an entity maintenance security check. Returns hasPermission=true
      * if the user has the ENTITY_MAINT permission.
-     * @param dctx
-     * @param context
-     * @return
+     * @param dctx the dispatch context
+     * @param context the context
+     * @return return the result of the service execution
      */
     public static Map<String, Object> entityMaintPermCheck(DispatchContext dctx, Map<String, ? extends Object> context) {
         GenericValue userLogin = (GenericValue) context.get("userLogin");
