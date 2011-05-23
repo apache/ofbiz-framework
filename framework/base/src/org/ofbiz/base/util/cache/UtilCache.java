85e097e217e2b994d97b97799e33d028c1cc4759
Jacques Le Roux <jleroux@apache.org>
2011-05-23 09:10:57 +0000

    Correct indentation for r1126379
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1126399 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/cache/UtilCache.java
+++ framework/base/src/org/ofbiz/base/util/cache/UtilCache.java
@@ -190,12 +190,11 @@ public class UtilCache<K, V> implements Serializable, EvictionListener<Object, C
     public static String getPropertyParam(ResourceBundle res, String[] propNames, String parameter) {
         try {
             for (String propName: propNames) {
-            	if(res.containsKey(propName+ '.' + parameter))
-            	{
-	                try {
-	                    return res.getString(propName + '.' + parameter);
-	                } catch (MissingResourceException e) {}
-            	}
+        	if(res.containsKey(propName+ '.' + parameter)) {
+        	    try {
+        		return res.getString(propName + '.' + parameter);
+        	    } catch (MissingResourceException e) {}
+        	}
             }
             // don't need this, just return null
             //if (value == null) {
