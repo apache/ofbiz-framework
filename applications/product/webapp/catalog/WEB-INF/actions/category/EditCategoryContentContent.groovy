66d25fea524ce92017e3a840673202e337c01ffc
Hans Bakker <hansbak@apache.org>
2011-05-24 07:30:42 +0000

    To be consistsent with product and the storage of alternative url, this function replaces the 'link' button on the category. We also improved the locale lookup with a search function
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1126904 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/product/webapp/catalog/WEB-INF/actions/category/EditCategoryContentContent.groovy
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.util.*;
+import org.ofbiz.base.util.*;
+import java.sql.Timestamp;
+
+uiLabelMap = UtilProperties.getResourceBundleMap("ProductUiLabels", locale);
+
+contentId = parameters.contentId;
+if (!contentId) {
+    contentId = null;
+}
+
+prodCatContentTypeId = parameters.prodCatContentTypeId;
+context.contentFormName = "EditCategoryContentSimpleText";
+context.contentFormTitle = "${uiLabelMap.ProductUpdateSimpleTextContentCategory}";
+
+if (("PAGE_TITLE".equals(prodCatContentTypeId))||("META_KEYWORD".equals(prodCatContentTypeId))||("META_DESCRIPTION".equals(prodCatContentTypeId))) {
+    context.contentFormName = "EditCategoryContentSEO";
+    context.contentFormTitle = "${uiLabelMap.ProductUpdateSEOContentCategory}";
+}
+if ("RELATED_URL".equals(prodCatContentTypeId)) {
+    contentList = delegator.findByAnd("ContentDataResourceView", UtilMisc.toMap("contentId", contentId));
+    if (contentList) {
+        context.contentId = contentList.get(0).contentId;
+        context.dataResourceId = contentList.get(0).dataResourceId;
+        context.title = contentList.get(0).drDataResourceName;
+        context.description = contentList.get(0).description;
+        context.url = contentList.get(0).drObjectInfo;
+        context.localeString = contentList.get(0).localeString;
+    }
+    context.contentFormName = "EditCategoryContentRelatedUrl";
+    context.contentFormTitle = "${uiLabelMap.ProductUpdateRelatedURLContentCategory}";
+}
