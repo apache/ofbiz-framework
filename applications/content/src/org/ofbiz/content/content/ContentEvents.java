602656a4b2543144aaa2b5133192751c081507f8
Johannes Cornelius Bakker <hansbak@apache.org>
2011-06-03 03:05:23 +0000

    add a button to content to (re) generate the keywords for all content. The whole keyword implementation was done by Antwebsystems employee Tom
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1130890 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/src/org/ofbiz/content/content/ContentEvents.java
+++ applications/content/src/org/ofbiz/content/content/ContentEvents.java
@@ -18,6 +18,23 @@
  *******************************************************************************/
 package org.ofbiz.content.content;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilHttp;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.transaction.TransactionUtil;
+import org.ofbiz.entity.util.EntityListIterator;
+import org.ofbiz.content.content.ContentKeywordIndex;
+import org.ofbiz.security.Security;
+
 
 /**
  * ContentEvents Class
@@ -25,6 +42,113 @@ package org.ofbiz.content.content;
 public class ContentEvents {
 
     public static final String module = ContentEvents.class.getName();
+    public static final String resource = "ContentErrorUiLabels";
 
+    /**
+     * Updates/adds keywords for all contents
+     *
+     * @param request HTTPRequest object for the current request
+     * @param response HTTPResponse object for the current request
+     * @return String specifying the exit status of this event
+     */
+    public static String updateAllContentKeywords(HttpServletRequest request, HttpServletResponse response) {
+        //String errMsg = "";
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        Security security = (Security) request.getAttribute("security");
 
+        String updateMode = "CREATE";
+        String errMsg=null;
+
+        String doAll = request.getParameter("doAll");
+
+        // check permissions before moving on...
+        if (!security.hasEntityPermission("CONTENTMGR", "_" + updateMode, request.getSession())) {
+            Map<String, String> messageMap = UtilMisc.toMap("updateMode", updateMode);
+            errMsg = UtilProperties.getMessage(resource,"contentevents.not_sufficient_permissions", messageMap, UtilHttp.getLocale(request));
+            request.setAttribute("_ERROR_MESSAGE_", errMsg);
+            return "error";
+        }
+
+        EntityListIterator entityListIterator = null;
+        int numConts = 0;
+        int errConts = 0;
+
+        boolean beganTx = false;
+        try {
+            // begin the transaction
+            beganTx = TransactionUtil.begin(7200);
+            try {
+                if (Debug.infoOn()) {
+                    long count = delegator.findCountByCondition("Content", null, null, null);
+                    Debug.logInfo("========== Found " + count + " contents to index ==========", module);
+                }
+                entityListIterator = delegator.find("Content", null, null, null, null, null);
+            } catch (GenericEntityException gee) {
+                Debug.logWarning(gee, gee.getMessage(), module);
+                Map<String, String> messageMap = UtilMisc.toMap("gee", gee.toString());
+                errMsg = UtilProperties.getMessage(resource,"contentevents.error_getting_content_list", messageMap, UtilHttp.getLocale(request));
+                request.setAttribute("_ERROR_MESSAGE_", errMsg);
+                throw gee;
+            }
+
+            GenericValue content;
+            while ((content = entityListIterator.next()) != null) {
+                try {
+                    ContentKeywordIndex.indexKeywords(content, "Y".equals(doAll));
+                } catch (GenericEntityException e) {
+                    //request.setAttribute("_ERROR_MESSAGE_", errMsg);
+                    Debug.logWarning("[ContentEvents.updateAllContentKeywords] Could not create content-keyword (write error); message: " + e.getMessage(), module);
+                    errConts++;
+                }
+                numConts++;
+                if (numConts % 500 == 0) {
+                    Debug.logInfo("Keywords indexed for " + numConts + " so far", module);
+                }
+            }
+        } catch (GenericEntityException e) {
+            try {
+                TransactionUtil.rollback(beganTx, e.getMessage(), e);
+            } catch (Exception e1) {
+                Debug.logError(e1, module);
+            }
+            return "error";
+        } catch (Throwable t) {
+            Debug.logError(t, module);
+            request.setAttribute("_ERROR_MESSAGE_", t.getMessage());
+            try {
+                TransactionUtil.rollback(beganTx, t.getMessage(), t);
+            } catch (Exception e2) {
+                Debug.logError(e2, module);
+            }
+            return "error";
+        } finally {
+            if (entityListIterator != null) {
+                try {
+                    entityListIterator.close();
+                } catch (GenericEntityException gee) {
+                    Debug.logError(gee, "Error closing EntityListIterator when indexing content keywords.", module);
+                }
+            }
+
+            // commit the transaction
+            try {
+                TransactionUtil.commit(beganTx);
+            } catch (Exception e) {
+                Debug.logError(e, module);
+            }
+        }
+
+        if (errConts == 0) {
+            Map<String, String> messageMap = UtilMisc.toMap("numConts", Integer.toString(numConts));
+            errMsg = UtilProperties.getMessage(resource,"contentevents.keyword_creation_complete_for_contents", messageMap, UtilHttp.getLocale(request));
+            request.setAttribute("_EVENT_MESSAGE_", errMsg);
+            return "success";
+        } else {
+            Map<String, String> messageMap = UtilMisc.toMap("numConts", Integer.toString(numConts));
+            messageMap.put("errConts", Integer.toString(errConts));
+            errMsg = UtilProperties.getMessage(resource,"contentevents.keyword_creation_complete_for_contents_with_errors", messageMap, UtilHttp.getLocale(request));
+            request.setAttribute("_ERROR_MESSAGE_", errMsg);
+            return "error";
+        }
+    }
 }
