399b52a033d9cf7f873de19e3dc9400b76440aa9
Jacques Le Roux <jleroux@apache.org>
2011-10-02 15:59:12 +0000

    Closes "The time part (cursors) of the Date Time picker is not internationalized" https://issues.apache.org/jira/browse/OFBIZ-4015
    Fixes the problem by:
    * Updating datetimepicker from 0.9.3 to 0.9.6 (where a localization folder exists)
    * Use the fantastic createJsLanguageFileMapping service Sascha introduced (I just removed the confusing wrong comment about JsLanguageFilesMapping.ftl being generated and added a block for datetimepicker, also in JsLanguageFileMappingCreator.java). A very good dynamic+static tool, kudos Sascha!
    
    Should work also in eCommerce though I did not found any uses
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1178199 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-de.js
@@ -0,0 +1,17 @@
+/* German translation for the jQuery Timepicker Addon */
+/* Written by Marvin */
+(function($) {
+	$.timepicker.regional['de'] = {
+		timeOnlyTitle: 'Zeit Wählen',
+		timeText: 'Zeit',
+		hourText: 'Stunde',
+		minuteText: 'Minute',
+		secondText: 'Sekunde',
+		timezoneText: 'Zeitzone',
+		currentText: 'Jetzt',
+		closeText: 'Fertig',
+		timeFormat: 'hh:mm tt',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['de']);
+})(jQuery);
---
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-ru.js
@@ -0,0 +1,17 @@
+/* Russian translation for the jQuery Timepicker Addon */
+/* Written by Trent Richardson */
+(function($) {
+	$.timepicker.regional['ru'] = {
+		timeOnlyTitle: 'Выберите время',
+		timeText: 'Время',
+		hourText: 'Часы',
+		minuteText: 'Минуты',
+		secondText: 'Секунды',
+		timezoneText: 'Время зоны',
+		currentText: 'Теперь',
+		closeText: 'Закрыть',
+		timeFormat: 'hh:mm tt',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['ru']);
+})(jQuery);
