2a00b12b5c4af2821872d9c2839760de1acddfcc
Jacques Le Roux <jleroux@apache.org>
2011-11-12 13:17:57 +0000

    The ZH patch of Leon's patch "add datetimepicker language file for zh-CN, zh-HK, zh-TW, and an en one in order to avoid "jquery-ui-timepicker-en.js" not found error" https://issues.apache.org/jira/browse/OFBIZ-4555
    1. add datetimepicker language file to system: jquery-ui-timepicker-zh-CN.js, jquery-ui-timepicker-zh-HK.js and jquery-ui-timepicker-zh-TW.js.
    
    jleroux: This time all is looking code. I viewed Chinese characters in WinMerge, but no in Scite, so I used using UTF8 encoding there and saved. Then was looking good in Toirtoise svn merge editor, and still good also in WinMerge (which certainly handled automatically the conversion 1st time, pfew... ;o)
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1201257 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-zh-CN.js
@@ -0,0 +1,17 @@
+/* Chinese translation for the jQuery Timepicker Addon */
+/* Written by Leon Sun */
+(function($) {
+    $.timepicker.regional['zh-CN'] = {
+        timeOnlyTitle: '选择时间',
+        timeText: '时间',
+        hourText: '时',
+        minuteText: '分',
+        secondText: '秒',
+        timezoneText: '时区',
+        currentText: '现在',
+        closeText: '完成',
+        timeFormat: 'hh:mm',
+        ampm: false
+    };
+    $.timepicker.setDefaults($.timepicker.regional['zh-CN']);
+})(jQuery);
---
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-zh-HK.js
@@ -0,0 +1,17 @@
+/* Chinese translation for the jQuery Timepicker Addon */
+/* Written by Leon Sun */
+(function($) {
+    $.timepicker.regional['zh-HK'] = {
+        timeOnlyTitle: '選擇時間',
+        timeText: '時間',
+        hourText: '時',
+        minuteText: '分',
+        secondText: '秒',
+        timezoneText: '時區',
+        currentText: '現在',
+        closeText: '完成',
+        timeFormat: 'hh:mm',
+        ampm: false
+    };
+    $.timepicker.setDefaults($.timepicker.regional['zh-HK']);
+})(jQuery);
