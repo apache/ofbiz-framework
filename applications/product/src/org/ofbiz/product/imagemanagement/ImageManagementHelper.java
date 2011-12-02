121762846310ec85fbe9fa3a255f1c80a25b3614
Johannes Cornelius Bakker <hansbak@apache.org>
2011-12-02 03:40:59 +0000

    files missing on last commit 1209351
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1209355 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/product/src/org/ofbiz/product/imagemanagement/ImageManagementHelper.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+
+package org.ofbiz.product.imagemanagement;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+
+public class ImageManagementHelper {
+
+    static String module = ImageManagementHelper.class.getName();
+
+    public static String getInternalImageUrl(HttpServletRequest request, String productId) {
+        String internalImageUrl = null;
+        try {
+            Delegator delegator = (Delegator) request.getAttribute("delegator");
+            List<GenericValue> defaultImageList = delegator.findByAnd("ProductContentAndInfo", UtilMisc.toMap("productId", productId, "productContentTypeId", "DEFAULT_IMAGE", "statusId", "IM_APPROVED", "drIsPublic", "N"), UtilMisc.toList("sequenceNum"));
+            if (UtilValidate.isNotEmpty(defaultImageList)) {
+                GenericValue productContent = EntityUtil.getFirst(defaultImageList);
+                if (UtilValidate.isNotEmpty(productContent.get("drObjectInfo"))) {
+                    internalImageUrl = (String) productContent.get("drObjectInfo");
+                }
+            } else {
+                List<GenericValue> productContentList = delegator.findByAnd("ProductContentAndInfo", UtilMisc.toMap("productId", productId, "productContentTypeId", "IMAGE", "statusId", "IM_APPROVED", "drIsPublic", "N"), UtilMisc.toList("sequenceNum"));
+                if (UtilValidate.isNotEmpty(productContentList)) {
+                    GenericValue productContent = EntityUtil.getFirst(productContentList);
+                    if (UtilValidate.isNotEmpty(productContent.get("drObjectInfo"))) {
+                        internalImageUrl = (String) productContent.get("drObjectInfo");
+                    }
+                }
+            }
+        } catch (GenericEntityException e) {
+            Debug.logError(e, "Cannot get internal image url", module);
+        }
+        return internalImageUrl;
+    }
+}
