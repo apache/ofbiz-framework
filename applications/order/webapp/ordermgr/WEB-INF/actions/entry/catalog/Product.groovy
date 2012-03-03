122e73500f9afb13e9565b7d9a0907c8d5caf5cc
Jacques Le Roux <jleroux@apache.org>
2012-03-03 11:01:19 +0000

    A patch from Mike (mz4wheeler) "Product NOTFOUND causes java.lang.NullPointerException, ugly screen, and excessive logging" https://issues.apache.org/jira/browse/OFBIZ-4599
    Fixes a possible NPE
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1296597 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/webapp/ordermgr/WEB-INF/actions/entry/catalog/Product.groovy
+++ applications/order/webapp/ordermgr/WEB-INF/actions/entry/catalog/Product.groovy
@@ -43,14 +43,15 @@ metaKeywords = null;
 // get the product entity
 if (productId) {
     product = delegator.findByPrimaryKeyCache("Product", [productId : productId]);
-
-    // first make sure this isn't a virtual-variant that has an associated virtual product, if it does show that instead of the variant
-    if("Y".equals(product.isVirtual) && "Y".equals(product.isVariant)){
-        virtualVariantProductAssocs = delegator.findByAndCache("ProductAssoc", ["productId": productId, "productAssocTypeId": "ALTERNATIVE_PACKAGE"], ["-fromDate"]);
-        virtualVariantProductAssocs = EntityUtil.filterByDate(virtualVariantProductAssocs);
-        if (virtualVariantProductAssocs) {
-            productAssoc = EntityUtil.getFirst(virtualVariantProductAssocs);
-            product = productAssoc.getRelatedOneCache("AssocProduct");
+    if (product) {
+        // first make sure this isn't a virtual-variant that has an associated virtual product, if it does show that instead of the variant
+        if("Y".equals(product.isVirtual) && "Y".equals(product.isVariant)){
+            virtualVariantProductAssocs = delegator.findByAndCache("ProductAssoc", ["productId": productId, "productAssocTypeId": "ALTERNATIVE_PACKAGE"], ["-fromDate"]);
+            virtualVariantProductAssocs = EntityUtil.filterByDate(virtualVariantProductAssocs);
+            if (virtualVariantProductAssocs) {
+                productAssoc = EntityUtil.getFirst(virtualVariantProductAssocs);
+                product = productAssoc.getRelatedOneCache("AssocProduct");
+            }
         }
     }
     
