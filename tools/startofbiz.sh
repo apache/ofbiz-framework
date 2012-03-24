ade899861b94cd67678eef67311d9429b114a2f2
Johannes Cornelius Bakker <hansbak@apache.org>
2012-03-24 23:46:04 +0000

    reverting revision 1304679 and 1304687, however leaving the original startofbiz.sh and stopofbiz.sh in the root deirectory, this in order not to block furher development
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1304965 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/startofbiz.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#####################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#####################################################################
+
+# shutdown settings
+ADMIN_PORT=10523
+ADMIN_KEY=so3du5kasd5dn
+
+# set the parent directory as OFBiz Home
+OFBIZ_HOME="$( cd -P "$( dirname "$0" )" && pwd )"/..
+
+# console log file
+OFBIZ_LOG=runtime/logs/console.log
+
+# delete the last log
+rm -f $OFBIZ_LOG
+
+# VM args
+ADMIN="-Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY"
+#DEBUG="-Dsun.rmi.server.exceptionTrace=true"
+#DEBUG="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8091"
+#automatic IP address for linux
+#IPADDR=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+#RMIIF="-Djava.rmi.server.hostname=$IPADDR"
+MEMIF="-Xms128M -Xmx512M -XX:MaxPermSize=512m"
+#JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=33333 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+#MISC="-Duser.language=en"
+VMARGS="$MEMIF $MISC $JMX $DEBUG $RMIIF $ADMIN"
+
+# Worldpay Config
+#VMARGS="-Xbootclasspath/p:applications/accounting/lib/cryptix.jar $VMARGS"
+
+# location of java executable
+if [ -f "$JAVA_HOME/bin/java" ]; then
+  JAVA="$JAVA_HOME/bin/java"
+else
+  JAVA=java
+fi
+
+# Allows to run from Jenkins. See http://wiki.jenkins-ci.org/display/JENKINS/ProcessTreeKiller. Cons: the calling Jenkins job does not terminate if the log is not enabled, pros: this allows to monitor the log in Jenkins
+#BUILD_ID=dontKillMe
+
+# start ofbiz
+#$JAVA $VMARGS -jar ofbiz.jar $* >>$OFBIZ_LOG 2>>$OFBIZ_LOG&
+(cd "$OFBIZ_HOME" && exec "$JAVA" $VMARGS -jar ofbiz.jar "$@")
---
+++ tools/stopofbiz.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#####################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#####################################################################
+####
+# ofbiz.admin.key and ofbiz.admin.port must match that which OFBIZ was started with
+####
+
+OFBIZ_HOME="$( cd -P "$( dirname "$0" )" && pwd )"/..
+
+# location of java executable
+if [ -f "$JAVA_HOME/bin/java" ]; then
+  JAVA=$JAVA_HOME/bin/java
+else
+  JAVA=java
+fi
+
+# shutdown settings
+ADMIN_PORT=10523
+ADMIN_KEY=so3du5kasd5dn
+
+(cd "$OFBIZ_HOME" && $JAVA -Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY -jar ofbiz.jar -shutdown)
+
