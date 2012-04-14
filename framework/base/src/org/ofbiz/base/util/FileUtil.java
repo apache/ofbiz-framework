8c481181d57b1e9457ff651e643812e1cd24634a
Jacques Le Roux <jleroux@apache.org>
2012-04-14 07:30:30 +0000

    Adapted by hand from Lon F. Binder "CatalinaContainer Doesn't Respect <distributable/> Node for web.xml files." https://issues.apache.org/jira/browse/OFBIZ-4242
    Per the servlet specification, the web.xml file should allow an optional <distributable/> node to indicate that the application is clusterable. Currently, OFBiz does not respect this setting and assumes all applications should be distributed if any cluster configuration is provided in the ofbiz-containers.xml file. As a result, if, for example, the DeltaManager is enable, all applications attempt to cluster and communicate via DeltaManager.
    
    The expected and proper functionality is to check the individual application's web.xml file for the <distributable/> node and only use the DeltaManager if found; otherwise the StandardManager should be used for sessions.
    
    jleroux: replaced some tabs, reformatted, added a comment about <distributable/> in the *-containers.file
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1326064 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/FileUtil.java
+++ framework/base/src/org/ofbiz/base/util/FileUtil.java
@@ -29,6 +29,7 @@ import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.io.Writer;
 import java.net.MalformedURLException;
 import java.util.List;
@@ -67,7 +68,7 @@ public class FileUtil {
 
     /**
      * Converts a file path to one that is compatible with the host operating system.
-     * 
+     *
      * @param path The file path to convert.
      * @return The converted file path.
      */
@@ -341,4 +342,57 @@ public class FileUtil {
             return false;
         }
     }
+
+    /**
+    *
+    *
+    * Search for the specified <code>searchString</code> in the given
+    * {@link Reader}.
+    *
+    * @param reader A Reader in which the String will be searched.
+    * @param searchString The String to search for
+    * @return <code>TRUE</code> if the <code>searchString</code> is found;
+    *         <code>FALSE</code> otherwise.
+    * @throws IOException
+    */
+   public static boolean containsString(Reader reader, final String searchString) throws IOException {
+       char[] buffer = new char[1024];
+       int numCharsRead;
+       int count = 0;
+       while((numCharsRead = reader.read(buffer)) > 0) {
+           for (int c = 0; c < numCharsRead; ++c) {
+               if (buffer[c] == searchString.charAt(count)) count++;
+               else count = 0;
+               if (count == searchString.length()) return true;
+           }
+       }
+       return false;
+   }
+
+   /**
+    *
+    *
+    * Search for the specified <code>searchString</code> in the given
+    * filename. If the specified file doesn't exist, <code>FALSE</code>
+    * returns.
+    *
+    * @param fileName A full path to a file in which the String will be searched.
+    * @param searchString The String to search for
+    * @return <code>TRUE</code> if the <code>searchString</code> is found;
+    *         <code>FALSE</code> otherwise.
+    * @throws IOException
+    */
+   public static boolean containsString(final String fileName, final String searchString) throws IOException {
+       File inFile = new File(fileName);
+       if (inFile.exists()) {
+           BufferedReader in = new BufferedReader(new FileReader(inFile));
+           try {
+               return containsString(in, searchString);
+           } finally {
+               if (in != null)in.close();
+           }
+       } else {
+           return false;
+       }
+   }
 }
