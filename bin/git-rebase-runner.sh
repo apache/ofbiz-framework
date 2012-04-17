942098cbcf0eb89d68f700873343fd1750616c30
Adam Heath <doogie@apache.org>
2012-04-17 19:57:00 +0000

    FEATURE: Finally add the shell scripts I use to do automated testing with git on several commits before I push upstream.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1327246 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bin/git-rebase-runner.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#####################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#####################################################################
+
+# args:
+#  $1 = loop count
+#  $2 = loop type
+
+top_dir="$(cd "$0/../..";echo "$PWD")"
+. "$top_dir/bin/functions.sh"
+
+cd "$top_dir"
+
+loops="$1"
+shift
+cleanup_worker=""
+loop_worker=""
+case "$1" in
+	(all-tests)
+		cleanup_worker=standard_cleanup
+		loop_worker=fulltestsuite_worker
+		;;
+	(install)
+		cleanup_worker=standard_cleanup
+		loop_worker=install_worker
+		;;
+	(*)
+		echo "Invalid loop type given($1)!" 1>&2
+		exit 1
+		;;
+esac
+git_rebase_runner $loops $cleanup_worker $loop_worker
