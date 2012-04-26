aba59f096d2f5ceea8eae37cfa638c58e0660ed2
Adrian Crum <adrianc@apache.org>
2012-04-26 15:25:03 +0000

    Added a new Mini-language element - <now> - that replaces the <now-timestamp> and <now-date-to-env> elements. It is a drop-in replacement for <now-timestamp>.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1330902 13f79535-47bb-0310-9956-ffa450edef68

--- framework/minilang/src/org/ofbiz/minilang/method/envops/Now.java
+++ framework/minilang/src/org/ofbiz/minilang/method/envops/Now.java
@@ -16,38 +16,63 @@
  * specific language governing permissions and limitations
  * under the License.
  *******************************************************************************/
-package org.ofbiz.minilang.method.entityops;
+package org.ofbiz.minilang.method.envops;
 
+import org.ofbiz.base.conversion.ConversionException;
+import org.ofbiz.base.conversion.Converter;
+import org.ofbiz.base.conversion.Converters;
+import org.ofbiz.base.util.ObjectType;
 import org.ofbiz.base.util.collections.FlexibleMapAccessor;
 import org.ofbiz.base.util.string.FlexibleStringExpander;
 import org.ofbiz.minilang.MiniLangException;
+import org.ofbiz.minilang.MiniLangRuntimeException;
 import org.ofbiz.minilang.MiniLangValidate;
 import org.ofbiz.minilang.SimpleMethod;
+import org.ofbiz.minilang.ValidationException;
 import org.ofbiz.minilang.method.MethodContext;
 import org.ofbiz.minilang.method.MethodOperation;
 import org.w3c.dom.Element;
 
 /**
- * Creates a <code>java.sql.Timestamp</code> object that is set to the current system time.
+ * Sets a field to the current system time.
  */
-public class NowDateToEnv extends MethodOperation {
+public final class Now extends MethodOperation {
 
     private final FlexibleMapAccessor<Object> fieldFma;
+    private final String type;
+    private final Converter<Long, Object> converter;
 
-    public NowDateToEnv(Element element, SimpleMethod simpleMethod) throws MiniLangException {
+    @SuppressWarnings("unchecked")
+    public Now(Element element, SimpleMethod simpleMethod) throws MiniLangException {
         super(element, simpleMethod);
         if (MiniLangValidate.validationOn()) {
-            MiniLangValidate.handleError("Deprecated - use <now>", simpleMethod, element);
-            MiniLangValidate.attributeNames(simpleMethod, element, "field");
+            MiniLangValidate.attributeNames(simpleMethod, element, "field", "type");
             MiniLangValidate.requiredAttributes(simpleMethod, element, "field");
             MiniLangValidate.noChildElements(simpleMethod, element);
         }
         this.fieldFma = FlexibleMapAccessor.getInstance(element.getAttribute("field"));
+        this.type = element.getAttribute("type");
+        Class<?> targetClass = null;
+        try {
+            if (this.type.length() > 0) {
+                targetClass = ObjectType.loadClass(this.type);
+            }
+            if (targetClass == null) {
+                targetClass = java.sql.Timestamp.class;
+            }
+            this.converter = (Converter<Long, Object>) Converters.getConverter(Long.class, targetClass);
+        } catch (ClassNotFoundException e) {
+            throw new ValidationException(e.getMessage(), simpleMethod, element);
+        }
     }
 
     @Override
     public boolean exec(MethodContext methodContext) throws MiniLangException {
-        this.fieldFma.put(methodContext.getEnvMap(), new java.sql.Date(System.currentTimeMillis()));
+        try {
+            this.fieldFma.put(methodContext.getEnvMap(), this.converter.convert(System.currentTimeMillis()));
+        } catch (ConversionException e) {
+            throw new MiniLangRuntimeException(e.getMessage(), this);
+        }
         return true;
     }
 
@@ -63,21 +88,25 @@ public class NowDateToEnv extends MethodOperation {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("<now-date-to-env ");
+        StringBuilder sb = new StringBuilder("<now ");
         if (!this.fieldFma.isEmpty()) {
             sb.append("field=\"").append(this.fieldFma).append("\" ");
+        }
+        if (this.type.length() > 0) {
+            sb.append("type=\"").append(this.type).append("\" ");
         }
         sb.append("/>");
         return sb.toString();
     }
 
-    public static final class NowDateToEnvFactory implements Factory<NowDateToEnv> {
-        public NowDateToEnv createMethodOperation(Element element, SimpleMethod simpleMethod) throws MiniLangException {
-            return new NowDateToEnv(element, simpleMethod);
+
+    public static final class NowFactory implements Factory<Now> {
+        public Now createMethodOperation(Element element, SimpleMethod simpleMethod) throws MiniLangException {
+            return new Now(element, simpleMethod);
         }
 
         public String getName() {
-            return "now-date-to-env";
+            return "now";
         }
     }
 }
