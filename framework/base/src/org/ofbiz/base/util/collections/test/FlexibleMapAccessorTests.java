f7eb7843b2e272863b16d8055cc3d6cd0aba8716
Adrian Crum <adrianc@apache.org>
2012-05-14 19:57:25 +0000

    Change new FlexibleMapAccessor static method to an object method.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1338372 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/util/collections/test/FlexibleMapAccessorTests.java
+++ framework/base/src/org/ofbiz/base/util/collections/test/FlexibleMapAccessorTests.java
@@ -139,8 +139,8 @@ public class FlexibleMapAccessorTests extends GenericTestCaseBase {
         fmaTest("UEL auto-vivify List", "parameters.someList[0]", "parameters.someList[+0]", "Hello ${parameters.someList[0]}!", null, "World", "Hello World!");
         fmaTest("fse", "para${'meter'}s.var", "Hello ${parameters.var}!", "World", "Hello World!");
         fmaTest("foo", "'The total is ${total?currency(USD)}.'", "total", "The total is ${total?currency(USD)}.", localeToTest, new BigDecimal("12345678.90"), "The total is $12,345,678.90.");
-        assertTrue("containsNestedExpression method returns true", FlexibleMapAccessor.containsNestedExpression(FlexibleMapAccessor.getInstance("Hello ${parameters.var}!")));
-        assertFalse("containsNestedExpression method returns false", FlexibleMapAccessor.containsNestedExpression(FlexibleMapAccessor.getInstance("Hello World!")));
+        assertTrue("containsNestedExpression method returns true", FlexibleMapAccessor.getInstance("Hello ${parameters.var}!").containsNestedExpression());
+        assertFalse("containsNestedExpression method returns false", FlexibleMapAccessor.getInstance("Hello World!").containsNestedExpression());
     }
 
     public static class ThrowException {
