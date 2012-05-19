8f40461db4f7404f96f28e5013199fda2be12b8e
Jacques Le Roux <jleroux@apache.org>
2012-05-19 11:13:38 +0000

    A patch from Ankit Jain "Split Into Multiple Shipment Button is not working in Order Manager quick checkout" https://issues.apache.org/jira/browse/OFBIZ-4866
    Split Into Multiple Shipment Button is not working on quick checkout page
    
    
    jleroux: part the current known issue with Freemarker and findByAnd(), it works well on both sides (Order Manager and eCommerce)
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1340415 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/order/webapp/ordermgr/WEB-INF/actions/entry/SplitShip.groovy
@@ -0,0 +1,32 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+import org.ofbiz.party.contact.ContactHelper;
+import org.ofbiz.product.store.ProductStoreWorker;
+
+shoppingCart = session.getAttribute("shoppingCart");
+partyId = shoppingCart.getPartyId();
+party = delegator.findOne("Party", [partyId : partyId], true);
+productStoreId = ProductStoreWorker.getProductStoreId(request);
+
+context.cart = shoppingCart;
+context.shippingContactMechList = ContactHelper.getContactMech(party, "SHIPPING_LOCATION", "POSTAL_ADDRESS", false);
+
+profiledefs = delegator.findOne("PartyProfileDefault", [partyId : partyId, productStoreId : productStoreId], true);
+context.profileDefs = profiledefs;
