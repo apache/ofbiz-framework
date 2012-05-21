d2dfc93a31661118f382a1a093f7142f605a4ea8
Adam Heath <doogie@apache.org>
2012-05-21 14:39:59 +0000

    FIX: Replace BeansWrapper.getDefaultInstance() with FreeMarkerWorker.getDefaultOfbizWrapper(); these were missed in v1340631.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1341053 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/webapp/content/WEB-INF/actions/layout/EditSubContent.groovy
+++ applications/content/webapp/content/WEB-INF/actions/layout/EditSubContent.groovy
@@ -18,13 +18,13 @@
  */
 
 import org.ofbiz.base.util.*
+import org.ofbiz.base.util.template.FreeMarkerWorker
 import org.ofbiz.entity.*
 import org.ofbiz.entity.model.*
 import org.ofbiz.content.data.DataResourceWorker
 import org.ofbiz.webapp.ftl.FreeMarkerViewHandler
 
 import java.io.StringWriter
-import freemarker.ext.beans.BeansWrapper
 import freemarker.template.WrappingTemplateModel
 
 import javax.servlet.*
@@ -37,7 +37,7 @@ if (currentValue) {
     if (dataResourceTypeId) {
         mimeTypeId = currentValue.drMimeTypeId;
         rootDir = request.getSession().getServletContext().getRealPath("/");
-        wrapper = BeansWrapper.getDefaultInstance();
+        wrapper = FreeMarkerWorker.getDefaultOfbizWrapper();
         WrappingTemplateModel.setDefaultObjectWrapper(wrapper);
         templateRoot = [:];
         FreeMarkerViewHandler.prepOfbizRoot(templateRoot, request, response);
