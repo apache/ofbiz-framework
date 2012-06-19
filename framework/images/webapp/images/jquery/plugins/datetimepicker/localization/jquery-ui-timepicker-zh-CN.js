1867992164116b0192e99d53f7f6976cfeae60b3
Sascha Rodekamp <sascharodekamp@apache.org>
2012-06-19 10:11:59 +0000

    Upgrade the datetime picker to version 1.0 and remove the old libraries
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1351638 13f79535-47bb-0310-9956-ffa450edef68

--- framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-zh-CN.js
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-zh-CN.js
@@ -1,17 +1,20 @@
-/* Chinese translation for the jQuery Timepicker Addon */
-/* Written by Leon Sun */
+/* Simplified Chinese translation for the jQuery Timepicker Addon /
+/ Written by Will Lu */
 (function($) {
-    $.timepicker.regional['zh-CN'] = {
-        timeOnlyTitle: '选择时间',
-        timeText: '时间',
-        hourText: '时',
-        minuteText: '分',
-        secondText: '秒',
-        timezoneText: '时区',
-        currentText: '现在',
-        closeText: '完成',
-        timeFormat: 'hh:mm',
-        ampm: false
-    };
-    $.timepicker.setDefaults($.timepicker.regional['zh-CN']);
+	$.timepicker.regional['zh-CN'] = {
+		timeOnlyTitle: '选择时间',
+		timeText: '时间',
+		hourText: '小时',
+		minuteText: '分钟',
+		secondText: '秒钟',
+		millisecText: '微秒',
+		timezoneText: '时区',
+		currentText: '现在时间',
+		closeText: '关闭',
+		timeFormat: 'hh:mm',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['zh-CN']);
 })(jQuery);
--- framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-zh-TW.js
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-zh-TW.js
@@ -1,17 +1,20 @@
 /* Chinese translation for the jQuery Timepicker Addon */
-/* Written by Leon Sun */
+/* Written by Alang.lin */
 (function($) {
-    $.timepicker.regional['zh-TW'] = {
-        timeOnlyTitle: '選擇時間',
-        timeText: '時間',
-        hourText: '時',
-        minuteText: '分',
-        secondText: '秒',
-        timezoneText: '時區',
-        currentText: '現在',
-        closeText: '完成',
-        timeFormat: 'hh:mm',
-        ampm: false
-    };
-    $.timepicker.setDefaults($.timepicker.regional['zh-TW']);
+	$.timepicker.regional['zh-TW'] = {
+		timeOnlyTitle: '選擇時分秒',
+		timeText: '時間',
+		hourText: '時',
+		minuteText: '分',
+		secondText: '秒',
+		millisecText: '毫秒',
+		timezoneText: '時區',
+		currentText: '現在時間',
+		closeText: '確定',
+		timeFormat: 'hh:mm tt',
+		amNames: ['上午', 'AM', 'A'],
+		pmNames: ['下午', 'PM', 'P'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['zh-TW']);
 })(jQuery);
