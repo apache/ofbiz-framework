759217cd52defb944e11fbb0f21153edf1af9abe
Johannes Cornelius Bakker <hansbak@apache.org>
2012-06-20 03:17:59 +0000

    re-installed locale lookup with an appropriate fix: adding the locale search
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1351928 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/common/webcommon/WEB-INF/actions/includes/GetLocaleList.groovy
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.List;
+import org.ofbiz.base.util.*;
+import org.ofbiz.base.util.string.*;
+import org.ofbiz.base.util.UtilMisc;
+
+locales = [] as LinkedList;
+availableLocales = UtilMisc.availableLocales()
+
+// Debug.logInfo(parameters.localeString + "==" +  parameters.localeName);
+
+if (availableLocales) {
+    availableLocales.each { availableLocale ->
+        locale = [:];
+        locale.localeName = availableLocale.getDisplayName(availableLocale);
+        locale.localeString = availableLocale.toString();
+        if (UtilValidate.isNotEmpty(parameters.localeString)) {
+            if (locale.localeString.toUpperCase().contains(parameters.localeString.toUpperCase())) {
+                locales.add(locale);
+            }
+        }
+        if (UtilValidate.isNotEmpty(parameters.localeName)) {
+            if (locale.localeName.toUpperCase().contains(parameters.localeName.toUpperCase())) {
+                locales.add(locale);
+            }
+        }
+        if (UtilValidate.isEmpty(parameters.localeString) && UtilValidate.isEmpty(parameters.localeName)) {
+            locales.add(locale);
+        }
+    }
+}
+
+context.locales = locales;
