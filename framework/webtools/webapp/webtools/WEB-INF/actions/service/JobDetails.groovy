99d58d0330b3506294c234e49f4d00e5b54c5493
Jacopo Cappellato <jacopoc@apache.org>
2012-06-20 15:53:39 +0000

    Implemented screen that shows details about JobSandbox records, including their input data (RuntimeData.runtimeInfo deserialized) if available. )
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1352171 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/webtools/webapp/webtools/WEB-INF/actions/service/JobDetails.groovy
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue
+import org.ofbiz.base.util.UtilGenerics
+import org.ofbiz.entity.serialize.XmlSerializer;
+
+GenericValue job = ((Delegator)delegator).findOne("JobSandbox", [jobId:parameters.jobId], false)
+context.job = job
+if (job) {
+    GenericValue runtimeData = job.getRelatedOne("RuntimeData", false)
+    if (runtimeData) {
+        runtimeInfoMap = UtilGenerics.checkMap(XmlSerializer.deserialize(runtimeData.getString("runtimeInfo"), delegator), String.class, Object.class)
+        runtimeInfoList = []
+        runtimeInfoMap.each { key, value ->
+            valueMap = [key : key, value : value.toString()]
+            runtimeInfoList.add(valueMap)
+        }
+        context.runtimeInfoList = runtimeInfoList
+    }
+}
