ed159d8c2a084cac35da281a30bf740aaa0a16e9
Jacopo Cappellato <jacopoc@apache.org>
2012-06-25 17:46:07 +0000

    Cleaned up some code that was adding the delegator/dispatcher/security/auth objects in the user session rather than the request; 1) removed the request-maps for changeDelegator from components' controller.xml files: they were not used 2) removed CoreEvents.changeDelegator(...): not used (apart from the request-maps removed at #1) 3) removed CoreEvents.changeDispatcher(...): not used 4) cleaned up some code (related to multitenant development): now the objects are set in the request rather than the session
    PS: as a consequence this will also fix the issue reported in OFBIZ-4289
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1353681 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/ContextFilter.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ContextFilter.java
@@ -317,10 +317,10 @@ public class ContextFilter implements Filter {
                     LocalDispatcher dispatcher = getDispatcher(config.getServletContext());
 
                     // set web context objects
-                    httpRequest.getSession().setAttribute("dispatcher", dispatcher);
-                    httpRequest.getSession().setAttribute("security", security);
+                    request.setAttribute("dispatcher", dispatcher);
+                    request.setAttribute("security", security);
                     
-                    httpRequest.setAttribute("tenantId", tenantId);
+                    request.setAttribute("tenantId", tenantId);
                 }
 
                 // NOTE DEJ20101130: do NOT always put the delegator name in the user's session because the user may 
