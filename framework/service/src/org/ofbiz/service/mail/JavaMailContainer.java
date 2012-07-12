5fc117341a48579f4f4572a2cba7a4f96e7fe648
Jacopo Cappellato <jacopoc@apache.org>
2012-07-12 14:16:43 +0000

    Replaced usage of java.util.Timer with the more modern and reliable ScheduledThreadPool executor.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1360691 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/mail/JavaMailContainer.java
+++ framework/service/src/org/ofbiz/service/mail/JavaMailContainer.java
@@ -22,8 +22,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import javax.mail.FetchProfile;
 import javax.mail.Flags;
 import javax.mail.Folder;
@@ -62,7 +63,7 @@ public class JavaMailContainer implements Container {
     protected GenericValue userLogin = null;
     protected long timerDelay = 300000;
     protected long maxSize = 1000000;
-    protected Timer pollTimer = null;
+    protected ScheduledExecutorService pollTimer = null;
     protected boolean deleteMail = false;    // whether to delete emails after fetching them.
 
     protected String configFile = null;
@@ -80,7 +81,7 @@ public class JavaMailContainer implements Container {
         this.name = name;
         this.configFile = configFile;
         this.stores = new LinkedHashMap<Store, Session>();
-        this.pollTimer = new Timer();
+        this.pollTimer = Executors.newScheduledThreadPool(1);
     }
 
     /**
@@ -126,7 +127,7 @@ public class JavaMailContainer implements Container {
 
         // start the polling timer
         if (UtilValidate.isNotEmpty(stores)) {
-            pollTimer.schedule(new PollerTask(dispatcher, userLogin), timerDelay, timerDelay);
+            pollTimer.scheduleAtFixedRate(new PollerTask(dispatcher, userLogin), timerDelay, timerDelay, TimeUnit.MILLISECONDS);
         } else {
             Debug.logWarning("No JavaMail Store(s) configured; poller disabled.", module);
         }
@@ -142,7 +143,7 @@ public class JavaMailContainer implements Container {
      */
     public void stop() throws ContainerException {
         // stop the poller
-        this.pollTimer.cancel();
+        this.pollTimer.shutdown();
         Debug.logWarning("stop JavaMail poller", module);
     }
 
@@ -250,7 +251,7 @@ public class JavaMailContainer implements Container {
         }
     }
 
-    class PollerTask extends TimerTask {
+    class PollerTask implements Runnable {
 
         LocalDispatcher dispatcher;
         GenericValue userLogin;
@@ -260,7 +261,6 @@ public class JavaMailContainer implements Container {
             this.userLogin = userLogin;
         }
 
-        @Override
         public void run() {
             if (UtilValidate.isNotEmpty(stores)) {
                 for (Map.Entry<Store, Session> entry: stores.entrySet()) {
