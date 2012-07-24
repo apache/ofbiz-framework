c1c4af837a3f5a4130182b9ff9e0f71d4e38d018
Adrian Crum <adrianc@apache.org>
2012-07-24 10:46:08 +0000

    Code cleanups/fixups and JavaDocs for the new service component classes.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1364972 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/LocalDispatcherFactory.java
+++ framework/service/src/org/ofbiz/service/LocalDispatcherFactory.java
@@ -20,6 +20,16 @@ package org.ofbiz.service;
 
 import org.ofbiz.entity.Delegator;
 
+/**
+ * A {@link LocalDispatcher} factory. Implementations register themselves in the ofbiz-component.xml file.
+ */
 public interface LocalDispatcherFactory {
+    /**
+     * Creates a <code>LocalDispatcher</code> instance based on <code>name</code> and <code>delegator</code>.
+     * If a matching <code>LocalDispatcher</code> was already created, then it will be returned.
+     * 
+     * @param name
+     * @param delegator
+     */
     public LocalDispatcher createLocalDispatcher(String name, Delegator delegator);
 }
