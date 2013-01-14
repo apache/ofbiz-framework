4f7d1dc42598694e2796056daca4adc96359aa95
Jacques Le Roux <jleroux@apache.org>
2013-01-14 09:52:43 +0000

    No functional change, forgot this one
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1432856 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/RequestHandler.java
+++ framework/webapp/src/org/ofbiz/webapp/control/RequestHandler.java
@@ -67,7 +67,7 @@ public class RequestHandler {
     public static final String module = RequestHandler.class.getName();
     private boolean throwRequestHandlerExceptionOnMissingLocalRequest = UtilProperties.propertyValueEqualsIgnoreCase(
             "requestHandler.properties", "throwRequestHandlerExceptionOnMissingLocalRequest", "Y");
-    private String statusCodeString = UtilProperties.getPropertyValue("requestHandler.properties", "status-code", "303");
+    private String statusCodeString = UtilProperties.getPropertyValue("requestHandler.properties", "status-code", "302");
     public static RequestHandler getRequestHandler(ServletContext servletContext) {
         RequestHandler rh = (RequestHandler) servletContext.getAttribute("_REQUEST_HANDLER_");
         if (rh == null) {
