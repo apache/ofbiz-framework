71af22144d548a94b2cd5e3f2c13188d21d5ebd2
Jacques Le Roux <jleroux@apache.org>
2013-03-31 13:41:34 +0000

    A refactored patch from Leon for "Bug introduced by OFBIZ-4769, it makes <entity-one> failed if the context contains incorrect data" https://issues.apache.org/jira/browse/OFBIZ-5148
    How to reproduce:
    1. open "https://demo-trunk.ofbiz.apache.org/partymgr/control/editpartygroup?create_new=Y" (try to create a new party group)
    2. input any words in group name field, input any "incorrect" non-digits characters (e.g. "abcd" blah blah) to field "Annual revenue"
    3. click save
    It leads to an "exception" page instead of original input form with error message.
    
    I digged some codes and found it's related to r1426231 commit (of OFBIZ-4769).
    
    jleroux: the logic of code seemed good to me, I just refactored the cod a bit (in a previous refactoring attempt I missed the point that parametersObjExists might not be present. This new code is a simplified version of what does setAllFields internally. The PK fieldValues are set before being possibly overriden by context values. There is no Map casting. I tested the previous issues, all are resolved. 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1462948 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/finder/PrimaryKeyFinder.java
+++ framework/entity/src/org/ofbiz/entity/finder/PrimaryKeyFinder.java
@@ -18,6 +18,7 @@
  */
 package org.ofbiz.entity.finder;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -26,8 +27,6 @@ import javolution.util.FastMap;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.GeneralException;
-import org.ofbiz.base.util.UtilGenerics;
-import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.base.util.collections.FlexibleMapAccessor;
 import org.ofbiz.base.util.string.FlexibleStringExpander;
@@ -36,6 +35,7 @@ import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericPK;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.model.ModelEntity;
+import org.ofbiz.entity.model.ModelField;
 import org.w3c.dom.Element;
 
 /**
@@ -103,28 +103,34 @@ public class PrimaryKeyFinder extends Finder {
         // assemble the field map
         Map<String, Object> entityContext = FastMap.newInstance();
         if (autoFieldMap) {
-            GenericValue tempVal = delegator.makeValue(modelEntity.getEntityName());
-
             // try a map called "parameters", try it first so values from here are overridden by values in the main context
             Object parametersObj = context.get("parameters");
-            if (parametersObj != null && parametersObj instanceof Map<?, ?>) {
-                Map<String, Object> parameters = UtilMisc.<String, Object>toMap(UtilGenerics.checkMap(parametersObj));
-                // need the timeZone and locale for conversion, so add here and remove after
-                parameters.put("locale", context.get("locale"));
-                parameters.put("timeZone", context.get("timeZone"));
-                modelEntity.convertFieldMapInPlace(parameters, delegator);
-                parameters.remove("timeZone");
-                parameters.remove("locale");
-                tempVal.setAllFields(parameters, true, null, Boolean.TRUE);
+            Boolean parametersObjExists = parametersObj != null && parametersObj instanceof Map<?, ?>;
+            // only need PK fields
+            Iterator<ModelField> iter = modelEntity.getPksIterator();
+            while (iter.hasNext()) {
+                ModelField curField = iter.next();
+                String fieldName = curField.getName();
+                Object fieldValue = null;
+                if (parametersObjExists) {        
+                    fieldValue = ((Map<?, ?>) parametersObj).get(fieldName);
+                }
+                if (context.containsKey(fieldName)) {
+                    fieldValue = context.get(fieldName);
+                }
+                entityContext.put(fieldName, fieldValue);
             }
-
-            // just get the primary keys, and hopefully will get all of them, if not they must be manually filled in below in the field-maps
-            modelEntity.convertFieldMapInPlace(context, delegator);
-            tempVal.setAllFields(context, true, null, Boolean.TRUE);
-
-            entityContext.putAll(tempVal);
         }
         EntityFinderUtil.expandFieldMapToContext(fieldMap, context, entityContext);
+        //Debug.logInfo("PrimaryKeyFinder: entityContext=" + entityContext, module);
+        // then convert the types...
+        
+        // need the timeZone and locale for conversion, so add here and remove after
+        entityContext.put("locale", context.get("locale"));
+        entityContext.put("timeZone", context.get("timeZone"));
+        modelEntity.convertFieldMapInPlace(entityContext, delegator);
+        entityContext.remove("locale");
+        entityContext.remove("timeZone");
 
         // get the list of fieldsToSelect from selectFieldExpanderList
         Set<String> fieldsToSelect = EntityFinderUtil.makeFieldsToSelect(selectFieldExpanderList, context);
@@ -161,4 +167,3 @@ public class PrimaryKeyFinder extends Finder {
         }
     }
 }
-
