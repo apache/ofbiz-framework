19e3b14945325a9aa4bb75bc84d028ebc3092792
Adrian Crum <adrianc@apache.org>
2013-04-03 07:53:11 +0000

    Fixed bug in BaseUnitTests.java where the original debugging levels were not restored.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1463856 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/test/BaseUnitTests.java
+++ framework/base/src/org/ofbiz/base/test/BaseUnitTests.java
@@ -32,14 +32,19 @@ public class BaseUnitTests extends TestCase {
     }
 
     public void testDebug() {
-        Debug.set(Debug.VERBOSE, true);
-        assertTrue(Debug.verboseOn());
-
-        Debug.set(Debug.VERBOSE, false);
-        assertFalse(Debug.verboseOn());
-
-        Debug.set(Debug.INFO, true);
-        assertTrue(Debug.infoOn());
+        boolean debugVerbose = Debug.get(Debug.VERBOSE);
+        boolean debugInfo = Debug.get(Debug.INFO);
+        try {
+            Debug.set(Debug.VERBOSE, true);
+            assertTrue(Debug.verboseOn());
+            Debug.set(Debug.VERBOSE, false);
+            assertFalse(Debug.verboseOn());
+            Debug.set(Debug.INFO, true);
+            assertTrue(Debug.infoOn());
+        } finally {
+            Debug.set(Debug.VERBOSE, debugVerbose);
+            Debug.set(Debug.INFO, debugInfo);
+        }
     }
 
     public void testFormatPrintableCreditCard_1() {
