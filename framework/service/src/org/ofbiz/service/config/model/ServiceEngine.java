f291b925388fbba1edb3670fe064db166678c573
Adrian Crum <adrianc@apache.org>
2013-06-02 05:15:57 +0000

    New models for the serviceengine.xml file. They are not used yet - the ServiceConfigUtil class needs to be updated to use them, as well as other code that uses ServiceConfigUtil. The new models were contributed by Christoph Neuroth and were refactored by me.
    https://issues.apache.org/jira/browse/OFBIZ-5204
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1488643 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/service/src/org/ofbiz/service/config/model/ServiceEngine.java
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.service.config.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.ofbiz.base.lang.ThreadSafe;
+import org.ofbiz.base.util.UtilXml;
+import org.ofbiz.service.config.ServiceConfigException;
+import org.w3c.dom.Element;
+
+/**
+ * An object that models the <code>&lt;service-engine&gt;</code> element.
+ */
+@ThreadSafe
+public final class ServiceEngine {
+
+    private final Authorization authorization;
+    private final List<Engine> engines;
+    private final List<GlobalServices> globalServices;
+    private final List<JmsService> jmsServices;
+    private final String name;
+    private final List<NotificationGroup> notificationGroups;
+    private final List<ResourceLoader> resourceLoaders;
+    private final List<ServiceEcas> serviceEcas;
+    private final List<ServiceGroups> serviceGroups;
+    private final List<ServiceLocation> serviceLocations;
+    private final List<StartupService> startupServices;
+    private final ThreadPool threadPool;
+
+    ServiceEngine(Element engineElement) throws ServiceConfigException {
+        String name = engineElement.getAttribute("name").intern();
+        if (name.isEmpty()) {
+            throw new ServiceConfigException("<service-engine> element name attribute is empty");
+        }
+        this.name = name;
+        Element authElement = UtilXml.firstChildElement(engineElement, "authorization");
+        if (authElement == null) {
+            throw new ServiceConfigException("<authorization> element is missing");
+        }
+        this.authorization = new Authorization(authElement);
+        Element poolElement = UtilXml.firstChildElement(engineElement, "thread-pool");
+        if (poolElement == null) {
+            throw new ServiceConfigException("<thread-pool> element is missing");
+        }
+        this.threadPool = new ThreadPool(poolElement);
+        List<? extends Element> engineElementList = UtilXml.childElementList(engineElement, "engine");
+        if (engineElementList.isEmpty()) {
+            this.engines = Collections.emptyList();
+        } else {
+            List<Engine> engines = new ArrayList<Engine>(engineElementList.size());
+            for (Element childEngineElement : engineElementList) {
+                engines.add(new Engine(childEngineElement));
+            }
+            this.engines = Collections.unmodifiableList(engines);
+        }
+        List<? extends Element> serviceLocationElementList = UtilXml.childElementList(engineElement, "service-location");
+        if (serviceLocationElementList.isEmpty()) {
+            this.serviceLocations = Collections.emptyList();
+        } else {
+            List<ServiceLocation> serviceLocations = new ArrayList<ServiceLocation>(serviceLocationElementList.size());
+            for (Element serviceLocationElement : serviceLocationElementList) {
+                serviceLocations.add(new ServiceLocation(serviceLocationElement));
+            }
+            this.serviceLocations = Collections.unmodifiableList(serviceLocations);
+        }
+        List<? extends Element> notificationGroupElementList = UtilXml.childElementList(engineElement, "notification-group");
+        if (notificationGroupElementList.isEmpty()) {
+            this.notificationGroups = Collections.emptyList();
+        } else {
+            List<NotificationGroup> notificationGroups = new ArrayList<NotificationGroup>(notificationGroupElementList.size());
+            for (Element notificationGroupElement : notificationGroupElementList) {
+                notificationGroups.add(new NotificationGroup(notificationGroupElement));
+            }
+            this.notificationGroups = Collections.unmodifiableList(notificationGroups);
+        }
+        List<? extends Element> startupServiceElementList = UtilXml.childElementList(engineElement, "startup-service");
+        if (startupServiceElementList.isEmpty()) {
+            this.startupServices = Collections.emptyList();
+        } else {
+            List<StartupService> startupServices = new ArrayList<StartupService>(startupServiceElementList.size());
+            for (Element startupServiceElement : startupServiceElementList) {
+                startupServices.add(new StartupService(startupServiceElement));
+            }
+            this.startupServices = Collections.unmodifiableList(startupServices);
+        }
+        List<? extends Element> resourceLoaderElementList = UtilXml.childElementList(engineElement, "resource-loader");
+        if (resourceLoaderElementList.isEmpty()) {
+            this.resourceLoaders = Collections.emptyList();
+        } else {
+            List<ResourceLoader> resourceLoaders = new ArrayList<ResourceLoader>(resourceLoaderElementList.size());
+            for (Element resourceLoaderElement : resourceLoaderElementList) {
+                resourceLoaders.add(new ResourceLoader(resourceLoaderElement));
+            }
+            this.resourceLoaders = Collections.unmodifiableList(resourceLoaders);
+        }
+        List<? extends Element> globalServicesElementList = UtilXml.childElementList(engineElement, "global-services");
+        if (globalServicesElementList.isEmpty()) {
+            this.globalServices = Collections.emptyList();
+        } else {
+            List<GlobalServices> globalServices = new ArrayList<GlobalServices>(globalServicesElementList.size());
+            for (Element globalServicesElement : globalServicesElementList) {
+                globalServices.add(new GlobalServices(globalServicesElement));
+            }
+            this.globalServices = Collections.unmodifiableList(globalServices);
+        }
+        List<? extends Element> serviceGroupsElementList = UtilXml.childElementList(engineElement, "service-groups");
+        if (serviceGroupsElementList.isEmpty()) {
+            this.serviceGroups = Collections.emptyList();
+        } else {
+            List<ServiceGroups> serviceGroups = new ArrayList<ServiceGroups>(serviceGroupsElementList.size());
+            for (Element serviceGroupsElement : serviceGroupsElementList) {
+                serviceGroups.add(new ServiceGroups(serviceGroupsElement));
+            }
+            this.serviceGroups = Collections.unmodifiableList(serviceGroups);
+        }
+        List<? extends Element> serviceEcasElementList = UtilXml.childElementList(engineElement, "service-ecas");
+        if (serviceEcasElementList.isEmpty()) {
+            this.serviceEcas = Collections.emptyList();
+        } else {
+            List<ServiceEcas> serviceEcas = new ArrayList<ServiceEcas>(serviceEcasElementList.size());
+            for (Element serviceEcasElement : serviceEcasElementList) {
+                serviceEcas.add(new ServiceEcas(serviceEcasElement));
+            }
+            this.serviceEcas = Collections.unmodifiableList(serviceEcas);
+        }
+        List<? extends Element> jmsServiceElementList = UtilXml.childElementList(engineElement, "jms-service");
+        if (jmsServiceElementList.isEmpty()) {
+            this.jmsServices = Collections.emptyList();
+        } else {
+            List<JmsService> jmsServices = new ArrayList<JmsService>(jmsServiceElementList.size());
+            for (Element jmsServiceElement : jmsServiceElementList) {
+                jmsServices.add(new JmsService(jmsServiceElement));
+            }
+            this.jmsServices = Collections.unmodifiableList(jmsServices);
+        }
+    }
+
+    public Authorization getAuthorization() {
+        return authorization;
+    }
+
+    public List<Engine> getEngines() {
+        return this.engines;
+    }
+
+    public List<GlobalServices> getGlobalServices() {
+        return this.globalServices;
+    }
+
+    public List<JmsService> getJmsServices() {
+        return this.jmsServices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<NotificationGroup> getNotificationGroups() {
+        return this.notificationGroups;
+    }
+
+    public List<ResourceLoader> getResourceLoaders() {
+        return this.resourceLoaders;
+    }
+
+    public List<ServiceEcas> getServiceEcas() {
+        return this.serviceEcas;
+    }
+
+    public List<ServiceGroups> getServiceGroups() {
+        return this.serviceGroups;
+    }
+
+    public List<ServiceLocation> getServiceLocations() {
+        return this.serviceLocations;
+    }
+
+    public List<StartupService> getStartupServices() {
+        return this.startupServices;
+    }
+
+    public ThreadPool getThreadPool() {
+        return threadPool;
+    }
+
+}
---
+++ framework/service/src/org/ofbiz/service/config/model/ThreadPool.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.service.config.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.ofbiz.base.lang.ThreadSafe;
+import org.ofbiz.base.util.UtilXml;
+import org.ofbiz.service.config.ServiceConfigException;
+import org.w3c.dom.Element;
+
+/**
+ * An object that models the <code>&lt;thread-pool&gt;</code> element.
+ */
+@ThreadSafe
+public final class ThreadPool {
+
+    private final int failedRetryMin;
+    private final int jobs;
+    private final int maxThreads;
+    private final int minThreads;
+    private final int pollDbMillis;
+    private final boolean pollEnabled;
+    private final int purgeJobDays;
+    private final List<RunFromPool> runFromPools;
+    private final String sendToPool;
+    private final int ttl;
+
+    ThreadPool(Element poolElement) throws ServiceConfigException {
+        String sendToPool = poolElement.getAttribute("send-to-pool").intern();
+        if (sendToPool.isEmpty()) {
+            throw new ServiceConfigException("<thread-pool> element send-to-pool attribute is empty");
+        }
+        this.sendToPool = sendToPool;
+        String purgeJobDays = poolElement.getAttribute("purge-job-days").intern();
+        if (purgeJobDays.isEmpty()) {
+            this.purgeJobDays = 30;
+        } else {
+            try {
+                this.purgeJobDays = Integer.parseInt(purgeJobDays);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element purge-job-days attribute value is invalid");
+            }
+        }
+        String failedRetryMin = poolElement.getAttribute("failed-retry-min").intern();
+        if (failedRetryMin.isEmpty()) {
+            this.failedRetryMin = 30;
+        } else {
+            try {
+                this.failedRetryMin = Integer.parseInt(failedRetryMin);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element failed-retry-min attribute value is invalid");
+            }
+        }
+        String ttl = poolElement.getAttribute("ttl").intern();
+        if (ttl.isEmpty()) {
+            this.ttl = 120000;
+        } else {
+            try {
+                this.ttl = Integer.parseInt(ttl);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element ttl attribute value is invalid");
+            }
+        }
+        String jobs = poolElement.getAttribute("jobs").intern();
+        if (ttl.isEmpty()) {
+            this.jobs = 100;
+        } else {
+            try {
+                this.jobs = Integer.parseInt(jobs);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element jobs attribute value is invalid");
+            }
+        }
+        String minThreads = poolElement.getAttribute("min-threads").intern();
+        if (minThreads.isEmpty()) {
+            this.minThreads = 1;
+        } else {
+            try {
+                this.minThreads = Integer.parseInt(minThreads);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element min-threads attribute value is invalid");
+            }
+        }
+        String maxThreads = poolElement.getAttribute("max-threads").intern();
+        if (maxThreads.isEmpty()) {
+            this.maxThreads = 5;
+        } else {
+            try {
+                this.maxThreads = Integer.parseInt(maxThreads);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element max-threads attribute value is invalid");
+            }
+        }
+        this.pollEnabled = !"false".equals(poolElement.getAttribute("poll-enabled"));
+        String pollDbMillis = poolElement.getAttribute("poll-db-millis").intern();
+        if (pollDbMillis.isEmpty()) {
+            this.pollDbMillis = 30000;
+        } else {
+            try {
+                this.pollDbMillis = Integer.parseInt(pollDbMillis);
+            } catch (Exception e) {
+                throw new ServiceConfigException("<thread-pool> element poll-db-millis attribute value is invalid");
+            }
+        }
+        List<? extends Element> runFromPoolElementList = UtilXml.childElementList(poolElement, "run-from-pool");
+        if (runFromPoolElementList.isEmpty()) {
+            this.runFromPools = Collections.emptyList();
+        } else {
+            List<RunFromPool> runFromPools = new ArrayList<RunFromPool>(runFromPoolElementList.size());
+            for (Element runFromPoolElement : runFromPoolElementList) {
+                runFromPools.add(new RunFromPool(runFromPoolElement));
+            }
+            this.runFromPools = Collections.unmodifiableList(runFromPools);
+        }
+    }
+
+    public int getFailedRetryMin() {
+        return failedRetryMin;
+    }
+
+    public int getJobs() {
+        return jobs;
+    }
+
+    public int getMaxThreads() {
+        return maxThreads;
+    }
+
+    public int getMinThreads() {
+        return minThreads;
+    }
+
+    public int getPollDbMillis() {
+        return pollDbMillis;
+    }
+
+    public boolean getPollEnabled() {
+        return pollEnabled;
+    }
+
+    public int getPurgeJobDays() {
+        return purgeJobDays;
+    }
+
+    public List<RunFromPool> getRunFromPools() {
+        return this.runFromPools;
+    }
+
+    public String getSendToPool() {
+        return sendToPool;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+}
