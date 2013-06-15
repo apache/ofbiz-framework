4c187842a137816e6f92445220e84562cf6ae27a
Jacques Le Roux <jleroux@apache.org>
2013-06-15 16:22:53 +0000

    A fixing patch from Sergei Biletnikov for "Search orders by good identification" https://issues.apache.org/jira/browse/OFBIZ-5031
      currentCarrierShipmentMethod = delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false);
    is not correct, the result is the list, so the remedy is simple:
      currentCarrierShipmentMethod = EntityUtil.getFirst(delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false));
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1493393 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/webapp/ordermgr/WEB-INF/actions/order/FindOrders.groovy
+++ applications/order/webapp/ordermgr/WEB-INF/actions/order/FindOrders.groovy
@@ -21,6 +21,7 @@ import java.util.*;
 import java.sql.Timestamp;
 import org.ofbiz.entity.*;
 import org.ofbiz.base.util.*;
+import org.ofbiz.entity.util.*;
 
 module = "FindOrders.groovy";
 
@@ -100,7 +101,7 @@ if (shipmentMethod) {
     carrierPartyId = shipmentMethod.substring(0, shipmentMethod.indexOf("@"));
     shipmentMethodTypeId = shipmentMethod.substring(shipmentMethod.indexOf("@")+1);
     if (carrierPartyId && shipmentMethodTypeId) {
-        currentCarrierShipmentMethod = delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false);
+        currentCarrierShipmentMethod = EntityUtil.getFirst(delegator.findByAnd("CarrierShipmentMethod", [partyId : carrierPartyId, shipmentMethodTypeId : shipmentMethodTypeId], null, false));
         context.currentCarrierShipmentMethod = currentCarrierShipmentMethod;
     }
 }
