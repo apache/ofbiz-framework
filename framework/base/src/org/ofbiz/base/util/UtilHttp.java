apache/ofbiz-framework

commit 1956e731a3e8a102c6de7b7f908c2abcf1a4da20

No functional changes, only formatting. While looking for other cases like r1459222 and r1509164 introduced by r1300812 and r1305309. I noticed there were a lot of wrong for loops formatting (miss a white space after for), this changes it
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1511742 13f79535-47bb-0310-9956-ffa450edef68

framework/base/src/org/ofbiz/base/util/UtilHttp.java
@@ -920,7 +920,7 @@ public class UtilHttp {
 
     public static String getContentTypeByFileName(String fileName) {
         FileNameMap mime = URLConnection.getFileNameMap();
-        return mime.getContentTypeFor(fileName);
+        return mime.getContentTypefor (fileName);
     }
 
     /**

specialpurpose/appserver/src/org/ofbiz/appservers/GenerateContainer.java
@@ -156,7 +156,7 @@ public class GenerateContainer implements Container {
                     throw new ContainerException("Error in Geronimo deployment, please check the log");
                 }
 
-            for(int inst = 0; inst <= instancesNumber; inst++) {
+            for (int inst = 0; inst <= instancesNumber; inst++) {
                 instanceNumber = (inst == 0 ? "" : inst).toString();
                 GenerateGeronimoDeployment geronimoDeployment = new GenerateGeronimoDeployment();
                 List<String> classpathJars = geronimoDeployment.generate(args[0], geronimoHostHome, instanceNumber);
@@ -291,8 +291,8 @@ public class GenerateContainer implements Container {
                         }
                     }
                     is.close();
-                        process.waitFor();
-    //                    System.out.println(process.waitFor());
+                        process.waitfor ();
+    //                    System.out.println(process.waitfor ());
     //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " undeployment ended" , module);
                 } catch (IOException e) {
@@ -352,8 +352,8 @@ public class GenerateContainer implements Container {
                         }
                     }
                     is.close();
-                    process.waitFor();
-//                    System.out.println(process.waitFor());
+                    process.waitfor ();
+//                    System.out.println(process.waitfor ());
 //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " deployment ended" , module);
                 } catch (IOException e) {

commit 15ff8e1ec28f6ff7047b42f67048f2e762bf5d65

corrections of errors introduced with r1511742
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1512129 13f79535-47bb-0310-9956-ffa450edef68

framework/base/src/org/ofbiz/base/util/UtilHttp.java
@@ -920,7 +920,7 @@ public class UtilHttp {
 
     public static String getContentTypeByFileName(String fileName) {
         FileNameMap mime = URLConnection.getFileNameMap();
-        return mime.getContentTypefor (fileName);
+        return mime.getContentTypeFor(fileName);
     }
 
     /**

specialpurpose/appserver/src/org/ofbiz/appservers/GenerateContainer.java
@@ -291,7 +291,7 @@ public class GenerateContainer implements Container {
                         }
                     }
                     is.close();
-                        process.waitfor ();
+                        process.waitFor();
     //                    System.out.println(process.waitfor ());
     //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " undeployment ended" , module);
@@ -352,7 +352,7 @@ public class GenerateContainer implements Container {
                         }
                     }
                     is.close();
-                    process.waitfor ();
+                    process.waitFor();
 //                    System.out.println(process.waitfor ());
 //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " deployment ended" , module);