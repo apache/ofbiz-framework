93e9949545b792352bd74b572374f6ab9e766715
Adrian Crum <adrianc@apache.org>
2013-08-25 18:00:43 +0000

    Converted menu widget HTML renderer to a macro renderer.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1517353 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/screen/MacroScreenViewHandler.java
+++ framework/widget/src/org/ofbiz/widget/screen/MacroScreenViewHandler.java
@@ -38,8 +38,10 @@ import org.ofbiz.webapp.view.AbstractViewHandler;
 import org.ofbiz.webapp.view.ViewHandlerException;
 import org.ofbiz.widget.form.FormStringRenderer;
 import org.ofbiz.widget.form.MacroFormRenderer;
-import org.ofbiz.widget.tree.TreeStringRenderer;
+import org.ofbiz.widget.menu.MacroMenuRenderer;
+import org.ofbiz.widget.menu.MenuStringRenderer;
 import org.ofbiz.widget.tree.MacroTreeRenderer;
+import org.ofbiz.widget.tree.TreeStringRenderer;
 import org.xml.sax.SAXException;
 
 import freemarker.template.TemplateException;
@@ -90,15 +92,13 @@ public class MacroScreenViewHandler extends AbstractViewHandler {
             ScreenStringRenderer screenStringRenderer = new MacroScreenRenderer(UtilProperties.getPropertyValue("widget", getName() + ".name"), UtilProperties.getPropertyValue("widget", getName() + ".screenrenderer"));
             FormStringRenderer formStringRenderer = new MacroFormRenderer(UtilProperties.getPropertyValue("widget", getName() + ".formrenderer"), request, response);
             TreeStringRenderer treeStringRenderer = new MacroTreeRenderer(UtilProperties.getPropertyValue("widget", getName() + ".treerenderer"), writer);
-            // TODO: uncomment these lines when the renderers are implemented
-            //MenuStringRenderer menuStringRenderer = new MacroMenuRenderer(UtilProperties.getPropertyValue("widget", getName() + ".menurenderer"), writer);
+            MenuStringRenderer menuStringRenderer = new MacroMenuRenderer(UtilProperties.getPropertyValue("widget", getName() + ".menurenderer"), request, response);
 
             ScreenRenderer screens = new ScreenRenderer(writer, null, screenStringRenderer);
             screens.populateContextForRequest(request, response, servletContext);
-            // this is the object used to render forms from their definitions
             screens.getContext().put("formStringRenderer", formStringRenderer);
             screens.getContext().put("treeStringRenderer", treeStringRenderer);
-            //screens.getContext().put("menuStringRenderer", menuStringRenderer);
+            screens.getContext().put("menuStringRenderer", menuStringRenderer);
             screens.getContext().put("simpleEncoder", StringUtil.getEncoder(UtilProperties.getPropertyValue("widget", getName() + ".encoder")));
             screenStringRenderer.renderScreenBegin(writer, screens.getContext());
             screens.render(page);
