f738be46f7b29af687345b1713311ea595bb2bcb
Johannes Cornelius Bakker <hansbak@apache.org>
2013-09-06 08:03:34 +0000

    added a new menu item in webtools: 'programmable export' where interactively a groovy program can be entered to export specifc records from the database. In order to apply some global changes for re-import
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1520505 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/webtools/webapp/webtools/WEB-INF/actions/entity/ProgramExport.groovy
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.model.ModelEntity;
+import org.ofbiz.base.util.*
+import org.w3c.dom.Document;
+
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.control.MultipleCompilationErrorsException;
+import org.codehaus.groovy.control.ErrorCollector;
+
+String groovyProgram = null;
+recordValues = [];
+errMsgList = [];
+
+if (UtilValidate.isEmpty(parameters.groovyProgram)) {
+    
+    groovyProgram = '''
+// Use the List variable recordValues to fill it with GenericValue maps.
+// full groovy syntaxt is available
+
+// examples:
+recordValues.add(delegator.findOne("Content", ["contentId": "BLOGROOTBIGAL"], false));
+recordValues.add(delegator.findOne("Content", ["contentId": "BLOGROOTMADMAX"], false));
+'''
+    parameters.groovyProgram = groovyProgram;
+} else {
+    groovyProgram = parameters.groovyProgram;
+}
+
+// Add imports for script.
+def importCustomizer = new ImportCustomizer()
+importCustomizer.addImport("org.ofbiz.entity.GenericValue");
+importCustomizer.addImport("org.ofbiz.entity.model.ModelEntity");
+def configuration = new CompilerConfiguration()
+configuration.addCompilationCustomizers(importCustomizer)
+
+Binding binding = new Binding();
+binding.setVariable("delegator", delegator);
+binding.setVariable("recordValues", recordValues);
+
+ClassLoader loader = Thread.currentThread().getContextClassLoader();
+def shell = new GroovyShell(loader, binding, configuration);
+
+if (UtilValidate.isNotEmpty(groovyProgram)) {
+    try {
+        shell.parse(groovyProgram);
+        shell.evaluate(groovyProgram)
+        recordValues = shell.getVariable("recordValues");
+        xmlDoc = GenericValue.makeXmlDocument(recordValues);
+        context.put("xmlDoc", xmlDoc);
+    } catch(MultipleCompilationErrorsException e) {
+        request.setAttribute("_ERROR_MESSAGE_", e);
+        return;
+    } catch(groovy.lang.MissingPropertyException e) {
+        request.setAttribute("_ERROR_MESSAGE_", e);
+        return;
+    } catch(IllegalArgumentException e) {
+        request.setAttribute("_ERROR_MESSAGE_", e);
+        return;
+    }
+}
