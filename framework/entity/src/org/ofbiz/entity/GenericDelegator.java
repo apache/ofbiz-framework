8c0049cb9c60e6cb8a50518f985e51d24b8f9a8a
Jacques Le Roux <jleroux@apache.org>
2013-09-16 13:38:08 +0000

    Since we decided to keep findAll, no reasons to deprecate
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1523645 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/GenericDelegator.java
+++ framework/entity/src/org/ofbiz/entity/GenericDelegator.java
@@ -1677,8 +1677,6 @@ public class GenericDelegator implements Delegator {
     /** Finds all Generic entities
      *@param entityName The Name of the Entity as defined in the entity XML file
      * @see org.ofbiz.entity.Delegator#findAll(java.lang.String, boolean)
-     * @deprecated use {@link #findList(java.lang.String, org.ofbiz.entity.condition.EntityCondition, java.util.Set, java.util.List, org.ofbiz.entity.util.EntityFindOptions, boolean)}
-     * 
      */
     public List<GenericValue> findAll(String entityName, boolean useCache) throws GenericEntityException {
         return this.findList(entityName, null, null, null, null, useCache);
