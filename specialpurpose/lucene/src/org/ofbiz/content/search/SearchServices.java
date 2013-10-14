974062c60a580ed0f75b862b9c1ded030dede172
Jacopo Cappellato <jacopoc@apache.org>
2013-10-14 14:42:53 +0000

    Implemented a series of ecas to submit for indexing products when product related information is added/updated/deleted. Thanks to Scott Gray for the initial implementation of these ecas.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1531918 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/lucene/src/org/ofbiz/content/search/SearchServices.java
+++ specialpurpose/lucene/src/org/ofbiz/content/search/SearchServices.java
@@ -26,12 +26,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.service.ServiceUtil;
 import org.ofbiz.service.LocalDispatcher;
 
@@ -69,4 +74,117 @@ public class SearchServices {
         if (Debug.infoOn()) Debug.logInfo("Indexing done in: " + (end.getTime()-start.getTime()) + " ms", module);
         return results;
     }
+
+    public static Map<String, Object> indexProduct(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Delegator delegator = dctx.getDelegator();
+        String productId = (String) context.get("productId");
+        ProductIndexer indexer = ProductIndexer.getInstance(delegator);
+        indexer.queue(productId);
+        return ServiceUtil.returnSuccess();
+    }
+
+    public static Map<String, Object> indexProductsFromFeature(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Delegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        try {
+            List<GenericValue> productFeatureAppls = delegator.findByAnd("ProductFeatureAppl", UtilMisc.toMap("productFeatureId", context.get("productFeatureId")), null, false);
+            // Only re-index the active appls, future dated ones will get picked up on that product's re-index date
+            productFeatureAppls = EntityUtil.filterByDate(productFeatureAppls);
+
+            for (GenericValue productFeatureAppl : productFeatureAppls) {
+                try {
+                    dispatcher.runSync("indexProduct", UtilMisc.toMap("productId", productFeatureAppl.get("productId")));
+                } catch (GenericServiceException e) {
+                    Debug.logError(e, module);
+                }
+            }
+
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+
+    public static Map<String, Object> indexProductsFromProductAssoc(DispatchContext dctx, Map<String, ? extends Object> context) {
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        try {
+            dispatcher.runSync("indexProduct", UtilMisc.toMap("productId", context.get("productId")));
+            dispatcher.runSync("indexProduct", UtilMisc.toMap("productId", context.get("productIdTo")));
+        } catch (GenericServiceException e) {
+            Debug.logError(e, module);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+
+    public static Map<String, Object> indexProductsFromDataResource(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Delegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        try {
+            List<GenericValue> contents = delegator.findByAnd("Content", UtilMisc.toMap("dataResourceId", context.get("dataResourceId")), null, false);
+            for (GenericValue content : contents) {
+                dispatcher.runSync("indexProductsFromContent",
+                        UtilMisc.toMap(
+                                "userLogin", context.get("userLogin"),
+                                "contentId", content.get("contentId")));
+            }
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+        } catch (GenericServiceException e) {
+            Debug.logError(e, module);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+    public static Map<String, Object> indexProductsFromContent(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Delegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        try {
+            List<GenericValue> productContents = delegator.findByAnd("ProductContent", UtilMisc.toMap("contentId", context.get("contentId")), null, false);
+            for (GenericValue productContent : productContents) {
+                try {
+                    dispatcher.runSync("indexProduct", UtilMisc.toMap("productId", productContent.get("productId")));
+                } catch (GenericServiceException e) {
+                    Debug.logError(e, module);
+                }
+            }
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+    public static Map<String, Object> indexProductsFromCategory(DispatchContext dctx, Map<String, Object> context) {
+        Delegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        try {
+            String productCategoryId = (String) context.get("productCategoryId");
+            indexProductCategoryMembers(productCategoryId, delegator, dispatcher);
+            indexProductCategoryRollup(productCategoryId, delegator, dispatcher, UtilMisc.<String>toSet(productCategoryId));
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+    private static void indexProductCategoryRollup(String parentProductCategoryId, Delegator delegator, LocalDispatcher dispatcher, Set<String> excludeProductCategoryIds) throws GenericEntityException {
+        List<GenericValue> productCategoryRollups = delegator.findByAnd("ProductCategoryRollup", UtilMisc.toMap("parentProductCategoryId", parentProductCategoryId), null, false);
+        for (GenericValue productCategoryRollup : productCategoryRollups) {
+            String productCategoryId = productCategoryRollup.getString("productCategoryId");
+            // Avoid infinite recursion
+            if (!excludeProductCategoryIds.add(productCategoryId)) {
+                continue;
+            }
+            indexProductCategoryMembers(productCategoryId, delegator, dispatcher);
+            indexProductCategoryRollup(productCategoryId, delegator, dispatcher, excludeProductCategoryIds);
+        }
+    }
+
+    private static void indexProductCategoryMembers(String productCategoryId, Delegator delegator, LocalDispatcher dispatcher) throws GenericEntityException {
+        List<GenericValue> productCategoryMembers = delegator.findByAnd("ProductCategoryMember", UtilMisc.toMap("productCategoryId", productCategoryId), null, false);
+        for (GenericValue productCategoryMember : productCategoryMembers) {
+            try {
+                dispatcher.runSync("indexProduct", UtilMisc.toMap("productId", productCategoryMember.get("productId")));
+            } catch (GenericServiceException e) {
+                Debug.logError(e, module);
+            }
+        }
+    }
+
 }
