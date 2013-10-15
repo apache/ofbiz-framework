044acebb725efb2aa80c3cee516bddd567820f97
Jacopo Cappellato <jacopoc@apache.org>
2013-10-15 10:13:11 +0000

    Refactored and simplified the old code to index Content record: it now leverages the new LuceneDocument interface and index handling facilities.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1532273 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/lucene/src/org/ofbiz/content/search/SearchWorker.java
+++ specialpurpose/lucene/src/org/ofbiz/content/search/SearchWorker.java
@@ -18,16 +18,12 @@
  *******************************************************************************/
 package org.ofbiz.content.search;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilDateTime;
-import org.ofbiz.base.util.UtilGenerics;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.base.util.UtilValidate;
@@ -37,17 +33,6 @@ import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.service.LocalDispatcher;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.util.Version;
 
 /**
@@ -59,9 +44,8 @@ public class SearchWorker {
 
     public static final Version LUCENE_VERSION = Version.LUCENE_45;
 
-    public static Map<String, Object> indexTree(LocalDispatcher dispatcher, Delegator delegator, String siteId, Map<String, Object> context) throws Exception {
+    public static void indexTree(LocalDispatcher dispatcher, Delegator delegator, String siteId) throws Exception {
         GenericValue content = delegator.makeValue("Content", UtilMisc.toMap("contentId", siteId));
-        if (Debug.infoOn()) Debug.logInfo("in indexTree, siteId:" + siteId + " content:" + content, module);
         List<GenericValue> siteList = ContentWorker.getAssociatedContent(content, "To", UtilMisc.toList("SUBSITE", "PUBLISH_LINK", "SUB_CONTENT"), null, UtilDateTime.nowTimestamp().toString(), null);
 
         if (siteList != null) {
@@ -74,19 +58,11 @@ public class SearchWorker {
                     for (GenericValue subContent : subContentList) {
                         contentIdList.add(subContent.getString("contentId"));
                     }
-                    indexContentList(dispatcher, delegator, context, contentIdList);
-                    indexTree(dispatcher, delegator, siteContentId, context);
-                } else {
-                    List<String> badIndexList = UtilGenerics.checkList(context.get("badIndexList"));
-                    badIndexList.add(siteContentId + " had no sub-entities.");
+                    indexContentList(dispatcher, delegator, contentIdList);
+                    indexTree(dispatcher, delegator, siteContentId);
                 }
             }
-        } else {
-            List<String> badIndexList = UtilGenerics.checkList(context.get("badIndexList"));
-            badIndexList.add(siteId + " had no sub-entities.");
         }
-
-        return UtilMisc.toMap("badIndexList", context.get("badIndexList"), "goodIndexCount", context.get("goodIndexCount"));
     }
 
     public static String getIndexPath(String path) {
@@ -94,40 +70,13 @@ public class SearchWorker {
         return (UtilValidate.isNotEmpty(path)? basePath + "/" + path: basePath);
     }
 
-    private static IndexWriter getDefaultIndexWriter(Directory directory) {
-        IndexWriter writer = null;
-        long savedWriteLockTimeout = IndexWriterConfig.getDefaultWriteLockTimeout();
-        Analyzer analyzer = new StandardAnalyzer(LUCENE_VERSION);
-        IndexWriterConfig conf = new IndexWriterConfig(LUCENE_VERSION, analyzer);
-        IndexWriterConfig.setDefaultWriteLockTimeout(2000);
-        try {
-            writer  = new IndexWriter(directory, conf);
-        } catch (CorruptIndexException e) {
-            Debug.logError("Corrupted lucene index: "  + e.getMessage(), module);
-        } catch (LockObtainFailedException e) {
-            Debug.logError("Could not obtain Lock on lucene index "  + e.getMessage(), module);
-        } catch (IOException e) {
-            Debug.logError(e.getMessage(), module);
-        } finally {
-            IndexWriterConfig.setDefaultWriteLockTimeout(savedWriteLockTimeout);
-        }
-        return writer;
-    }
-
-    public static void indexContentList(LocalDispatcher dispatcher, Delegator delegator, Map<String, Object> context,List<String> idList) throws Exception {
-        Directory directory = FSDirectory.open(new File(getIndexPath("content")));
-        if (Debug.infoOn()) Debug.logInfo("in indexContentList, indexAllPath: " + directory.toString(), module);
-        // Delete existing documents
-        IndexWriter writer = getDefaultIndexWriter(directory);
+    public static void indexContentList(LocalDispatcher dispatcher, Delegator delegator, List<String> idList) throws Exception {
+        DocumentIndexer indexer = DocumentIndexer.getInstance(delegator, "content");
         List<GenericValue> contentList = new ArrayList<GenericValue>();
         for (String id : idList) {
-            if (Debug.infoOn()) Debug.logInfo("in indexContentList, id:" + id, module);
             try {
                 GenericValue content = delegator.findOne("Content", UtilMisc .toMap("contentId", id), true);
                 if (content != null) {
-                    if (writer != null) {
-                        deleteContentDocuments(content, writer);
-                    }
                     contentList.add(content);
                 }
             } catch (GenericEntityException e) {
@@ -135,50 +84,8 @@ public class SearchWorker {
                 return;
             }
         }
-        for (GenericValue gv : contentList) {
-            indexContent(dispatcher, context, gv, writer);
-        }
-        try {
-            writer.forceMerge(1);
-        } catch (NullPointerException e) {
-            Debug.logError(e, module);
-        }
-        writer.close();
-    }
-
-    private static void deleteContentDocuments(GenericValue content, IndexWriter writer) throws Exception {
-        String contentId = content.getString("contentId");
-        Term term = new Term("contentId", contentId);
-        deleteDocumentsByTerm(term, writer);
-        String dataResourceId = content.getString("dataResourceId");
-        if (dataResourceId != null) {
-            term = new Term("dataResourceId", dataResourceId);
-            deleteDocumentsByTerm(term, writer);
+        for (GenericValue content : contentList) {
+            indexer.queue(new ContentDocument(content, dispatcher));
         }
     }
-
-    private static void deleteDocumentsByTerm(Term term, IndexWriter writer) throws Exception {
-        DirectoryReader reader = DirectoryReader.open(writer, false);
-        int qtyBefore = reader.docFreq(term);
-
-        //deletes documents, all the rest is for logging
-        writer.deleteDocuments(term);
-
-        int qtyAfter = reader.docFreq(term);
-        reader.close();
-
-        if (Debug.infoOn()) Debug.logInfo("For term " + term.toString() + ", documents deleted: " + qtyBefore + ", remaining: " + qtyAfter, module);
-    }
-
-    private static void indexContent(LocalDispatcher dispatcher, Map<String, Object> context, GenericValue content, IndexWriter writer) throws Exception {
-        Document doc = ContentDocument.Document(content, context, dispatcher);
-
-        if (doc != null) {
-            writer.addDocument(doc);
-            Integer goodIndexCount = (Integer)context.get("goodIndexCount");
-            Integer newIndexCount = goodIndexCount + 1;
-            context.put("goodIndexCount", newIndexCount);
-        }
-    }
-
 }
