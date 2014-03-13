ba28208c2fc2fb7f662da4f984f77028270b18fc
Jacques Le Roux <jleroux@apache.org>
2014-03-13 18:23:47 +0000

    A patch from simon maskell for "Paginate in screen fails" https://issues.apache.org/jira/browse/OFBIZ-5574
    When you use the paginate = true in a screen and the total number of rows returned is greater than 999 the string is formatted with a coma and an exception is thrown when trying to convert it to an Integer.
    
    jleroux: this depends on language (French would be space) but Simon's fix works for all (we are only dealing with integers there)
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1577268 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/form/ModelForm.java
+++ framework/widget/src/org/ofbiz/widget/form/ModelForm.java
@@ -2688,6 +2688,7 @@ public class ModelForm extends ModelWidget {
         String size = this.overrideListSize.expandString(context);
         if (UtilValidate.isNotEmpty(size)) {
             try {
+            	size =size.replaceAll("[^0-9.]", "");
                 listSize = Integer.parseInt(size);
             } catch (NumberFormatException e) {
                 Debug.logError(e, "Error getting override list size from value " + size, module);
