25312fa05c81e85ecc84233c6c03864da16c986d
Hans Bakker <hansbak@apache.org>
2014-04-24 03:57:06 +0000

    adminport in start.properties was ignored. fixed
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1589562 13f79535-47bb-0310-9956-ffa450edef68

--- framework/start/src/org/ofbiz/base/start/Config.java
+++ framework/start/src/org/ofbiz/base/start/Config.java
@@ -353,7 +353,7 @@ public class Config {
             if (args.length > 0) {
                 for (String arg : args) {
                     if (arg.toLowerCase().contains("portoffset=") && !arg.toLowerCase().contains("${portoffset}")) {
-                        adminPort = adminPort != 0 ? adminPort : 10523; // This is necessary because the ASF machines don't allow ports 1 to 3, see  INFRA-6790
+                        adminPort = adminPort == 0 ? adminPort : 10523; // This is necessary because the ASF machines don't allow ports 1 to 3, see  INFRA-6790
                         adminPort += Integer.parseInt(arg.split("=")[1]);
                     }
                 }
