af0717e4155e7d36948b76b6a5a17a85ba7acfda
Adam Heath <doogie@apache.org>
2014-06-24 01:30:56 +0000

    Fix OFBIZ-5565: FinAccountHelper.getFinAccountFromCode() no longer returns financial account.
    Now that EntityCondition lookups work against encrypted fields, there is
    no longer a need to do the field encryption locally in the above method.
    So, just pass the value directly into the entity engine.
    
    There are also test cases, to prevent this breakage from occurring
    again.
    
    This commit depends on r1604967 and r1604968, at the minimum.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1604993 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/src/org/ofbiz/order/finaccount/FinAccountHelper.java
+++ applications/order/src/org/ofbiz/order/finaccount/FinAccountHelper.java
@@ -140,11 +140,8 @@ public class FinAccountHelper {
          }
          finAccountCode = finAccountCode.toUpperCase().replaceAll("[^0-9A-Z]", "");
 
-         // now we need to get the encrypted version of the fin account code the user passed in to look up against FinAccount
-         String encryptedFinAccountCode = (String) delegator.encryptFieldValue("FinAccount", finAccountCode);
-
          // now look for the account
-         List<GenericValue> accounts = delegator.findByAnd("FinAccount", UtilMisc.toMap("finAccountCode", encryptedFinAccountCode), null, false);
+         List<GenericValue> accounts = delegator.findByAnd("FinAccount", UtilMisc.toMap("finAccountCode", finAccountCode), null, false);
          accounts = EntityUtil.filterByDate(accounts);
 
          if (UtilValidate.isEmpty(accounts)) {
