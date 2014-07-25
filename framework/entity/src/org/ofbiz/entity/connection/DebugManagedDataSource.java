4f4f95092c2811c3a18e9bcf01a551c92d2da734
Jacopo Cappellato <jacopoc@apache.org>
2014-07-25 13:19:59 +0000

    OFBIZ-4283: miscellaneous enhancements to give more control in the configuration of the DBCP connection pool. Thanks to Philippe Mouawad for the initial contribution that I have used as a starting point for a different implementation.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1613431 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/connection/DebugManagedDataSource.java
+++ framework/entity/src/org/ofbiz/entity/connection/DebugManagedDataSource.java
@@ -44,9 +44,9 @@ public class DebugManagedDataSource extends ManagedDataSource {
         if (Debug.verboseOn()) {
             if (super.getPool() instanceof GenericObjectPool) {
                 GenericObjectPool objectPool = (GenericObjectPool)super.getPool();
-                Debug.logVerbose("Borrowing a connection from the pool; used/total: " + objectPool.getNumActive() + "/" + objectPool.getNumActive() + objectPool.getNumIdle() + "; min idle/max idle/max total: " + objectPool.getMinIdle() + "/" + objectPool.getMaxIdle() + "/" + objectPool.getMaxTotal(), module);
+                Debug.logVerbose("Borrowing a connection from the pool; used/idle/total: " + objectPool.getNumActive() + "/" + objectPool.getNumIdle() + "/" + (objectPool.getNumActive() + objectPool.getNumIdle()) + "; min idle/max idle/max total: " + objectPool.getMinIdle() + "/" + objectPool.getMaxIdle() + "/" + objectPool.getMaxTotal(), module);
             } else {
-                Debug.logVerbose("Borrowing a connection from the pool; used/total: " + super.getPool().getNumActive() + "/" + (super.getPool().getNumActive() + super.getPool().getNumIdle()), module);
+                Debug.logVerbose("Borrowing a connection from the pool; used/idle/total: " + super.getPool().getNumActive() + "/" + super.getPool().getNumIdle() + "/" + (super.getPool().getNumActive() + super.getPool().getNumIdle()), module);
             }
         }
         return super.getConnection();
