dbe9fd76daa4d551ab43ce70ef6024f5dba28d87
Adam Heath <doogie@apache.org>
2014-08-13 19:38:48 +0000

    Now that the thread-pool startup dead-locks are fixed, we can configure the entirety of the delegator in a thread pool.  This will become a big win very shortly, when multiple delegators are created at startup.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1617816 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/DelegatorFactory.java
+++ framework/entity/src/org/ofbiz/entity/DelegatorFactory.java
@@ -18,7 +18,14 @@
  */
 package org.ofbiz.entity;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.ofbiz.base.concurrent.ExecutionPool;
 
 import org.ofbiz.base.lang.Factory;
 import org.ofbiz.base.util.Debug;
@@ -27,33 +34,65 @@ import org.ofbiz.base.util.UtilObject;
 /** <code>Delegator</code> factory abstract class. */
 public abstract class DelegatorFactory implements Factory<Delegator, String> {
     public static final String module = DelegatorFactoryImpl.class.getName();
-    private static final ConcurrentHashMap<String, Delegator> delegatorCache = new ConcurrentHashMap<String, Delegator>();
+    private static final ConcurrentHashMap<String, Future<Delegator>> delegators = new ConcurrentHashMap<String, Future<Delegator>>();
+    private static final ThreadGroup DELEGATOR_THREAD_GROUP = new ThreadGroup("DelegatorFactory");
+    private static final ScheduledExecutorService executor = ExecutionPool.getExecutor(DELEGATOR_THREAD_GROUP, "delegator-startup", -1, true);
 
     public static Delegator getDelegator(String delegatorName) {
+        Future<Delegator> future = getDelegatorFuture(delegatorName);
+        try {
+            return future.get();
+        } catch (ExecutionException e) {
+            Debug.logError(e, module);
+            return null;
+        } catch (InterruptedException e) {
+            Debug.logError(e, module);
+            return null;
+        }
+    }
+
+    public static Future<Delegator> getDelegatorFuture(String delegatorName) {
         if (delegatorName == null) {
             delegatorName = "default";
             //Debug.logWarning(new Exception("Location where getting delegator with null name"), "Got a getGenericDelegator call with a null delegatorName, assuming default for the name.", module);
         }
         do {
-            Delegator delegator = delegatorCache.get(delegatorName);
+            Future<Delegator> future = delegators.get(delegatorName);
+            if (future != null) {
+                //Debug.logInfo("got delegator(future(" + delegatorName + ")) from cache", module);
+                return future;
+            }
+            FutureTask<Delegator> futureTask = new FutureTask<Delegator>(new DelegatorConfigurable(delegatorName));
+            //Debug.logInfo("putting delegator(future(" + delegatorName + ")) into cache", module);
+            if (delegators.putIfAbsent(delegatorName, futureTask) != null) {
+                continue;
+            }
+            executor.submit(futureTask);
+        } while (true);
+    }
 
-            if (delegator != null) {
+    public static final class DelegatorConfigurable implements Callable<Delegator> {
+        private final String delegatorName;
+
+        public DelegatorConfigurable(String delegatorName) {
+            this.delegatorName = delegatorName;
+        }
+
+        public Delegator call() throws ClassNotFoundException {
+            try {
+                Delegator delegator = UtilObject.getObjectFromFactory(DelegatorFactory.class, delegatorName);
+
                 // setup the Entity ECA Handler
                 delegator.initEntityEcaHandler();
-                //Debug.logInfo("got delegator(" + delegatorName + ") from cache", module);
-                
+
                 // setup the distributed CacheClear
                 delegator.initDistributedCacheClear();
 
                 return delegator;
-            }
-            try {
-                delegator = UtilObject.getObjectFromFactory(DelegatorFactory.class, delegatorName);
             } catch (ClassNotFoundException e) {
                 Debug.logError(e, module);
+                throw e;
             }
-            //Debug.logInfo("putting delegator(" + delegatorName + ") into cache", module);
-            delegatorCache.putIfAbsent(delegatorName, delegator);
-        } while (true);
+        }
     }
 }
