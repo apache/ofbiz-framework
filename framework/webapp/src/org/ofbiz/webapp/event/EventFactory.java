0083faf85b811acfff37644729b5568eb5479cc7
Jacopo Cappellato <jacopoc@apache.org>
2014-09-02 07:26:03 +0000

    Greatly cleaned-up and simplified the EventFactory and ViewFactory classes used by the RequestHandler: they are now (mostly) immutable and thread safe.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1621942 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/event/EventFactory.java
+++ framework/webapp/src/org/ofbiz/webapp/event/EventFactory.java
@@ -19,18 +19,16 @@
 package org.ofbiz.webapp.event;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.ServletContext;
 
-import javolution.util.FastMap;
-
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.GeneralRuntimeException;
 import org.ofbiz.base.util.ObjectType;
 import org.ofbiz.webapp.control.ConfigXMLReader;
-import org.ofbiz.webapp.control.WebAppConfigurationException;
 
 /**
  * EventFactory - Event Handler Factory
@@ -39,88 +37,29 @@ public class EventFactory {
 
     public static final String module = EventFactory.class.getName();
 
-    private final URL controllerConfigURL;
-    private final ServletContext context;
-    protected Map<String, EventHandler> handlers = null;
+    private final Map<String, EventHandler> handlers = new HashMap<String, EventHandler>();
 
     public EventFactory(ServletContext context, URL controllerConfigURL) {
-        handlers = FastMap.newInstance();
-        this.controllerConfigURL = controllerConfigURL;
-        this.context = context;
-
-        // pre-load all event handlers
+        // load all the event handlers
         try {
-            this.preLoadAll();
-        } catch (EventHandlerException e) {
+            Set<Map.Entry<String,String>> handlerEntries = ConfigXMLReader.getControllerConfig(controllerConfigURL).getEventHandlerMap().entrySet();
+            if (handlerEntries != null) {
+                for (Map.Entry<String,String> handlerEntry: handlerEntries) {
+                    EventHandler handler = (EventHandler) ObjectType.getInstance(handlerEntry.getValue());
+                    handler.init(context);
+                    this.handlers.put(handlerEntry.getKey(), handler);
+                }
+            }
+        } catch (Exception e) {
             Debug.logError(e, module);
             throw new GeneralRuntimeException(e);
         }
     }
 
-    private void preLoadAll() throws EventHandlerException {
-        Set<String> handlers = null;
-        try {
-            handlers = ConfigXMLReader.getControllerConfig(this.controllerConfigURL).getEventHandlerMap().keySet();
-        } catch (WebAppConfigurationException e) {
-            Debug.logError(e, "Exception thrown while parsing controller.xml file: ", module);
-        }
-        if (handlers != null) {
-            for (String type: handlers) {
-                this.handlers.put(type, this.loadEventHandler(type));
-            }
-        }
-    }
-
     public EventHandler getEventHandler(String type) throws EventHandlerException {
-        // check if we are new / empty and add the default handler in
-        if (handlers.size() == 0) {
-            this.preLoadAll();
-        }
-
-        // attempt to get a pre-loaded handler
         EventHandler handler = handlers.get(type);
-
         if (handler == null) {
-            synchronized (EventHandler.class) {
-                handler = handlers.get(type);
-                if (handler == null) {
-                    handler = this.loadEventHandler(type);
-                    handlers.put(type, handler);
-                }
-            }
-            if (handler == null)
-                throw new EventHandlerException("No handler found for type: " + type);
-        }
-        return handler;
-    }
-
-    public void clear() {
-        handlers.clear();
-    }
-
-    private EventHandler loadEventHandler(String type) throws EventHandlerException {
-        EventHandler handler = null;
-        String handlerClass = null;
-        try {
-            handlerClass = ConfigXMLReader.getControllerConfig(this.controllerConfigURL).getEventHandlerMap().get(type);
-        } catch (WebAppConfigurationException e) {
-            Debug.logError(e, "Exception thrown while parsing controller.xml file: ", module);
-        }
-        if (handlerClass == null) {
-            throw new EventHandlerException("Unknown handler type: " + type);
-        }
-
-        try {
-            handler = (EventHandler) ObjectType.getInstance(handlerClass);
-            handler.init(context);
-        } catch (NoClassDefFoundError e) {
-            throw new EventHandlerException("No class def found for handler [" + handlerClass + "]", e);
-        } catch (ClassNotFoundException cnf) {
-            throw new EventHandlerException("Cannot load handler class [" + handlerClass + "]", cnf);
-        } catch (InstantiationException ie) {
-            throw new EventHandlerException("Cannot get instance of the handler [" + handlerClass + "]", ie);
-        } catch (IllegalAccessException iae) {
-            throw new EventHandlerException(iae.getMessage(), iae);
+            throw new EventHandlerException("No handler found for type: " + type);
         }
         return handler;
     }
