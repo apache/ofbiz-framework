4656af219dedca789d71db9d6e360d46f09b5cca
Ashish Vijaywargiya <ashish@apache.org>
2014-12-22 05:04:18 +0000

    Based on Adrian's comment on Dev list removing the ternary operator check which was not needed actually.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1647239 13f79535-47bb-0310-9956-ffa450edef68

--- framework/common/src/org/ofbiz/common/FindServices.java
+++ framework/common/src/org/ofbiz/common/FindServices.java
@@ -603,7 +603,7 @@ public class FindServices {
         EntityConditionList<EntityCondition> entityConditionList = UtilGenerics.cast(context.get("entityConditionList"));
         List<String> orderByList = checkList(context.get("orderByList"), String.class);
         boolean noConditionFind = "Y".equals(context.get("noConditionFind"));
-        boolean distinct = "Y".equals(context.get("distinct"))? true : false;
+        boolean distinct = "Y".equals(context.get("distinct"));
         List<String> fieldList =  UtilGenerics.checkList(context.get("fieldList"));
         Locale locale = (Locale) context.get("locale");
         Set<String> fieldSet = null;
