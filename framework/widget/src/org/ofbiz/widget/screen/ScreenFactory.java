28fecf1efcb70b0d9e81a0fae3b8f090db0c3020
Adrian Crum <adrianc@apache.org>
2015-01-17 16:59:30 +0000

    Revert unintentional change in my last commit.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1652641 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/screen/ScreenFactory.java
+++ framework/widget/src/org/ofbiz/widget/screen/ScreenFactory.java
@@ -168,7 +168,7 @@ public class ScreenFactory {
             List<? extends Element> screenElements = UtilXml.childElementList(rootElement, "screen");
             for (Element screenElement: screenElements) {
                 ModelScreen modelScreen = new ModelScreen(screenElement, modelScreenMap, sourceLocation);
-                Debug.logInfo("Read Screen with name: " + modelScreen.getName() + "\n" + modelScreen, module);
+                //Debug.logInfo("Read Screen with name: " + modelScreen.getName(), module);
                 modelScreenMap.put(modelScreen.getName(), modelScreen);
             }
         }
