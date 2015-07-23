34de251fe7983202eabd56e4a1bc3bcdcb9e4f97
Jacques Le Roux <jleroux@apache.org>
2015-07-23 08:36:36 +0000

    The description attribute of the display-entity element is now escaped to prevent the risk of an XSS attack. [CVE-2015-3268] Stored Cross-Site Scripting Vulnerability affecting the description attribute of the display-entity element because it was not escaped.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1692357 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/model/ModelFormField.java
+++ framework/widget/src/org/ofbiz/widget/model/ModelFormField.java
@@ -1338,10 +1338,17 @@ public class ModelFormField {
                 retVal = this.getDescription().expandString(localContext, locale);
             }
             // try to get the entry for the field if description doesn't expand to anything
-            if (UtilValidate.isEmpty(retVal))
+            if (UtilValidate.isEmpty(retVal)) {
                 retVal = fieldValue;
-            if (UtilValidate.isEmpty(retVal))
+            } 
+            if (UtilValidate.isEmpty(retVal)) {
                 retVal = "";
+            } else if (this.getModelFormField().getEncodeOutput()) {
+                UtilCodec.SimpleEncoder simpleEncoder = (UtilCodec.SimpleEncoder) context.get("simpleEncoder");
+                if (simpleEncoder != null) {
+                    retVal = simpleEncoder.encode(retVal);
+                }
+            }
             return retVal;
         }
 
