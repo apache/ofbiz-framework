772c2b94b593a2901ed9c767dac6a5647a981700
Shi Jinghai <shijh@apache.org>
2015-08-21 10:03:11 +0000

    OFBIZ-4154 Use ZXing to generate QR 2d barcode.
    1. Add zxing-core-3.2.0.jar to framework/base/lib and .classpath.
    2. Add services_qrcode.xml and QRCodeService.java in framework/common/.
    3. Add qrcode request in common-controller.xml and QRCodeEvents.java under framework/common/.
    4. Add qrcode.properties and QRCodeUiLabels.xml under framework/common/.
    5. Add a barcode example page in specialpurpose/example, entry is on page: /example/control/EditExample?exampleId=EX01, the example can be seen on page: /example/control/ExampleReportPdfBarcode?exampleId=EX01
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1696921 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/common/src/org/ofbiz/common/qrcode/QRCodeEvents.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ofbiz.common.qrcode;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Locale;
+import java.util.Map;
+import java.lang.Integer;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.ofbiz.base.util.UtilHttp;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.ServiceUtil;
+
+/**
+ * Events for QRCode.
+ */
+public class QRCodeEvents {
+
+    public static final String module = QRCodeEvents.class.getName();
+    
+    /** Streams QR Code to the output. */
+    public static String serveQRCodeImage(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+        Map<String, Object> parameters = UtilHttp.getParameterMap(request);
+        String message = (String) parameters.get("message");
+        GenericValue userLogin = (GenericValue) request.getAttribute("userLogin");
+        if (userLogin == null) userLogin = (GenericValue) session.getAttribute("userLogin");
+        if (userLogin == null) userLogin = (GenericValue) session.getAttribute("autoUserLogin");
+        Locale locale = UtilHttp.getLocale(request);
+        
+        if (UtilValidate.isEmpty(message)) {
+            message = "Error get message parameter.";
+        }
+        String format = (String) parameters.get("format");
+        if (UtilValidate.isEmpty(format)) {
+            format = "jpg";
+        }
+        String mimeType = "image/" + format;
+        String width = (String) parameters.get("width");
+        String height = (String) parameters.get("height");
+        String encoding = (String) parameters.get("encoding");
+        Boolean verifyOutput = Boolean.valueOf((String) parameters.get("verifyOutput"));
+        String logoImageMaxWidth = (String) parameters.get("logoImageMaxWidth");
+        String logoImageMaxHeight = (String) parameters.get("logoImageMaxHeight");
+
+        try {
+            if (mimeType != null) {
+                response.setContentType(mimeType);
+            }
+            OutputStream os = response.getOutputStream();
+            Map<String, Object> context = UtilMisc.<String, Object>toMap("message", message, "format", format, "userLogin", userLogin, "locale", locale);
+            if (UtilValidate.isNotEmpty(width)) {
+            	try {
+                    context.put("width", Integer.parseInt(width));
+            	} catch (NumberFormatException e) {
+            		// do nothing
+            	}
+                if (UtilValidate.isEmpty(height)) {
+                	try {
+                        context.put("height", Integer.parseInt(width));
+                	} catch (NumberFormatException e) {
+                		// do nothing
+                	}
+                }
+            }
+            if (UtilValidate.isNotEmpty(height)) {
+            	try {
+                    context.put("height", Integer.parseInt(height));
+            	} catch (NumberFormatException e) {
+            		// do nothing
+            	}
+                if (UtilValidate.isEmpty(width)) {
+                	try {
+                        context.put("width", Integer.parseInt(height));
+                	} catch (NumberFormatException e) {
+                		// do nothing
+                	}
+                }
+            }
+            if (UtilValidate.isNotEmpty(encoding)) {
+                context.put("encoding", encoding);
+            }
+            if (UtilValidate.isNotEmpty(verifyOutput) && verifyOutput.booleanValue()) {
+            	context.put("verifyOutput", verifyOutput);
+            }
+            if (UtilValidate.isNotEmpty(logoImageMaxWidth)) {
+            	try {
+                    context.put("logoImageMaxWidth", Integer.parseInt(logoImageMaxWidth));
+            	} catch (NumberFormatException e) {
+            		// do nothing
+            	}
+            }
+            if (UtilValidate.isNotEmpty(logoImageMaxHeight)) {
+            	try {
+                    context.put("logoImageMaxHeight", Integer.parseInt(logoImageMaxHeight));
+            	} catch (NumberFormatException e) {
+            		// do nothing
+            	}
+            }
+            Map<String, Object> results = dispatcher.runSync("generateQRCodeImage", context);
+            if (!ServiceUtil.isError(results)) {
+                BufferedImage bufferedImage = (BufferedImage) results.get("bufferedImage");
+                if (!ImageIO.write(bufferedImage, format, os)) {
+                    String errMsg = UtilProperties.getMessage("QRCodeUiLabels", "ErrorWriteFormatToFile", new Object[] { format }, locale);
+                    request.setAttribute("_ERROR_MESSAGE_", errMsg);
+                    return "error";
+                }
+                os.flush();
+            } else {
+                String errMsg = ServiceUtil.getErrorMessage(results);
+                request.setAttribute("_ERROR_MESSAGE_", errMsg);
+                return "error";
+            }
+        } catch (IOException e) {
+            String errMsg = UtilProperties.getMessage("QRCodeUiLabels", "ErrorGenerateQRCode", new Object[] { e.getMessage() }, locale);
+            request.setAttribute("_ERROR_MESSAGE_", errMsg);
+            return "error";
+        } catch (GenericServiceException e) {
+            String errMsg = UtilProperties.getMessage("QRCodeUiLabels", "ErrorGenerateQRCode", new Object[] { e.getMessage() }, locale);
+            request.setAttribute("_ERROR_MESSAGE_", errMsg);
+            return "error";
+        }
+        return "success";
+    }
+}
---
+++ specialpurpose/example/webapp/example/reports/BarCode.fo.ftl
@@ -0,0 +1,63 @@
+<#--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+-->
+
+<#escape x as x?xml>
+<fo:root xmlns:fo="http://www.w3.org/1999/XSL/Format">
+
+    <fo:layout-master-set>
+        <fo:simple-page-master master-name="main" page-height="4in" page-width="8in"
+                               margin-top="0.5in" margin-bottom="0.25in" margin-left="0.25in" margin-right="0.25in">
+            <fo:region-body margin-top="0in"/>
+            <fo:region-before extent="0in"/>
+            <fo:region-after extent="0in"/>
+        </fo:simple-page-master>
+    </fo:layout-master-set>
+
+    <fo:page-sequence master-reference="main">
+        <fo:flow flow-name="xsl-region-body" font-family="Helvetica">
+            <fo:block text-align="center">
+                 ${example.exampleName!}
+            </fo:block>
+            <fo:block text-align="center">
+                <fo:instream-foreign-object>
+                    <barcode:barcode xmlns:barcode="http://barcode4j.krysalis.org/ns"
+                                     message="${exampleId}">
+                        <barcode:code39>
+                            <barcode:height>0.75in</barcode:height>
+                            <barcode:module-width>.375mm</barcode:module-width>
+                        </barcode:code39>
+                        <barcode:human-readable>
+                            <barcode:placement>bottom</barcode:placement>
+                            <barcode:font-name>Helvetica</barcode:font-name>
+                            <barcode:font-size>18pt</barcode:font-size>
+                            <barcode:display-start-stop>false</barcode:display-start-stop>
+                            <barcode:display-checksum>false</barcode:display-checksum>
+                        </barcode:human-readable>
+                    </barcode:barcode>
+                </fo:instream-foreign-object>
+            </fo:block>
+            <fo:block><fo:leader/></fo:block>
+            <fo:block text-align="center">
+                <#setting url_escaping_charset='UTF-8'>
+                <fo:external-graphic src="/example/control/qrcode;jsessionid=${session.id}?message=${example.exampleName?url}&amp;format=bmp&amp;encoding=UTF-8&amp;verifyOutput=true&amp;logoImageMaxWidth=20&amp;logoImageMaxHeight=20" overflow="hidden" height="1.5in" content-height="scale-to-fit"/>
+            </fo:block>
+        </fo:flow>
+    </fo:page-sequence>
+</fo:root>
+</#escape>
