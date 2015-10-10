3d444b6b8d0c4a757cf7ec952a930642a03e632e
Arun Patidar <arunpatidar@apache.org>
2015-10-10 06:16:51 +0000

    [OFBIZ-6666] Added missing service model attributes while cloning it.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1707837 13f79535-47bb-0310-9956-ffa450edef68

--- framework/service/src/org/ofbiz/service/ModelService.java
+++ framework/service/src/org/ofbiz/service/ModelService.java
@@ -215,6 +215,12 @@ public class ModelService extends AbstractMap<String, Object> implements Seriali
 
     public ModelService(ModelService model) {
         this.name = model.name;
+        this.semaphore = model.semaphore;
+        this.fromLoader = model.fromLoader;
+        this.debug = model.debug;
+        this.semaphoreWait = model.semaphoreWait;
+        this.semaphoreSleep = model.semaphoreSleep;
+        this.contextInfo = model.contextInfo;
         this.definitionLocation = model.definitionLocation;
         this.description = model.description;
         this.engineName = model.engineName;
