92888a634a274dfcd077606b9f49cf6b41d7e0fa
Jacques Le Roux <jleroux@apache.org>
2015-10-21 05:43:52 +0000

    Since we now use Java 8, we no longer need to use MaxPermSize
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1709718 13f79535-47bb-0310-9956-ffa450edef68

--- tools/startofbizPos.bat
+++ tools/startofbizPos.bat
@@ -28,7 +28,7 @@ rem ### Delete the last log
 rem del %OFBIZ_LOG%
 
 rem ###VM args block ####################################################
-rem set MEMIF=-Xms128M -Xmx512M -XX:MaxPermSize=512m
+rem set MEMIF=-Xms128M -Xmx512M
 rem # RMI settings
 rem set DEBUG=-Dsun.rmi.server.exceptionTrace=true
 rem # Automatic IP address for Windows
@@ -50,13 +50,13 @@ rem ### start ofbiz with previous set VMARGS
 rem "%JAVA_HOME%\bin\java" %VMARGS% -jar ofbiz.jar > %OFBIZ_LOG%
 
 rem ### This one is for more of a debugging mode
-rem "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -XX:MaxPermSize=512m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > runtime\logs\console.log
+rem "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar > runtime\logs\console.log
 
 rem ### Simple easy to read line
 cd %OFBIZ_HOME%
 echo on
-"%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -XX:MaxPermSize=512m -jar ofbiz.jar -pos
+"%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -jar ofbiz.jar -pos
 echo off
 rem ### If you would prefer the console output to be logged rather than displayed switch out the above line for this one
-rem "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -XX:MaxPermSize=512m -jar ofbiz.jar > runtime\logs\console.log
+rem "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -jar ofbiz.jar > runtime\logs\console.log
  
