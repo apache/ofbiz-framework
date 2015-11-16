1b8b15b45dada9113b1a53167941d8350826462d
Jacques Le Roux <jleroux@apache.org>
2015-11-16 21:54:10 +0000

    Fixed a bug introduced w/ r1714652
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1714688 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/org/ofbiz/base/location/FlexibleLocation.java
+++ framework/base/src/org/ofbiz/base/location/FlexibleLocation.java
@@ -53,7 +53,7 @@ public final class FlexibleLocation {
              * because this class may be initialized before all components
              * are loaded.
              */
-            Properties properties = UtilProperties.createProperties("locationresolvers");
+            Properties properties = UtilProperties.createProperties("locationresolvers.properties");
             if (properties != null) {
                 ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
                 for (Entry<Object, Object> entry : properties.entrySet()) {
