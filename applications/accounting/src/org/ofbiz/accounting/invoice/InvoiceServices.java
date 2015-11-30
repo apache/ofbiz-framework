3b84a03fabd0ba4a244bbbdc39c414e8aa10c214
Jacques Le Roux <jleroux@apache.org>
2015-11-30 13:01:47 +0000

    Updates commons collections from 3.2.2 to 4.1 - OFBIZ-6748
    Even if we are covered from the Java Object possible deserialisation exploit by the 3.2.2 version, since it's easy to update, this issue to update the trunk to the last available release.
    
    I updated the .classpath file by hand, but kept the tabs in it, so I could move misplaced entries to their right alphabetical places.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1717247 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/invoice/InvoiceServices.java
+++ applications/accounting/src/org/ofbiz/accounting/invoice/InvoiceServices.java
@@ -34,7 +34,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.ofbiz.accounting.payment.PaymentGatewayServices;
