44ac968e790cfe2a77c0dddd9573fa78caa85652
Deepak Dixit <deepak@apache.org>
2016-03-19 13:31:02 +0000

    (OFBIZ-5918) Fixed NPE for ecommerce/products url, If user enter invalid url ecommerce/products then redirect it to main.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1735773 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/src/org/ofbiz/product/category/CatalogUrlServlet.java
+++ applications/product/src/org/ofbiz/product/category/CatalogUrlServlet.java
@@ -82,6 +82,11 @@ public class CatalogUrlServlet extends HttpServlet {
 
         String productId = null;
         String categoryId = null;
+
+        if (pathElements == null) {
+            RequestDispatcher rd = request.getRequestDispatcher("/" + CONTROL_MOUNT_POINT + "/main");
+            rd.forward(request, response);
+        } else {
         try {
             String lastPathElement = pathElements.get(pathElements.size() - 1);
             if (lastPathElement.startsWith("p_")) {
@@ -146,6 +151,7 @@ public class CatalogUrlServlet extends HttpServlet {
 
         RequestDispatcher rd = request.getRequestDispatcher("/" + CONTROL_MOUNT_POINT + "/" + (productId != null ? PRODUCT_REQUEST : CATEGORY_REQUEST));
         rd.forward(request, response);
+        }
     }
 
     /**
