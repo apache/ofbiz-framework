6055cc411faac3e67c748bbca6ae1506a51c6142
Arun Patidar <arunpatidar@apache.org>
2016-06-28 14:02:14 +0000

    Applied patch from jira issue - OFBIZ-7420 - Enforce noninstantiability to WidgetPortalPageWorker Class. Thanks Rishi Solanki and Rahul Singh Kushwah for your contribution.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1750513 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/portal/WidgetPortalPageWorker.java
+++ framework/widget/src/org/ofbiz/widget/portal/WidgetPortalPageWorker.java
@@ -23,9 +23,12 @@ import org.ofbiz.base.util.Debug;
 /**
  * PortalPageWorker Class
  */
-public class WidgetPortalPageWorker {
+public final class WidgetPortalPageWorker {
     public static final String module = WidgetPortalPageWorker.class.getName();
-    public static PortalPageWorkerInterface portalPageWorker = null;
+    private static PortalPageWorkerInterface portalPageWorker = null;
+
+    private WidgetPortalPageWorker() {}
+
     static {
         try {
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
