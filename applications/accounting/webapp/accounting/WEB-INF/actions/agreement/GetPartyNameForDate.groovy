c604b73934d6af6cc1d8995368bb9a5ab13eb558
Ashish Vijaywargiya <ashish@apache.org>
2016-07-03 05:16:56 +0000

    Applied patch from jira issue - OFBIZ-7143 - Error on cancelling agreement. Thanks Ravi for the contribution. Thanks Montalbano for the discussion.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1751109 13f79535-47bb-0310-9956-ffa450edef68

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/agreement/GetPartyNameForDate.groovy
@@ -0,0 +1,8 @@
+partyIdFrom = parameters.partyIdFrom;
+partyIdTo = parameters.partyIdTo;
+if (partyIdFrom) {
+    context.partyNameResultFrom = runService("getPartyNameForDate", [partyId: partyIdFrom, compareDate: agreementDate, lastNameFirst: "Y"])
+}
+if (partyIdTo) {
+    context.partyNameResultTo = runService("getPartyNameForDate", [partyId: partyIdTo, compareDate: agreementDate, lastNameFirst: "Y"])
+}
