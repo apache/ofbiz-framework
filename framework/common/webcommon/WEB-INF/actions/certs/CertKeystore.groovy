414c48d1f24c16bb45d06cb2dbee1b589762f11e
Jacques Le Roux <jleroux@apache.org>
2016-07-17 08:14:46 +0000

    A modified patch from Pierre Smits for "Ftl to Widget: improve ViewCertificate to use widgets instead of ftl" https://issues.apache.org/jira/browse/OFBIZ-6302
    jleroux: I updated the patch. I fixed things by hand: tabs in the groovy files, removed the ftl file because of a tree conflicts due to the recent ftl files move and updated org.ofbiz to org.apache.ofbiz in CertKeystore.groovy
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1753024 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/common/webcommon/WEB-INF/actions/certs/CertKeystore.groovy
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.ofbiz.base.component.ComponentConfig;
+import org.apache.ofbiz.base.component.ComponentConfig.KeystoreInfo;
+import org.apache.ofbiz.base.util.*;
+import org.apache.ofbiz.base.util.KeyStoreUtil;
+
+import org.apache.ofbiz.entity.*;
+import org.apache.ofbiz.entity.util.*;
+
+cert = org.apache.ofbiz.base.util.KeyStoreUtil.pemToCert(certString)
+if (cert){
+    context.certType = cert.getType()
+    context.certName = cert.getSubjectX500Principal().getName()
+    context.certSerialNumber = cert.getSerialNumber().toString(16)
+    context.certPublicKey = cert.getPublicKey()
+}
+
+stores = []
+store = []
+Collection<ComponentConfig> allComponentConfigs = ComponentConfig.getAllComponents();
+for (ComponentConfig cc: allComponentConfigs) {
+    if (cc.getKeystoreInfos()){
+        componentName = cc.getComponentName()
+        store = ["componentId" : componentName]
+        store.componentName = componentName
+        for (KeystoreInfo ks : cc.getKeystoreInfos()) {keystoreName = ks.getName()
+            store.keystoreName = ks.getName()
+            }
+        stores.add(store)
+    }
+    
+}
+context.stores = stores
