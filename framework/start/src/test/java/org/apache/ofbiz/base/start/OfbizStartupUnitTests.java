d5cf4875a2e2e733fc51461f45f319cf7548b625
Taher A. Alkhateeb <taher@apache.org>
2016-07-18 17:28:31 +0000

    Introduce unit tests to the start component - OFBIZ-7897
    This is the first batch of unit tests for the Start component
    which introduces 8 unit tests for parsing the start commands
    and also some tests for the AdminClient.
    
    At some point if the tests become numerous we might need to
    break it down into multiple files. The tests are implemented
    with standard JUnit tests. A minor correction was done to the
    StartupCommandUtil class by converting the List of startup
    commands to a Set to remove duplicates. This was discovered
    thank to the unit tests being introduced.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1753274 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/start/src/test/java/org/apache/ofbiz/base/start/OfbizStartupUnitTests.java
@@ -0,0 +1,83 @@
+package org.apache.ofbiz.base.start;
+
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class OfbizStartupUnitTests {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void commandParserDoesNotAcceptMoreThanOneCommand() throws StartupException {
+        expectedException.expectMessage("an option from this group has already been selected");
+
+        StartupCommandUtil.parseOfbizCommands(new String[]{"--help", "--status"});
+    }
+
+    @Test
+    public void commandParserDoesNotAcceptPortoffsetWithoutArgument() throws StartupException {
+        expectedException.expectMessage("Missing argument for option");
+
+        StartupCommandUtil.parseOfbizCommands(new String[]{"--portoffset"});
+    }
+
+    @Test
+    public void commandParserDoesNotAcceptPortoffsetWithoutPositiveInteger() throws StartupException {
+        expectedException.expectMessage("you can only pass positive integers");
+
+        StartupCommandUtil.parseOfbizCommands(new String[]{"--portoffset", "ThisMustBeInteger54321"});
+    }
+
+    @Test
+    public void commandParserDoesNotAcceptArgumentForStatus() throws StartupException {
+        expectedException.expectMessage("unrecognized options / properties");
+
+        StartupCommandUtil.parseOfbizCommands(new String[]{"--status", "thisArgNotAllowed"});
+    }
+
+    @Test
+    public void commandParserDoesNotAcceptArgumentForStart() throws StartupException {
+        expectedException.expectMessage("unrecognized options / properties");
+
+        StartupCommandUtil.parseOfbizCommands(new String[]{"--start", "thisArgNotAllowed"});
+    }
+
+    @Test
+    public void commandParserDoesNotAcceptArgumentForShutdown() throws StartupException {
+        expectedException.expectMessage("unrecognized options / properties");
+
+        StartupCommandUtil.parseOfbizCommands(new String[]{"--shutdown", "thisArgNotAllowed"});
+    }
+
+    @Test
+    public void commandParserCombinesMultipleArgsIntoOneCommand() throws StartupException {
+        String[] multiArgCommand = new String[] {
+                "--load-data", "readers=seed,seed-initial",
+                "--load-data", "delegator=default",
+                "-l", "timeout=7200" };
+
+        List<StartupCommand> startupCommands = StartupCommandUtil.parseOfbizCommands(multiArgCommand);
+
+        assertThat(startupCommands.size(), equalTo(1));
+        assertThat(startupCommands.get(0).getProperties().size(), equalTo(3));
+    }
+
+    @Test
+    public void adminClientReturnsTheCorrectMessageIfServerIsDown() throws StartupException {
+        assertThat(sendRequestToAdminClient("--status"), equalTo("OFBiz is Down"));
+        assertThat(sendRequestToAdminClient("--shutdown"), equalTo("OFBiz is Down"));
+    }
+
+    private String sendRequestToAdminClient(String request) throws StartupException {
+        List<StartupCommand> startupCommands = StartupCommandUtil.parseOfbizCommands(new String[]{request});
+        Config config = new Config(startupCommands);
+        return AdminClient.requestStatus(config);
+    }
+}
