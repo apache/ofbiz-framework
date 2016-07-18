e0a82dc93251145fcbecb284dbf763420f21790c
Jacques Le Roux <jleroux@apache.org>
2016-07-18 07:51:10 +0000

    Updates modified demos scripts
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1753166 13f79535-47bb-0310-9956-ffa450edef68

--- tools/demo-backup/check-svn-update.sh
+++ tools/demo-backup/check-svn-update.sh
@@ -1,39 +1,24 @@
 #!/bin/sh
 
-# check trunk for updates finally no longer done, restart each morning at 3
+# Restart each morning at 3
 
 cd /home/ofbizDemo/trunk
-#svn st -u | grep '*'
-
-#if [ $? = 0 ]; then
-    svn up
-    ./ant stop
-    ./ant clean-all
-    ./ant load-demo
-    ./ant svninfo
-    #./ant start  > console.log
-    ./ant start-batch-secure
-#fi
-
-# check branch for updates
+svn up
+./gradlew "ofbiz --shutdown" 
+./gradlew cleanAll 
+./gradlew loadDefault 
+./gradlew svnInfoFooter 
+./gradlew "ofbizBackgroundSecure --start" 
 
 cd /home/ofbizDemo/branch13.7
-#svn st -u | grep '*'
-
-#if [ $? = 0 ]; then
-    svn up
-    ./ant stop -Dportoffset=10000
-    ./ant clean-all
-    ./ant load-demo
-    ./ant svninfo
-#    #./ant start -Dportoffset=10000 > console.log
-    ./ant start-batch-secure -Dportoffset=10000
-#fi
+svn up
+./ant stop -Dportoffset=10000
+./ant clean-all
+./ant load-demo
+./ant svninfo
+./ant start-batch-secure -Dportoffset=10000
 
 cd /home/ofbizDemo/branch12.4
-#svn st -u | grep '*'
-
-#if [ $? = 0 ]; then
     svn up
     tools/stopofbiz.sh
     sleep 10
@@ -46,4 +31,3 @@ cd /home/ofbizDemo/branch12.4
     ./ant svninfo
     sleep 10
     nohup tools/startofbiz-secure.sh &
-#fi
