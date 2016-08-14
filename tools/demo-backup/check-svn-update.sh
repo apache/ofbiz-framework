c7bc14b254e3b9e558e992f0d65dbb969d27721c
Jacques Le Roux <jleroux@apache.org>
2016-08-14 21:42:14 +0000

    Updates trunk scripts for OFBIZ-7951
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1756332 13f79535-47bb-0310-9956-ffa450edef68

--- tools/demo-backup/check-svn-update.sh
+++ tools/demo-backup/check-svn-update.sh
@@ -3,31 +3,31 @@
 # Restart each morning at 3
 
 cd /home/ofbizDemo/trunk
-svn up
-./gradlew terminateOfbiz
-./gradlew cleanAll 
-./gradlew loadDefault 
-./gradlew svnInfoFooter 
-./gradlew "ofbizBackgroundSecure --start" 
+    svn up
+    ./gradlew terminateOfbiz
+    ./gradlew cleanAll 
+    ./gradlew loadDefault 
+    ./gradlew svnInfoFooter 
+    ./gradlew ofbizBackground 
 
 cd /home/ofbizDemo/branch13.7
-svn up
-./ant stop -Dportoffset=10000
-./ant clean-all
-./ant load-demo
-./ant svninfo
-./ant start-batch-secure -Dportoffset=10000
-
-cd /home/ofbizDemo/branch12.4
     svn up
-    tools/stopofbiz.sh
-    sleep 10
-    tools/stopofbiz.sh
-    sleep 10
-    tools/stopofbiz.sh
-    sleep 10
+    ./ant stop -Dportoffset=10000
     ./ant clean-all
     ./ant load-demo
     ./ant svninfo
-    sleep 10
-    nohup tools/startofbiz-secure.sh &
+    ./ant start-batch-secure -Dportoffset=10000
+
+cd /home/ofbizDemo/branch12.4
+        svn up
+        tools/stopofbiz.sh
+        sleep 10
+        tools/stopofbiz.sh
+        sleep 10
+        tools/stopofbiz.sh
+        sleep 10
+        ./ant clean-all
+        ./ant load-demo
+        ./ant svninfo
+        sleep 10
+        nohup tools/startofbiz-secure.sh &
