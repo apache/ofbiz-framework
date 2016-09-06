c750a95810b94c44b932033dc0c4e68650bd187b
Jacques Le Roux <jleroux@apache.org>
2016-09-06 16:38:14 +0000

    Fixes "The "Only Not Used Labels" option of the Label Manager is broken" OFBIZ-8114
    Simply replaces the parenthesis in ServiceUtil.getResource() by 2 spaces when parsing files with getLabelsFromJavaFiles() and getLabelsFromFtlFiles(). 
    ServiceUtil.getResource() breaks the parsing
    Note: no occurrences of ServiceUtil.getResource() yet in FTL files, and only 16 in Java files.
    
    Also replaces "script" by "minilang" in getLabelsFromSimpleMethodFiles()
    
    This adds a getResourceRegex  in a new webtools.properties files. This property
    * Allow to remove this string from files when parsing with Label Manager, else it breaks 
    * Moreover the string can't be in the LabelReferences.java file, to avoid side effects
    
    I have also increased the transaction-timeout of the screen "SearchLabels" to 600. 
    Because when using the "Only Not Used Labels" option the 1st pass can be long. 10 min should plenty enough, even on old machines. Not blocking anyway, just a bad stack trace in log
    I'm sorry you will see a lot of false changes because the LabelManagerScreens.xml had no svn:properties at all, here they are now.
    
    
    We have 5237 "Not Used Labels". I wondered if they were all real, I just checked some an indeed, they were not used. 
    But there are also a huge bunch of *.description.* labels which certainly make sense. I'll remove them from the "Only Not Used Labels" result in another Jira I'll create
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1759457 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webtools/src/main/java/org/apache/ofbiz/webtools/labelmanager/LabelReferences.java
+++ framework/webtools/src/main/java/org/apache/ofbiz/webtools/labelmanager/LabelReferences.java
@@ -36,6 +36,7 @@ import org.apache.ofbiz.base.component.ComponentConfig;
 import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.base.util.FileUtil;
 import org.apache.ofbiz.base.util.UtilFormatOut;
+import org.apache.ofbiz.base.util.UtilProperties;
 import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.base.util.UtilXml;
 import org.apache.ofbiz.entity.Delegator;
@@ -59,6 +60,8 @@ public class LabelReferences {
     private static final String uiLabelMap = "uiLabelMap.";
     private static final String formFieldTitle = "FormFieldTitle_";
     private static final String getMessage = "UtilProperties.getMessage(";
+    private static final String getResourceRegex = UtilProperties.getPropertyValue("webtools", "getResourceRegex");
+    private static final String getResource = "ServiceUtil.getResource  ";
 
     protected Map<String, Map<String, Integer>> references = new TreeMap<String, Map<String, Integer>>();
     protected Delegator delegator;
@@ -159,6 +162,7 @@ public class LabelReferences {
             List<File> ftlFiles = FileUtil.findFiles("ftl", rootFolder, null, null);
             for (File file : ftlFiles) {
                 String inFile = FileUtil.readString("UTF-8", file);
+                inFile = inFile.replaceAll(getResourceRegex, getResource);
                 int pos = inFile.indexOf(bracketedUiLabelMap);
                 while (pos >= 0) {
                     int endPos = inFile.indexOf("}", pos);
@@ -182,6 +186,7 @@ public class LabelReferences {
             List<File> javaFiles = FileUtil.findFiles("java", rootFolder + "src", null, null);
             for (File javaFile : javaFiles) {
                 String inFile = FileUtil.readString("UTF-8", javaFile);
+                inFile = inFile.replaceAll(getResourceRegex, getResource);
                 int pos = inFile.indexOf(getMessage);
                 while (pos >= 0) {
                     int endLabel = inFile.indexOf(")", pos);
@@ -247,7 +252,7 @@ public class LabelReferences {
 
     private void getLabelsFromSimpleMethodFiles() throws IOException {
         for (String rootFolder : this.rootFolders) {
-            List<File> simpleMethodsFiles = FileUtil.findFiles("xml", rootFolder + "script", null, null);
+            List<File> simpleMethodsFiles = FileUtil.findFiles("xml", rootFolder + "minilang", null, null);
             for (File file : simpleMethodsFiles) {
                 String inFile = FileUtil.readString("UTF-8", file);
                 findUiLabelMapInFile(inFile, file.getPath());
