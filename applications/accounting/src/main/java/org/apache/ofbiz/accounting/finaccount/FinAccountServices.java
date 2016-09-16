581080d4fd707b4ab0a12253466e0cdc5af32026
Jacques Le Roux <jleroux@apache.org>
2016-09-16 11:53:27 +0000

    Improves: Use try-with-resources statement wherever it's possible (OFBIZ-8202)
    These are a non functional changes for the accounting component
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1761023 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/main/java/org/apache/ofbiz/accounting/finaccount/FinAccountServices.java
+++ applications/accounting/src/main/java/org/apache/ofbiz/accounting/finaccount/FinAccountServices.java
@@ -376,10 +376,7 @@ public class FinAccountServices {
                         EntityCondition.makeCondition("finAccountId", EntityOperator.EQUALS, finAccountId));
                 EntityCondition condition = EntityCondition.makeCondition(exprs, EntityOperator.AND);
 
-                EntityListIterator eli = null;
-                try {
-                    eli = EntityQuery.use(delegator).from("FinAccountTrans").where(condition).orderBy("-transactionDate").queryIterator();
-
+                try (EntityListIterator eli  = EntityQuery.use(delegator).from("FinAccountTrans").where(condition).orderBy("-transactionDate").queryIterator()) {
                     GenericValue trans;
                     while (remainingBalance.compareTo(FinAccountHelper.ZERO) < 0 && (trans = eli.next()) != null) {
                         String orderId = trans.getString("orderId");
@@ -475,14 +472,6 @@ public class FinAccountServices {
                 } catch (GeneralException e) {
                     Debug.logError(e, module);
                     return ServiceUtil.returnError(e.getMessage());
-                } finally {
-                    if (eli != null) {
-                        try {
-                            eli.close();
-                        } catch (GenericEntityException e) {
-                            Debug.logWarning(e, module);
-                        }
-                    }
                 }
 
                 // check to make sure we balanced out
