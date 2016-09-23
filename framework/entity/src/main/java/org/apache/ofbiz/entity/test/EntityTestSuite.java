774c8a0dba9d77c152602e148f013a251fc659ab
Jacopo Cappellato <jacopoc@apache.org>
2016-09-23 18:23:23 +0000

    Implemented: converted EntitySaxReader to use the default Sax reader instead of the legacy implementation from Javolution; removed a bunch of unused or deprecated methods; created some unit tests to test the EntitySaxReader in isolation. Removed the dependency from Javolution since this was the last class dependent on it. (OFBIZ-5169)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1762079 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/main/java/org/apache/ofbiz/entity/test/EntityTestSuite.java
+++ framework/entity/src/main/java/org/apache/ofbiz/entity/test/EntityTestSuite.java
@@ -1041,13 +1041,15 @@ public class EntityTestSuite extends EntityTestCase {
      * Tests EntitySaxReader, verification loading data with tag create, create-update, create-replace, delete 
      */
     public void testEntitySaxReaderCreation() throws Exception {
-        String xmlContentLoad = 
+        String xmlContentLoad =
+                "<entity-engine-xml>" +
                 "<TestingType testingTypeId=\"JUNIT-TEST\" description=\"junit test\"/>" +
                 "<create>" +
                 "    <TestingType testingTypeId=\"JUNIT-TEST2\" description=\"junit test\"/>" +
                 "    <Testing testingId=\"T1\" testingTypeId=\"JUNIT-TEST\" testingName=\"First test\" testingSize=\"10\" testingDate=\"2010-01-01 00:00:00\"/>" +
                 "</create>" +
-                "<Testing testingId=\"T2\" testingTypeId=\"JUNIT-TEST2\" testingName=\"Second test\" testingSize=\"20\" testingDate=\"2010-02-01 00:00:00\"/>";
+                "<Testing testingId=\"T2\" testingTypeId=\"JUNIT-TEST2\" testingName=\"Second test\" testingSize=\"20\" testingDate=\"2010-02-01 00:00:00\"/>" +
+                "</entity-engine-xml>";
         EntitySaxReader reader = new EntitySaxReader(delegator);
         long numberLoaded = reader.parse(xmlContentLoad);
         assertEquals("Create Entity loaded ", 4, numberLoaded);
@@ -1068,8 +1070,10 @@ public class EntityTestSuite extends EntityTestCase {
 
     public void testEntitySaxReaderCreateSkip() throws Exception {
         String xmlContentLoad =
+                "<entity-engine-xml>" +
                 "<TestingType testingTypeId=\"reader-create-skip\" description=\"reader create skip\"/>" +
-                "<Testing testingId=\"reader-create-skip\" testingTypeId=\"reader-create-skip\" testingName=\"reader create skip\" testingSize=\"10\" testingDate=\"2010-01-01 00:00:00\"/>";
+                "<Testing testingId=\"reader-create-skip\" testingTypeId=\"reader-create-skip\" testingName=\"reader create skip\" testingSize=\"10\" testingDate=\"2010-01-01 00:00:00\"/>" +
+                "</entity-engine-xml>";
         EntitySaxReader reader = new EntitySaxReader(delegator);
         long numberLoaded = reader.parse(xmlContentLoad);
         xmlContentLoad =
@@ -1089,13 +1093,15 @@ public class EntityTestSuite extends EntityTestCase {
 
     public void testEntitySaxReaderUpdate() throws Exception {
         String xmlContentLoad =
+                "<entity-engine-xml>" +
                 "<TestingType testingTypeId=\"create-update\" description=\"create update\"/>" +
                 "<TestingType testingTypeId=\"create-updated\" description=\"create update updated\"/>" +
                 "<Testing testingId=\"create-update-T3\" testingTypeId=\"create-update\" testingName=\"Test 3\" testingSize=\"10\" testingDate=\"2010-01-01 00:00:00\"/>" +
                 "<create-update>" +
                 "    <Testing testingId=\"create-update-T1\" testingTypeId=\"create-update\" testingName=\"First test update\" testingSize=\"20\" testingDate=\"2010-01-01 00:00:00\"/>" +
                 "    <Testing testingId=\"create-update-T3\" testingTypeId=\"create-updated\" testingName=\"Third test\" testingSize=\"30\" testingDate=\"2010-03-01 00:00:00\"/>" +
-                "</create-update>";
+                "</create-update>" +
+                "</entity-engine-xml>";
         EntitySaxReader reader = new EntitySaxReader(delegator);
         long numberLoaded = reader.parse(xmlContentLoad);
         assertEquals("Update Entity loaded ", 5, numberLoaded);
@@ -1116,12 +1122,14 @@ public class EntityTestSuite extends EntityTestCase {
 
     public void testEntitySaxReaderReplace() throws Exception {
         String xmlContentLoad =
+                "<entity-engine-xml>" +
                 "<TestingType testingTypeId=\"create-replace\" description=\"reader create skip\"/>" +
                 "<Testing testingTypeId=\"create-replace\" testingId=\"create-replace-T1\" testingName=\"First test\" testingSize=\"10\" testingDate=\"2010-01-01 00:00:00\"/>" +
                 "<create-replace>" +
                 "    <Testing testingTypeId=\"create-replace\" testingId=\"create-replace-T1\" testingName=\"First test replace\" />" +
                 "</create-replace>" +
-                "<Testing testingTypeId=\"create-replace\" testingId=\"create-replace-T2\" testingName=\"Second test update\" testingSize=\"20\" testingDate=\"2010-02-01 00:00:00\"/>";
+                "<Testing testingTypeId=\"create-replace\" testingId=\"create-replace-T2\" testingName=\"Second test update\" testingSize=\"20\" testingDate=\"2010-02-01 00:00:00\"/>" +
+                "</entity-engine-xml>";
         EntitySaxReader reader = new EntitySaxReader(delegator);
         long numberLoaded = reader.parse(xmlContentLoad);
         assertEquals("Replace Entity loaded ", 4, numberLoaded);
---
+++ framework/entity/src/test/java/org/apache/ofbiz/entity/util/EntitySaxReaderTests.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.apache.ofbiz.entity.util;
+
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.model.ModelEntity;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class EntitySaxReaderTests {
+    @Test
+    public void constructorWithDefaultTimeout() {
+        Delegator delegator = mock(Delegator.class);
+        EntitySaxReader esr = new EntitySaxReader(delegator); // create a reader with default tx timeout
+        verify(delegator).cloneDelegator();
+        verifyNoMoreInteractions(delegator);
+        assertEquals(esr.DEFAULT_TX_TIMEOUT, esr.getTransactionTimeout());
+    }
+
+    @Test
+    public void constructorWithTimeout() {
+        Delegator delegator = mock(Delegator.class);
+        EntitySaxReader esr = new EntitySaxReader(delegator, 14400); // create a reader with a non default tx timeout
+        verify(delegator).cloneDelegator();
+        verifyNoMoreInteractions(delegator);
+        assertEquals(14400, esr.getTransactionTimeout());
+    }
+
+    @Test
+    public void parse() throws Exception {
+        Delegator delegator = mock(Delegator.class);
+        Delegator clonedDelegator = mock(Delegator.class);
+        GenericValue genericValue = mock(GenericValue.class);
+        ModelEntity modelEntity = mock(ModelEntity.class);
+        when(delegator.cloneDelegator()).thenReturn(clonedDelegator);
+        when(clonedDelegator.makeValue("EntityName")).thenReturn(genericValue);
+        when(genericValue.getModelEntity()).thenReturn(modelEntity);
+        when(genericValue.containsPrimaryKey()).thenReturn(true);
+        when(modelEntity.isField("fieldName")).thenReturn(true);
+
+        EntitySaxReader esr = new EntitySaxReader(delegator);
+        String input = "<entity-engine-xml><EntityName fieldName=\"field value\"/></entity-engine-xml>";
+        long recordsProcessed = esr.parse(input);
+        verify(clonedDelegator).makeValue("EntityName");
+        assertEquals(1, recordsProcessed);
+    }
+}
