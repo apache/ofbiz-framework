e97cc781411ed45c46cc09fc0ff55ca1dfcc0966
Jacques Le Roux <jleroux@apache.org>
2016-09-29 14:34:32 +0000

    Improves: adds minor information to not forget when changing
    contrast-rO0.jar will be only useful as long as we use R13.07 and R12.04 as
    respectively stable and old
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1762786 13f79535-47bb-0310-9956-ffa450edef68

--- tools/demo-backup/check-svn-update.sh
+++ tools/demo-backup/check-svn-update.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # Restart each morning at 3
+# crontab -e
+# add : 0 3 * * * nice -n 19 ionice -c2 -n7 /home/ofbizDemo/check-svn-update.sh > /home/ofbizDemo/cronlog-svn-update.log 2>&1
 
 cd /home/ofbizDemo/trunk
     svn up
     ./gradlew terminateOfbiz
-    ./gradlew cleanAll 
-    ./gradlew loadDefault 
-    ./gradlew svnInfoFooter 
+    ./gradlew cleanAll
+    ./gradlew loadDefault
+    ./gradlew svnInfoFooter
     ./gradlew ofbizBackground
 
 cd /home/ofbizDemo/branch13.7
