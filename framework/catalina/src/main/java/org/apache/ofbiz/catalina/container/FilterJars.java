03c1a2438f0c276669e3c1eb2fbda67ef24a1470
Jacques Le Roux <jleroux@apache.org>
2016-10-04 08:09:00 +0000

    Improves: Add WebSocket support in OFBiz (OFBIZ-7073)
    With last Amardeep Singh Jhajj's patch we introduced a slow down when starting.
    
    The simple trick is to check if we want to use websocket or not. 
    By default (webSocket=false in catalina.properties) we don't and I gain 17 sec 
    on my machine (13 instead of 30). Not a big deal but the log is also cleaner. 
    And I guess not everybody needs websockets.
    
    This should be documented in the wiki page to come with OFBIZ-7538. Then the 
    comment for webSocket in catalina.properties should be changed accordingly
    
    If we want to always have webSocket available, then we should start from 
    Tomcat StandardJarScanFilter class and adapt it to our needs.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1763241 13f79535-47bb-0310-9956-ffa450edef68

--- framework/catalina/src/main/java/org/apache/ofbiz/catalina/container/FilterJars.java
+++ framework/catalina/src/main/java/org/apache/ofbiz/catalina/container/FilterJars.java
@@ -1,16 +1,13 @@
 package org.apache.ofbiz.catalina.container;
 
-import org.apache.tomcat.JarScanType; 
+import org.apache.ofbiz.base.util.UtilProperties;
 import org.apache.tomcat.JarScanFilter;
+import org.apache.tomcat.JarScanType;
 
 final class FilterJars implements JarScanFilter {
 
     @Override
     public boolean check(final JarScanType jarScanType, final String jarName) {
-        if (jarName.contains("ofbiz.jar")) {
-            return true; 
-        } else {
-            return false;
-        }
+        return UtilProperties.getPropertyAsBoolean("catalina", "webSocket", false) ? jarName.contains("ofbiz.jar") : false;
     } 
 }
