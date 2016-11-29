090c93d37f785dc50004d789d9a6a563e585a1db
Jacopo Cappellato <jacopoc@apache.org>
2016-11-29 17:41:30 +0000

    [Implemented]: Upgraded Freemarker to the latest release 2.3.25.
    This upgrade fixes the SuppressFBWarnings warnings occuring at compilation time.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1771927 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/main/java/org/apache/ofbiz/base/util/template/FreeMarkerWorker.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/template/FreeMarkerWorker.java
@@ -71,7 +71,7 @@ public final class FreeMarkerWorker {
 
     public static final String module = FreeMarkerWorker.class.getName();
 
-    public static final Version version = Configuration.VERSION_2_3_24;
+    public static final Version version = Configuration.VERSION_2_3_25;
 
     private FreeMarkerWorker () {}
 
