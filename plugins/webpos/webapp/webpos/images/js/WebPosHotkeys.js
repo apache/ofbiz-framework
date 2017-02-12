4b479981714c08188c4973f5262c5136172b21fd
Deepak Dixit <deepak@apache.org>
2017-02-12 10:32:53 +0000

    Improved: Moves plugins and creates the new structure (OFBIZ-9182)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1782651 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/webpos/webapp/webpos/images/js/WebPosHotkeys.js
+++
@@ -1,8 +0,0 @@
-WebPosHotkeys = {
-    hotkeys: [],
-    
-    bind: function (type, data, fnCode, fn, label) {
-        var arr = [type, data, fnCode, fn, label];
-        this.hotkeys.push(arr);
-    }
-}
