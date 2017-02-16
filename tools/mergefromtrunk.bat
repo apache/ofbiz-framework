a83259ebcee62ee2167bb5b41f5d97f3ebf4bd70
Jacques Le Roux <jleroux@apache.org>
2017-02-16 13:39:13 +0000

    No functional change, updates files with new svn url
    Removes useless cleanBuild.bat
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1783204 13f79535-47bb-0310-9956-ffa450edef68

--- tools/mergefromtrunk.bat
+++ tools/mergefromtrunk.bat
@@ -1,4 +1,4 @@
-@echo off
+s@echo off
 rem #####################################################################
 rem Licensed to the Apache Software Foundation (ASF) under one
 rem or more contributor license agreements.  See the NOTICE file
@@ -30,14 +30,14 @@ set /a prevRev=%version% - 1
 
 rem build the comment
 echo "Applied fix from trunk for revision: %version%" > comment.tmp
-svn log https://svn.apache.org/repos/asf/ofbiz/trunk -r %version% > log.tmp
+svn log https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk -r %version% > log.tmp
 copy comment.tmp + log.tmp = comment.tmp
 del log.tmp
 rem keep the comment.tmp file svn ignored. In case of trouble always happier to keep trace.  It will be overidden in next backport.
 
 rem commit the backport to release with comment fom file
 echo on
-svn merge -r %prevRev%:%version% https://svn.apache.org/repos/asf/ofbiz/trunk
+svn merge -r %prevRev%:%version% https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk
 echo off
 
 :menu
