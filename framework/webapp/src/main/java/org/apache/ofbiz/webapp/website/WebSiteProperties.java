80cae274c020ee43d985a48b6d611c5659229766
Jacques Le Roux <jleroux@apache.org>
2017-03-01 13:36:05 +0000

    Fixed: "Login and logout process in demos shows a certificate issue" (OFBIZ-9206)
    After my conclusions at OFBIZ-9240 I decided to reapply the changes and will 
    test on demos.
    
    Because WebSiteProperties reuse the port initially found in the 1st login URL
    I tried to replace locally
    port.https=8443
    by
    port.https=
    in url.properties (w/o SystemProperty) and did not face any issue but with 
    portOffset. This is due to how the WebSiteProperties class works and there is also 
    an easy fix: don't add twice the portOffset when it's build from the request, 
    and only then. Keep it as is when it's build from a WebSite GenericValue. 
    We then trust the user and don't rely on the request.
    
    I also removed the deprecated RequestHandler.getDefaultServerRootUrl()
    I think it was time...
    
    Thanks: Pierre Smits for report
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1784930 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/website/WebSiteProperties.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/website/WebSiteProperties.java
@@ -57,6 +57,7 @@ public final class WebSiteProperties {
         Assert.notNull("request", request);
         WebSiteProperties webSiteProps = (WebSiteProperties) request.getAttribute("_WEBSITE_PROPS_");
         if (webSiteProps == null) {
+            Boolean dontAddPortoffset = false;
             Delegator delegator = (Delegator) request.getAttribute("delegator");
             WebSiteProperties defaults = new WebSiteProperties(delegator);
             String httpPort = defaults.getHttpPort();
@@ -95,6 +96,7 @@ public final class WebSiteProperties {
             }
             if (httpsPort.isEmpty() && request.isSecure()) {
                 httpsPort = String.valueOf(request.getServerPort());
+                dontAddPortoffset = true; // We take the port from the request, don't add the portOffset
             }
             if (httpsHost.isEmpty()) {
                 httpsHost = request.getServerName();
@@ -104,10 +106,14 @@ public final class WebSiteProperties {
                 Integer httpPortValue = Integer.valueOf(httpPort);
                 httpPortValue += Start.getInstance().getConfig().portOffset;
                 httpPort = httpPortValue.toString();
-                Integer httpsPortValue = Integer.valueOf(httpsPort);
-                httpsPortValue += Start.getInstance().getConfig().portOffset;
-                httpsPort = httpsPortValue.toString();
-            }                
+                if (!dontAddPortoffset) {
+                    Integer httpsPortValue = Integer.valueOf(httpsPort);
+                    if (!httpsPort.isEmpty()) {
+                        httpsPortValue += Start.getInstance().getConfig().portOffset;
+                    }
+                    httpsPort = httpsPortValue.toString();
+                }
+            }
             
             webSiteProps = new WebSiteProperties(httpPort, httpHost, httpsPort, httpsHost, enableHttps);
             request.setAttribute("_WEBSITE_PROPS_", webSiteProps);
@@ -138,9 +144,9 @@ public final class WebSiteProperties {
             httpPortValue += Start.getInstance().getConfig().portOffset;
             httpPort = httpPortValue.toString();
             Integer httpsPortValue = Integer.valueOf(httpsPort);
-            httpsPortValue += Start.getInstance().getConfig().portOffset;
+            httpsPortValue += Start.getInstance().getConfig().portOffset; // Here unlike above we trust the user and don't rely on the request, no dontAddPortoffset.
             httpsPort = httpsPortValue.toString();
-        }                
+        }
         
         return new WebSiteProperties(httpPort, httpHost, httpsPort, httpsHost, enableHttps);
     }
