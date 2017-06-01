apache/ofbiz-framework

commit 703f96caa6dd07bdde47cd5a8181199e57344da7

No functional changes.
Replaces entityListIterator.close() in finally by try-with-ressource

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1797222 13f79535-47bb-0310-9956-ffa450edef68

applications/party/src/main/java/org/apache/ofbiz/party/communication/CommunicationEventServices.java
@@ -1151,21 +1151,12 @@ public class CommunicationEventServices {
                         inputFields.put("infoString_ic", caseInsensitiveEmail);
                         result = dispatcher.runSync("performFind", UtilMisc.<String, Object>toMap("entityName", "WorkEffortContactMechView"
                                 ,"inputFields", inputFields, "userLogin", userLogin));
-                        EntityListIterator listIt = (EntityListIterator) result.get("listIt");
-
-                        try {
+                        try (EntityListIterator listIt = (EntityListIterator) result.get("listIt")) {
                             List<GenericValue> list = listIt.getCompleteList();
                             List<GenericValue> filteredList = EntityUtil.filterByDate(list);
                             tempResults.addAll(filteredList);
                         } catch (GenericEntityException e) {
                             Debug.logError(e, module);
-                        } finally {
-                            try {
-                                listIt.close();
-                            } catch (GenericEntityException e) {
-                                Debug.logError(e, module);
-                            }
-                        }
                     }
                 }
             }

framework/common/src/main/java/org/apache/ofbiz/common/test/PerformFindTests.java
@@ -42,20 +42,11 @@ public class PerformFindTests extends OFBizTestCase {
     }
 
     private List<GenericValue> getCompleteList(Map<String, Object> context) {
-        EntityListIterator listIt = (EntityListIterator) context.get("listIt");
         List<GenericValue> foundElements = new LinkedList<GenericValue>();
-        if (listIt != null) {
-            try {
+            try (EntityListIterator listIt = (EntityListIterator) context.get("listIt")) {
                 foundElements = listIt.getCompleteList();
             } catch (GenericEntityException e) {
                 Debug.logError(" Failed to extract values from EntityListIterator after a performFind service", module);
-            } finally {
-                try {
-                    listIt.close();
-                } catch (GenericEntityException e) {
-                    Debug.logError(" Failed to close EntityListIterator after a performFind service", module);
-                }
-            }
         }
         return foundElements;
     }

commit b9fe4b96199c11173f31458b499edd07d9dde866

Fixes a typo just introduced with r1797222 (missing "}")
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1797224 13f79535-47bb-0310-9956-ffa450edef68

applications/party/src/main/java/org/apache/ofbiz/party/communication/CommunicationEventServices.java
@@ -18,8 +18,6 @@
  *******************************************************************************/
 
 package org.apache.ofbiz.party.communication;
-import org.apache.ofbiz.base.util.GeneralException;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -47,6 +45,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ofbiz.base.location.FlexibleLocation;
 import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.GeneralException;
 import org.apache.ofbiz.base.util.StringUtil;
 import org.apache.ofbiz.base.util.UtilDateTime;
 import org.apache.ofbiz.base.util.UtilHttp;
@@ -1144,7 +1143,6 @@ public class CommunicationEventServices {
             for (Address addr: addresses) {
                 if (addr instanceof InternetAddress) {
                     emailAddress = (InternetAddress)addr;
-
                     if (emailAddress != null) {
                         Map<String, String> inputFields = new HashMap<String, String>();
                         inputFields.put("infoString", emailAddress.getAddress());
@@ -1157,6 +1155,7 @@ public class CommunicationEventServices {
                             tempResults.addAll(filteredList);
                         } catch (GenericEntityException e) {
                             Debug.logError(e, module);
+                        }
                     }
                 }
             }