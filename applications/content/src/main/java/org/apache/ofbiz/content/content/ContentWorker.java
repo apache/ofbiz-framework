5333451e044006460ba7027755abc9282f11600b
Michael Brohl <mbrohl@apache.org>
2017-07-04 22:11:55 +0000

    Reverted: ContentWorker#findAlternateLocaleContent(Delegator, GenericValue, Locale) does not use fallback locale. (OFBIZ-9445)
    r1800780 introduced a bug which breaks the functionality. 
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1800832 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/src/main/java/org/apache/ofbiz/content/content/ContentWorker.java
+++ applications/content/src/main/java/org/apache/ofbiz/content/content/ContentWorker.java
@@ -39,7 +39,6 @@ import org.apache.ofbiz.base.util.UtilCodec;
 import org.apache.ofbiz.base.util.UtilDateTime;
 import org.apache.ofbiz.base.util.UtilGenerics;
 import org.apache.ofbiz.base.util.UtilMisc;
-import org.apache.ofbiz.base.util.UtilProperties;
 import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.base.util.string.FlexibleStringExpander;
 import org.apache.ofbiz.content.ContentManagementWorker;
@@ -400,9 +399,9 @@ public class ContentWorker implements org.apache.ofbiz.widget.content.ContentWor
     }
 
     public static GenericValue findAlternateLocaleContent(Delegator delegator, GenericValue view, Locale locale) {
-        GenericValue contentAssocDataResourceViewFrom = null;
+        GenericValue contentAssocDataResourceViewFrom = view;
         if (locale == null) {
-            return view;
+            return contentAssocDataResourceViewFrom;
         }
 
         String localeStr = locale.toString();
@@ -413,11 +412,8 @@ public class ContentWorker implements org.apache.ofbiz.widget.content.ContentWor
             alternateViews = view.getRelated("ContentAssocDataResourceViewTo", UtilMisc.toMap("caContentAssocTypeId", "ALTERNATE_LOCALE"), UtilMisc.toList("-caFromDate"), true);
         } catch (GenericEntityException e) {
             Debug.logError(e, "Error finding alternate locale content: " + e.toString(), module);
-            return view;
+            return contentAssocDataResourceViewFrom;
         }
-
-        // also check the given view for a matching locale
-        alternateViews.add(0, view);
 
         alternateViews = EntityUtil.filterByDate(alternateViews, UtilDateTime.nowTimestamp(), "caFromDate", "caThruDate", true);
         for (GenericValue thisView : alternateViews) {
@@ -456,14 +452,6 @@ public class ContentWorker implements org.apache.ofbiz.widget.content.ContentWor
                     }
                 }
             }
-        }
-
-        if (contentAssocDataResourceViewFrom == null) {
-            // no content matching the given locale found.
-            Locale fallbackLocale = UtilProperties.getFallbackLocale();
-            contentAssocDataResourceViewFrom = locale.equals(fallbackLocale) ? view
-                    // only search for a content with the fallbackLocale if it is different to the given locale
-                    : findAlternateLocaleContent(delegator, view, fallbackLocale);
         }
 
         return contentAssocDataResourceViewFrom;
