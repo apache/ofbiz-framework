apache/ofbiz-framework

commit 0e96e6daad107abb2e49474ebfe388da45ce0531

Fixed: For service getRateAmount don't use parameters map to propage variable to ensure the isolation, Thans Scott for this remark
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1802668 13f79535-47bb-0310-9956-ffa450edef68

applications/accounting/groovyScripts/rate/RateServices.groovy
@@ -147,9 +147,6 @@ def getRateAmount() {
     For a easier debugging time, there is a log triggered when no records are found for the input. This log
     shows up when there are rateAmounts corresponding to the input parameters without the rateCurrencyUomId and
     the periodTypeId.*/
-    if (!parameters.rateCurrencyUomId) {
-        parameters.rateCurrencyUomId = UtilProperties.getPropertyValue('general.properties', 'currency.uom.id.default', 'USD')
-    }
     String serviceName = null;
     if (parameters.workEffortId && parameters.workEffortId != '_NA_') {
         // workeffort level
@@ -165,29 +162,33 @@ def getRateAmount() {
         serviceName = 'getRatesAmountsFromEmplPositionTypeId'
     }
     if (serviceName) {
-        logError(parameters.toString() + " " + serviceName.toString())
-        Map result = run service: serviceName, with: parameters
-        parameters.ratesList = result.ratesList
-        logError(parameters.ratesList.toString())
-        result = run service: 'filterRateAmountList', with: parameters
-        parameters.ratesList = result.filteredRatesList
+        Map serviceContextMap = dispatcher.getDispatchContext().makeValidContext(serviceName, "IN", parameters)
+        if (!parameters.rateCurrencyUomId) {
+            serviceContextMap.rateCurrencyUomId = UtilProperties.getPropertyValue('general.properties', 'currency.uom.id.default', 'USD')
+        }
+        Map result = run service: serviceName, with: serviceContextMap
+        serviceContextMap.ratesList = result.ratesList
+        result = run service: 'filterRateAmountList', with: serviceContextMap
+        ratesList = result.filteredRatesList
     }
 
-    if (!parameters.ratesList) {
-        parameters.ratesList = from('RateAmount').where([rateTypeId: parameters.rateTypeId]).queryList();
-        Map result = run service: 'filterRateAmountList', with: parameters
-        parameters.ratesList = EntityUtil.filterByDate(result.filteredRatesList)
+    if (!ratesList) {
+        ratesList = from('RateAmount').where([rateTypeId: parameters.rateTypeId]).queryList();
+        Map serviceContextMap = dispatcher.getDispatchContext().makeValidContext("filteredRatesList", "IN", parameters)
+        serviceContextMap.ratesList = ratesList
+        Map result = run service: 'filterRateAmountList', with: serviceContextMap
+        ratesList = EntityUtil.filterByDate(result.filteredRatesList)
     }
 
-    if (!parameters.ratesList) {
-        rateType = from('RateAmount').where([rateTypeId: parameters.rateTypeId]).queryOne()
+    if (!ratesList) {
+        rateType = from('RateAmount').where(parameters).queryOne()
         logError('A valid rate amount could not be found for rateType: ' + rateType.description)
     }
 
     // We narrowed as much as we could the result, now returning the first record of the list
     Map result = success()
-    if (parameters.ratesList) {
-        rateAmount = parameters.ratesList[0]
+    if (ratesList) {
+        rateAmount = ratesList[0]
         if (! rateAmount.rateAmount) rateAmount.rateAmount = BigDecimal.ZERO
         result.rateAmount = rateAmount.rateAmount
         result.periodTypeId = rateAmount.periodTypeId
@@ -256,10 +257,11 @@ def filterRateAmountList() {
         filterMap.rateTypeId = parameters.rateTypeId
     }
     List tempRatesFilteredList = EntityUtil.filterByAnd(parameters.ratesList, filterMap)
+    List ratesList = []
     if (tempRatesFilteredList) {
-        parameters.ratesList = tempRatesFilteredList
+        ratesList = tempRatesFilteredList
     }
     Map result = success()
-    result.filteredRatesList = parameters.ratesList
+    result.filteredRatesList = ratesList
     return result
 }

commit 0d2eb90234597fd0c2076a52908a35b15effc513

Fixes a test issue introduced with r1802668
See https://ci.apache.org/builders/ofbiz-trunk-framework-plugins?numbuilds=30

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1803406 13f79535-47bb-0310-9956-ffa450edef68

applications/accounting/groovyScripts/rate/RateServices.groovy
@@ -180,7 +180,7 @@ def getRateAmount() {
 
     if (!ratesList) {
         rateType = from('RateAmount').where(parameters).queryOne()
-        logError('A valid rate amount could not be found for rateType: ' + rateType.description)
+        logError('A valid rate amount could not be found for rateType: ' + rateType?.description)
     }
 
     // We narrowed as much as we could the result, now returning the first record of the list