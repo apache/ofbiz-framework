apache/ofbiz-framework

commit b5208ab9d0a2c97a9b4ef71403b3754dbbe7b1e3

Inconsistent String Comparisons, Applied patch for framework base classes. Thanks Devanshu Vyas for your contribution (OFBIZ-9254)
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1812255 13f79535-47bb-0310-9956-ffa450edef68

framework/base/src/main/java/org/apache/ofbiz/base/util/UtilValidate.java
@@ -1046,7 +1046,7 @@ public final class UtilValidate {
      * @return true, if the credit card number is a valid VISA number, false otherwise
      */
     public static boolean isVisa(String cc) {
-        if (((cc.length() == 16) || (cc.length() == 13)) && (cc.substring(0, 1).equals("4")))
+        if (((cc.length() == 16) || (cc.length() == 13)) && ("4".equals(cc.substring(0, 1))))
             return isCreditCard(cc);
         return false;
     }
@@ -1108,7 +1108,7 @@ public final class UtilValidate {
     public static boolean isDiscover(String cc) {
         String first4digs = cc.substring(0, 4);
 
-        if ((cc.length() == 16) && (first4digs.equals("6011")))
+        if ((cc.length() == 16) && ("6011".equals(first4digs)))
             return isCreditCard(cc);
         return false;
     }
@@ -1120,7 +1120,7 @@ public final class UtilValidate {
     public static boolean isEnRoute(String cc) {
         String first4digs = cc.substring(0, 4);
 
-        if ((cc.length() == 15) && (first4digs.equals("2014") || first4digs.equals("2149")))
+        if ((cc.length() == 15) && ("2014".equals(first4digs) || "2149".equals(first4digs)))
             return isCreditCard(cc);
         return false;
     }
@@ -1133,12 +1133,12 @@ public final class UtilValidate {
         String first4digs = cc.substring(0, 4);
 
         if ((cc.length() == 16) &&
-            (first4digs.equals("3088") ||
-                first4digs.equals("3096") ||
-                first4digs.equals("3112") ||
-                first4digs.equals("3158") ||
-                first4digs.equals("3337") ||
-                first4digs.equals("3528")))
+            ("3088".equals(first4digs) ||
+                "3096".equals(first4digs) ||
+                "3112".equals(first4digs) ||
+                "3158".equals(first4digs) ||
+                "3337".equals(first4digs) ||
+                "3528".equals(first4digs)))
             return isCreditCard(cc);
         return false;
     }
@@ -1152,14 +1152,14 @@ public final class UtilValidate {
         String first6digs = cc.substring(0, 6);
 
         if (((cc.length() == 16) || (cc.length() == 18) || (cc.length() == 19)) &&
-            (first4digs.equals("4903") ||
-                first4digs.equals("4905") ||
-                first4digs.equals("4911") ||
-                first4digs.equals("4936") ||
-                first6digs.equals("564182") ||
-                first6digs.equals("633110") ||
-                first4digs.equals("6333") ||
-                first4digs.equals("6759")))
+            ("4903".equals(first4digs) ||
+                "4905".equals(first4digs) ||
+                "4911".equals(first4digs) ||
+                "4936".equals(first4digs) ||
+                "564182".equals(first4digs) ||
+                "633110".equals(first4digs) ||
+                "6333".equals(first4digs) ||
+                "6759".equals(first4digs)))
             return isCreditCard(cc);
         return false;
     }
@@ -1172,7 +1172,7 @@ public final class UtilValidate {
         String first4digs = cc.substring(0, 4);
         String first2digs = cc.substring(0, 2);
         if (((cc.length() == 16) || (cc.length() == 18) || (cc.length() == 19)) &&
-                (first2digs.equals("63") || first4digs.equals("6767")))
+                ("63".equals(first2digs) || "6767".equals(first4digs)))
             return isCreditCard(cc);
         return false;
     }
@@ -1186,12 +1186,12 @@ public final class UtilValidate {
         String first4digs = cc.substring(0, 4);
 
         if ((cc.length() == 16) &&
-            (first6digs.equals("417500") ||
-                first4digs.equals("4917") ||
-                first4digs.equals("4913") ||
-                first4digs.equals("4508") ||
-                first4digs.equals("4844") ||
-                first4digs.equals("4027")))
+            ("417500".equals(first6digs) ||
+                "4917".equals(first6digs) ||
+                "4913".equals(first6digs) ||
+                "4508".equals(first6digs) ||
+                "4844".equals(first6digs) ||
+                "4027".equals(first6digs)))
             return isCreditCard(cc);
         return false;
     }

framework/base/src/main/java/org/apache/ofbiz/base/util/HttpClient.java
@@ -423,7 +423,7 @@ public class HttpClient {
         }
 
         // Append the arguments to the query string if GET.
-        if (method.equalsIgnoreCase("get") && arguments != null) {
+        if ("get".equalsIgnoreCase(method) && arguments != null) {
             if (url.contains("?")) {
                 url = url + "&" + arguments;
             } else {
@@ -458,7 +458,7 @@ public class HttpClient {
                 con.setRequestProperty("Connection", "Keep-Alive");
             }
 
-            if (method.equalsIgnoreCase("post")) {
+            if ("post".equalsIgnoreCase(method)) {
                 if (contentType == null) {
                     con.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
                 }
@@ -481,7 +481,7 @@ public class HttpClient {
                 }
             }
 
-            if (method.equalsIgnoreCase("post")) {
+            if ("post".equalsIgnoreCase(method)) {
                 try (
                         OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream(),
                                 this.streamCharset != null ? this.streamCharset : "UTF-8")) {

framework/base/src/main/java/org/apache/ofbiz/base/util/SSLUtil.java
@@ -299,16 +299,16 @@ public final class SSLUtil {
             String proxyHost = UtilProperties.getPropertyValue("jsse", "https.proxyHost", "NONE");
             String proxyPort = UtilProperties.getPropertyValue("jsse", "https.proxyPort", "NONE");
             String cypher = UtilProperties.getPropertyValue("jsse", "https.cipherSuites", "NONE");
-            if (protocol != null && !protocol.equals("NONE")) {
+            if (protocol != null && !"NONE".equals(protocol)) {
                 System.setProperty("java.protocol.handler.pkgs", protocol);
             }
-            if (proxyHost != null && !proxyHost.equals("NONE")) {
+            if (proxyHost != null && !"NONE".equals(proxyHost)) {
                 System.setProperty("https.proxyHost", proxyHost);
             }
-            if (proxyPort != null && !proxyPort.equals("NONE")) {
+            if (proxyPort != null && !"NONE".equals(proxyPort)) {
                 System.setProperty("https.proxyPort", proxyPort);
             }
-            if (cypher != null && !cypher.equals("NONE")) {
+            if (cypher != null && !"NONE".equals(cypher)) {
                 System.setProperty("https.cipherSuites", cypher);
             }
 

framework/base/src/main/java/org/apache/ofbiz/base/util/UtilHttp.java
@@ -1157,7 +1157,7 @@ public final class UtilHttp {
             if (key == null || key.length() <= ROW_SUBMIT_PREFIX_LENGTH) continue;
             if (key.indexOf(MULTI_ROW_DELIMITER) <= 0) continue;
             if (!key.substring(0, ROW_SUBMIT_PREFIX_LENGTH).equals(ROW_SUBMIT_PREFIX)) continue;
-            if (!entry.getValue().equals("Y"))
+            if (!"Y".equals(entry.getValue()))
                 continue;
 
             // decode the value of N and create a new map for it

framework/base/src/main/java/org/apache/ofbiz/base/util/UtilIO.java
@@ -248,7 +248,7 @@ public final class UtilIO {
 
     private static StringBuilder filterLineEndings(StringBuilder sb) {
         String nl = System.getProperty("line.separator");
-        if (!nl.equals("\n")) {
+        if (!"\n".equals(nl)) {
             int r = 0;
             while (r < sb.length()) {
                 int i = sb.indexOf(nl, r);

framework/base/src/main/java/org/apache/ofbiz/base/util/string/test/FlexibleStringExpanderTests.java
@@ -212,7 +212,7 @@ public class FlexibleStringExpanderTests extends TestCase {
     @Override
     public void setUp() {
         wasVerbose = Debug.isOn(Debug.VERBOSE);
-        if (getName().equals("testWithVerbosity")) {
+        if ("testWithVerbosity".equals(getName())) {
             Debug.set(Debug.VERBOSE, true);
         }
         Converters.registerConverter(new SpecialNumberToString());

commit 59370de2bca51a2f10a6a7144d61bee69858b25f

Fixed: Wrong String comparisons introduced in r1812255. Also changed the method name from isswitch to isSwitch.
Thanks to FindBugs for pointing me to this :-)

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1812794 13f79535-47bb-0310-9956-ffa450edef68

framework/base/src/main/java/org/apache/ofbiz/base/util/UtilValidate.java
@@ -30,7 +30,6 @@ import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.entity.util.EntityQuery;
 import org.apache.ofbiz.entity.util.EntityUtilProperties;
-import org.apache.ofbiz.base.util.UtilMisc;
 
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
@@ -1147,7 +1146,7 @@ public final class UtilValidate {
      *   @param     cc - a string representing a credit card number; Sample number: 6331100000000096(16 digits)
      *   @return  true, if the credit card number is a valid Switch card number, false otherwise
      */
-    public static boolean isswitch (String cc) {
+    public static boolean isSwitch(String cc) {
         String first4digs = cc.substring(0, 4);
         String first6digs = cc.substring(0, 6);
 
@@ -1156,8 +1155,8 @@ public final class UtilValidate {
                 "4905".equals(first4digs) ||
                 "4911".equals(first4digs) ||
                 "4936".equals(first4digs) ||
-                "564182".equals(first4digs) ||
-                "633110".equals(first4digs) ||
+                "564182".equals(first6digs) ||
+                "633110".equals(first6digs) ||
                 "6333".equals(first4digs) ||
                 "6759".equals(first4digs)))
             return isCreditCard(cc);
@@ -1187,11 +1186,11 @@ public final class UtilValidate {
 
         if ((cc.length() == 16) &&
             ("417500".equals(first6digs) ||
-                "4917".equals(first6digs) ||
-                "4913".equals(first6digs) ||
-                "4508".equals(first6digs) ||
-                "4844".equals(first6digs) ||
-                "4027".equals(first6digs)))
+                "4917".equals(first4digs) ||
+                "4913".equals(first4digs) ||
+                "4508".equals(first4digs) ||
+                "4844".equals(first4digs) ||
+                "4027".equals(first4digs)))
             return isCreditCard(cc);
         return false;
     }
@@ -1208,7 +1207,7 @@ public final class UtilValidate {
 
         if (!isCreditCard(cc)) return false;
         if (isMasterCard(cc) || isVisa(cc) || isAmericanExpress(cc) || isDinersClub(cc) ||
-                isDiscover(cc) || isEnRoute(cc) || isJCB(cc) || isSolo(cc)|| isswitch (cc)|| isVisaElectron(cc))
+                isDiscover(cc) || isEnRoute(cc) || isJCB(cc) || isSolo(cc)|| isSwitch (cc)|| isVisaElectron(cc))
             return true;
         return false;
     }
@@ -1231,7 +1230,7 @@ public final class UtilValidate {
         if (isEnRoute(cc)) return "CCT_ENROUTE";
         if (isJCB(cc)) return "CCT_JCB";
         if (isSolo(cc)) return "CCT_SOLO";
-        if (isswitch (cc)) return "CCT_SWITCH";
+        if (isSwitch (cc)) return "CCT_SWITCH";
         if (isVisaElectron(cc)) return "CCT_VISAELECTRON";
         return "Unknown";
     }
@@ -1255,7 +1254,7 @@ public final class UtilValidate {
         if (("CCT_CARTEBLANCHE".equalsIgnoreCase(cardType)) && (isCarteBlanche(cardNumber))) return true;
         if (("CCT_ENROUTE".equalsIgnoreCase(cardType)) && (isEnRoute(cardNumber))) return true;
         if (("CCT_SOLO".equalsIgnoreCase(cardType)) && (isSolo(cardNumber))) return true;
-        if (("CCT_SWITCH".equalsIgnoreCase(cardType)) && (isswitch (cardNumber))) return true;
+        if (("CCT_SWITCH".equalsIgnoreCase(cardType)) && (isSwitch (cardNumber))) return true;
         if (("CCT_VISAELECTRON".equalsIgnoreCase(cardType)) && (isVisaElectron(cardNumber))) return true;
         return false;
     }