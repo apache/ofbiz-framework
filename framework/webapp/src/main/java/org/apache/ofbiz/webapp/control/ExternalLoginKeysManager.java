90a349b441a3074cac0edd5f32bb1a1d3657acef
Jacques Le Roux <jleroux@apache.org>
2017-10-29 11:02:00 +0000

    Implemented: Token Based Authentication (OFBIZ-9833)
    This works the same way than externalLoginKey but between 2 servers, 
    not 2 webapps on the same server. 
    
    The Single Sign On (SSO) is ensured by a JWT token, then all is handled as 
    normal by a session on the reached server. The servers may or may not share a 
    database but the loginUserIds on the 2 servers must be the same.
    
    OOTB the JWT masterSecretKey is not properly initialised and can not be OOTB.
    As we sign on on several servers, so have different sessions, we can't use the 
    externalLoginKey way to create the JWT masterSecretKey.
    The best way to create the JWT masterSecretKey is to use a temporary way to load 
    in a static final key when compiling. This is simple and most secure. 
    One of the proposed way is to use sed and uuidgen to modify the masterSecretKey 
    value. The magic words here are TEMPORARY and FINAL!
    
    I have not tested this between 2 servers yet, only locally where it works well.
    I'll do after this commit between my local instance and the trunk demo.
    
    Thanks: Nicolas for the sed ans uuidgen suggestion
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1813679 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ExternalLoginKeysManager.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ExternalLoginKeysManager.java
@@ -18,20 +18,33 @@
  */
 package org.apache.ofbiz.webapp.control;
 
+import java.security.Key;
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.bind.DatatypeConverter;
 
 import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.UtilHttp;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.DelegatorFactory;
+import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityUtilProperties;
 import org.apache.ofbiz.service.LocalDispatcher;
 import org.apache.ofbiz.webapp.WebAppUtil;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * This class manages the authentication tokens that provide single sign-on authentication to the OFBiz applications.
@@ -41,6 +54,17 @@ public class ExternalLoginKeysManager {
     private static final String EXTERNAL_LOGIN_KEY_ATTR = "externalLoginKey";
     // This Map is keyed by the randomly generated externalLoginKey and the value is a UserLogin GenericValue object
     private static final Map<String, GenericValue> externalLoginKeys = new ConcurrentHashMap<>();
+    public static final String EXTERNAL_SERVER_LOGIN_KEY = "externalServerLoginKey";
+    // This works the same way than externalLoginKey but between 2 servers, not 2 webapps on the same server. 
+    // The Single Sign On (SSO) is ensured by a JWT token, then all is handled as normal by a session on the reached server. 
+    // The servers may or may not share a database but the 2 loginUserId must be the same.
+    
+    // OOTB the JWT masterSecretKey is not properly initialised and can not be OOTB.
+    // As we sign on on several servers, so have different sessions, we can't use the externalLoginKey way to create the JWT masterSecretKey.
+    // The best way to create the JWT masterSecretKey is to use a temporary way to load in a static final key when compiling. 
+    // This is simple and most secure. One of the proposed way is to use sed and uuidgen to modify the masterSecretKey value
+    // The magic words here are TEMPORARY and FINAL!
+    private static final String ExternalServerJwtMasterSecretKey = "ExternalServerJwtMasterSecretKey";
 
     /**
      * Gets (and creates if necessary) an authentication token to be used for an external login parameter.
@@ -145,6 +169,159 @@ public class ExternalLoginKeysManager {
 
     private static boolean isAjax(HttpServletRequest request) {
        return "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+    }
+    
+    public static String externalServerLoginCheck(HttpServletRequest request, HttpServletResponse response) {
+
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        HttpSession session = request.getSession();
+
+        String externalServerUserLoginId = request.getParameter(EXTERNAL_SERVER_LOGIN_KEY);
+        if (externalServerUserLoginId == null) return "success"; // Nothing to do here
+
+        GenericValue currentUserLogin = (GenericValue) session.getAttribute("userLogin");
+
+        try {
+            GenericValue userLogin = delegator.findOne("UserLogin", false, "userLoginId", externalServerUserLoginId);
+            if (userLogin != null) {
+                //to check it's the right tenant
+                //in case username and password are the same in different tenants
+                LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+                delegator = (Delegator) request.getAttribute("delegator");
+                String oldDelegatorName = delegator.getDelegatorName();
+                ServletContext servletContext = session.getServletContext();
+                if (!oldDelegatorName.equals(userLogin.getDelegator().getDelegatorName())) {
+                    delegator = DelegatorFactory.getDelegator(userLogin.getDelegator().getDelegatorName());
+                    dispatcher = WebAppUtil.makeWebappDispatcher(servletContext, delegator);
+                    LoginWorker.setWebContextObjects(request, response, delegator, dispatcher);
+                }
+
+                String authorisationHeader = request.getHeader("Authorisation");
+                if (authorisationHeader != null) {
+                    boolean jwtOK = checkJwt(authorisationHeader, userLogin.getString("userLoginId"), getExternalServerName(request), UtilHttp.getApplicationName(request));
+                    if (!jwtOK) {
+                        Debug.logWarning("*** There was a problem with the JWT token, loging out the current user: " + externalServerUserLoginId, module);
+                        LoginWorker.logout(request, response);
+                        return "success";
+                    }
+                } else {
+                    // Something weird happened here => logout current user
+                    Debug.logWarning("*** There was a problem with the JWT token, loging out the current user: " + externalServerUserLoginId, module);
+                    LoginWorker.logout(request, response);
+                    return "success";
+                }
+
+                // if the user is already logged in and the login is different, logout the other user
+                if (currentUserLogin != null) {
+                    if (currentUserLogin.getString("userLoginId").equals(userLogin.getString("userLoginId"))) {
+                        // is the same user, just carry on...
+                        return "success";
+                    }
+
+                    // logout the current user and login the new user...
+                    LoginWorker.logout(request, response);
+                    // ignore the return value; even if the operation failed we want to set the new UserLogin
+                }
+
+                //connect
+                String enabled = userLogin.getString("enabled");
+                if (enabled == null || "Y".equals(enabled)) {
+                    userLogin.set("hasLoggedOut", "N");
+                    userLogin.store();
+                }
+                LoginWorker.doBasicLogin(userLogin, request);
+            } else {
+                Debug.logWarning("Could not find userLogin for external login key: " + externalServerUserLoginId, module);
+            }
+        } catch (GenericEntityException e) {
+            Debug.logError(e, "Cannot get autoUserLogin information: " + e.getMessage(), module);
+        }
+
+        return "success";
+    }
+    
+    /**
+     * Generate and return a JWT key
+     * 
+     * @param id is an Id, I suggest userLoginId
+     * @param issuer is who/what issued the token. I suggest the server DNS
+     * @param subject is the subject of the token. I suggest the destination webapp
+     * @param ttlMillis the expiration time
+     * @return a JWT token
+     */
+    public static String createJwt(String id, String issuer, String subject, long ttlMillis) {
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS512;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(ExternalServerJwtMasterSecretKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                                    .setIssuedAt(now)
+                                    .setSubject(subject)
+                                    .setIssuer(issuer)
+                                    .setIssuedAt(now)
+                                    .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration date, this should always be true
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serialises it to a compact, URL-safe string
+        return builder.compact();
+    }
+    
+    /**
+     * Reads and validates a JWT token
+     * Throws a SignatureException if it is not a signed JWS (as expected) or has been tampered
+     * @param jwt a JWT token
+     * @param id is an Id, I suggest userLoginId
+     * @param issuer is who/what issued the token. I suggest the server DNS
+     * @param subject is the subject of the token. I suggest the destination webapp
+     * @return true if the JWT token corresponds to the one sent and is not expired
+     */
+    private static boolean checkJwt(String jwt, String id, String issuer, String subject) {
+        //The JWT signature algorithm is using this to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS512;
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(ExternalServerJwtMasterSecretKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //This line will throw a SignatureException if it is not a signed JWS (as expected) or has been tampered
+        Claims claims = Jwts.parser()
+           .setSigningKey(signingKey)
+           .parseClaimsJws(jwt).getBody();
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        return claims.getId().equals(id) 
+                && claims.getIssuer().equals(issuer)
+                && claims.getSubject().equals(subject)
+                && claims.getExpiration().after(now);
+    }
+
+    public static String getExternalServerName(HttpServletRequest request) {
+        String reportingServerName = "";
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        if (delegator != null && "Y".equals(EntityUtilProperties.getPropertyValue("embisphere", "use-external-server", "Y", delegator))) {
+            reportingServerName = EntityUtilProperties.getPropertyValue("embisphere", "external-server-name", "localhost:8443", delegator);
+            String reportingServerQuery = EntityUtilProperties.getPropertyValue("embisphere", "external-server-query", "/catalog/control/", delegator);
+            reportingServerName = "https://" + reportingServerName + reportingServerQuery;
+        }
+        return reportingServerName;
+    }
+    
+    public static long getJwtTokenTimeToLive(HttpServletRequest request) {
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        if (delegator != null) return 1000 * Long.parseLong(EntityUtilProperties.getPropertyValue("embisphere", "external-server-token-duration", "30", delegator));
+        else return 1000 * 30;
     }
 
 }
