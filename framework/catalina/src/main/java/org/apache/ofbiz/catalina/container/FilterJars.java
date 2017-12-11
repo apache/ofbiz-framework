6fb373c63e5342cbbcee359887b8d65884bba66b
Michael Brohl <mbrohl@apache.org>
2017-12-11 07:49:49 +0000

    Improved: General refactoring and code improvements, package org.apache.ofbiz.catalina.container. (OFBIZ-9871)
    Thanks Dennis Balkir for reporting and providing the patches.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1817749 13f79535-47bb-0310-9956-ffa450edef68

--- framework/catalina/src/main/java/org/apache/ofbiz/catalina/container/FilterJars.java
+++ framework/catalina/src/main/java/org/apache/ofbiz/catalina/container/FilterJars.java
@@ -27,5 +27,5 @@ final class FilterJars implements JarScanFilter {
     @Override
     public boolean check(final JarScanType jarScanType, final String jarName) {
         return UtilProperties.getPropertyAsBoolean("catalina", "webSocket", false) ? jarName.contains("ofbiz.jar") : false;
-    } 
+    }
 }
