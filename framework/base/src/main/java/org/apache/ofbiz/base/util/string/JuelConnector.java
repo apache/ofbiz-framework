06dda4ae17c3a65dfe9687e9e3038f18fbc680fc
Nicolas Malin <nmalin@apache.org>
2017-12-17 20:01:06 +0000

    Fixed: Massive stacktraces in JuelConnector.
    Added a constraint to verbose logging to avoid logging too much.
    
    Complete the revision 1818498
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1818510 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/main/java/org/apache/ofbiz/base/util/string/JuelConnector.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/string/JuelConnector.java
@@ -117,7 +117,9 @@ public class JuelConnector {
             try {
                 base = prefix.eval(bindings, context);
             } catch (Exception e) {
-                Debug.log(e, module);
+                if (Debug.verboseOn()) {
+                    Debug.logVerbose(e, module);
+                }
             }
             Object property = getProperty(bindings, context);
             if (property == null && strict) {
