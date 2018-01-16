57ec71a0080b02c74504f48587bfb9d8650d5888
Jacques Le Roux <jleroux@apache.org>
2018-01-16 10:56:09 +0000

    "Applied fix from trunk for revision: 1821226" ------------------------------------------------------------------------ r1821226 | jleroux | 2018-01-16 11:25:41 +0100 (mar., 16 janv. 2018) | 1 line
    Fixed: Fix the HttpServletRequestWrapper in ContextFilter.doFilter()
    (OFBIZ-10167)
    
    As reported by Deepak on dev ML the returned name should be a parameter and not 
    enforced to "Authorisation"
    
    Thanks: Deepak for bug report
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/branches/release17.12@1821229 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ContextFilter.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ContextFilter.java
@@ -206,7 +206,7 @@ public class ContextFilter implements Filter {
                     value = ExternalLoginKeysManager.createJwt(externalServerUserLoginId, dnsName, webAppName , timeToLive);
                 }
                 if (value != null) return value;
-                return super.getHeader("Authorisation");
+                return super.getHeader(name); // Here name is always "Authorisation"
             }
         };
         // we're done checking; continue on
