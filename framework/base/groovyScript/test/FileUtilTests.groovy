5da30ba0674dd483f885d389633dfe0c12ce984e
Gil Portenseigne <pgil@apache.org>
2018-02-08 15:32:31 +0000

    Implemented : Add new FileUtil methods for zip management (OFBIZ-10188) Add zipFileStream method that will zip FileStream to a zipped one. Add unzipFileToFolder method to unzip a given archive to a given Folder. Add fileUtil groovy test suite that create zip file containing README.md, then unzip it to tmp folder and compare original and unzipped files. Thanks Jacques for the review.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1823567 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/base/groovyScript/test/FileUtilTests.groovy
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+
+
+import org.apache.commons.io.FileUtils
+import org.apache.ofbiz.base.util.FileUtil
+import org.apache.ofbiz.base.util.UtilProperties
+import org.apache.ofbiz.testtools.GroovyScriptTestCase
+
+class FileUtilTests extends GroovyScriptTestCase {
+
+    /**
+     * Test FileUtil zipFileStream and unzipFileToFolder methods, using README.md
+     */
+    void testZipReadme() {
+        String zipFilePath = UtilProperties.getPropertyValue("general", "http.upload.tmprepository", "runtime/tmp")
+        String zipName = 'README.md.zip'
+        String fileName = 'README.md'
+        File originalReadme = new File(fileName)
+
+        //validate zipStream from README.md is not null
+        def zipStream = FileUtil.zipFileStream(originalReadme.newInputStream(), fileName)
+        assert zipStream
+
+        //ensure no zip already exists
+        File readmeZipped = new File(zipFilePath, zipName)
+        if (readmeZipped.exists()) readmeZipped.delete()
+
+        //write it down into tmp folder
+        OutputStream out = new FileOutputStream(readmeZipped)
+        byte[] buf = new byte[8192]
+        int len
+        while ((len = zipStream.read(buf)) > 0) {
+            out.write(buf, 0, len)
+        }
+        out.close()
+        zipStream.close()
+
+        //ensure no README.md exist in tmp folder
+        File readme = new File(zipFilePath, fileName)
+        if (readme.exists()) readme.delete()
+
+        //validate unzip and compare the two files
+        FileUtil.unzipFileToFolder(readmeZipped, zipFilePath)
+
+        assert FileUtils.contentEquals(originalReadme, new File(zipFilePath, fileName))
+    }
+}
