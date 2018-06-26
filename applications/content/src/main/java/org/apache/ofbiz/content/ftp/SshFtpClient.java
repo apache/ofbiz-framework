a11bde36be15c1189103888a755a506e24306d75
Jacques Le Roux <jleroux@apache.org>
2018-06-26 07:35:28 +0000

    Improved: Adds missing license headers (OFBIZ-10245)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1834403 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/src/main/java/org/apache/ofbiz/content/ftp/SshFtpClient.java
+++ applications/content/src/main/java/org/apache/ofbiz/content/ftp/SshFtpClient.java
@@ -1,16 +1,34 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
 package org.apache.ofbiz.content.ftp;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.sshd.client.SshClient;
-import org.apache.sshd.client.session.ClientSession;
-import org.apache.sshd.client.subsystem.sftp.SftpClient;
-import org.apache.ofbiz.base.util.UtilValidate;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.ofbiz.base.util.UtilValidate;
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.client.subsystem.sftp.SftpClient;
 
 /**
  * Basic client to copy files to an ssh ftp server
