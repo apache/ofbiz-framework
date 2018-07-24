60678b4fc436e3b68a2b00318f12350a08ad378a
Deepak Dixit <deepak@apache.org>
2018-07-24 12:41:50 +0000

    Improved: Remove use of deprecated language attribute from script tag (OFBIZ-10491)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1836550 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/groovyScripts/entry/catalog/InlineProductDetail.groovy
+++ applications/order/groovyScripts/entry/catalog/InlineProductDetail.groovy
@@ -181,7 +181,7 @@ if (inlineProduct) {
 
                 if (variantTree && imageMap) {
                     jsBuf = new StringBuffer()
-                    jsBuf.append("<script language=\"JavaScript\" type=\"text/javascript\">")
+                    jsBuf.append("<script type=\"text/javascript\">")
                     jsBuf.append("var DET" + inlineCounter + "= new Array(" + variantTree.size() + ");")
                     jsBuf.append("var IMG" + inlineCounter + " = new Array(" + variantTree.size() + ");")
                     jsBuf.append("var OPT" + inlineCounter + " = new Array(" + featureOrder.size() + ");")
