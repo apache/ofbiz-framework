6f937efbe09f7403156d33199c1072cff519e29e
Suraj Khurana <surajk@apache.org>
2018-08-24 11:37:16 +0000

    Reverted: Empty header row should not render if list to render in file is empty. (OFBIZ-7598)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1838823 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/main/java/org/apache/ofbiz/widget/model/ModelForm.java
+++ framework/widget/src/main/java/org/apache/ofbiz/widget/model/ModelForm.java
@@ -147,7 +147,7 @@ public abstract class ModelForm extends ModelWidget {
     private final String formWidgetAreaStyle;
     private final boolean groupColumns;
     private final String headerRowStyle;
-    private boolean hideHeader;
+    private final boolean hideHeader;
     private final String itemIndexSeparator;
     private final List<String> lastOrderFields;
     private final String listEntryName;
@@ -184,7 +184,6 @@ public abstract class ModelForm extends ModelWidget {
     private final String targetType;
     private final FlexibleStringExpander targetWindowExdr;
     private final String title;
-    private final String emptyFormDataMessage;
     private final String tooltip;
     private final String type;
     private final boolean useRowSubmit;
@@ -242,13 +241,6 @@ public abstract class ModelForm extends ModelWidget {
             title = parentModel.title;
         }
         this.title = title;
-        String emptyFormDataMessage = formElement.getAttribute("empty-form-data-message");
-        if (emptyFormDataMessage.isEmpty() && parentModel != null) {
-            emptyFormDataMessage = parentModel.emptyFormDataMessage;
-        } else if (emptyFormDataMessage.isEmpty()) {
-            emptyFormDataMessage = UtilProperties.getMessage("CommonUiLabels", "CommonNoRecordFound", Locale.getDefault());
-        }
-        this.emptyFormDataMessage = emptyFormDataMessage;
         String tooltip = formElement.getAttribute("tooltip");
         if (tooltip.isEmpty() && parentModel != null) {
             tooltip = parentModel.tooltip;
@@ -1015,9 +1007,6 @@ public abstract class ModelForm extends ModelWidget {
     public boolean getHideHeader() {
         return this.hideHeader;
     }
-    public void setHideHeader(Boolean hideHeader) {
-        this.hideHeader = hideHeader;
-    }
 
     public String getItemIndexSeparator() {
         if (UtilValidate.isNotEmpty(this.itemIndexSeparator)) {
@@ -1383,10 +1372,6 @@ public abstract class ModelForm extends ModelWidget {
 
     public String getTitle() {
         return this.title;
-    }
-
-    public String getEmptyFormDataMessage() {
-        return this.emptyFormDataMessage;
     }
 
     public String getTooltip() {
