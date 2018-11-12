de27131a9329e41ab525177997a23c6e48d6635a
Jacques Le Roux <jleroux@apache.org>
2018-11-12 10:57:27 +0000

    Improved: Allow unit tests to be written in Groovy (OFBIZ-10611)
    Since OFBIZ-9996 it is possible to write integration tests in Groovy, meaning 
    test that depend on the dispatcher and the delegator. It would be nice if the 
    unit tests could be written in Groovy too.
    
    The major benefit of writing tests in Groovy is that you create inputs and 
    expected outputs more easily with objects literals. 
    For example the following java code:
    
    Map<String, Integer> input = new HashMap<>();
      input.put("foo", 42);
      input.put("bar", 37);
    
    can be rewritten in Groovy like this:
      def input = [foo: 42, bar: 37]
    
    Thanks: Mathieu Lirzin
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1846393 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/test/groovy/org/apache/ofbiz/base/util/FileUtilTests.groovy
+++ framework/base/src/test/groovy/org/apache/ofbiz/base/util/FileUtilTests.groovy
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,20 +15,18 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *******************************************************************************/
-
+ */
+package org.apache.ofbiz.base.util;
 
 import org.apache.commons.io.FileUtils
-import org.apache.ofbiz.base.util.FileUtil
-import org.apache.ofbiz.base.util.UtilProperties
-import org.apache.ofbiz.testtools.GroovyScriptTestCase
+import org.junit.Test
 
-class FileUtilTests extends GroovyScriptTestCase {
-
+public class FileUtilTests {
     /**
      * Test FileUtil zipFileStream and unzipFileToFolder methods, using README.adoc
      */
-    void testZipReadme() {
+    @Test
+    void zipReadme() {
         String zipFilePath = UtilProperties.getPropertyValue("general", "http.upload.tmprepository", "runtime/tmp")
         String zipName = 'README.adoc.zip'
         String fileName = 'README.adoc'
