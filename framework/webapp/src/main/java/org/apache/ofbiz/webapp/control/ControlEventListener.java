4260c161d1d6c15187eb7cc3d4f12924929b2526
Michael Brohl <mbrohl@apache.org>
2018-12-08 12:51:27 +0000

    Improved: ControlEventListener - Change Log-Level (OFBIZ-10330)
    The Log-Level of the Message "Could not find visit value object in 
    session..." is changed from "warning" to "info".
    
    Thanks Benjamin Jugl for reporting and providing the patch.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1848467 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ControlEventListener.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ControlEventListener.java
@@ -85,7 +85,7 @@ public class ControlEventListener implements HttpSessionListener {
                     visit.store();
                 }
             } else {
-                Debug.logWarning("Could not find visit value object in session [" + ControlActivationEventListener.showSessionId(session) + "] that is being destroyed", module);
+                Debug.logInfo("Could not find visit value object in session [" + ControlActivationEventListener.showSessionId(session) + "] that is being destroyed", module);
             }
 
             // Store the UserLoginSession
