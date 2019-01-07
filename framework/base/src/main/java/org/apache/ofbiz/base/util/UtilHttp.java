a37cf7ecd5003016fac1aa25a9a3a241042085d2
Jacques Le Roux <jleroux@apache.org>
2019-01-07 20:37:10 +0000

    Fixed: Impossible secure and autologin cookie names when mountpoint contains a slash inside its name (OFBIZ-10766)
    My previous commit was wrong this one fixes it.
    I made a mistake trying to generalise changing UtilHttp::getApplicationName. 
    This method is used in other places where the name should not be changed. 
    Only cookies are concerned.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1850685 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/main/java/org/apache/ofbiz/base/util/UtilHttp.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/UtilHttp.java
@@ -668,7 +668,7 @@ public final class UtilHttp {
         if (request.getContextPath().length() > 1) {
             appName = request.getContextPath().substring(1);
         }
-        return appName.replaceAll("/","_");
+        return appName;
     }
 
     public static void setInitialRequestInfo(HttpServletRequest request) {
