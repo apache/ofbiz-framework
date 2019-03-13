9ee0bbc979b85821932fe7f13332dc12c4d1c4f1
Rishi Solanki <rishi@apache.org>
2019-03-13 10:02:15 +0000

    Implemented: Short Messaging Service(SMS) Gateway Integration. Added data model, configuration settings, data and services to support the sms services. For the specific service provider user of this implementaion needs to go for custom implementation. That means ticket contains the example of the integration how to use with sample plugin for message91 services. (OFBIZ-10457) Thanks to Pritam Kute for your contribution. Thanks to Pierre Smits and Michael Brohi for your review and inputs.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1855389 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/common/src/main/java/org/apache/ofbiz/common/telecom/TelecomServices.java
@@ -0,0 +1,103 @@
+package org.apache.ofbiz.common.telecom;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.UtilValidate;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.entity.util.EntityUtilProperties;
+import org.apache.ofbiz.service.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.ofbiz.base.util.UtilGenerics.checkList;
+
+public class TelecomServices {
+
+    public final static String module = TelecomServices.class.getName();
+
+    public static Map<String, Object> sendTelecomMessage(DispatchContext ctx, Map<String, ? extends Object> context) {
+        Delegator delegator = ctx.getDelegator();
+        LocalDispatcher dispatcher = ctx.getDispatcher();
+        Map<String, Object> results = ServiceUtil.returnSuccess();
+
+        GenericValue userLogin = (GenericValue) context.get("userLogin");
+        String productStoreId = (String) context.get("productStoreId");
+        String telecomMsgTypeEnumId = (String) context.get("telecomMsgTypeEnumId");
+        String telecomMethodTypeId = (String) context.get("telecomMethodTypeId");
+        String telecomGatewayConfigId = (String) context.get("telecomGatewayConfigId");
+        List<String> numbers = checkList(context.get("numbers"), String.class);;
+        String message = (String) context.get("message");
+        
+        String telecomEnabled = EntityUtilProperties.getPropertyValue("general", "telecom.notifications.enabled", delegator);
+        if (!"Y".equals(telecomEnabled)) {
+            Debug.logImportant("Telecom message not sent to " + numbers.toString() +" because telecom.notifications.enabled property is set to N or empty", module);
+            return ServiceUtil.returnSuccess("Telecom message not sent to " + numbers.toString() +" because sms.notifications.enabled property is set to N or empty");
+        }
+
+        String redirectNumber = EntityUtilProperties.getPropertyValue("general", "telecom.notifications.redirectTo", delegator);
+        if (UtilValidate.isNotEmpty(redirectNumber)) {
+            numbers.clear();
+            numbers.add(redirectNumber);
+        }
+
+
+        try {
+            Map<String, Object> createCommEventCtx = new HashMap<String, Object>();
+            createCommEventCtx = ctx.makeValidContext("createCommunicationEvent", ModelService.IN_PARAM, context);
+            createCommEventCtx.put("content", message);
+            createCommEventCtx.put("communicationEventTypeId", "PHONE_COMMUNICATION");
+            createCommEventCtx.put("fromString", EntityUtilProperties.getPropertyValue("general", "defaultFromTelecomAddress", delegator));
+            createCommEventCtx.put("subject", telecomMsgTypeEnumId);
+            createCommEventCtx.put("toString", numbers.toString());
+            Map<String, Object> createCommEventResult = dispatcher.runSync("createCommunicationEvent", createCommEventCtx);
+            if (!ServiceUtil.isSuccess(createCommEventResult)) {
+                Debug.logError(ServiceUtil.getErrorMessage(createCommEventResult), module);
+                return ServiceUtil.returnError(ServiceUtil.getErrorMessage(createCommEventResult));
+            }
+            String communicationEventId = (String) createCommEventResult.get("communicationEventId");
+
+            Map<String, Object> conditions = new HashMap<String, Object>();
+            conditions.put("productStoreId", productStoreId);
+            conditions.put("telecomMsgTypeEnumId", telecomMsgTypeEnumId);
+            conditions.put("telecomMethodTypeId", telecomMethodTypeId);
+            GenericValue productStoreTelecomSetting = EntityQuery.use(delegator).from("ProductStoreTelecomSetting").where(conditions).queryOne();
+            if (productStoreTelecomSetting != null) {
+                GenericValue customMethod = productStoreTelecomSetting.getRelatedOne("CustomMethod", false);
+                if (UtilValidate.isNotEmpty(customMethod.getString("customMethodName"))) {
+                    Map<String, Object> serviceCtx = new HashMap<String, Object>();
+                    serviceCtx.put("numbers", numbers);
+                    serviceCtx.put("message", message);
+                    if (telecomGatewayConfigId != null) {
+                        serviceCtx.put("configId", telecomGatewayConfigId);
+                    }
+                    serviceCtx.put("userLogin", userLogin);
+                    Map<String, Object> customMethodResult = dispatcher.runSync(customMethod.getString("customMethodName"), serviceCtx);
+                    if (ServiceUtil.isError(customMethodResult) || ServiceUtil.isFailure(customMethodResult)) {
+                        String errorMessage = ServiceUtil.getErrorMessage(customMethodResult);
+                        Debug.logError(errorMessage, module);
+                        return ServiceUtil.returnError(errorMessage);
+                    }
+
+                    createCommEventCtx.clear();
+                    createCommEventCtx.put("communicationEventId", communicationEventId);
+                    if (UtilValidate.isNotEmpty(createCommEventResult.get("response")))
+                        createCommEventCtx.put("note", customMethodResult.get("response"));
+                    createCommEventCtx.put("statusId", "COM_COMPLETE");
+                    createCommEventCtx.put("userLogin", userLogin);
+                    dispatcher.runSync("updateCommunicationEvent", createCommEventCtx);
+                }
+            } else {
+                return ServiceUtil.returnError("Not sending SMS as no ProductStoreEmailSetting found for the passed inputs.");
+            }
+        } catch (GenericEntityException | GenericServiceException e) {
+            Debug.logError(e.getMessage(), module);
+            return ServiceUtil.returnError(e.getMessage());
+        }
+
+        return results;
+    }
+}
