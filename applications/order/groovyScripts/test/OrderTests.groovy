362f154b748ea7e9894ba6c857e20e38866e8768
Suraj Khurana <surajk@apache.org>
2019-03-30 07:12:25 +0000

    Added: Unit test case for service - CreateReturnAdjustment. (OFBIZ-8857) Thanks Avnindra Sharma for reporting, Pawan Verma for discussion and Vivek Singh Bisen for providing the updated patch.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1856609 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/groovyScripts/test/OrderTests.groovy
+++ applications/order/groovyScripts/test/OrderTests.groovy
@@ -1,6 +1,7 @@
 import org.apache.ofbiz.entity.util.EntityQuery
 import org.apache.ofbiz.service.ServiceUtil
 import org.apache.ofbiz.testtools.GroovyScriptTestCase
+
 class OrderTests extends GroovyScriptTestCase {
     void testAddRequirementTask() {
         Map serviceCtx = [:]
@@ -9,5 +10,14 @@ class OrderTests extends GroovyScriptTestCase {
         serviceCtx.userLogin = EntityQuery.use(delegator).from("UserLogin").where("userLoginId", "system").cache().queryOne()
         Map serviceResult = dispatcher.runSync("addRequirementTask", serviceCtx)
         assert ServiceUtil.isSuccess(serviceResult)
+    }
+    void testCreateReturnAdjustment() {
+        Map serviceCtx = [:]
+        serviceCtx.amount = '2.0000'
+        serviceCtx.returnId = '1009'
+        serviceCtx.userLogin = EntityQuery.use(delegator).from('UserLogin').where('userLoginId', 'system').cache().queryOne()
+        Map serviceResult = dispatcher.runSync('createReturnAdjustment', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+        assert serviceResult.returnAdjustmentId != null
     }
 }
