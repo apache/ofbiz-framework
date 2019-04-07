3111fc6b40c56dfdbbd8a5e68896bc84a588ce68
Mathieu Lirzin <mthl@apache.org>
2019-04-07 19:35:37 +0000

    Fixed: Remove unused ‘java.util.TreeMap’ import
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1857083 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/main/java/org/apache/ofbiz/base/component/ComponentConfig.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/component/ComponentConfig.java
@@ -29,7 +29,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
