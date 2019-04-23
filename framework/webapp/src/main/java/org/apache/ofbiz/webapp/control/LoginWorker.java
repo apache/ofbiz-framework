9cec8c6443d4bd270d8f81f7eb0986d25235a09f
Gil Portenseigne <pgil@apache.org>
2019-04-23 07:31:10 +0000

    Fixed: User depersonation do not clean out impersonated user session. (OFBIZ-10942)
    Thank you Leila Mekika for reporting and providing the patch.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1857991 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/LoginWorker.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/LoginWorker.java
@@ -719,8 +719,9 @@ public class LoginWorker {
         }
 
         //update the userLogin history, only one impersonation of this user can be active at the same time
+        GenericValue userLogin = (GenericValue) session.getAttribute("userLogin");
         EntityCondition conditions = EntityCondition.makeCondition(
-                EntityCondition.makeCondition("userLoginId", ((GenericValue) session.getAttribute("userLogin")).get("userLoginId")),
+                EntityCondition.makeCondition("userLoginId", userLogin.get("userLoginId")),
                 EntityCondition.makeCondition("originUserLoginId", originUserLogin.get("userLoginId")),
                 EntityUtil.getFilterByDateExpr());
         try {
@@ -735,6 +736,9 @@ public class LoginWorker {
         } catch (GenericEntityException e) {
             return "error";
         }
+
+        // Log out currentLogin to clean session
+        doBasicLogout(userLogin, request, response);
 
         // Log back the impersonating user
         return doMainLogin(request, response, originUserLogin, null);
