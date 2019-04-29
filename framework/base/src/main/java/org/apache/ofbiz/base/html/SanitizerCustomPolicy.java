0ee8973ea4305ca382c838671218346919cd8f01
Michael Brohl <mbrohl@apache.org>
2019-04-29 09:42:55 +0000

    Fixed: OWASP sanitizer breaks proper rendering of HTML code (OFBIZ-10187)
    This makes the sanitizing enabled/disabled by configuration and enhances
    the functionality to support custom sanitizer policies. A reasonable 
    example policy class is also included.
    
    Thanks Dennis Balkir for reporting and providing the patch.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/branches/release17.12@1858354 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/base/src/main/java/org/apache/ofbiz/base/html/SanitizerCustomPolicy.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+
+package org.apache.ofbiz.base.html;
+
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
+
+/**
+ * This interface is used to build a custom sanitizer policy which then can be
+ * used instead of the default permissive policy. The custom policy will the be
+ * used in
+ * {@link org.apache.ofbiz.base.util.UtilCodec.HtmlEncoder#sanitize(String, String)}
+ */
+public interface SanitizerCustomPolicy {
+
+    public static final PolicyFactory POLICY_DEFINITION = new HtmlPolicyBuilder().toFactory();
+
+    /**
+     * Used for getting the policy from the custom class which implements this
+     * interface
+     *
+     * @return the policy specified in the class will be returned
+     */
+    PolicyFactory getSanitizerPolicy();
+}
