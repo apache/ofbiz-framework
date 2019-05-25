b601acc979c45ad55c2b10282a9e02b27faa04a5
Mathieu Lirzin <mthl@apache.org>
2019-05-25 09:25:41 +0000

    Improved: Remove unused ‘StringUtil#collapseSpaces’ method (OFBIZ-11014)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1859930 13f79535-47bb-0310-9956-ffa450edef68

--- framework/base/src/main/java/org/apache/ofbiz/base/util/StringUtil.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/StringUtil.java
@@ -426,16 +426,6 @@ public class StringUtil {
     }
 
     /**
-     * Remove/collapse multiple spaces
-     *
-     * @param str string to collapse spaces in
-     * @return the converted string
-     */
-    public static String collapseSpaces(String str) {
-        return collapseCharacter(str, ' ');
-    }
-
-    /**
      * Remove/collapse multiple characters
      *
      * @param str string to collapse characters in
