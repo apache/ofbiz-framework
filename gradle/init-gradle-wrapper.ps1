8f6dde8d455bb8b34065fe682695be6872f44ec5
Jacques Le Roux <jleroux@apache.org>
2019-10-05 08:10:03 +0000

    Improved: no functional change
    As reported by Ethan Vos on user ML, sometimes the alias between wget and 
    Invoke-WebRequest is not present on Windows 7, better keep it simple!
    
    Also removes the comment:
      Anyway I believe this should be only used in dev environment
    which is no longer true
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/branches/release17.12@1868000 13f79535-47bb-0310-9956-ffa450edef68

--- gradle/init-gradle-wrapper.ps1
+++ gradle/init-gradle-wrapper.ps1
@@ -20,10 +20,8 @@ if ((Test-Path -Path ((Get-Item -Path ".\").FullName + "\gradle\wrapper\gradle-w
     exit
 }
 
-# This uses PowerShell Invoke-WebRequest command (aliased as wget here)
 # HTTPS is not used because it gets complicated with Powershell and .Net framework versions depending on Windows versions
-# Anyway I believe this should be only used in dev environment
-wget -outf gradle\wrapper\gradle-wrapper.jar http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradle-wrapper.jar
-wget -outf gradle\wrapper\gradle-wrapper.properties http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradle-wrapper.properties
-wget -outf gradlew.bat http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradlew.bat
+Invoke-WebRequest -outf gradle\wrapper\gradle-wrapper.jar http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradle-wrapper.jar
+Invoke-WebRequest -outf gradle\wrapper\gradle-wrapper.properties http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradle-wrapper.properties
+Invoke-WebRequest -outf gradlew.bat http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradlew.bat
 
