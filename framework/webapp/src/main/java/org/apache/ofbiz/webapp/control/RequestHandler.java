apache/ofbiz-framework

commit 7f4dec944154b3bb3c584398fca913d9f9b5481c

Fixed: Add method attribute to request-map to controll a uri can be called GET or POST only (OFBIZ-10438)
The problem was r1838081 prevented to serialise requests.

Like in ordermgr/control/updateCheckoutOptions/quickcheckout where 
updateCheckoutOptions and quickcheckout are serialised to update the check out 
after updateCheckoutOptions is done and then get back to the screen with 
quickcheckout. 

CheckoutOptions.ftl is a good example of this usage.

Thanks: Deepak Nigam for initial report and Mathieu for fixing it

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1839451 13f79535-47bb-0310-9956-ffa450edef68

framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/RequestHandler.java
@@ -194,7 +194,9 @@ public class RequestHandler {
         String requestUri = getRequestUri(path);
         String viewUri = getOverrideViewUri(path);
         Collection<RequestMap> rmaps;
-        if (requestMapMap.containsKey(requestUri) && !viewMapMap.containsKey(viewUri)) {
+        if (requestMapMap.containsKey(requestUri)
+                // Ensure that overridden view exists.
+                && (viewUri == null || viewMapMap.containsKey(viewUri))) {
             rmaps = requestMapMap.get(requestUri);
         } else if (defaultRequest != null) {
             rmaps = requestMapMap.get(defaultRequest);

framework/webapp/src/test/java/org/apache/ofbiz/webapp/control/RequestHandlerTests.java
@@ -120,13 +120,25 @@ public class RequestHandlerTests {
         }
 
         @Test
-        public void resolveURIOverrideView() throws Exception {
+        public void resolveURIBasicOverrideView() throws Exception {
             RequestMap foo = new RequestMap(dummyElement);
             RequestMap bar = new RequestMap(dummyElement);
             reqMaps.putSingle("foo", foo);
             reqMaps.putSingle("bar", bar);
 
             viewMaps.put("baz", new ViewMap(dummyElement));
+
+            when(req.getPathInfo()).thenReturn("/foo/baz");
+            when(ccfg.getDefaultRequest()).thenReturn("bar");
+            assertThat(RequestHandler.resolveURI(ccfg, req), hasItem(foo));
+        }
+
+        @Test
+        public void resolveURIMissingOverrideView() throws Exception {
+            RequestMap foo = new RequestMap(dummyElement);
+            RequestMap bar = new RequestMap(dummyElement);
+            reqMaps.putSingle("foo", foo);
+            reqMaps.putSingle("bar", bar);
 
             when(req.getPathInfo()).thenReturn("/foo/baz");
             when(ccfg.getDefaultRequest()).thenReturn("bar");

commit 34d30421dc4ca5eb0ce19d74292a2f377aea78fa

Fixed: SOAPService does not work (OFBIZ-11259)
This was broken by r1839451 committed at the bottom of OFBIZ-10438
A simple fix is to allow "webtools/control/SOAPService/serviceName?WSDL" scheme. 


git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk@1869155 13f79535-47bb-0310-9956-ffa450edef68

framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/RequestHandler.java
@@ -203,9 +203,9 @@ public class RequestHandler {
             String overrideViewUri = getOverrideViewUri(path);
             if (requestMapMap.containsKey(requestUri)
                     // Ensure that overridden view exists.
-                    && (overrideViewUri == null || viewMapMap.containsKey(overrideViewUri))) {
+                    && (overrideViewUri == null || viewMapMap.containsKey(overrideViewUri) 
+                    || ("SOAPService".equals(requestUri) && "wsdl".equalsIgnoreCase(req.getQueryString())))){
                 rmaps = requestMapMap.get(requestUri);
-                req.setAttribute("overriddenView", overrideViewUri);
             } else if (defaultRequest != null) {
                 rmaps = requestMapMap.get(defaultRequest);
             } else {