fbf8531e493c82e89ef9b2910078e685510dee22
Pawan Verma <pawan.verma@hotwaxsystems.com>
2019-11-11 12:12:21 +0530

    Improved: Convert AutoAcctgInvoiceTests Unit Test from XML to Groovy (OFBIZ-11277)
    Thanks: Jacques Le Roux for the review.

--- applications/accounting/src/main/groovy/org/apache/ofbiz/accounting/AutoAcctgInvoiceTests.groovy
+++ applications/accounting/src/main/groovy/org/apache/ofbiz/accounting/AutoAcctgInvoiceTests.groovy
@@ -23,6 +23,9 @@ import org.apache.ofbiz.entity.GenericValue
 import org.apache.ofbiz.entity.util.EntityQuery
 import org.apache.ofbiz.service.ServiceUtil
 import org.apache.ofbiz.service.testtools.OFBizTestCase
+
+import java.sql.Timestamp
+
 class AutoAcctgInvoiceTests extends OFBizTestCase {
     public AutoAcctgInvoiceTests(String name) {
         super(name)
@@ -78,4 +81,151 @@ class AutoAcctgInvoiceTests extends OFBizTestCase {
 
         assert invoiceContent != null
     }
+
+    void testCopyInvoice() {
+        Map serviceCtx = [
+                invoiceIdToCopyFrom: '1000',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('copyInvoice', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        assert serviceResult.invoiceId != null
+    }
+
+    void testCreateInvoice() {
+        Map serviceCtx = [
+                invoiceTypeId: 'PURCHASE_INVOICE',
+                partyIdFrom: 'DEMO_COMPANY',
+                partyId: 'DEMO_COMPANY1',
+                invoiceDate: UtilDateTime.nowTimestamp(),
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('createInvoice', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        assert serviceResult.invoiceId != null
+    }
+
+    void testGetInvoice() {
+        Map serviceCtx = [
+                invoiceId: '1001',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('getInvoice', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        assert serviceResult.invoice != null
+        assert serviceResult.invoiceItems != null
+    }
+
+    void testSetInvoiceStatus() {
+        Map serviceCtx = [
+                invoiceId: '1002',
+                statusId: 'INVOICE_APPROVED',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('setInvoiceStatus', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        GenericValue invoice = EntityQuery.use(delegator).from('Invoice')
+                .where('invoiceId', '1002')
+                .queryOne()
+
+        assert invoice != null
+        assert invoice.statusId == 'INVOICE_APPROVED'
+    }
+
+    void testCopyInvoiceToTemplate() {
+        Map serviceCtx = [
+                invoiceId: '1002',
+                invoiceTypeId: 'PURCHASE_INVOICE',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('copyInvoiceToTemplate', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        assert serviceResult.invoiceId != null
+    }
+
+    void testCreateInvoiceItem() {
+        Map serviceCtx = [
+                invoiceId: '1003',
+                invoiceTypeId: 'PINV_FXASTPRD_ITEM',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('createInvoiceItem', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        assert serviceResult.invoiceItemSeqId != null
+    }
+
+    void testCreateInvoiceStatus() {
+        Timestamp nowTimestamp = UtilDateTime.nowTimestamp()
+        Map serviceCtx = [
+                invoiceId: '1004',
+                statusId: 'INVOICE_IN_PROCESS',
+                statusDate: nowTimestamp,
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('createInvoiceStatus', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        GenericValue invoiceStatus = EntityQuery.use(delegator).from('InvoiceStatus')
+                .where('invoiceId', '1004',
+                        'statusId', 'INVOICE_IN_PROCESS',
+                        'statusDate', nowTimestamp)
+                .queryOne()
+
+        assert invoiceStatus != null
+    }
+
+    void testCreateInvoiceRole() {
+        Map serviceCtx = [
+                invoiceId: '1006',
+                partyId: 'DEMO_COMPANY',
+                roleTypeId: 'INTERNAL_ORGANIZATIO',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('createInvoiceRole', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        GenericValue invoiceRole = EntityQuery.use(delegator).from('InvoiceRole')
+                .where('invoiceId', '1006',
+                        'partyId', 'DEMO_COMPANY',
+                        'roleTypeId', 'INTERNAL_ORGANIZATIO')
+                .queryOne()
+
+        assert invoiceRole != null
+    }
+
+    void testCreateInvoiceTerm() {
+        Map serviceCtx = [
+                invoiceId: '1006',
+                invoiceItemSeqId: '00001',
+                termTypeId: 'FINANCIAL_TERM',
+                termValue: 50.00,
+                termDays: 10,
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('createInvoiceTerm', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+
+        GenericValue invoiceTerm = EntityQuery.use(delegator).from('InvoiceTerm')
+                .where('invoiceTermId', serviceResult.invoiceTermId)
+                .queryOne()
+
+        assert invoiceTerm != null
+    }
+
+    void testCancelInvoice() {
+        Map serviceCtx = [
+                invoiceId: '1007',
+                userLogin: userLogin
+        ]
+        Map serviceResult = dispatcher.runSync('cancelInvoice', serviceCtx)
+        assert ServiceUtil.isSuccess(serviceResult)
+        assert serviceResult.invoiceTypeId != null
+    }
+
 }
