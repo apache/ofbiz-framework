b79c95b151a2daaccefa6436390865e83dff2841
Gil Portenseigne <gil.portenseigne@nereide.fr>
2020-03-06 17:36:14 +0100

    Fixed: Convert OrderServices.xml mini-lang to groovyDSL : getNextOrderId
    (OFBIZ-9984)
    Fix introduced NPE detected by the introduced test.

--- applications/order/groovyScripts/order/OrderServices.groovy
+++ applications/order/groovyScripts/order/OrderServices.groovy
@@ -43,7 +43,7 @@ def getNextOrderId() {
 
     if (customMethod) {
         customMethodName = customMethod.customMethodName
-    } else if (partyAcctgPreference.oldOrderSequenceEnumId == 'ODRSQ_ENF_SEQ') {
+    } else if (partyAcctgPreference && partyAcctgPreference.oldOrderSequenceEnumId == 'ODRSQ_ENF_SEQ') {
         customMethodName = 'orderSequence_enforced'
     }
 
