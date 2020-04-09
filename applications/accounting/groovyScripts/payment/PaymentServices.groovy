b8848355d188ff91ce84e71b16433c9cf278b8d0
Jacques Le Roux <jacques.le.roux@les7arts.com>
2020-04-09 10:23:56 +0200

    Implemented: Convert createPayment service from mini-lang to groovy DSL
    (OFBIZ-11479)
    
    jleroux: I have only changed the MODULE name (and uppercased it)
    
    Thanks: Priya Sharma

---
+++ applications/accounting/groovyScripts/payment/PaymentServices.groovy
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import org.apache.ofbiz.base.util.Debug
+import org.apache.ofbiz.base.util.UtilDateTime
+import org.apache.ofbiz.base.util.UtilProperties
+import org.apache.ofbiz.base.util.UtilValidate
+import org.apache.ofbiz.entity.GenericValue
+
+MODULE = "PaymentServices.groovy"
+def createPayment() {
+    if (!security.hasEntityPermission("ACCOUNTING", "_CREATE", parameters.userLogin) && (!security.hasEntityPermission("PAY_INFO", "_CREATE", parameters.userLogin) && userLogin.partyId != parameters.partyIdFrom && userLogin.partyId != parameters.partyIdTo)) {
+        return error(UtilProperties.getResourceBundleMap("AccountingUiLabels", locale)?.AccountingCreatePaymentPermissionError)
+    }
+    GenericValue payment = delegator.makeValue("Payment")
+    payment.paymentId = parameters.paymentId ?: delegator.getNextSeqId("Payment")
+    paymentId = payment.paymentId
+    parameters.statusId = parameters.statusId ?: "PMNT_NOT_PAID"
+    if (UtilValidate.isNotEmpty(parameters.paymentMethodId)) {
+        GenericValue paymentMethod = from("PaymentMethod").where("paymentMethodId", parameters.paymentMethodId).queryOne()
+        if (parameters.paymentMethodTypeId != paymentMethod.paymentMethodTypeId) {
+            Debug.logInfo("Replacing passed payment method type [" + parameters.paymentMethodTypeId + "] with payment method type [" + paymentMethod.paymentMethodTypeId + "] for payment method [" + parameters.paymentMethodId +"]", MODULE)
+            parameters.paymentMethodTypeId = paymentMethod.paymentMethodTypeId
+        }
+    }
+    if (UtilValidate.isNotEmpty(parameters.paymentPreferenceId)) {
+        GenericValue orderPaymentPreference = from("OrderPaymentPreference").where("orderPaymentPreferenceId", parameters.paymentPreferenceId).queryOne()
+        parameters.paymentId = parameters.paymentId ?: orderPaymentPreference.paymentMethodId
+        parameters.paymentMethodTypeId = parameters.paymentMethodTypeId ?: orderPaymentPreference.paymentMethodTypeId
+    }
+    if (UtilValidate.isEmpty(parameters.paymentMethodTypeId)) {
+        return error(UtilProperties.getResourceBundleMap("AccountingUiLabels", locale)?.AccountingPaymentMethodIdPaymentMethodTypeIdNullError)
+    }
+    payment.setNonPKFields(parameters)
+    payment.effectiveDate = payment.effectiveDate ?: UtilDateTime.nowTimestamp()
+    delegator.create(payment)
+    Map result = success()
+    result.paymentId = paymentId
+    return result
+}
