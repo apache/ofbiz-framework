c7a5b22e0ed287cfa4073da8b0037da7567ffea6
Jacques Le Roux <jacques.le.roux@les7arts.com>
2020-05-18 22:50:28 +0200

    Improved: Prevent FreeMarker Template Injection (SSTI)
    (OFBIZ-11709)
    
    This commit does 2 things:
    
    Send a correct commit comment (kind of amendment, w/o push force)
    Previous code compiled but SAFER_RESOLVER is not a class but a field, better KISS
    
    Real change:
    Better style with line not too long:

--- framework/base/src/main/java/org/apache/ofbiz/base/util/template/FreeMarkerWorker.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/template/FreeMarkerWorker.java
@@ -116,7 +116,8 @@ public final class FreeMarkerWorker {
         } catch (TemplateException e) {
             Debug.logError("Unable to set date/time and number formats in FreeMarker: " + e, module);
         }
-        String templateClassResolver = UtilProperties.getPropertyValue("security", "templateClassResolver", "SAFER_RESOLVER");
+        String templateClassResolver = UtilProperties.getPropertyValue("security", "templateClassResolver", 
+                "SAFER_RESOLVER");
         switch (templateClassResolver) {
             case "UNRESTRICTED_RESOLVER":
                 newConfig.setNewBuiltinClassResolver(TemplateClassResolver.UNRESTRICTED_RESOLVER);
