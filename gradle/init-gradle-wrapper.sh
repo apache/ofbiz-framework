577326221ed6332549a1e2800752b0511d25b1e8
Girish Vasmatkar <girish.vasmatkar@hotwaxsystems.com>
2020-11-30 16:17:57 +0530

    Fixed: Updated checksum and modified path of the files as input to shasum command(OFBIZ-12066).

--- gradle/init-gradle-wrapper.sh
+++ gradle/init-gradle-wrapper.sh
@@ -27,9 +27,9 @@ GRADLE_WRAPPER_URI="https://dl.bintray.com/apacheofbiz/GradleWrapper/v$RELEASE/"
 GRADLE_WRAPPER_URI_BACKUP="https://github.com/gradle/gradle/raw/v$RELEASE/gradle/wrapper/"
 
 # checksum to verify the downloaded file
-SHASUM_GRADLE_WRAPPER_FILES="33b0acb4572934fc1dd7d5880c65036724974e06 gradle-wrapper.jar
-e0f1942760af0faec6c059220291dc3a1401a506 gradle-wrapper.properties
-43e0b1f46530cf90c60deb4040eb01ed8fbd3f60 gradlew"
+SHASUM_GRADLE_WRAPPER_FILES="1d23286bcb9e7d3debff18c1b892b9dbb9a4ec6c  gradle/wrapper/gradle-wrapper.jar
+f9c2ad227ef1fe774cb0e141abfc431b05fc9fd4  gradle/wrapper/gradle-wrapper.properties
+67bc176bca816684cf5218570a931f57d5f2b423  gradlew"
 
 GRADLE_WRAPPER_JAR="gradle-wrapper.jar"
 GRADLE_WRAPPER_PROPERTIES="gradle-wrapper.properties"
@@ -48,9 +48,7 @@ downloadFile() {
            return 0;
        fi
    elif [ -n "$(whereIsBinary wget)" ]; then
-       GET_CMD="wget -q -O $GRADLE_WRAPPER_OFBIZ_PATH/$1 --server-response $2/$1";
-       GET_CMD="$GET_CMD 2>&1 > /dev/null | grep 'HTTP/.* 200'";
-       if [ -n "$($GET_CMD)" ]; then
+       if [[ `wget -q -S -O $GRADLE_WRAPPER_OFBIZ_PATH/$1 $2/$1 2>&1 > /dev/null | grep 'HTTP/1.1 200 OK'` ]]; then
            return 0;
        fi
    fi
