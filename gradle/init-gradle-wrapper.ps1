9c0c43365f70558f1eee00d20dae60d24ddc8320
Michael Brohl <mbrohl@apache.org>
2021-03-13 09:16:03 +0100

    Fixed: Replace Bintray by a new place to upload the Gradle Wrapper (OFBIZ-12192)
    Changed Windows powershell script to use the proper Gradle wrapper
    version. Removed the properties and batch file download.

--- gradle/init-gradle-wrapper.ps1
+++ gradle/init-gradle-wrapper.ps1
@@ -15,13 +15,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
-if ((Test-Path -Path ((Get-Item -Path ".\").FullName + "\gradle\wrapper\gradle-wrapper.jar")) -and (Test-Path -Path ((Get-Item -Path ".\").FullName + "\gradle\wrapper\gradle-wrapper.properties"))) {
+if ((Test-Path -Path ((Get-Item -Path ".\").FullName + "\gradle\wrapper\gradle-wrapper.jar"))) {
     Write-Host "The Gradle Wrapper has already been downloaded.";
     exit
 }
 
 # HTTPS is not used because it gets complicated with Powershell and .Net framework versions depending on Windows versions
-Invoke-WebRequest -outf gradle\wrapper\gradle-wrapper.jar http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradle-wrapper.jar
-Invoke-WebRequest -outf gradle\wrapper\gradle-wrapper.properties http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradle-wrapper.properties
-Invoke-WebRequest -outf gradlew.bat http://dl.bintray.com/apacheofbiz/GradleWrapper/v3.2.1/gradlew.bat
-
+Invoke-WebRequest -outf gradle\wrapper\gradle-wrapper.jar https://github.com/gradle/gradle/raw/v4.5.1/gradle/wrapper/gradle-wrapper.jar
