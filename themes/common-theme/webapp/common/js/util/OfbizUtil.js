5fbd11e75ec1594f1cf4e1eb41699d94c2c9dcee
Jacques Le Roux <jacques.le.roux@les7arts.com>
2021-11-14 16:58:56 +0100

    Improved: Fix OFBiz specific Javascript security issues reported by GH CodeQL (OFBIZ-12366)
    Fixes OfbizUtil.js and fieldlookup.js by using dompurify
    
    Also adds in .gitignore
    /themes/common-theme/webapp/common/js/node_modules/
    /docs/asciidoc/images/
    as I have enough to see them when I switch between branches

--- themes/common-theme/webapp/common/js/util/OfbizUtil.js
+++ themes/common-theme/webapp/common/js/util/OfbizUtil.js
@@ -746,7 +746,7 @@ function ajaxAutoCompleter(areaCsvString, showDescription, defaultMinLength, def
                 var queryArgs = {"term": request.term};
                 if (typeof args == "object" && jQuery.isArray(args)) {
                      for (var i = 0; i < args.length; i++) {
-                         queryArgs["parm" + i] = jQuery(args[i]).val();
+                         queryArgs["parm" + i] = jQuery(DOMPurify.sanitize(args[i]).val());
                      }
                 }
                 jQuery.ajax({
@@ -771,7 +771,7 @@ function ajaxAutoCompleter(areaCsvString, showDescription, defaultMinLength, def
 
                         if (typeof autocomp != 'undefined') {
                             jQuery.each(autocomp, function(index, item){
-                                item.label = jQuery("<div>").html(item.label).text();
+                                item.label = DOMPurify.sanitize(jQuery("<div>").html(item.label).text());
                             })
                             // autocomp is the JSON Object which will be used for the autocomplete box
                             response(autocomp);
