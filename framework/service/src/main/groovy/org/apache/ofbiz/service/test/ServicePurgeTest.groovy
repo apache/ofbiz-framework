d9d73419fae4c0b0a0ec43de3ce24e48a6342249
MLeila <leila.mekika@free.fr>
2022-12-22 11:28:37 +0100

    Fixed: RuntimeData and RecurrenceInfo not cleaned after job purge (OFBIZ-12679)
    Add jobSandbox related runtimeDataId and recurrenceInfoId fields when
    searching for jobs to purge so that the PurgeJob can retrieve and clean
    job related entities (JobUtil.removeJob)
    Also add a test that validate the complete purge
    
    Thanks: Gaëtan Chaboussie for report and corrective patch

---
+++ framework/service/src/main/groovy/org/apache/ofbiz/service/test/ServicePurgeTest.groovy
@@ -0,0 +1,36 @@
+package org.apache.ofbiz.service
+
+import org.apache.ofbiz.base.util.UtilDateTime
+import org.apache.ofbiz.entity.GenericValue
+import org.apache.ofbiz.entity.util.EntityQuery
+import org.apache.ofbiz.service.config.ServiceConfigUtil
+import org.apache.ofbiz.testtools.GroovyScriptTestCase
+
+class ServicePurgeTest extends GroovyScriptTestCase {
+
+    void testRuntimeDataIsCleanedAfterServicePurge() {
+        GenericValue sysUserLogin = delegator.findOne('UserLogin', true, 'userLoginId', 'system')
+        String jobId = delegator.getNextSeqId('JobSandbox')
+
+        def createRuntimeResult = dispatcher.runSync('createRuntimeData', [
+                runtimeInfo: 'This is a runtimeInfo',
+                userLogin  : sysUserLogin
+        ])
+        String runtimeDataId = createRuntimeResult.runtimeDataId
+
+        dispatcher.runSync('createJobSandbox', [
+                userLogin     : sysUserLogin,
+                poolId        : ServiceConfigUtil.getServiceEngine().getThreadPool().getSendToPool(),
+                jobId         : jobId,
+                runtimeDataId : runtimeDataId,
+                statusId      : 'SERVICE_FINISHED',
+                serviceName   : 'sendMail',
+                finishDateTime: UtilDateTime.addDaysToTimestamp(UtilDateTime.nowTimestamp(), -10)
+        ])
+
+        dispatcher.runSync('purgeOldJobs', [userLogin: sysUserLogin])
+
+        assert EntityQuery.use(delegator).from('JobSandbox').where('jobId', jobId).queryCount() == 0
+        assert EntityQuery.use(delegator).from('RuntimeData').where('runtimeDataId', runtimeDataId).queryCount() == 0
+    }
+}
