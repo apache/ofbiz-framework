54a7f8a99e0983d7885b2cb2be084fb3391318c5
Gil Portenseigne <gil.portenseigne@nereide.fr>
2023-03-24 17:28:44 +0100

    Apply suggestion from Jacques in
    https://github.com/apache/ofbiz-framework/pull/517#discussion_r1143019683

--- applications/product/groovyScripts/facility/inventory/InventoryItemTotals.groovy
+++ applications/product/groovyScripts/facility/inventory/InventoryItemTotals.groovy
@@ -54,7 +54,7 @@ if (action) {
                 productPrices = product.getRelated('ProductPrice', null, null, false)
                 if (productPrices) {
                     productPrices.each { productPrice ->
-                        if (productPrice.productPriceTypeId == ("DEFAULT_PRICE")) {
+                        if (productPrice.productPriceTypeId == 'DEFAULT_PRICE') {
                             retailPrice = productPrice.getDouble('price')
                         }
                     }
