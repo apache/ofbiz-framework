598e343475195c9c46024665aef74c6dce05b0dd
MaximeFlame <flammangmaxime@gmail.com>
2023-05-23 09:57:05 +0200

    Patch ?

--- framework/widget/src/main/java/org/apache/ofbiz/widget/renderer/macro/MacroFormRenderer.java
+++ framework/widget/src/main/java/org/apache/ofbiz/widget/renderer/macro/MacroFormRenderer.java
@@ -530,7 +530,10 @@ public final class MacroFormRenderer implements FormStringRenderer {
         String event = modelFormField.getEvent();
         String action = modelFormField.getAction(context);
         StringBuilder items = new StringBuilder();
-        List<String> checkedByDefault = checkField.getCheckedByDefault(context);
+        //List<String> checkedByDefault = checkField.getCheckedByDefault(context);
+        List<String> checkedByDefault = context.containsKey(checkField.getModelFormField().getAttributeName())
+                ? StringUtil.toList(context.get(checkField.getModelFormField().getAttributeName()).toString())
+                : List.of();
         if (UtilValidate.isNotEmpty(modelFormField.getWidgetStyle())) {
             className = modelFormField.getWidgetStyle();
             if (modelFormField.shouldBeRed(context)) {
