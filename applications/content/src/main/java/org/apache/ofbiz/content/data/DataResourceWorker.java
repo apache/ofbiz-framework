cb28708ee00b8107bb6d9525f0e47e45e2d2bf1a
Cheng Hu Shan <cheng-hu.shan@ecomify.de>
2023-07-27 16:22:49 +0200

    Improved: Refactored DataResourceWorker getMimeType (OFBIZ-9403)

--- applications/content/src/main/java/org/apache/ofbiz/content/data/DataResourceWorker.java
+++ applications/content/src/main/java/org/apache/ofbiz/content/data/DataResourceWorker.java
@@ -107,6 +107,7 @@ public class DataResourceWorker implements org.apache.ofbiz.widget.content.DataR
 
     private static final String MODULE = DataResourceWorker.class.getName();
     private static final String ERR_RESOURCE = "ContentErrorUiLabels";
+    private static final String PROPERTY_RESOURCE = "content";
 
     /**
      * Traverses the DataCategory parent/child structure and put it in categoryNode. Returns non-null error string if there is an error.
@@ -377,34 +378,62 @@ public class DataResourceWorker implements org.apache.ofbiz.widget.content.DataR
         return b;
     }
 
+    /**
+     * Gets the MIME-Type from a given data resource, using the default value set in properties as fallback.
+     * @param dataResource
+     * @return MIME-Type
+     */
     public static String getMimeType(GenericValue dataResource) {
+        String defaultMimeType = EntityUtilProperties.getPropertyValue(PROPERTY_RESOURCE, "defaultMimeType", "application/octet-stream",
+                dataResource.getDelegator());
+        return getMimeType(dataResource, defaultMimeType);
+    }
+
+    /**
+     * Gets the MIME-Type from a given data resource.
+     * @param dataResource
+     * @param defaultMimeTypeId
+     * @return MIME-Type
+     */
+    public static String getMimeType(GenericValue dataResource, String defaultMimeTypeId) {
         String mimeTypeId = null;
         if (dataResource != null) {
             mimeTypeId = (String) dataResource.get("mimeTypeId");
             if (UtilValidate.isEmpty(mimeTypeId)) {
                 String fileName = (String) dataResource.get("objectInfo");
-                if (fileName != null && fileName.indexOf('.') > -1) {
-                    String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
-                    if (UtilValidate.isNotEmpty(fileExtension)) {
-                        GenericValue ext = null;
-                        try {
-                            ext = dataResource.getDelegator().findOne("FileExtension",
-                                    UtilMisc.toMap("fileExtensionId", fileExtension), false);
-                        } catch (GenericEntityException e) {
-                            Debug.logError(e, MODULE);
-                        }
-                        if (ext != null) {
-                            mimeTypeId = ext.getString("mimeTypeId");
-                        }
-                    }
-                }
+                mimeTypeId = getMimeType(dataResource.getDelegator(), fileName, defaultMimeTypeId);
+            }
+        }
+        return mimeTypeId;
+    }
 
-                // check one last time
-                if (UtilValidate.isEmpty(mimeTypeId)) {
-                    // use a default mime type
-                    mimeTypeId = "application/octet-stream";
+    /**
+     * Gets the MIME-Type from a given filename.
+     * @param delegator
+     * @param fileName
+     * @param defaultMimeTypeId
+     * @return MIME-Type
+     */
+    public static String getMimeType(Delegator delegator, String fileName, String defaultMimeTypeId) {
+        String mimeTypeId = null;
+
+        if (UtilValidate.isNotEmpty(fileName) && fileName.indexOf('.') > -1) {
+            String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+            if (UtilValidate.isNotEmpty(fileExtension)) {
+                GenericValue ext = null;
+                try {
+                    ext = delegator.findOne("FileExtension", true, "fileExtensionId", fileExtension);
+                    if (ext != null) {
+                        mimeTypeId = ext.getString("mimeTypeId");
+                    }
+                } catch (GenericEntityException e) {
+                    Debug.logError(e, MODULE);
                 }
             }
+        }
+        // check one last time, if we have to return a default mime type
+        if (UtilValidate.isEmpty(mimeTypeId) && UtilValidate.isNotEmpty(defaultMimeTypeId)) {
+            mimeTypeId = defaultMimeTypeId;
         }
         return mimeTypeId;
     }
