6d4a135b80e33ee531bf56140b9e02a47c1cd291
Jacques Le Roux <jacques.le.roux@les7arts.com>
2024-04-06 08:50:35 +0200

    Improved: Fix Marketing Groovy tests (OFBIZ-13001)
    Fixes |ModelTestSuite |E| Unable to load test suite class :
    org.apache.ofbiz.marketing.MarketingTests

--- applications/marketing/src/main/groovy/org/apache/ofbiz/marketing/marketing/test/MarketingTests.groovy
+++ applications/marketing/src/main/groovy/org/apache/ofbiz/marketing/marketing/test/MarketingTests.groovy
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.marketing.marketing
+package org.apache.ofbiz.marketing.marketing.test
 
 import org.apache.ofbiz.entity.GenericValue
 import org.apache.ofbiz.service.ServiceUtil
