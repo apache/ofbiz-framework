b6e87fcfb4649b189f960ed7661eb57dedf65a29
Jacques Le Roux <jacques.le.roux@les7arts.com>
2024-04-28 08:48:20 +0200

    Improved: Use new Tomcat 9 maxDays Access_Log_Valve Attribute (OFBIZ-13073)
    https://tomcat.apache.org/tomcat-9.0-doc/config/valve.html#Access_Log_Valve

--- framework/catalina/src/main/java/org/apache/ofbiz/catalina/container/CatalinaContainer.java
+++ framework/catalina/src/main/java/org/apache/ofbiz/catalina/container/CatalinaContainer.java
@@ -405,6 +405,7 @@ public class CatalinaContainer implements Container {
                 accessLogValve.setPrefix(accessLogPrefix);
             }
             accessLogValve.setRotatable(ContainerConfig.getPropertyValue(engineConfig, "access-log-rotate", false));
+            accessLogValve.setMaxDays(Integer.valueOf(ContainerConfig.getPropertyValue(engineConfig, "access-log-maxDays", null)));
 
             engineValves.add(accessLogValve);
         }
