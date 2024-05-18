776eacc2a46de3779cea1f9b9748077d701e7929
Sandeep Rajput <90825597+SandeepRajput20@users.noreply.github.com>
2024-05-18 18:26:43 +0530

    Improved: Corrected the condition to fetch currency UOM id from shipment instead of shipment route segment (#801)
    There is a typo in the condition while fetching a currency UOM id from the shipment route segment entity instead of shipment at the time of creating a shipment on the FedEx carrier.

--- applications/product/src/main/java/org/apache/ofbiz/shipment/thirdparty/fedex/FedexServices.java
+++ applications/product/src/main/java/org/apache/ofbiz/shipment/thirdparty/fedex/FedexServices.java
@@ -538,7 +538,7 @@ public class FedexServices {
             String currencyCode = null;
             if (UtilValidate.isNotEmpty(shipmentRouteSegment.getString("currencyUomId"))) {
                 currencyCode = shipmentRouteSegment.getString("currencyUomId");
-            } else if (UtilValidate.isNotEmpty(shipmentRouteSegment.getString("currencyUomId"))) {
+            } else if (UtilValidate.isNotEmpty(shipment.getString("currencyUomId"))) {
                 currencyCode = shipment.getString("currencyUomId");
             } else {
                 currencyCode = EntityUtilProperties.getPropertyValue("general", "currency.uom.id.default", "USD", delegator);
