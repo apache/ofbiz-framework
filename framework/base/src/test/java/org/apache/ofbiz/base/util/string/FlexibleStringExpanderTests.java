d200480e771c3db55c7b5037ef6f0c2fa3cb3c05
Nicolas Malin <nicolas.malin@nereide.fr>
2024-10-10 14:07:49 +0200

    Improved: Allow to use GroovyDsl in FlexibleStringExpander (OFBIZ-13133)
    Second improvement on this functionality with increase the security by analyse each script to control the presence of potential code injection.
    
    The regexp to control is a property: security.deniedScriptletsTokens.
    If a script match the regexp, OFBiz raise in log an alert with the script and the script hash. The script is disabled and can't run.
    
    If you have a safe script who is matched by the regexp, you can add the hash given by OFBiz on the property: security.allowedScriptletHashes

--- framework/base/src/test/java/org/apache/ofbiz/base/util/string/FlexibleStringExpanderTests.java
+++ framework/base/src/test/java/org/apache/ofbiz/base/util/string/FlexibleStringExpanderTests.java
@@ -305,6 +305,8 @@ public class FlexibleStringExpanderTests {
         fseTest("groovy: null", "${groovy:return null;}!", testMap, "!", false);
         fseTest("groovy missing property", "${groovy: return noList[0]}", testMap, null, null, "", null, false);
         fseTest("groovy: throw Exception", "${groovy:return throwException.value;}!", testMap, "!", false);
+        fseTest("groovy: generate security issue", "${groovy: java.util.Map.of('key', 'value')}!", testMap, "!", false);
+        fseTest("groovy: another generate security issue", "${groovy: 'ls /'.execute()}!", testMap, "!", false);
         fseTest("groovy: converter exception", "${groovy:return specialNumber;}!", testMap, "1!", false);
         fseTest("UEL integration: Map", "Hello ${testMap.var}!", testMap, "Hello World!", false);
         fseTest("UEL integration: blank", "Hello ${testMap.blank}World!", testMap, "Hello World!", false);
