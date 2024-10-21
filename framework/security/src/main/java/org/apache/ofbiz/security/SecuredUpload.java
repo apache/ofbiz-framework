fffa395d17a930bfc6c10b6986876b48ebc63c6d
Jacques Le Roux <jacques.le.roux@les7arts.com>
2024-10-21 18:47:44 +0200

    Improved: Update to Apache Tika 3.0.0 (OFBIZ-13155)
    This reverts commit 7c3a622f08e37d6a68125844383820dac1592f23.
    
    When running testSendOrderConfirmation and testFopMacroLibrary
    there are a lot of issues with dependencies related to
    org.apache.xmlgraphics Tika, FOP and PDFBox
    
    To be checked before going further using PDFBox 33.0.3 and FOP 2.10
    
    I'll put the the error stack for testSendOrderConfirmation in the Jira

--- framework/security/src/main/java/org/apache/ofbiz/security/SecuredUpload.java
+++ framework/security/src/main/java/org/apache/ofbiz/security/SecuredUpload.java
@@ -76,7 +76,6 @@ import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.base.util.UtilXml;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.util.EntityUtilProperties;
-import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentNameDictionary;
 import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
@@ -531,7 +530,7 @@ public class SecuredUpload {
             }
             // OK no JS code, pass to check 2: detect if the document has any embedded files
             PDEmbeddedFilesNameTreeNode efTree = null;
-            try (PDDocument pdDocument = Loader.loadPDF(file)) {
+            try (PDDocument pdDocument = PDDocument.load(file)) {
                 PDDocumentNameDictionary names = new PDDocumentNameDictionary(pdDocument.getDocumentCatalog());
                 efTree = names.getEmbeddedFiles();
             }
