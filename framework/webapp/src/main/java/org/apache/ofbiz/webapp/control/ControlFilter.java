166bbdf6d75edb41831fa21f2b83f0a056036fd2
Jacques Le Roux <jacques.le.roux@les7arts.com>
2024-10-23 13:49:17 +0200

    Improved: Prevent URL parameters manipulation (OFBIZ-13147)
    Fixes previous commit MODULE is not module

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ControlFilter.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ControlFilter.java
@@ -143,7 +143,7 @@ public class ControlFilter implements Filter {
             if (queryString != null) {
                 queryString = URLDecoder.decode(queryString, "UTF-8");
                 if (UtilValidate.isUrl(queryString) || !SecuredUpload.isValidText(queryString, Collections.emptyList())) {
-                    Debug.logError("For security reason this URL is not accepted", MODULE);
+                    Debug.logError("For security reason this URL is not accepted", module);
                     throw new RuntimeException("For security reason this URL is not accepted");
                 }
             }
