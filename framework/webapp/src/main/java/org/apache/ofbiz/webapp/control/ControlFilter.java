58ed838b4a0816555f928f4b453509b18082708b
Jacques Le Roux <jacques.le.roux@les7arts.com>
2024-10-23 14:22:39 +0200

    Improved: Prevent URL parameters manipulation (OFBIZ-13147)
    Allows JavaScriptEnabled=Y to pass.

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ControlFilter.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/control/ControlFilter.java
@@ -170,7 +170,9 @@ public class ControlFilter extends HttpFilter {
             String queryString = req.getQueryString();
             if (queryString != null) {
                 queryString = URLDecoder.decode(queryString, "UTF-8");
-                if (UtilValidate.isUrl(queryString) || !SecuredUpload.isValidText(queryString, Collections.emptyList())) {
+                if (UtilValidate.isUrl(queryString)
+                        || (!SecuredUpload.isValidText(queryString, Collections.emptyList())
+                                && !queryString.contains("JavaScriptEnabled=Y"))) {
                     Debug.logError("For security reason this URL is not accepted", MODULE);
                     throw new RuntimeException("For security reason this URL is not accepted");
                 }
