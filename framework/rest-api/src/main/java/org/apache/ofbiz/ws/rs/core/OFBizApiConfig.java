55901f2198e95ae449f4893c655b60cf2cf552b4
Deepak Dixit <deepak.dixit@hotwax.co>
2025-08-21 00:43:58 +0530

    Comment out unused import, ServerProperties.TRACING will be helpful while debugging

--- framework/rest-api/src/main/java/org/apache/ofbiz/ws/rs/core/OFBizApiConfig.java
+++ framework/rest-api/src/main/java/org/apache/ofbiz/ws/rs/core/OFBizApiConfig.java
@@ -27,7 +27,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import jakarta.ws.rs.HttpMethod;
-import org.glassfish.jersey.server.ServerProperties;
+//import org.glassfish.jersey.server.ServerProperties;
 import jakarta.ws.rs.core.MediaType;
 
 import org.apache.ofbiz.base.component.ComponentConfig;
