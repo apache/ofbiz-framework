d1803174ce183d9cde47671917436b47113a5670
cgaetan <gaetan.chaboussie@nereide.fr>
2026-03-18 11:21:40 +0100

    Adds the descriptions of uel methods

--- framework/base/src/main/groovy/org/apache/ofbiz/base/util/string/uel/MiscUel.groovy
+++ framework/base/src/main/groovy/org/apache/ofbiz/base/util/string/uel/MiscUel.groovy
@@ -32,18 +32,30 @@ class MiscUel implements IUelMappingLibrary {
     @Override
     List<UelMapping> getUelMappingList() {
         return [
-                new UelMapping('sys:getenv', UelFunctions.getMethod('sysGetEnv', String)),
-                new UelMapping('sys:getProperty', UelFunctions.getMethod('sysGetProp', String)),
-                new UelMapping('util:size', UelFunctions.getMethod('getSize', Object)),
-                new UelMapping('util:defaultLocale', Locale.getMethod('getDefault')),
-                new UelMapping('util:defaultTimeZone', TimeZone.getMethod('getDefault')),
-                new UelMapping('util:label', UelFunctions.getMethod('label', String, String, Locale)),
-                new UelMapping('screen:id', UelFunctions.getMethod('resolveCurrentScreenId', ScreenRenderer.ScreenStack)),
-                new UelMapping('dom:readHtmlDocument', UelFunctions.getMethod('readHtmlDocument', String)),
-                new UelMapping('dom:readXmlDocument', UelFunctions.getMethod('readXmlDocument', String)),
-                new UelMapping('dom:toHtmlString', UelFunctions.getMethod('toHtmlString', Node, String, boolean, int)),
-                new UelMapping('dom:toXmlString', UelFunctions.getMethod('toXmlString', Node, String, boolean, boolean, int)),
-                new UelMapping('dom:writeXmlDocument', UelFunctions.getMethod('writeXmlDocument', String, Node, String, boolean, boolean, int)),
+                new UelMapping('sys:getenv', UelFunctions.getMethod('sysGetEnv', String),
+                        'Gets the value of the specified environment variable.'),
+                new UelMapping('sys:getProperty', UelFunctions.getMethod('sysGetProp', String),
+                        'Gets the system property indicated by the specified key.'),
+                new UelMapping('util:size', UelFunctions.getMethod('getSize', Object),
+                        'Returns the default Locale.'),
+                new UelMapping('util:defaultLocale', Locale.getMethod('getDefault'),
+                        'Returns the default TimeZone.'),
+                new UelMapping('util:defaultTimeZone', TimeZone.getMethod('getDefault'),
+                        'Return the label present in ressource on the given locale.'),
+                new UelMapping('util:label', UelFunctions.getMethod('label', String, String, Locale),
+                        'Returns the size of Maps,'),
+                new UelMapping('screen:id', UelFunctions.getMethod('resolveCurrentScreenId', ScreenRenderer.ScreenStack),
+                        'Returns the id of the current screen, Collections, and Strings. Invalid Object types return -1.'),
+                new UelMapping('dom:readHtmlDocument', UelFunctions.getMethod('readHtmlDocument', String),
+                        'Reads an HTML file and returns a org.w3c.dom.Document instance.'),
+                new UelMapping('dom:readXmlDocument', UelFunctions.getMethod('readXmlDocument', String),
+                        'Reads an XML file and returns a org.w3c.dom.Document instance.'),
+                new UelMapping('dom:toHtmlString', UelFunctions.getMethod('toHtmlString', Node, String, boolean, int),
+                        'Returns a org.w3c.dom.Node as an HTML String.'),
+                new UelMapping('dom:toXmlString', UelFunctions.getMethod('toXmlString', Node, String, boolean, boolean, int),
+                        'Returns a org.w3c.dom.Node as an XML String.'),
+                new UelMapping('dom:writeXmlDocument', UelFunctions.getMethod('writeXmlDocument', String, Node, String, boolean, boolean, int),
+                        'Writes a org.w3c.dom.Node to an XML file and returns true if successful.')
         ]
     }
 
--- framework/base/src/main/groovy/org/apache/ofbiz/base/util/string/uel/StringUel.groovy
+++ framework/base/src/main/groovy/org/apache/ofbiz/base/util/string/uel/StringUel.groovy
@@ -30,20 +30,38 @@ class StringUel implements IUelMappingLibrary {
     @Override
     List<UelMapping> getUelMappingList() {
         return [
-                new UelMapping('str:endsWith', UelFunctions.getMethod('endsWith', String, String)),
-                new UelMapping('str:indexOf', UelFunctions.getMethod('indexOf', String, String)),
-                new UelMapping('str:lastIndexOf', UelFunctions.getMethod('lastIndexOf', String, String)),
-                new UelMapping('str:length', UelFunctions.getMethod('length', String)),
-                new UelMapping('str:replace', UelFunctions.getMethod('replace', String, String, String)),
-                new UelMapping('str:replaceAll', UelFunctions.getMethod('replaceAll', String, String, String)),
-                new UelMapping('str:replaceFirst', UelFunctions.getMethod('replaceFirst', String, String, String)),
-                new UelMapping('str:startsWith', UelFunctions.getMethod('startsWith', String, String)),
-                new UelMapping('str:endstring', UelFunctions.getMethod('endString', String, int)),
-                new UelMapping('str:substring', UelFunctions.getMethod('subString', String, int, int)),
-                new UelMapping('str:toString', UelFunctions.getMethod('toString', Object)),
-                new UelMapping('str:toLowerCase', UelFunctions.getMethod('toLowerCase', String)),
-                new UelMapping('str:toUpperCase', UelFunctions.getMethod('toUpperCase', String)),
-                new UelMapping('str:trim', UelFunctions.getMethod('trim', String)),
+                new UelMapping('str:endsWith', UelFunctions.getMethod('endsWith', String, String),
+                        'Returns true if this string ends with the specified suffix.'),
+                new UelMapping('str:indexOf', UelFunctions.getMethod('indexOf', String, String),
+                        'Returns the index within this string of the first occurrence of the specified substring .'),
+                new UelMapping('str:lastIndexOf', UelFunctions.getMethod('lastIndexOf', String, String),
+                        'Returns the index within this string of the last occurrence of the specified character.'),
+                new UelMapping('str:length', UelFunctions.getMethod('length', String),
+                        'Returns the length of this string.'),
+                new UelMapping('str:replace', UelFunctions.getMethod('replace', String, String, String),
+                        'Replaces each substring of this string that matches the literal target sequence with the' +
+                                ' specified literal replacement sequence.'),
+                new UelMapping('str:replaceAll', UelFunctions.getMethod('replaceAll', String, String, String),
+                        'Replaces each substring of this string that matches the given regular expression with the given replacement.'),
+                new UelMapping('str:replaceFirst', UelFunctions.getMethod('replaceFirst', String, String, String),
+                        'Replaces the first substring of this string that matches the given regular expression with the given replacement.'),
+                new UelMapping('str:startsWith', UelFunctions.getMethod('startsWith', String, String),
+                        'Returns true if this string starts with the specified prefix.'),
+                new UelMapping('str:endstring', UelFunctions.getMethod('endString', String, int),
+                        'Returns a new string that is a substring of this string. The substring begins with the' +
+                                ' character at the specified index and extends to the end of this string.'),
+                new UelMapping('str:substring', UelFunctions.getMethod('subString', String, int, int),
+                        'Returns a new string that is a substring of this string. The substring begins at the ' +
+                                'specified beginIndex and extends to the character at index endIndex - 1. Thus ' +
+                                'the length of the substring is endIndex-beginIndex.'),
+                new UelMapping('str:toString', UelFunctions.getMethod('toString', Object),
+                        'Converts all of the characters in this String to lower case using the rules of the default locale.'),
+                new UelMapping('str:toLowerCase', UelFunctions.getMethod('toLowerCase', String),
+                        'Converts Object to a String - bypassing localization.'),
+                new UelMapping('str:toUpperCase', UelFunctions.getMethod('toUpperCase', String),
+                        'Converts all of the characters in this String to upper case using the rules of the default locale.'),
+                new UelMapping('str:trim', UelFunctions.getMethod('trim', String),
+                        'Returns a copy of the string, with leading and trailing whitespace omitted.')
         ]
     }
 
