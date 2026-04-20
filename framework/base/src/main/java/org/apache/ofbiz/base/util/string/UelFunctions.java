32037a6aa12b8c2e853f29712446521725d6ad8e
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2026-04-20 16:48:41 +0200

    Fixed: Enhance parsing security by disabling external entity resolution

--- framework/base/src/main/java/org/apache/ofbiz/base/util/string/UelFunctions.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/string/UelFunctions.java
@@ -368,6 +368,11 @@ public class UelFunctions {
             URL url = FlexibleLocation.resolveLocation(str);
             if (url != null) {
                 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+                factory.setXIncludeAware(false);
+                factory.setExpandEntityReferences(false);
                 DocumentBuilder builder = factory.newDocumentBuilder();
                 document = builder.parse(url.openStream());
                 document.getDocumentElement().normalize();
