20086822ef5ba22500491fec71a2a4a423aa57fc
Arun Patidar <arun.patidar@hotwax.co>
2026-06-23 15:33:37 +0530

    Added services to use with external app, enabled export for picking related services.

--- applications/product/src/main/groovy/org/apache/ofbiz/product/shipment/PicklistServices.groovy
+++ applications/product/src/main/groovy/org/apache/ofbiz/product/shipment/PicklistServices.groovy
@@ -19,6 +19,7 @@
 package org.apache.ofbiz.product.shipment
 
 import org.apache.ofbiz.entity.GenericValue
+import org.apache.ofbiz.service.ServiceUtil
 
 /*
  * Migrate all element present on entity OldPicklistStatusHistory to entity PickListStatus
@@ -38,3 +39,94 @@ Map migrateOldPicklistStatusHistoryToPickListStatus() {
     }
     return success()
 }
+
+/**
+ * Retrieves a list of orders available to be picked for a given facility.
+ * Wraps existing findOrdersToPickMove service.
+ */
+Map getOrdersToPick() {
+    Map result = ServiceUtil.returnSuccess()
+
+    Map findResult = runService('findOrdersToPickMove', [facilityId: context.facilityId, userLogin: context.userLogin])
+    if (ServiceUtil.isError(findResult)) {
+        return findResult
+    }
+
+    // Transform pickMoveInfoList to a simpler list for the PWA
+    List orderList = []
+    if (findResult.pickMoveInfoList) {
+        findResult.pickMoveInfoList.each { pickMoveInfo ->
+            if (pickMoveInfo.orderReadyToPickInfoList) {
+                pickMoveInfo.orderReadyToPickInfoList.each { orderReadyInfo ->
+                    GenericValue orderHeader = orderReadyInfo.orderHeader
+                    if (orderHeader) {
+                        if (!orderList.any { it.orderId == orderHeader.orderId }) {
+                            orderList << [
+                                orderId: orderHeader.orderId,
+                                orderDate: orderHeader.orderDate,
+                                statusId: orderHeader.statusId,
+                                grandTotal: orderHeader.grandTotal
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    result.orderList = orderList
+    return result
+}
+
+/**
+ * Retrieves details of a specific picklist, including items sorted by location.
+ * Wraps existing getPickAndPackReportInfo service.
+ */
+Map getPicklistDetails() {
+    Map result = ServiceUtil.returnSuccess()
+
+    Map reportResult = runService('getPickAndPackReportInfo', [
+        picklistId: context.picklistId,
+        userLogin: context.userLogin
+    ])
+
+    if (ServiceUtil.isError(reportResult)) {
+        return reportResult
+    }
+
+    // The reportResult contains a complex structure of picklistBins and their items.
+    // We will extract and flatten this for the PWA.
+    Map picklistDetails = [:]
+    picklistDetails.picklistId = context.picklistId
+
+    List items = []
+    if (reportResult.picklistInfo && reportResult.picklistInfo.picklistBinInfoList) {
+        reportResult.picklistInfo.picklistBinInfoList.each { binInfo ->
+            if (binInfo.picklistItemInfoList) {
+                binInfo.picklistItemInfoList.each { itemInfo ->
+                    items << [
+                        picklistBinId: binInfo.picklistBin?.picklistBinId,
+                        orderId: itemInfo.orderItem?.orderId,
+                        orderItemSeqId: itemInfo.orderItem?.orderItemSeqId,
+                        shipGroupSeqId: itemInfo.picklistItem?.shipGroupSeqId,
+                        inventoryItemId: itemInfo.picklistItem?.inventoryItemId,
+                        itemStatusId: itemInfo.picklistItem?.itemStatusId,
+                        productId: itemInfo.product?.productId,
+                        productName: itemInfo.product?.internalName,
+                        quantity: itemInfo.picklistItem?.quantity,
+                        locationSeqId: itemInfo.inventoryItemAndLocation?.locationSeqId,
+                        area: itemInfo.inventoryItemAndLocation?.areaId,
+                        aisle: itemInfo.inventoryItemAndLocation?.aisleId,
+                        section: itemInfo.inventoryItemAndLocation?.sectionId,
+                        level: itemInfo.inventoryItemAndLocation?.levelId
+                    ]
+                }
+            }
+        }
+    }
+
+    result.picklistDetails = picklistDetails
+    result.picklistDetails.items = items
+    return result
+}
+
