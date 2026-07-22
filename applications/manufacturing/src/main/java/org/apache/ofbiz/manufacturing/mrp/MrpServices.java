292074b3da94d9539787d292a51fa6829202e3bb
diveshdut <divesh.dutta@hotwax.co>
2026-07-22 13:28:42 +0530

    OFBIZ-13463: Fixed false late flag on REQUIRED_MRP proposed supply in MRP engine

--- applications/manufacturing/src/main/java/org/apache/ofbiz/manufacturing/mrp/MrpServices.java
+++ applications/manufacturing/src/main/java/org/apache/ofbiz/manufacturing/mrp/MrpServices.java
@@ -598,6 +598,16 @@ public class MrpServices {
     }
 
     /**
+     * Returns whether a proposed supply order should be classified as late for the current MRP run.
+     */
+    public static boolean isProposedOrderLate(Timestamp requirementStartDate, Timestamp runStartedAt, GenericValue sourceEvent) {
+        if (sourceEvent != null && "REQUIRED_MRP".equals(sourceEvent.getString("mrpEventTypeId"))) {
+            return false;
+        }
+        return requirementStartDate.compareTo(runStartedAt) < 0;
+    }
+
+    /**
      * Process the bill of material (bom) of the product  to insert components in the MrpEvent table.
      * Before inserting in the entity, test if there is the record already existing to add quantity rather to create a new one.
      * @param mrpId                the mrp id
@@ -918,7 +928,7 @@ public class MrpServices {
                                 "mrpEventTypeId", (isBuilt ? "PROP_MANUF_O_RECP" : "PROP_PUR_O_RECP"));
                         try {
                             InventoryEventPlannedServices.createOrUpdateMrpEvent(eventMap, proposedOrder.getQuantity(), null,
-                                    eventName, (proposedOrder.getRequirementStartDate().compareTo(now) < 0), delegator);
+                                    eventName, isProposedOrderLate(proposedOrder.getRequirementStartDate(), now, inventoryEventForMRP), delegator);
                         } catch (GenericEntityException e) {
                             return ServiceUtil.returnError(UtilProperties.getMessage(RESOURCE, "ManufacturingMrpCreateOrUpdateEvent",
                                     UtilMisc.toMap("parameters", parameters), locale));
