eb7018835fa6e706fab1bfa6c60642332579e946
Mridul Pathak <mridul.72@gmail.com>
2026-07-31 12:29:39 +0530

    Improved: Migrate mail integration from javax.mail to Jakarta Mail (OFBIZ-13465) (#1527)
    Improved: Migrate mail integration from javax.mail to Jakarta Mail
    (OFBIZ-13465)
    
    Replace com.sun.mail:javax.mail:1.6.2 with jakarta.mail:jakarta.mail-api
    +
    org.eclipse.angus:angus-mail, updating imports across EmailServices,
    CommonServices,
    MimeMessageWrapper, ServiceMcaCondition, JavaMailContainer, and
    CommunicationEventServices.
    
    Note: a few public method signatures move from javax.mail.* to
    jakarta.mail.*, and Angus
    Mail defaults TLS server-identity checking to on (was off under the old
    library).

--- framework/common/src/main/java/org/apache/ofbiz/common/email/EmailServices.java
+++ framework/common/src/main/java/org/apache/ofbiz/common/email/EmailServices.java
@@ -38,17 +38,17 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.SendFailedException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import jakarta.activation.DataHandler;
+import jakarta.activation.DataSource;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.SendFailedException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
 
@@ -80,7 +80,7 @@ import org.apache.ofbiz.widget.renderer.VisualTheme;
 import org.apache.ofbiz.widget.renderer.macro.MacroScreenRenderer;
 import org.xml.sax.SAXException;
 
-import com.sun.mail.smtp.SMTPAddressFailedException;
+import org.eclipse.angus.mail.smtp.SMTPAddressFailedException;
 
 import freemarker.template.TemplateException;
 
