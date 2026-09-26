4aa8042c3ec132e259d214bb17d851b47a0ac79f
Mridul Pathak <mridul.72@gmail.com>
2026-09-26 17:59:41 +0530

    Fixed: Regression in the minilang-to-Groovy conversion of ContactMechServicesScript.groovy (OFBIZ-13612)
    sendVerifyEmailAddressNotification() now also requires storeEmail.bodyScreenLocation to be set before attempting to send, matching the original mini-lang, instead of calling sendMailFromScreen with a null bodyScreenUri whenever a ProductStoreEmailSetting row for PRDS_EMAIL_VERIFY exists but its bodyScreenLocation is blank.

--- applications/party/src/main/groovy/org/apache/ofbiz/party/contact/ContactMechServicesScript.groovy
+++ applications/party/src/main/groovy/org/apache/ofbiz/party/contact/ContactMechServicesScript.groovy
@@ -322,7 +322,7 @@ Map sendVerifyEmailAddressNotification() {
     GenericValue emailAddressVerification = from('EmailAddressVerification')
             .where('emailAddress', parameters.emailAddress)
             .queryOne()
-    if (emailAddressVerification && storeEmail) {
+    if (emailAddressVerification && storeEmail?.bodyScreenLocation) {
         Map emailParams = [
             sendTo: parameters.emailAddress,
             subject: storeEmail.subject,
